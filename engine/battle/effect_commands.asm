3584c07270c512fd9ebaab272d246adb01baac88
RaJah239 <ravijagroo239@gmail.com>
2024-06-18 12:50:52 -0400

    Inverse Battles/ Reverse Type Match-ups added Credit: https://github.com/pret/pokecrystal/wiki/Inverse-Battles

--- engine/battle/effect_commands.asm
+++ engine/battle/effect_commands.asm
@@ -1210,6 +1210,11 @@ INCLUDE "data/battle/critical_hit_chances.asm"
 
 INCLUDE "engine/battle/move_effects/triple_kick.asm"
 
+GetNextTypeMatchupsByte:
+   ld a, BANK(TypeMatchups)
+   call GetFarByte
+   ret
+
 BattleCommand_Stab:
 ; STAB = Same Type Attack Bonus
 	ld a, BATTLE_VARS_MOVE_ANIM
@@ -1286,10 +1291,16 @@ BattleCommand_Stab:
 	call GetBattleVar
 	and TYPE_MASK
 	ld b, a
+	ld a, [wBattleType]
+	cp BATTLETYPE_INVERSE
+	jr z, .inverse
 	ld hl, TypeMatchups
-
+	jr .TypesLoop
+.inverse
+ 	ld hl, InverseTypeMatchups
 .TypesLoop:
-	ld a, [hli]
+	call GetNextTypeMatchupsByte
+	inc hl
 
 	cp -1
 	jr z, .end
@@ -1307,7 +1318,7 @@ BattleCommand_Stab:
 .SkipForesightCheck:
 	cp b
 	jr nz, .SkipType
-	ld a, [hl]
+	call GetNextTypeMatchupsByte
 	cp d
 	jr z, .GotMatchup
 	cp e
@@ -1322,7 +1333,7 @@ BattleCommand_Stab:
 	and %10000000
 	ld b, a
 ; If the target is immune to the move, treat it as a miss and calculate the damage as 0
-	ld a, [hl]
+	call GetNextTypeMatchupsByte
 	and a
 	jr nz, .NotImmune
 	inc a
@@ -1411,9 +1422,16 @@ CheckTypeMatchup:
 	ld c, [hl]
 	ld a, EFFECTIVE
 	ld [wTypeMatchup], a
+	ld a, [wBattleType]
+	cp BATTLETYPE_INVERSE
+	jr z, .inverse
 	ld hl, TypeMatchups
+	jr .TypesLoop
+.inverse
+	ld hl, InverseTypeMatchups
 .TypesLoop:
-	ld a, [hli]
+	call GetNextTypeMatchupsByte
+	inc hl
 	cp -1
 	jr z, .End
 	cp -2
@@ -1427,7 +1445,8 @@ CheckTypeMatchup:
 .Next:
 	cp d
 	jr nz, .Nope
-	ld a, [hli]
+	call GetNextTypeMatchupsByte
+	inc hl
 	cp b
 	jr z, .Yup
 	cp c
@@ -1445,7 +1464,8 @@ CheckTypeMatchup:
 	ldh [hDividend + 0], a
 	ldh [hMultiplicand + 0], a
 	ldh [hMultiplicand + 1], a
-	ld a, [hli]
+	call GetNextTypeMatchupsByte
+	inc hl
 	ldh [hMultiplicand + 2], a
 	ld a, [wTypeMatchup]
 	ldh [hMultiplier], a
@@ -1486,8 +1506,6 @@ BattleCommand_ResetTypeMatchup:
 	ret
 
 INCLUDE "engine/battle/ai/switch.asm"
-
-INCLUDE "data/types/type_matchups.asm"
 
 BattleCommand_DamageVariation:
 ; Modify the damage spread between 85% and 100%.
--- main.asm
+++ main.asm
@@ -152,6 +152,9 @@ SECTION "Effect Commands", ROMX
 
 INCLUDE "engine/battle/effect_commands.asm"
 
+SECTION "Type Matchups", ROMX
+
+INCLUDE "data/types/type_matchups.asm"
 
 SECTION "Enemy Trainers", ROMX
 
