d3ba430c81f796bb32c1ea49207c633e05659582
RaJah239 <ravijagroo239@gmail.com>
2024-09-19 13:56:58 -0400

    Chansey and Porygon PC added to  maps/SilverCaveRoom2.asm + map edit

--- maps/SilverCaveRoom2.asm
+++ maps/SilverCaveRoom2.asm
@@ -2,6 +2,8 @@
 	const SILVERCAVEROOM2_POKE_BALL1
 	const SILVERCAVEROOM2_POKE_BALL2
 	const SILVERCAVEROOM2_POKE_BALL3
+	const SILVERCAVEROOM2_CHANSEY
+	const SILVERCAVEROOM2_PORYGON_PC
 
 SilverCaveRoom2_MapScripts:
 	def_scene_scripts
@@ -19,6 +21,17 @@ SilverCaveRoom2PPUp:
 
 SilverCaveRoom2HiddenMaxPotion:
 	hiddenitem MAX_POTION, EVENT_SILVER_CAVE_ROOM_2_HIDDEN_MAX_POTION
+
+SilverCaveRoom2ChanseyScript:
+	jumpstd ChanseyHealsOWScript
+
+SilverCaveRoom2PorygonPCScript:
+	jumpstd PorygonPCScript
+
+SilverCaveRoom2PorygonPCText:
+	text "#MON Storage"
+	line "System opened."
+	done
 
 SilverCaveRoom2_MapEvents:
 	db 0, 0 ; filler
@@ -38,3 +51,5 @@ SilverCaveRoom2_MapEvents:
 	object_event 24, 10, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, SilverCaveRoom2Calcium, EVENT_SILVER_CAVE_ROOM_2_CALCIUM
 	object_event 22, 24, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, SilverCaveRoom2UltraBall, EVENT_SILVER_CAVE_ROOM_2_ULTRA_BALL
 	object_event  4, 20, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, SilverCaveRoom2PPUp, EVENT_SILVER_CAVE_ROOM_2_PP_UP
+	object_event 24, 30, SPRITE_CHANSEY_OW, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, SilverCaveRoom2ChanseyScript, -1
+	object_event 25, 31, SPRITE_PORYGON_OW, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, SilverCaveRoom2PorygonPCScript, -1
