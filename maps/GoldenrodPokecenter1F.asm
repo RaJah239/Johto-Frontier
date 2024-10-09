619eeb294441e712d139be20c71562cc41de0daa
RaJah239 <ravijagroo239@gmail.com>
2024-10-09 14:11:01 -0400

    Eon Ticket trades for Master Ball in Goldenrod Pokecentre

--- maps/GoldenrodPokecenter1F.asm
+++ maps/GoldenrodPokecenter1F.asm
@@ -32,7 +32,7 @@ GoldenrodPokecenter1FPokefanF:
 	waitbutton
 	writetext GoldenrodPokecenter1FPokefanFThisIsForYouText
 	waitbutton
-	verbosegiveitem REVIVE
+	verbosegiveitem MASTER_BALL
 	iffalse .NoRoom
 	writetext GoldenrodPokecenter1FPokefanFDaughterWillBeDelightedText
 	waitbutton
