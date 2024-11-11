3616ebd5458f79cd1ec70c5dd90bcc3ec9cd31c8
RaJah239 <ravijagroo239@gmail.com>
2024-11-10 22:10:38 -0400

    Move Deleter and Name Rater's menu loops to party after use

--- data/text/common_3.asm
+++ data/text/common_3.asm
@@ -1404,7 +1404,7 @@ _KnowsMoveText::
 _MoveKnowsOneText::
 	text "That #MON knows"
 	line "only one move."
-	done
+	prompt
 
 _AskDeleteMoveText::
 	text "Oh, make it forget"
@@ -1416,16 +1416,16 @@ _AskDeleteMoveText::
 _DeleterForgotMoveText::
 	text "Done! Your #MON"
 	line "forgot the move."
-	done
+	prompt
 
 _DeleterEggText::
 	text "An EGG doesn't"
 	line "know any moves!"
-	done
+	prompt
 
 _DeleterNoComeAgainText::
-	text "No? Come visit me"
-	line "again."
+	text "Come visit us any-"
+	line "time."
 	done
 
 _DeleterAskWhichMoveText::
