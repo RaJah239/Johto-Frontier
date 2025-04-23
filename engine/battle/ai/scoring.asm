c688315d97b2ea78f09992206801a0503e86829e
RaJah239 <ravijagroo239@gmail.com>
2025-04-23 14:36:32 -0400

    Replaced Sleep check with Leech Seed check in AI_Status

--- engine/battle/ai/scoring.asm
+++ engine/battle/ai/scoring.asm
@@ -3157,8 +3157,8 @@ AI_Status:
 	jr z, .poisonimmunity
 	cp EFFECT_POISON
 	jr z, .poisonimmunity
-	cp EFFECT_SLEEP
-	jr z, .typeimmunity
+	cp EFFECT_LEECH_SEED
+	jr z, .leechseedimmunity
 	cp EFFECT_PARALYZE
 	jr z, .typeimmunity
 
@@ -3172,8 +3172,21 @@ AI_Status:
 	ld a, [wBattleMonType1]
 	cp POISON
 	jr z, .immune
+	cp STEEL
+	jr z, .immune
 	ld a, [wBattleMonType2]
 	cp POISON
+	jr z, .immune
+	cp STEEL
+	jr z, .immune
+	jr .typeimmunity
+
+.leechseedimmunity
+	ld a, [wBattleMonType1]
+	cp GRASS
+	jr z, .immune
+	ld a, [wBattleMonType2]
+	cp GRASS
 	jr z, .immune
 
 .typeimmunity
