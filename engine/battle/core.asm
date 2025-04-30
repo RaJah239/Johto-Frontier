492266c5f0e0f0b5ec631185a98fb2d5ff40c931
RaJah239 <ravijagroo239@gmail.com>
2025-04-30 19:28:51 -0400

    Display TOX instead of PSN if badly poisoned Only in the main menu of the battle, it will still display PSN when checking stats and the party as that's how it works in Generation 2 Credit: https://github.com/PurnPum/Sketchy-Crystal

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -4762,6 +4762,7 @@ PrintPlayerHUD:
 	hlcoord 14, 8
 	push af ; back up gender
 	push hl
+	ld bc, wPlayerSubStatus5 ; To check for Toxic
 	ld de, wBattleMonStatus
 	predef PlaceNonFaintStatus
 	pop hl
@@ -4847,6 +4848,7 @@ DrawEnemyHUD:
 	hlcoord 6, 1
 	push af
 	push hl
+	ld bc, wEnemySubStatus5 ; To check for Toxic
 	ld de, wEnemyMonStatus
 	predef PlaceNonFaintStatus
 	pop hl
