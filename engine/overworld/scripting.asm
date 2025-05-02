a16ffa5953d55b37910f6231e7f36ba607d83c0c
RaJah239 <ravijagroo239@gmail.com>
2025-05-02 03:31:41 -0400

    Modify `checkitem` to check for quantity Credit: https://github.com/AzureKeys/BW3G

--- engine/overworld/scripting.asm
+++ engine/overworld/scripting.asm
@@ -1791,10 +1791,13 @@ Script_takeitem:
 	ret
 
 Script_checkitem:
+; parameters: item, quantity
 	xor a
 	ld [wScriptVar], a
 	call GetScriptByte
 	ld [wCurItem], a
+	call GetScriptByte
+	ld [wItemQuantityChange], a
 	ld hl, wNumItems
 	call CheckItem
 	ret nc
