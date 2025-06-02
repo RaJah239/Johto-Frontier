ad50a3a6b03b0023af463737b49fbaf8d919a627
RaJah239 <ravijagroo239@gmail.com>
2025-06-02 11:44:12 -0400

    Removed Rock Tunnel fly point

--- data/events/engine_flags.asm
+++ data/events/engine_flags.asm
@@ -80,7 +80,6 @@ EngineFlags:
 	engine_flag wVisitedSpawns, SPAWN_VIRIDIAN
 	engine_flag wVisitedSpawns, SPAWN_PEWTER
 	engine_flag wVisitedSpawns, SPAWN_CERULEAN
-	engine_flag wVisitedSpawns, SPAWN_ROCK_TUNNEL
 	engine_flag wVisitedSpawns, SPAWN_VERMILION
 	engine_flag wVisitedSpawns, SPAWN_LAVENDER
 	engine_flag wVisitedSpawns, SPAWN_SAFFRON
--- maps/Route10North.asm
+++ maps/Route10North.asm
@@ -2,21 +2,6 @@ Route10North_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
-	callback MAPCALLBACK_NEWMAP, .Flypoint
-
-.Flypoint:
-	setflag ENGINE_FLYPOINT_ROCK_TUNNEL
-	endcallback
-
-PowerPlantSign:
-	jumptext PowerPlantSignText
-
-Route10PokecenterSign:
-	jumpstd PokecenterSignScript
-
-PowerPlantSignText:
-	text "KANTO POWER PLANT"
-	done
 
 Route10North_MapEvents:
 	db 0, 0 ; filler
@@ -27,7 +12,5 @@ Route10North_MapEvents:
 	def_coord_events
 
 	def_bg_events
-	bg_event  5, 11, BGEVENT_READ, PowerPlantSign
-	bg_event 12,  1, BGEVENT_READ, Route10PokecenterSign
 
 	def_object_events
