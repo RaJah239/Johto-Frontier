ee2708309221267ea42256ec48172297d0788119
RaJah239 <ravijagroo239@gmail.com>
2025-06-10 13:36:00 -0400

    Porgyon's OW sprite added Sprite by https://www.deviantart.com/megaman-omega

--- constants/sprite_constants.asm
+++ constants/sprite_constants.asm
@@ -99,6 +99,7 @@
 	const SPRITE_CHRIS_SURF ; 65
 	const SPRITE_KRIS_SURF ; 66
 	const SPRITE_CHANSEY_OW ; 67
+	const SPRITE_PORYGON_OW ; 68
 DEF NUM_OVERWORLD_SPRITES EQU const_value - 1
 
 ; SpriteMons indexes (see data/sprites/sprite_mons.asm)
--- gfx/sprites.asm
+++ gfx/sprites.asm
@@ -104,3 +104,4 @@ SECTION "Sprites 3", ROMX
 ChrisSurfSpriteGFX::           INCBIN "gfx/sprites/chris_surf.2bpp"
 KrisSurfSpriteGFX::            INCBIN "gfx/sprites/kris_surf.2bpp"
 ChanseyOWSpriteGFX::           INCBIN "gfx/sprites/chansey_ow.2bpp"
+PorygonOWSpriteGFX::           INCBIN "gfx/sprites/porygon_ow.2bpp"
