8240479f87fb65a5b9a061e8fbd09480b9e7b7ea
RaJah239 <ravijagroo239@gmail.com>
2025-06-26 20:52:46 -0400

    Coloured float icon in battle

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -4729,7 +4729,7 @@ PrintPlayerHUD:
 	call IsInByteArray
 	jr nc, .skip_floaticon
 	hlcoord 19, 8
-	ld [hl], "<float>"
+	ld [hl], $72
 .skip_floaticon
 
 	ld a, [wBattleMonItem]
@@ -4862,7 +4862,7 @@ DrawEnemyHUD:
 	call IsInByteArray
 	jr nc, .skip_floaticon
 	hlcoord 10, 1
-	ld [hl], "<float>"
+	ld [hl], $72
 .skip_floaticon
 
 	ld hl, wEnemyMonHP
--- engine/gfx/cgb_layouts.asm
+++ engine/gfx/cgb_layouts.asm
@@ -143,8 +143,14 @@ _CGB_FinishBattleScreenLayout:
 	lb bc, 5, 10
 	ld a, PAL_BATTLE_BG_PLAYER_HP
 	call FillBoxCGB
+	hlcoord 19, 8, wAttrmap
+    ld a, PAL_BATTLE_BG_PLAYER_HP
+    ld [hl], a
 	hlcoord 10, 8, wAttrmap
     ld a, PAL_BATTLE_BG_5
+    ld [hl], a
+	hlcoord 10, 1, wAttrmap
+    ld a, PAL_BATTLE_BG_ENEMY_HP 
     ld [hl], a
 	hlcoord 10, 11, wAttrmap
 	lb bc, 1, 9
