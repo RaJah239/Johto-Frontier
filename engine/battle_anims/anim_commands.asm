cdb5e3860ef6ffd3e458e623ff6000710182f3bc
RaJah239 <ravijagroo239@gmail.com>
2025-07-03 10:00:19 -0400

    Add stars and audio after each "successful catch" animation Darken Pokéball colors when catch is successful Implement white background for thrown Pokéballs (needed to add successful capture stars) Deleted redundant animation for thrown Pokéballs Credit: https://github.com/Idain/Another-PKMN-Crystal-Remaster

--- engine/battle_anims/anim_commands.asm
+++ engine/battle_anims/anim_commands.asm
@@ -73,7 +73,7 @@ BattleAnimRunScript:
 .disabled
 	ld a, [wNumHits]
 	and a
-	jr z, .done
+	jp z, BattleAnim_RevertPals
 
 	ld l, a
 	ld h, 0
@@ -89,8 +89,42 @@ BattleAnimRunScript:
 	call PlayHitSound
 	call RunBattleAnimScript
 
-.done
-	call BattleAnim_RevertPals
+	ld a, [wBattleAnimFlags]
+	bit BATTLEANIM_KEEPSPRITES_F, a
+	jp z, BattleAnim_RevertPals
+	; fallthrough
+
+BattleAnimDarkenObjPals:
+; Shade colors by 3/4 of their original value.
+	push hl
+	push de
+	push bc
+
+	; Preserve VRAM bank
+	ldh a, [rSVBK]
+	push af
+	ld a, BANK(wOBPals2)
+	ldh [rSVBK], a
+
+	; Darken selected palettes by 1/4
+	ld hl, wOBPals2 palette PAL_BATTLE_OB_RED color 1
+	call DarkenColorByAQuarter
+	ld hl, wOBPals2 palette PAL_BATTLE_OB_RED color 2
+	call DarkenColorByAQuarter
+
+	ld hl, wOBPals2 palette PAL_BATTLE_OB_GREEN color 1
+	call DarkenColorByAQuarter
+
+	; Request palette update
+	ld a, TRUE
+	ldh [hCGBPalUpdate], a
+
+	; Restore previous VRAM bank
+	pop af
+	ld [rSVBK], a
+	pop bc
+	pop de
+	pop hl
 	ret
 
 RunBattleAnimScript:
@@ -203,24 +237,8 @@ BattleAnim_ClearOAM:
 	bit BATTLEANIM_KEEPOAM_F, a
 	ret nz
 	bit BATTLEANIM_KEEPSPRITES_F, a
-	jr z, .delete
+	ret nz
 
-	; Instead of deleting the sprites, make them all use PAL_BATTLE_OB_ENEMY
-	ld hl, wShadowOAMSprite00Attributes
-	ld c, NUM_SPRITE_OAM_STRUCTS
-.loop
-	ld a, [hl]
-	and ~(PALETTE_MASK | VRAM_BANK_1) ; zeros out the palette bits
-	assert PAL_BATTLE_OB_ENEMY == 0
-	ld [hli], a
-rept SPRITEOAMSTRUCT_LENGTH - 1
-	inc hl
-endr
-	dec c
-	jr nz, .loop
-	ret
-
-.delete
 	ld hl, wShadowOAM
 	ld c, wShadowOAMEnd - wShadowOAM
 	xor a
