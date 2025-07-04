0b324acf8a13e80575e93e6e46054c7ea9448de6
RaJah239 <ravijagroo239@gmail.com>
2025-07-03 20:06:09 -0400

    Added a "Clear Weather" icon for no active Weather Credit: https://github.com/TimKun55/pokecrystal

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -9180,8 +9180,10 @@ BattleStartMessage:
 
 GetWeatherImage:
 	ld a, [wBattleWeather]
-	and a
-	ret z
+	ld de, ClearWeatherImage
+	lb bc, PAL_BATTLE_OB_BLUE, 4
+	cp WEATHER_NONE
+	jr z, .done
 	ld de, RainWeatherImage
 	lb bc, PAL_BATTLE_OB_BLUE, 4
 	dec a
