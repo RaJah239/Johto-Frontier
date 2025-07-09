0270aed2cbdbbe5217093bbac37f099960d3d8de
RaJah239 <ravijagroo239@gmail.com>
2025-07-09 19:45:18 -0400

    Removed leftover code from Pokemon Red/Blue and the Level ball bypass check

--- engine/items/item_effects.asm
+++ engine/items/item_effects.asm
@@ -282,11 +282,6 @@ PokeBallEffect:
 	jp hl
 
 .skip_or_return_from_ball_fn
-	ld a, [wCurItem]
-	cp LEVEL_BALL
-	ld a, b
-	jmp z, .skip_hp_calc
-
 	ld a, b
 	ldh [hMultiplicand + 2], a
 
@@ -390,7 +385,6 @@ PokeBallEffect:
 	ld a, $ff
 .max_2
 
-.skip_hp_calc
 	ld b, a
 	ld [wFinalCatchRate], a
 	call Random
@@ -767,7 +761,6 @@ BallMultiplierFunctionTable:
 ; which ball is used in a certain situation.
 	dbw ULTRA_BALL,  UltraBallMultiplier
 	dbw GREAT_BALL,  GreatBallMultiplier
-	dbw SAFARI_BALL, SafariBallMultiplier ; Safari Ball, leftover from RBY
 	dbw HEAVY_BALL,  HeavyBallMultiplier
 	dbw LEVEL_BALL,  LevelBallMultiplier
 	dbw LURE_BALL,   LureBallMultiplier
