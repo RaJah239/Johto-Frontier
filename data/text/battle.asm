7ee7eb8049a60de079e400100493805a3367c297
RaJah239 <ravijagroo239@gmail.com>
2025-07-12 08:16:48 -0400

    After an Choice Band or Specs is stolen, no longer locked to a move Tested on Player and Enemy

--- data/text/battle.asm
+++ data/text/battle.asm
@@ -948,11 +948,9 @@ MirrorMoveFailedText:
 	prompt
 
 StoleText:
-	text "<USER>"
-	line "stole @"
 	text_ram wStringBuffer1
-	text_start
-	cont "from its foe!"
+	text " was"
+	line "stolen!"
 	prompt
 
 CantEscapeNowText:
