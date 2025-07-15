2c90eaf039ec7bcb026d1528cb6d0a186090c261
RaJah239 <ravijagroo239@gmail.com>
2025-07-15 07:00:43 -0400

    Rock Throw's animation sped up

--- data/moves/animations.asm
+++ data/moves/animations.asm
@@ -1443,7 +1443,7 @@ BattleAnim_RockThrow:
 	anim_2gfx BATTLE_ANIM_GFX_ROCKS, BATTLE_ANIM_GFX_HIT
 	anim_sound 6, 2, SFX_SPARK
 	anim_obj BATTLE_ANIM_OBJ_ROCK_BLAST, 64, 92, $4
-	anim_wait 24
+	anim_wait 16
 	anim_sound 0, 1, SFX_KARATE_CHOP
 	anim_obj BATTLE_ANIM_OBJ_HIT_YFIX, 128, 56, $0
 	anim_obj BATTLE_ANIM_OBJ_ROCK_SMASH, 128, 56, $5c
