0e6e507d5a8413fade7d3730e47c7ff766a89f40
RaJah239 <ravijagroo239@gmail.com>
2025-08-06 08:25:48 -0400

    Removed some unused text

--- data/text/battle.asm
+++ data/text/battle.asm
@@ -341,20 +341,6 @@ BattleText_StringBuffer1GrewToLevel:
 	sound_dex_fanfare_50_79
 	text_end
 
-BattleText_WildMonIsEating:
-	text "Wild @"
-	text_ram wEnemyMonNickname
-	text_start
-	line "is eating!"
-	prompt
-
-BattleText_WildMonIsAngry:
-	text "Wild @"
-	text_ram wEnemyMonNickname
-	text_start
-	line "is angry!"
-	prompt
-
 FastAsleepText:
 	text "<USER>"
 	line "is fast asleep!"
@@ -837,14 +823,6 @@ WasDisabledText:
 	text_ram wStringBuffer1
 	text " was"
 	cont "DISABLED!"
-	prompt
-
-TransformedTypeText:
-	text "<USER>"
-	line "transformed into"
-	cont "the @"
-	text_ram wStringBuffer1
-	text "-type!"
 	prompt
 
 EliminatedStatsText:
