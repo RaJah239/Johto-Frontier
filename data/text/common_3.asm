0c96482b9262e01cfd149fb97720ae9471b280be
RaJah239 <ravijagroo239@gmail.com>
2025-08-06 12:35:20 -0400

    Removed unused text

--- data/text/common_3.asm
+++ data/text/common_3.asm
@@ -1265,11 +1265,6 @@ _ItemCantUseOnEggText::
 	line "on an EGG."
 	prompt
 
-_ItemBelongsToSomeoneElseText::
-	text "That belongs to"
-	line "someone else!"
-	prompt
-
 _ItemWontHaveEffectText::
 	text "It won't have any"
 	line "effect."
@@ -1284,18 +1279,6 @@ _BallDontBeAThiefText::
 	text "Don't be a thief!"
 	prompt
 
-_NoCyclingText::
-	text "Cycling isn't"
-	line "allowed here."
-	prompt
-
-_ItemCantGetOnText::
-	text "Can't get on your"
-	line "@"
-	text_ram wStringBuffer1
-	text " now."
-	prompt
-
 _CurBoxFullText::
 	text "@"
 	text_ram wStringBuffer1
@@ -1306,15 +1289,6 @@ _StorageFullText::
 	text "The STORAGE SYSTEM"
 	line "is full. That"
 	cont "can't be used now."
-	prompt
-
-_DatabaseFullText::
-	; too technical? needs improvement
-	text "The STORAGE SYSTEM"
-	line "is overtaxed."
-
-	para "You need to save"
-	line "the game."
 	prompt
 
 _ItemUsedText::
--- engine/items/item_effects.asm
+++ engine/items/item_effects.asm
@@ -2689,10 +2689,6 @@ ItemOakWarningText:
 	text_far _OakThisIsntTheTimeText
 	text_end
 
-ItemBelongsToSomeoneElseText:
-	text_far _ItemBelongsToSomeoneElseText
-	text_end
-
 ItemWontHaveEffectText:
 	text_far _ItemWontHaveEffectText
 	text_end
@@ -2703,14 +2699,6 @@ BallBlockedText:
 
 BallDontBeAThiefText:
 	text_far _BallDontBeAThiefText
-	text_end
-
-NoCyclingText:
-	text_far _NoCyclingText
-	text_end
-
-ItemCantGetOnText:
-	text_far _ItemCantGetOnText
 	text_end
 
 StorageFullText:
