d83a138b2816c623de6fff062b89df57276ff30f
RaJah239 <ravijagroo239@gmail.com>
2025-08-12 09:31:19 -0400

    Move Pokémon in party menu with Select button (Only outside Battles)

--- engine/pokemon/mon_menu.asm
+++ engine/pokemon/mon_menu.asm
@@ -182,8 +182,17 @@ SwitchPartyMons:
 	call SetDefaultBGPAndOBP
 	call DelayFrame
 
+	ld a, [wMenuCursorY]
+	cp 1
+	ld a, 1
+	jr nz, .move_default_cursor_pos
+
+	ld a, 2
+.move_default_cursor_pos
+	ld [wMenuCursorY], a
+
 	farcall PartyMenuSelect
-	bit 1, b
+	bit B_BUTTON_F, b
 	jr c, .DontSwitch
 
 	farcall _SwitchPartyMons
--- engine/pokemon/party_menu.asm
+++ engine/pokemon/party_menu.asm
@@ -667,8 +667,31 @@ PartyMenu2DMenuData:
 	dn 2, 0 ; cursor offset
 	db 0 ; accepted buttons
 
-PartyMenuSelect:
 ; sets carry if exitted menu.
+PartyMenuSelect:
+	; can't switch pokemon order
+	; in battle with the Select button
+	; need to learn how to optimise this..
+	ld a, [wBattleMode]
+	cp WILD_BATTLE
+	jr z, .skip_select_input
+	cp TRAINER_BATTLE
+	jr z, .skip_select_input
+
+	; The Select button must only be allowed in the Start Party menu, 
+	; or in the Move party menu.
+	ld a, [wPartyMenuActionText]
+	and $ff - $4
+	jr nz, .skip_select_input
+
+	; Only values (0) PARTYMENUACTION_CHOOSE_POKEMON
+	; and 4 (PARTYMENUACTION_MOVE)
+	; are allowed through the filter.
+	ld a, [wMenuJoypadFilter]
+	or SELECT
+	ld [wMenuJoypadFilter], a
+
+.skip_select_input
 	call StaticMenuJoypad
 	call PlaceHollowCursor
 	ld a, [wPartyCount]
@@ -676,12 +699,25 @@ PartyMenuSelect:
 	ld b, a
 	ld a, [wMenuCursorY] ; menu selection?
 	cp b
-	jr z, .exitmenu ; CANCEL
+	jr c, .check_b_button
+
+	; Can't exit by pressing Select on the Cancel button.
+	ldh a, [hJoyLast]
+	cp a
+	bit SELECT_F, a
+	ld a, [wMenuCursorY]
+	jr z, .check_b_button ; Ignore select on cancel.
+
+	call HideCursor
+	jr .skip_select_input ; Ignore select on cancel.
+
+.check_b_button
 	ld [wPartyMenuCursor], a
 	ldh a, [hJoyLast]
 	ld b, a
 	bit B_BUTTON_F, b
 	jr nz, .exitmenu ; B button
+
 	ld a, [wMenuCursorY]
 	dec a
 	ld [wCurPartyMon], a
@@ -692,6 +728,25 @@ PartyMenuSelect:
 	ld a, [hl]
 	ld [wCurPartySpecies], a
 
+	ldh a, [hJoyLast]
+	cp SELECT
+	jp nz, .play_sfx
+
+	; If the player presses select, either we start the switch process,
+	; or we validate it.
+	ld a, [wPartyMenuActionText]
+	cp PARTYMENUACTION_MOVE
+	jr z, .play_sfx
+
+	push hl
+	farcall SwitchPartyMons
+	farcall WritePartyMenuTilemap
+	farcall PlacePartyMenuText
+	pop hl
+	jr PartyMenuSelect
+
+.play_sfx
+	ld a, [hl]
 	ld de, SFX_READ_TEXT_2
 	call PlaySFX
 	call WaitSFX
