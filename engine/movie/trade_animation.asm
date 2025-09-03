38163358b1798e8ddd5bb723a9aa4590e6bc8d80
RaJah239 <ravijagroo239@gmail.com>
2025-09-03 17:25:58 -0400

    Updated engine/movie/trade_animation.asm

--- engine/movie/trade_animation.asm
+++ engine/movie/trade_animation.asm
@@ -933,8 +933,7 @@ TrademonStats_MonTemplate:
 	ld a, HIGH(vBGMap1)
 	ldh [hBGMapAddress + 1], a
 	hlcoord 3, 0
-	ld b, $6
-	ld c, $d
+	lb bc, 6, 14
 	call Textbox
 	hlcoord 4, 0
 	ld de, .OTMonData
@@ -953,8 +952,7 @@ TrademonStats_Egg:
 	ld a, HIGH(vBGMap1)
 	ldh [hBGMapAddress + 1], a
 	hlcoord 3, 0
-	ld b, 6
-	ld c, 13
+	lb bc, 6, 13
 	call Textbox
 	hlcoord 4, 2
 	ld de, .EggData
