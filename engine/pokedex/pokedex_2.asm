02bcb9880ed6a7219ddb37a140d59289db938ac5
RaJah239 <ravijagroo239@gmail.com>
2025-09-22 10:03:56 -0400

    Nayru's Pokédex Bugfix: Used an older version of Nayru's Pokedex's `engine/pokedex/pokedex_moves_page.asm` since my Move Tutor moves, when more was added, wasn't showing the field moves correctly

--- engine/pokedex/pokedex_2.asm
+++ engine/pokedex/pokedex_2.asm
@@ -5,7 +5,7 @@
  	const DEXENTRY_FIELD     ; 3 ; bit 3, $8
  	const DEXENTRY_EGG		 ; 4 ; bit 4, $10, 16
  	const DEXENTRY_TMS       ; 5 ; bit 5, $20, 32
- 	; const DEXENTRY_HMS       ; bit 6, $40, 64
+ 	const DEXENTRY_HMS       ; bit 6, $40, 64
  	const DEXENTRY_MTS       ; 6 ;bit 7, $80, 128
  	const DEXENTRY_EVO		 ; 7
  	const DEXENTRY_PICS      ; 8
@@ -33,7 +33,7 @@
  EXPORT DEXENTRY_FIELD
  EXPORT DEXENTRY_EGG
  EXPORT DEXENTRY_TMS
- ; EXPORT DEXENTRY_HMS
+ EXPORT DEXENTRY_HMS
  EXPORT DEXENTRY_MTS
  EXPORT DEXENTRY_EVO		 ; 7
  EXPORT DEXENTRY_PICS      ; 8
