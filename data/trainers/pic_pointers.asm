Ryuzac/pokecrystal-johto

Link Battle/Trade Rooms Genders

data/trainers/pic_pointers.asm
@@ -70,4 +70,5 @@ TrainerPicPointers::
 	dba_pic OfficerPic
 	dba_pic GruntfPic
 	dba_pic MysticalmanPic
+	dba_pic KrisPic
 	assert_table_length NUM_TRAINER_CLASSES

RaJah239/Johto-Frontier

data/trainers/pic_pointers.asm
@@ -70,4 +70,5 @@ TrainerPicPointers::
  	dba OfficerPic
  	dba GruntfPic
  	dba MysticalmanPic
+	dba KrisPic
 	assert_table_length NUM_TRAINER_CLASSES