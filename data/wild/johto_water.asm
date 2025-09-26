f5750de589aced235e543b6c9285c8a3b4259640
RaJah239 <ravijagroo239@gmail.com>
2025-09-26 16:38:32 -0400

    Standardised Johto & Kanto's Grass and Water for easier Pokémon replacement

--- data/wild/johto_water.asm
+++ data/wild/johto_water.asm
@@ -25,79 +25,79 @@ JohtoWaterWildMons:
 
 	def_water_wildmons UNION_CAVE_B2F
 	db 4 percent ; encounter rate
-	db 15, TENTACOOL
+	db 15, WOOPER
 	db 20, QUAGSIRE
-	db 20, TENTACRUEL
+	db 15, QUAGSIRE
 	end_water_wildmons
 
 	def_water_wildmons SLOWPOKE_WELL_B1F
 	db 2 percent ; encounter rate
-	db 15, SLOWPOKE
-	db 20, SLOWPOKE
-	db 10, SLOWPOKE
+	db 15, WOOPER
+	db 20, QUAGSIRE
+	db 15, QUAGSIRE
 	end_water_wildmons
 
 	def_water_wildmons SLOWPOKE_WELL_B2F
 	db 2 percent ; encounter rate
-	db 15, SLOWPOKE
-	db 20, SLOWPOKE
-	db 20, SLOWBRO
+	db 15, WOOPER
+	db 20, QUAGSIRE
+	db 15, QUAGSIRE
 	end_water_wildmons
 
 	def_water_wildmons ILEX_FOREST
 	db 2 percent ; encounter rate
-	db 15, PSYDUCK
-	db 10, PSYDUCK
-	db 15, GOLDUCK
+	db 15, WOOPER
+	db 20, QUAGSIRE
+	db 15, QUAGSIRE
 	end_water_wildmons
 
 	def_water_wildmons MOUNT_MORTAR_1F_OUTSIDE
 	db 4 percent ; encounter rate
-	db 15, GOLDEEN
-	db 20, MARILL
-	db 20, SEAKING
+	db 15, WOOPER
+	db 20, QUAGSIRE
+	db 15, QUAGSIRE
 	end_water_wildmons
 
 	def_water_wildmons MOUNT_MORTAR_2F_INSIDE
 	db 2 percent ; encounter rate
-	db 20, GOLDEEN
-	db 25, MARILL
-	db 25, SEAKING
+	db 15, WOOPER
+	db 20, QUAGSIRE
+	db 15, QUAGSIRE
 	end_water_wildmons
 
 	def_water_wildmons MOUNT_MORTAR_B1F
 	db 2 percent ; encounter rate
-	db 15, GOLDEEN
-	db 20, MARILL
-	db 20, SEAKING
+	db 15, WOOPER
+	db 20, QUAGSIRE
+	db 15, QUAGSIRE
 	end_water_wildmons
 
 	def_water_wildmons WHIRL_ISLAND_SW
 	db 4 percent ; encounter rate
-	db 20, TENTACOOL
-	db 15, HORSEA
-	db 20, TENTACRUEL
+	db 15, WOOPER
+	db 20, QUAGSIRE
+	db 15, QUAGSIRE
 	end_water_wildmons
 
 	def_water_wildmons WHIRL_ISLAND_B2F
 	db 4 percent ; encounter rate
-	db 15, HORSEA
-	db 20, HORSEA
-	db 20, TENTACRUEL
+	db 15, WOOPER
+	db 20, QUAGSIRE
+	db 15, QUAGSIRE
 	end_water_wildmons
 
 	def_water_wildmons WHIRL_ISLAND_LUGIA_CHAMBER
 	db 4 percent ; encounter rate
-	db 20, HORSEA
-	db 20, TENTACRUEL
-	db 20, SEADRA
+	db 15, WOOPER
+	db 20, QUAGSIRE
+	db 15, QUAGSIRE
 	end_water_wildmons
 
 	def_water_wildmons SILVER_CAVE_ROOM_2
 	db 2 percent ; encounter rate
-	db 35, SEAKING
-	db 35, GOLDUCK
-	db 35, GOLDEEN
+	db 15, WOOPER
+	db 20, QUAGSIRE
+	db 15, QUAGSIRE
 	end_water_wildmons
 
 	def_water_wildmons DARK_CAVE_VIOLET_ENTRANCE
@@ -130,58 +130,58 @@ JohtoWaterWildMons:
 
 	def_water_wildmons ROUTE_30
 	db 2 percent ; encounter rate
-	db 20, POLIWAG
-	db 15, POLIWAG
-	db 20, POLIWHIRL
+	db 15, WOOPER
+	db 20, QUAGSIRE
+	db 15, QUAGSIRE
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_31
 	db 2 percent ; encounter rate
-	db 20, POLIWAG
-	db 15, POLIWAG
-	db 20, POLIWHIRL
+	db 15, WOOPER
+	db 20, QUAGSIRE
+	db 15, QUAGSIRE
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_32
 	db 6 percent ; encounter rate
-	db 15, TENTACOOL
+	db 15, WOOPER
 	db 20, QUAGSIRE
-	db 20, TENTACRUEL
+	db 15, QUAGSIRE
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_34
 	db 6 percent ; encounter rate
-	db 20, TENTACOOL
-	db 15, TENTACOOL
-	db 20, TENTACRUEL
+	db 15, WOOPER
+	db 20, QUAGSIRE
+	db 15, QUAGSIRE
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_35
 	db 4 percent ; encounter rate
-	db 20, PSYDUCK
-	db 15, PSYDUCK
-	db 20, GOLDUCK
+	db 15, WOOPER
+	db 20, QUAGSIRE
+	db 15, QUAGSIRE
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_40
 	db 6 percent ; encounter rate
-	db 20, TENTACOOL
-	db 15, TENTACOOL
-	db 20, TENTACRUEL
+	db 15, WOOPER
+	db 20, QUAGSIRE
+	db 15, QUAGSIRE
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_41
 	db 6 percent ; encounter rate
-	db 20, TENTACOOL
-	db 20, TENTACRUEL
-	db 20, MANTINE
+	db 15, WOOPER
+	db 20, QUAGSIRE
+	db 15, QUAGSIRE
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_42
 	db 4 percent ; encounter rate
-	db 20, GOLDEEN
-	db 15, GOLDEEN
-	db 20, SEAKING
+	db 15, WOOPER
+	db 20, QUAGSIRE
+	db 15, QUAGSIRE
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_43
@@ -193,9 +193,9 @@ JohtoWaterWildMons:
 
 	def_water_wildmons ROUTE_44
 	db 2 percent ; encounter rate
-	db 25, POLIWAG
-	db 20, POLIWAG
-	db 25, POLIWHIRL
+	db 15, WOOPER
+	db 20, QUAGSIRE
+	db 15, QUAGSIRE
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_45
@@ -221,9 +221,9 @@ JohtoWaterWildMons:
 
 	def_water_wildmons VIOLET_CITY
 	db 2 percent ; encounter rate
-	db 20, POLIWAG
-	db 15, POLIWAG
-	db 20, POLIWHIRL
+	db 15, WOOPER
+	db 20, QUAGSIRE
+	db 15, QUAGSIRE
 	end_water_wildmons
 
 	def_water_wildmons CIANWOOD_CITY
@@ -242,9 +242,9 @@ JohtoWaterWildMons:
 
 	def_water_wildmons ECRUTEAK_CITY
 	db 2 percent ; encounter rate
-	db 20, POLIWAG
-	db 15, POLIWAG
-	db 20, POLIWHIRL
+	db 15, WOOPER
+	db 20, QUAGSIRE
+	db 15, QUAGSIRE
 	end_water_wildmons
 
 	def_water_wildmons LAKE_OF_RAGE
@@ -263,9 +263,9 @@ JohtoWaterWildMons:
 
 	def_water_wildmons SILVER_CAVE_OUTSIDE
 	db 2 percent ; encounter rate
-	db 35, POLIWHIRL
-	db 40, POLIWHIRL
-	db 35, POLIWAG
+	db 15, WOOPER
+	db 20, QUAGSIRE
+	db 15, QUAGSIRE
 	end_water_wildmons
 
 	db -1 ; end
