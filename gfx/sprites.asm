fe0abe5b7badba5f4106709db2d223ddb8d09250
RaJah239 <ravijagroo239@gmail.com>
2025-11-11 17:54:17 -0400

    New surfing sprite added

--- gfx/sprites.asm
+++ gfx/sprites.asm
@@ -104,3 +104,8 @@ KrisBikeSpriteGFX::            INCBIN "gfx/sprites/kris_bike.2bpp"
 SuicuneSpriteGFX::             INCBIN "gfx/sprites/suicune.2bpp"
 EnteiSpriteGFX::               INCBIN "gfx/sprites/entei.2bpp"
 RaikouSpriteGFX::              INCBIN "gfx/sprites/raikou.2bpp"
+
+SECTION "Sprites 3", ROMX
+
+ChrisSurfSpriteGFX::           INCBIN "gfx/sprites/chris_surf.2bpp"
+KrisSurfSpriteGFX::            INCBIN "gfx/sprites/kris_surf.2bpp"
