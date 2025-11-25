RaJah239/Crystal-Trove

Made evening the fourth time of day Credit: https://github.com/pret/pokecrystal/wiki/Make-evening-the-fourth-time-of-day

engine/events/std_scripts.asm
@@ -61,6 +61,8 @@ PokecenterNurseScript:
 	iftrue .morn
 	checktime DAY
 	iftrue .day
+	checktime EVE
+	iftrue .eve
 	checktime NITE
 	iftrue .nite
 	sjump .ok
@@ -80,8 +82,18 @@ PokecenterNurseScript:
 	iffalse .day_comcenter
 	sjump .end_comcenter
 .day_comcenter
-   opentext
-   farwritetext NurseDayText
+	opentext
+	farwritetext NurseDayText
+	promptbutton
+	sjump .ok_first_time
+
+.eve
+	checkevent EVENT_WELCOMED_TO_POKECOM_CENTER
+	iffalse .eve_comcenter
+	sjump .end_comcenter
+.eve_comcenter
+	opentext
+	farwritetext NurseEveText
 	promptbutton
 	sjump .ok_first_time
 

engine/pokemon/evolve.asm
@@ -129,16 +129,16 @@ EvolveAfterBattle_MasterLoop:
 	cp TR_MORNDAY
 	jr z, .happiness_daylight
 
-; TR_NITE
+; TR_EVENITE
 	ld a, [wTimeOfDay]
 	cp NITE_F
-	jp nz, .dont_evolve_3
+	jp c, .dont_evolve_3 ; MORN_F or DAY_F < NITE_F
 	jr .proceed
 
 .happiness_daylight
 	ld a, [wTimeOfDay]
 	cp NITE_F
-	jp z, .dont_evolve_3
+	jp nc, .dont_evolve_3 ; MORN_F or DAY_F < NITE_F
 	jr .proceed
 
 .trade

RaJah239/Johto-Frontier

engine/events/std_scripts.asm
@@ -64,6 +64,8 @@ PokecenterNurseScript:
 	iftrue .morn
 	checktime DAY
 	iftrue .day
+	checktime EVE
+	iftrue .eve
 	checktime NITE
 	iftrue .nite
 	sjump .ok
@@ -87,6 +89,17 @@ PokecenterNurseScript:
 	sjump .ok
 .day_comcenter
 	farwritetext PokeComNurseDayText
+	promptbutton
+	sjump .ok
+
+.eve
+	checkevent EVENT_WELCOMED_TO_POKECOM_CENTER
+	iftrue .eve_comcenter
+	farwritetext NurseEveText
+	promptbutton
+	sjump .ok
+.eve_comcenter
+	farwritetext PokeComNurseEveText
 	promptbutton
 	sjump .ok
 

engine/pokemon/evolve.asm
@@ -129,16 +129,16 @@ EvolveAfterBattle_MasterLoop:
 	cp TR_MORNDAY
 	jr z, .happiness_daylight
 
-; TR_NITE
+; TR_EVENITE
 	ld a, [wTimeOfDay]
 	cp NITE_F
-	jmp nz, .dont_evolve_3
+	jmp nz, .dont_evolve_3 ; MORN_F or DAY_F < NITE_F
 	jr .proceed
 
 .happiness_daylight
 	ld a, [wTimeOfDay]
 	cp NITE_F
-	jmp z, .dont_evolve_3
+	jmp z, .dont_evolve_3 ; NITE_F or EVE_F >= NITE_F
 	jr .proceed
 
 .trade