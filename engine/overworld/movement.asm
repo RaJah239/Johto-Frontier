RaJah239/Crystal-Trove

60FPS and running shoes Can't Run past spinning Trainers
Credit: Vulcandth
Co-Authored-By: luckytyphlosion <10688458+luckytyphlosion@users.noreply.github.com>
Co-Authored-By: Fredrik Ljungdahl <fredde1994@gmail.com>

engine/overworld/movement.asm
@@ -91,6 +91,10 @@ MovementPointers:
 	dw Movement_rock_smash            ; 57
 	dw Movement_return_dig            ; 58
 	dw Movement_skyfall_top           ; 59
+	dw Movement_bike_step_down        ; 5a
+	dw Movement_bike_step_up          ; 5b
+	dw Movement_bike_step_left        ; 5c
+	dw Movement_bike_step_right       ; 5d
 	assert_table_length NUM_MOVEMENT_CMDS
 
 Movement_teleport_from:
@@ -441,51 +445,72 @@ TurnHead:
 
 Movement_slow_step_down:
 	ld a, STEP_SLOW << 2 | DOWN
-	jp NormalStep
+	jp Movement_do_step
 
 Movement_slow_step_up:
 	ld a, STEP_SLOW << 2 | UP
-	jp NormalStep
+	jp Movement_do_step
 
 Movement_slow_step_left:
 	ld a, STEP_SLOW << 2 | LEFT
-	jp NormalStep
+	jp Movement_do_step
 
 Movement_slow_step_right:
 	ld a, STEP_SLOW << 2 | RIGHT
-	jp NormalStep
+	jp Movement_do_step
+
+Movement_bike_step_down:
+	ld a, STEP_BIKE << 2 | DOWN
+	jp Movement_do_step
+
+Movement_bike_step_up:
+	ld a, STEP_BIKE << 2 | UP
+	jp Movement_do_step
+
+Movement_bike_step_left:
+	ld a, STEP_BIKE << 2 | LEFT
+	jp Movement_do_step
+
+Movement_bike_step_right:
+	ld a, STEP_BIKE << 2 | RIGHT
+	jp Movement_do_step
 
 Movement_step_down:
 	ld a, STEP_WALK << 2 | DOWN
-	jp NormalStep
+	jp Movement_do_step
 
 Movement_step_up:
 	ld a, STEP_WALK << 2 | UP
-	jp NormalStep
+	jp Movement_do_step
 
 Movement_step_left:
 	ld a, STEP_WALK << 2 | LEFT
-	jp NormalStep
+	jp Movement_do_step
 
 Movement_step_right:
 	ld a, STEP_WALK << 2 | RIGHT
+Movement_do_step:
+	ld d, OBJECT_ACTION_STEP
+Movement_normal_step:
 	jp NormalStep
 
 Movement_big_step_down:
-	ld a, STEP_BIKE << 2 | DOWN
-	jp NormalStep
+	ld a, STEP_RUN << 2 | DOWN
+	jp Movement_do_run
 
 Movement_big_step_up:
-	ld a, STEP_BIKE << 2 | UP
-	jp NormalStep
+	ld a, STEP_RUN << 2 | UP
+	jp Movement_do_run
 
 Movement_big_step_left:
-	ld a, STEP_BIKE << 2 | LEFT
-	jp NormalStep
+	ld a, STEP_RUN << 2 | LEFT
+	jp Movement_do_run
 
 Movement_big_step_right:
-	ld a, STEP_BIKE << 2 | RIGHT
-	jp NormalStep
+	ld a, STEP_RUN << 2 | RIGHT
+Movement_do_run:
+	ld d, OBJECT_ACTION_RUN
+	jr Movement_normal_step
 
 Movement_turn_away_down:
 	ld a, STEP_SLOW << 2 | DOWN
@@ -520,19 +545,19 @@ Movement_turn_in_right:
 	jp TurningStep
 
 Movement_turn_waterfall_down:
-	ld a, STEP_BIKE << 2 | DOWN
+	ld a, STEP_RUN << 2 | DOWN
 	jp TurningStep
 
 Movement_turn_waterfall_up:
-	ld a, STEP_BIKE << 2 | UP
+	ld a, STEP_RUN << 2 | UP
 	jp TurningStep
 
 Movement_turn_waterfall_left:
-	ld a, STEP_BIKE << 2 | LEFT
+	ld a, STEP_RUN << 2 | LEFT
 	jp TurningStep
 
 Movement_turn_waterfall_right:
-	ld a, STEP_BIKE << 2 | RIGHT
+	ld a, STEP_RUN << 2 | RIGHT
 	jp TurningStep
 
 Movement_slow_slide_step_down:
@@ -568,19 +593,19 @@ Movement_slide_step_right:
 	jp SlideStep
 
 Movement_fast_slide_step_down:
-	ld a, STEP_BIKE << 2 | DOWN
+	ld a, STEP_RUN << 2 | DOWN
 	jp SlideStep
 
 Movement_fast_slide_step_up:
-	ld a, STEP_BIKE << 2 | UP
+	ld a, STEP_RUN << 2 | UP
 	jp SlideStep
 
 Movement_fast_slide_step_left:
-	ld a, STEP_BIKE << 2 | LEFT
+	ld a, STEP_RUN << 2 | LEFT
 	jp SlideStep
 
 Movement_fast_slide_step_right:
-	ld a, STEP_BIKE << 2 | RIGHT
+	ld a, STEP_RUN << 2 | RIGHT
 	jp SlideStep
 
 Movement_slow_jump_step_down:
@@ -616,19 +641,19 @@ Movement_jump_step_right:
 	jp JumpStep
 
 Movement_fast_jump_step_down:
-	ld a, STEP_BIKE << 2 | DOWN
+	ld a, STEP_RUN << 2 | DOWN
 	jp JumpStep
 
 Movement_fast_jump_step_up:
-	ld a, STEP_BIKE << 2 | UP
+	ld a, STEP_RUN << 2 | UP
 	jp JumpStep
 
 Movement_fast_jump_step_left:
-	ld a, STEP_BIKE << 2 | LEFT
+	ld a, STEP_RUN << 2 | LEFT
 	jp JumpStep
 
 Movement_fast_jump_step_right:
-	ld a, STEP_BIKE << 2 | RIGHT
+	ld a, STEP_RUN << 2 | RIGHT
 	jp JumpStep
 
 Movement_turn_step_down:
@@ -662,11 +687,13 @@ TurnStep:
 	ret
 
 NormalStep:
+	push de
 	call InitStep
 	call UpdateTallGrassFlags
 	ld hl, OBJECT_ACTION
 	add hl, bc
-	ld [hl], OBJECT_ACTION_STEP
+	pop de
+	ld [hl], d
 
 	ld hl, OBJECT_TILE_COLLISION
 	add hl, bc

RaJah239/Johto-Frontier

engine/overworld/movement.asm
@@ -91,6 +91,10 @@ MovementPointers:
 	dw Movement_rock_smash            ; 57
 	dw Movement_return_dig            ; 58
 	dw Movement_skyfall_top           ; 59
+	dw Movement_bike_step_down        ; 5a
+	dw Movement_bike_step_up          ; 5b
+	dw Movement_bike_step_left        ; 5c
+	dw Movement_bike_step_right       ; 5d
 	assert_table_length NUM_MOVEMENT_CMDS
 
 Movement_teleport_from:
@@ -441,51 +445,72 @@ TurnHead:
 
 Movement_slow_step_down:
 	ld a, STEP_SLOW << 2 | DOWN
-	jmp NormalStep
+	jr Movement_do_step
 
 Movement_slow_step_up:
 	ld a, STEP_SLOW << 2 | UP
-	jmp NormalStep
+	jr Movement_do_step
 
 Movement_slow_step_left:
 	ld a, STEP_SLOW << 2 | LEFT
-	jmp NormalStep
+	jr Movement_do_step
 
 Movement_slow_step_right:
 	ld a, STEP_SLOW << 2 | RIGHT
-	jmp NormalStep
+	jr Movement_do_step
+
+Movement_bike_step_down:
+	ld a, STEP_BIKE << 2 | DOWN
+	jr Movement_do_step
+
+Movement_bike_step_up:
+	ld a, STEP_BIKE << 2 | UP
+	jr Movement_do_step
+
+Movement_bike_step_left:
+	ld a, STEP_BIKE << 2 | LEFT
+	jr Movement_do_step
+
+Movement_bike_step_right:
+	ld a, STEP_BIKE << 2 | RIGHT
+	jr Movement_do_step
 
 Movement_step_down:
 	ld a, STEP_WALK << 2 | DOWN
-	jmp NormalStep
+	jr Movement_do_step
 
 Movement_step_up:
 	ld a, STEP_WALK << 2 | UP
-	jmp NormalStep
+	jr Movement_do_step
 
 Movement_step_left:
 	ld a, STEP_WALK << 2 | LEFT
-	jmp NormalStep
+	jr Movement_do_step
 
 Movement_step_right:
 	ld a, STEP_WALK << 2 | RIGHT
+Movement_do_step:
+	ld d, OBJECT_ACTION_STEP
+Movement_normal_step:
 	jmp NormalStep
 
 Movement_big_step_down:
-	ld a, STEP_BIKE << 2 | DOWN
-	jmp NormalStep
+	ld a, STEP_RUN << 2 | DOWN
+	jr Movement_do_run
 
 Movement_big_step_up:
-	ld a, STEP_BIKE << 2 | UP
-	jmp NormalStep
+	ld a, STEP_RUN << 2 | UP
+	jr Movement_do_run
 
 Movement_big_step_left:
-	ld a, STEP_BIKE << 2 | LEFT
-	jmp NormalStep
+	ld a, STEP_RUN << 2 | LEFT
+	jr Movement_do_run
 
 Movement_big_step_right:
-	ld a, STEP_BIKE << 2 | RIGHT
-	jmp NormalStep
+	ld a, STEP_RUN << 2 | RIGHT
+Movement_do_run:
+	ld d, OBJECT_ACTION_RUN
+	jr Movement_normal_step
 
 Movement_turn_away_down:
 	ld a, STEP_SLOW << 2 | DOWN
@@ -520,19 +545,19 @@ Movement_turn_in_right:
 	jmp TurningStep
 
 Movement_turn_waterfall_down:
-	ld a, STEP_BIKE << 2 | DOWN
+	ld a, STEP_RUN << 2 | DOWN
 	jmp TurningStep
 
 Movement_turn_waterfall_up:
-	ld a, STEP_BIKE << 2 | UP
+	ld a, STEP_RUN << 2 | UP
 	jmp TurningStep
 
 Movement_turn_waterfall_left:
-	ld a, STEP_BIKE << 2 | LEFT
+	ld a, STEP_RUN << 2 | LEFT
 	jmp TurningStep
 
 Movement_turn_waterfall_right:
-	ld a, STEP_BIKE << 2 | RIGHT
+	ld a, STEP_RUN << 2 | RIGHT
 	jmp TurningStep
 
 Movement_slow_slide_step_down:
@@ -568,19 +593,19 @@ Movement_slide_step_right:
 	jmp SlideStep
 
 Movement_fast_slide_step_down:
-	ld a, STEP_BIKE << 2 | DOWN
+	ld a, STEP_RUN << 2 | DOWN
 	jmp SlideStep
 
 Movement_fast_slide_step_up:
-	ld a, STEP_BIKE << 2 | UP
+	ld a, STEP_RUN << 2 | UP
 	jmp SlideStep
 
 Movement_fast_slide_step_left:
-	ld a, STEP_BIKE << 2 | LEFT
+	ld a, STEP_RUN << 2 | LEFT
 	jmp SlideStep
 
 Movement_fast_slide_step_right:
-	ld a, STEP_BIKE << 2 | RIGHT
+	ld a, STEP_RUN << 2 | RIGHT
 	jmp SlideStep
 
 Movement_slow_jump_step_down:
@@ -616,19 +641,19 @@ Movement_jump_step_right:
 	jmp JumpStep
 
 Movement_fast_jump_step_down:
-	ld a, STEP_BIKE << 2 | DOWN
+	ld a, STEP_RUN << 2 | DOWN
 	jmp JumpStep
 
 Movement_fast_jump_step_up:
-	ld a, STEP_BIKE << 2 | UP
+	ld a, STEP_RUN << 2 | UP
 	jmp JumpStep
 
 Movement_fast_jump_step_left:
-	ld a, STEP_BIKE << 2 | LEFT
+	ld a, STEP_RUN << 2 | LEFT
 	jmp JumpStep
 
 Movement_fast_jump_step_right:
-	ld a, STEP_BIKE << 2 | RIGHT
+	ld a, STEP_RUN << 2 | RIGHT
 	jmp JumpStep
 
 Movement_turn_step_down:
@@ -662,11 +687,13 @@ TurnStep:
 	ret
 
 NormalStep:
+	push de
 	call InitStep
 	call UpdateTallGrassFlags
 	ld hl, OBJECT_ACTION
 	add hl, bc
-	ld [hl], OBJECT_ACTION_STEP
+	pop de
+	ld [hl], d
 
 	ld hl, OBJECT_TILE_COLLISION
 	add hl, bc