RaJah239/Crystal-Trove

Bugfix: Palettes for Pokémon's icons in trade animation

engine/gfx/color.asm
@@ -639,6 +639,19 @@ SetFirstOBJPalette::
 	ldh [hCGBPalUpdate], a
 	jp ApplyPals
 
+SetSecondOBJPalette::
+; input: e must contain the offset of the selected palette from PartyMenuOBPals
+	ld hl, PartyMenuOBPals
+	ld d, 0
+	add hl, de
+	ld de, wOBPals1 palette 1
+	ld bc, 1 palettes
+	ld a, BANK(wOBPals1)
+	call FarCopyWRAM
+	ld a, TRUE
+	ldh [hCGBPalUpdate], a
+	jp ApplyPals
+
 GetBattlemonBackpicPalettePointer:
 	push de
 	farcall GetPartyMonDVs

RaJah239/Johto-Frontier

engine/gfx/color.asm
@@ -559,6 +559,19 @@ SetFirstOBJPalette::
 	ldh [hCGBPalUpdate], a
 	jmp ApplyPals
 
+SetSecondOBJPalette::
+; input: e must contain the offset of the selected palette from PartyMenuOBPals
+	ld hl, PartyMenuOBPals
+	ld d, 0
+	add hl, de
+	ld de, wOBPals1 palette 1
+	ld bc, 1 palettes
+	ld a, BANK(wOBPals1)
+	call FarCopyWRAM
+	ld a, TRUE
+	ldh [hCGBPalUpdate], a
+	jmp ApplyPals
+
 GetBattlemonBackpicPalettePointer:
 	push de
 	farcall GetPartyMonDVs