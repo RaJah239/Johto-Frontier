fellowship-of-the-roms/CrystalShireEngine

Minor home back optimizations

home/audio.asm
@@ -18,11 +18,7 @@ InitSound::
 	ldh [hROMBank], a
 	ld [MBC3RomBank], a
 
-	pop af
-	pop bc
-	pop de
-	pop hl
-	ret
+	jp PopAFBCDEHL
 
 UpdateSound::
 	push hl
@@ -42,11 +38,7 @@ UpdateSound::
 	ldh [hROMBank], a
 	ld [MBC3RomBank], a
 
-	pop af
-	pop bc
-	pop de
-	pop hl
-	ret
+	jp PopAFBCDEHL
 
 _LoadMusicByte::
 ; [wCurMusicByte] = [a:de]
@@ -89,11 +81,7 @@ PlayMusic::
 	pop af
 	ldh [hROMBank], a
 	ld [MBC3RomBank], a
-	pop af
-	pop bc
-	pop de
-	pop hl
-	ret
+	jp PopAFBCDEHL
 
 PlayMusic2::
 ; Stop playing music, then play music de.
@@ -120,11 +108,7 @@ PlayMusic2::
 	ldh [hROMBank], a
 	ld [MBC3RomBank], a
 
-	pop af
-	pop bc
-	pop de
-	pop hl
-	ret
+	jp PopAFBCDEHL
 
 PlayCry::
 ; Play cry de.
@@ -171,11 +155,7 @@ endr
 	ldh [hROMBank], a
 	ld [MBC3RomBank], a
 
-	pop af
-	pop bc
-	pop de
-	pop hl
-	ret
+	jp PopAFBCDEHL
 
 PlaySFX::
 ; Play sound effect de.
@@ -211,11 +191,7 @@ PlaySFX::
 	ld [MBC3RomBank], a
 
 .done
-	pop af
-	pop bc
-	pop de
-	pop hl
-	ret
+	jp PopAFBCDEHL
 
 WaitPlaySFX::
 	call WaitSFX
@@ -320,11 +296,7 @@ FadeToMapMusic::
 	ld [wMapMusic], a
 
 .done
-	pop af
-	pop bc
-	pop de
-	pop hl
-	ret
+	jp PopAFBCDEHL
 
 PlayMapMusic::
 	push hl
@@ -347,11 +319,7 @@ PlayMapMusic::
 	call PlayMusic
 
 .done
-	pop af
-	pop bc
-	pop de
-	pop hl
-	ret
+	jp PopAFBCDEHL
 
 PlayMapMusicBike::
 ; If the player's on a bike, play the bike music instead of the map music
@@ -378,11 +346,7 @@ PlayMapMusicBike::
 	ld [wMapMusic], a
 	call PlayMusic
 
-	pop af
-	pop bc
-	pop de
-	pop hl
-	ret
+	jp PopAFBCDEHL
 
 TryRestartMapMusic::
 	ld a, [wDontPlayMapMusicOnReload]
@@ -409,11 +373,7 @@ RestartMapMusic::
 	ld e, a
 	ld d, 0
 	call PlayMusic
-	pop af
-	pop bc
-	pop de
-	pop hl
-	ret
+	jp PopAFBCDEHL
 
 SpecialMapMusic::
 	ld a, [wPlayerState]

RaJah239/Johto-Frontier

home/audio.asm
@@ -18,11 +18,7 @@ InitSound::
 	ldh [hROMBank], a
 	ld [MBC3RomBank], a
 
-	pop af
-	pop bc
-	pop de
-	pop hl
-	ret
+	jmp PopAFBCDEHL
 
 UpdateSound::
 	push hl
@@ -42,11 +38,7 @@ UpdateSound::
 	ldh [hROMBank], a
 	ld [MBC3RomBank], a
 
-	pop af
-	pop bc
-	pop de
-	pop hl
-	ret
+	jmp PopAFBCDEHL
 
 _LoadMusicByte::
 ; [wCurMusicByte] = [a:de]
@@ -89,11 +81,7 @@ PlayMusic::
 	pop af
 	ldh [hROMBank], a
 	ld [MBC3RomBank], a
-	pop af
-	pop bc
-	pop de
-	pop hl
-	ret
+	jmp PopAFBCDEHL
 
 PlayMusic2::
 ; Stop playing music, then play music de.
@@ -120,11 +108,7 @@ PlayMusic2::
 	ldh [hROMBank], a
 	ld [MBC3RomBank], a
 
-	pop af
-	pop bc
-	pop de
-	pop hl
-	ret
+	jmp PopAFBCDEHL
 
 PlayCry::
 ; Play cry de.
@@ -171,11 +155,7 @@ endr
 	ldh [hROMBank], a
 	ld [MBC3RomBank], a
 
-	pop af
-	pop bc
-	pop de
-	pop hl
-	ret
+	jmp PopAFBCDEHL
 
 WaitPlaySFX::
 	call WaitSFX
@@ -214,11 +194,7 @@ PlaySFX::
 	ld [MBC3RomBank], a
 
 .done
-	pop af
-	pop bc
-	pop de
-	pop hl
-	ret
+	jmp PopAFBCDEHL
 
 WaitSFX::
 ; infinite loop until sfx is done playing
@@ -326,11 +302,7 @@ FadeToMapMusic::
 	ld [wMapMusic], a
 
 .done
-	pop af
-	pop bc
-	pop de
-	pop hl
-	ret
+	jmp PopAFBCDEHL
 
 PlayMapMusic::
 	push hl
@@ -353,11 +325,7 @@ PlayMapMusic::
 	call PlayMusic
 
 .done
-	pop af
-	pop bc
-	pop de
-	pop hl
-	ret
+	jmp PopAFBCDEHL
 
 PlayMapMusicBike::
 ; If the player's on a bike, play the bike music instead of the map music
@@ -384,11 +352,7 @@ PlayMapMusicBike::
 	ld [wMapMusic], a
 	call PlayMusic
 
-	pop af
-	pop bc
-	pop de
-	pop hl
-	ret
+	jmp PopAFBCDEHL
 
 TryRestartMapMusic::
 	ld a, [wDontPlayMapMusicOnReload]
@@ -415,11 +379,7 @@ RestartMapMusic::
 	ld e, a
 	ld d, 0
 	call PlayMusic
-	pop af
-	pop bc
-	pop de
-	pop hl
-	ret
+	jmp PopAFBCDEHL
 
 SpecialMapMusic::
 	ld a, [wPlayerState]