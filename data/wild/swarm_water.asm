RaJah239/Crystal-Trove

Improved the Swarm System Credit: https://github.com/pret/pokecrystal/wiki/Improving-the-Swarm-System
Added swarm gramps + swarms (temporary)

data/wild/swarm_water.asm
@@ -2,6 +2,11 @@
 
 SwarmWaterWildMons:
 
-	; No swarms encountered while surfing in Crystal
+       ; qwilfish
+       map_id ROUTE_32
+       db 6 percent ; encounter rate
+	db 15, QWILFISH
+	db 15, QWILFISH
+	db 20, QWILFISH
 
 	db -1 ; end

RaJah239/Johto-Frontier

data/wild/swarm_water.asm
@@ -2,6 +2,11 @@
 
 SwarmWaterWildMons:
 
-	; No swarms encountered while surfing in Crystal
+       ; qwilfish
+       map_id ROUTE_32
+       db 6 percent ; encounter rate
+	db 15, TENTACOOL
+	db 15, TENTACOOL
+	db 20, TENTACOOL
 
 	db -1 ; end