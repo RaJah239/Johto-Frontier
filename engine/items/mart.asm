RaJah239/Crystal-Trove

Gholdengo's event added

engine/items/mart.asm
@@ -725,6 +725,34 @@ CrystalCountInBag:
 CrystalString:
 	db "Crystal@"
 
+NuggetCountInBag:
+	; Place a text box of size 1x7 at 11, 0.
+	hlcoord 11, 0
+	ld b, 1
+	ld c, 7
+	call Textbox
+	hlcoord 12, 0
+	ld de, NuggetString
+	call PlaceString
+
+	ld a, NUGGET
+	ld [wCurItem], a
+	ld hl, wNumLoot
+	call CheckItem
+
+	call CheckQuantityInBag
+
+	hlcoord 14, 1
+	ld [hl], "×"
+	inc hl
+	ld de, wMenuSelectionQuantity
+	lb bc, PRINTNUM_LEADINGZEROS | 1, 2
+	call PrintNum
+	ret
+
+NuggetString:
+	db "Nuggets@"
+
 HerbShopLadyIntroText:
 	text_far _HerbShopLadyIntroText
 	text_end

RaJah239/Johto-Frontier

engine/items/mart.asm
@@ -710,6 +710,34 @@ CrystalCountInBag:
 CrystalString:
 	db "Crystal@"
 
+NuggetCountInBag:
+	; Place a text box of size 1x7 at 11, 0.
+	hlcoord 11, 0
+	ld b, 1
+	ld c, 7
+	call Textbox
+	hlcoord 12, 0
+	ld de, NuggetString
+	call PlaceString
+
+	ld a, NUGGET
+	ld [wCurItem], a
+	ld hl, wNumItems
+	call CheckItem
+
+	call CheckQuantityInBag
+
+	hlcoord 14, 1
+	ld [hl], "×"
+	inc hl
+	ld de, wMenuSelectionQuantity
+	lb bc, PRINTNUM_LEADINGZEROS | 1, 2
+	call PrintNum
+	ret
+
+NuggetString:
+	db "Nuggets@"
+
 HerbShopLadyIntroText:
 	text_far _HerbShopLadyIntroText
 	text_end