RaJah239/Crystal-Trove

Hail added Blizzard 100% hits during Hail Hail boosts Ice types Defense by 50% Hail replaced Synthesis Decapitated all move names core.asm split into core2.asm as well

macros/scripts/battle_commands.asm
@@ -176,6 +176,7 @@ ENDM
 	command startloop               ; ae
 	command curl                    ; af
 	command hitandswitch            ; b1
+	command starthail               ; b2
 DEF NUM_EFFECT_COMMANDS EQU const_value - 1
 
 	const_def -1, -1

RaJah239/Johto-Frontier

macros/scripts/battle_commands.asm
@@ -175,6 +175,7 @@ ENDM
 	command supereffectivelooptext  ; ad
 	command startloop               ; ae
 	command curl                    ; af
+	command starthail               ; b0
 DEF NUM_EFFECT_COMMANDS EQU const_value - 1
 
 	const_def -1, -1