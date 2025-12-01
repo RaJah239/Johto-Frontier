0e73097e2cc64bfb53bc9ab12679f310720919e4
RaJah239 <ravijagroo239@gmail.com>
2025-12-01 18:01:41 -0400

    Commented out engine/battle/move_effects/mirror_move.asm

--- engine/battle/move_effects/mirror_move.asm
+++ engine/battle/move_effects/mirror_move.asm
@@ -1,49 +1,50 @@
 BattleCommand_MirrorMove:
-	call ClearLastMove
-
-	ld a, BATTLE_VARS_MOVE
-	call GetBattleVarAddr
-
-	ld a, BATTLE_VARS_LAST_COUNTER_MOVE_OPP
-	call GetBattleVar
-	and a
-	jr z, .failed
-
-	call CheckUserMove
-	jr nz, .use
-
-.failed
-	call AnimateFailedMove
-
-	ld hl, MirrorMoveFailedText
-	call StdBattleTextbox
-	jmp EndMoveEffect
-
-.use
-	ld a, b
-	ld [hl], a
-	ld [wNamedObjectIndex], a
-
-	push af
-	ld a, BATTLE_VARS_MOVE_ANIM
-	call GetBattleVarAddr
-	ld d, h
-	ld e, l
-	pop af
-
-	dec a
-	call GetMoveData
-	call GetMoveName
-	call CopyName1
-	call CheckUserIsCharging
-	jr nz, .done
-
-	ld a, [wBattleAnimParam]
-	push af
-	call BattleCommand_LowerSub
-	pop af
-	ld [wBattleAnimParam], a
-
-.done
-	call BattleCommand_MoveDelay
-	jr ResetTurn
+	ret
+;	call ClearLastMove
+;
+;	ld a, BATTLE_VARS_MOVE
+;	call GetBattleVarAddr
+;
+;	ld a, BATTLE_VARS_LAST_COUNTER_MOVE_OPP
+;	call GetBattleVar
+;	and a
+;	jr z, .failed
+;
+;	call CheckUserMove
+;	jr nz, .use
+;
+;.failed
+;	call AnimateFailedMove
+;
+;	ld hl, MirrorMoveFailedText
+;	call StdBattleTextbox
+;	jmp EndMoveEffect
+;
+;.use
+;	ld a, b
+;	ld [hl], a
+;	ld [wNamedObjectIndex], a
+;
+;	push af
+;	ld a, BATTLE_VARS_MOVE_ANIM
+;	call GetBattleVarAddr
+;	ld d, h
+;	ld e, l
+;	pop af
+;
+;	dec a
+;	call GetMoveData
+;	call GetMoveName
+;	call CopyName1
+;	call CheckUserIsCharging
+;	jr nz, .done
+;
+;	ld a, [wBattleAnimParam]
+;	push af
+;	call BattleCommand_LowerSub
+;	pop af
+;	ld [wBattleAnimParam], a
+;
+;.done
+;	call BattleCommand_MoveDelay
+;	jr ResetTurn
