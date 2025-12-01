f812cc4cdb74106382da44f4a834713a86e6a061
RaJah239 <ravijagroo239@gmail.com>
2025-12-01 18:17:46 -0400

    Commented out engine/battle/move_effects/rage.asm

--- engine/battle/move_effects/rage.asm
+++ engine/battle/move_effects/rage.asm
@@ -1,5 +1,5 @@
 BattleCommand_Rage:
-	ld a, BATTLE_VARS_SUBSTATUS4
-	call GetBattleVarAddr
-	set SUBSTATUS_RAGE, [hl]
+;	ld a, BATTLE_VARS_SUBSTATUS4
+;	call GetBattleVarAddr
+;	set SUBSTATUS_RAGE, [hl]
 	ret
