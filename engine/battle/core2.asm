f788294932b8bc1137022c3b21779b102393b775
RaJah239 <ravijagroo239@gmail.com>
2025-12-01 16:27:41 -0400

    Freed up space in effect_commands.asm again

--- engine/battle/core2.asm
+++ engine/battle/core2.asm
@@ -395,3 +395,30 @@ BlizzardHail:
 	ld a, [wBattleWeather]
 	cp WEATHER_HAIL
 	ret
+
+FlyDigMovesMiss:
+; Check for moves that can hit underground/flying opponents.
+; Return z if the current move can hit the opponent.
+	ld a, BATTLE_VARS_SUBSTATUS3_OPP
+	call GetBattleVar
+	and 1 << SUBSTATUS_FLYING | 1 << SUBSTATUS_UNDERGROUND
+	ret z
+	bit SUBSTATUS_FLYING, a
+	jr z, .DigMoves
+	ld a, BATTLE_VARS_MOVE_ANIM
+	call GetBattleVar
+	cp GUST
+	ret z
+	cp WHIRLWIND
+	ret z
+	cp THUNDER
+	ret z
+	ret
+
+.DigMoves:
+	ld a, BATTLE_VARS_MOVE_ANIM
+	call GetBattleVar
+	cp EARTHQUAKE
+	ret z
+	cp FISSURE
+	ret
--- engine/battle/effect_commands.asm
+++ engine/battle/effect_commands.asm
@@ -1633,7 +1633,7 @@ BattleCommand_CheckHit:
 	call .LockOn
 	ret nz
 
-	call .FlyDigMoves
+	farcall FlyDigMovesMiss
 	jr nz, .Miss
 
 	farcall ThunderRain
@@ -1819,39 +1819,6 @@ BattleCommand_CheckHit:
 .not_draining_sub
 	ld a, 1
 	and a
-	ret
-
-.FlyDigMoves:
-; Check for moves that can hit underground/flying opponents.
-; Return z if the current move can hit the opponent.
-
-	ld a, BATTLE_VARS_SUBSTATUS3_OPP
-	call GetBattleVar
-	and 1 << SUBSTATUS_FLYING | 1 << SUBSTATUS_UNDERGROUND
-	ret z
-
-	bit SUBSTATUS_FLYING, a
-	jr z, .DigMoves
-
-	ld a, BATTLE_VARS_MOVE_ANIM
-	call GetBattleVar
-
-	cp GUST
-	ret z
-	cp WHIRLWIND
-	ret z
-	cp THUNDER
-	ret z
-	cp TWISTER
-	ret
-
-.DigMoves:
-	ld a, BATTLE_VARS_MOVE_ANIM
-	call GetBattleVar
-
-	cp EARTHQUAKE
-	ret z
-	cp FISSURE
 	ret
 
 .StatModifiers:
