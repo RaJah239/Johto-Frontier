RaJah239/Crystal-Trove

Gunk Shot added

data/moves/animations.asm
@@ -105,7 +105,7 @@ BattleAnimations::
 	dw BattleAnim_NightShade
 	dw BattleAnim_Mimic
 	dw BattleAnim_Screech
-	dw BattleAnim_DoubleTeam
+	dw BattleAnim_GunkShot
 	dw BattleAnim_Recover
 	dw BattleAnim_Harden
 	dw BattleAnim_Minimize
@@ -1731,15 +1731,14 @@ BattleAnim_Fly:
 	anim_wait 64
 	anim_ret
 
-BattleAnim_DoubleTeam:
-	anim_call BattleAnim_TargetObj_2Row
-	anim_sound 0, 0, SFX_PSYBEAM
-	anim_bgeffect BATTLE_BG_EFFECT_DOUBLE_TEAM, $0, BG_EFFECT_USER, $0
-	anim_wait 96
-	anim_incbgeffect BATTLE_BG_EFFECT_DOUBLE_TEAM
-	anim_wait 24
-	anim_incbgeffect BATTLE_BG_EFFECT_DOUBLE_TEAM
-	anim_call BattleAnim_ShowMon_0
+BattleAnim_GunkShot:
+	anim_1gfx BATTLE_ANIM_GFX_POISON
+	anim_bgeffect BATTLE_BG_EFFECT_FLASH_INVERTED, $0, $8, $24
+	anim_bgeffect BATTLE_BG_EFFECT_SHAKE_SCREEN_X, $60, $4, $10
+	anim_call BattleAnimSub_Acid
+	anim_wait 4
+	anim_call BattleAnimSub_Sludge
+	anim_wait 16
 	anim_ret
 
 BattleAnim_Recover:
@@ -5132,4 +5131,15 @@ BattleAnimSub_SpeedLines:
 ;	anim_sound 0, 1, SFX_PAY_DAY
 ;	anim_obj BATTLE_ANIM_OBJ_PAY_DAY, 120, 76, $1
 ;	anim_wait 64
+;	anim_ret
+
+;BattleAnim_DoubleTeam:
+;	anim_call BattleAnim_TargetObj_2Row
+;	anim_sound 0, 0, SFX_PSYBEAM
+;	anim_bgeffect BATTLE_BG_EFFECT_DOUBLE_TEAM, $0, BG_EFFECT_USER, $0
+;	anim_wait 96
+;	anim_incbgeffect BATTLE_BG_EFFECT_DOUBLE_TEAM
+;	anim_wait 24
+;	anim_incbgeffect BATTLE_BG_EFFECT_DOUBLE_TEAM
+;	anim_call BattleAnim_ShowMon_0
 ;	anim_ret

data/moves/moves.asm
@@ -117,7 +117,7 @@ Moves:
 	move NIGHT_SHADE,  EFFECT_LEVEL_DAMAGE,        1, GHOST,         SPECIAL,  100, 15,   0
 	move MIMIC,        EFFECT_MIMIC,               0, NORMAL,        STATUS,   100, 10,   0
 	move SCREECH,      EFFECT_DEFENSE_DOWN_2,      0, NORMAL,        STATUS,    85, 40,   0
-	move DOUBLE_TEAM,  EFFECT_EVASION_UP,          0, NORMAL,        STATUS,   100, 15,   0
+	move GUNK_SHOT,    EFFECT_POISON_HIT,        120, POISON,        STATUS,    85, 16,  20
 	move RECOVER,      EFFECT_HEAL,                0, NORMAL,        STATUS,   100, 20,   0
 	move HARDEN,       EFFECT_DEFENSE_UP,          0, NORMAL,        STATUS,   100, 30,   0
 	move MINIMIZE,     EFFECT_EVASION_UP,          0, NORMAL,        STATUS,   100, 20,   0

RaJah239/Johto-Frontier

data/moves/animations.asm
@@ -105,7 +105,7 @@ BattleAnimations::
 	dw BattleAnim_NightShade
 	dw BattleAnim_Mimic
 	dw BattleAnim_Screech
-	dw BattleAnim_DoubleTeam
+	dw BattleAnim_GunkShot
 	dw BattleAnim_Recover
 	dw BattleAnim_Harden
 	dw BattleAnim_Minimize
@@ -1682,16 +1682,13 @@ BattleAnim_Fly:
 	anim_wait 64
 	anim_ret
 
-BattleAnim_DoubleTeam:
-	anim_call BattleAnim_TargetObj_2Row
-	anim_sound 0, 0, SFX_PSYBEAM
-	anim_bgeffect BATTLE_BG_EFFECT_DOUBLE_TEAM, $0, BG_EFFECT_USER, $0
-	anim_wait 96
-	anim_incbgeffect BATTLE_BG_EFFECT_DOUBLE_TEAM
-	anim_wait 24
-	anim_incbgeffect BATTLE_BG_EFFECT_DOUBLE_TEAM
-	anim_call BattleAnim_ShowMon_0
-	anim_ret
+BattleAnim_GunkShot:
+	anim_1gfx BATTLE_ANIM_GFX_POISON
+	anim_bgeffect BATTLE_BG_EFFECT_FLASH_INVERTED, $0, $8, $24
+	anim_bgeffect BATTLE_BG_EFFECT_SHAKE_SCREEN_X, $60, $4, $10
+	anim_call BattleAnimSub_Acid
+	anim_wait 4
+	anim_jump BattleAnimSub_Sludge
 
 BattleAnim_Recover:
 	anim_1gfx BATTLE_ANIM_GFX_BUBBLE
@@ -4849,7 +4846,7 @@ BattleAnimSub_Sludge:
 	anim_sound 0, 1, SFX_TOXIC
 	anim_obj BATTLE_ANIM_OBJ_SLUDGE, 148, 72, $0
 	anim_wait 8
-	anim_loop 5, .loop
+	anim_loop 3, .loop
 	anim_ret
 
 BattleAnimSub_Acid:
@@ -5077,4 +5074,15 @@ BattleAnimSub_SpeedLines:
 ;	anim_sound 0, 1, SFX_PAY_DAY
 ;	anim_obj BATTLE_ANIM_OBJ_PAY_DAY, 120, 76, $1
 ;	anim_wait 64
+;	anim_ret
+
+;BattleAnim_DoubleTeam:
+;	anim_call BattleAnim_TargetObj_2Row
+;	anim_sound 0, 0, SFX_PSYBEAM
+;	anim_bgeffect BATTLE_BG_EFFECT_DOUBLE_TEAM, $0, BG_EFFECT_USER, $0
+;	anim_wait 96
+;	anim_incbgeffect BATTLE_BG_EFFECT_DOUBLE_TEAM
+;	anim_wait 24
+;	anim_incbgeffect BATTLE_BG_EFFECT_DOUBLE_TEAM
+;	anim_call BattleAnim_ShowMon_0
 ;	anim_ret

data/moves/moves.asm
@@ -117,7 +117,7 @@ Moves:
 	move NIGHT_SHADE,  EFFECT_LEVEL_DAMAGE,        1, GHOST,         SPECIAL,  100, 15,   0
 	move MIMIC,        EFFECT_MIMIC,               0, NORMAL,        STATUS,   100, 10,   0
 	move SCREECH,      EFFECT_DEFENSE_DOWN_2,      0, NORMAL,        STATUS,    85, 40,   0
-	move DOUBLE_TEAM,  EFFECT_EVASION_UP,          0, NORMAL,        STATUS,   100, 15,   0
+	move GUNK_SHOT,    EFFECT_POISON_HIT,        120, POISON,        PHYSICAL,  85, 16,  20
 	move RECOVER,      EFFECT_HEAL,                0, NORMAL,        STATUS,   100, 20,   0
 	move HARDEN,       EFFECT_DEFENSE_UP,          0, NORMAL,        STATUS,   100, 30,   0
 	move MINIMIZE,     EFFECT_EVASION_UP,          0, NORMAL,        STATUS,   100, 20,   0