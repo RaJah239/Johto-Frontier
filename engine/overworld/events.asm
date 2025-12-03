82b8ee15cb0aa073d98d7c726838a88cf2b72c24
RaJah239 <ravijagroo239@gmail.com>
2025-12-03 19:10:14 -0400

    Updated engine/overworld/events.asm

--- engine/overworld/events.asm
+++ engine/overworld/events.asm
@@ -1067,14 +1067,12 @@ PlayerEventScriptPointers:
 	dba InvalidEventScript      ; (NUM_PLAYER_EVENTS)
 	assert_table_length NUM_PLAYER_EVENTS + 1
 
-InvalidEventScript:
-	end
-
 UnusedPlayerEventScript: ; unreferenced
 	end
 
 HatchEggScript:
 	callasm OverworldHatchEgg
+InvalidEventScript:
 	end
 
 WarpToNewMapScript:
