09fc14e79307c40d3e0a951b709775f6045f9809
RaJah239 <ravijagroo239@gmail.com>
2025-12-04 15:25:36 -0400

    Updated Debug ROM

--- engine/menus/intro_menu.asm
+++ engine/menus/intro_menu.asm
@@ -649,6 +649,10 @@ if DEF(_DEBUG)
 	set FAST_BOOT, [hl]
 	set AUTO_BICYCLE, [hl]
 	set RUNNING_SHOES, [hl]
+
+	ld hl, wOptions3
+	set FIELD_ACTIONS, [hl]
+	set FAST_BATTLES, [hl]
 endc
 
 	farcall InitClock
