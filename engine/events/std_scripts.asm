RaJah239/Crystal-Trove

Replaced `EVENT_NURSE_QUICK_HEAL` with its macro

engine/events/std_scripts.asm
@@ -83,8 +83,8 @@ ChanseyHealsOWScript:
 	end
 
 PokecenterNurseScript:
-	checkevent EVENT_NURSE_QUICK_HEAL
-	iftrue .ok
+	isquicknurseset
+	iffalse .ok
 
 	checktime MORN
 	iftrue .morn
@@ -176,8 +176,8 @@ PokecenterNurseScript:
 	special CheckPokerus
 	iftrue .pokerus
 .no
-	checkevent EVENT_NURSE_QUICK_HEAL
-	iftrue .done
+	isquicknurseset
+	iffalse .done
 
 	checkevent EVENT_WELCOMED_TO_POKECOM_CENTER
 	iffalse .NurseReturnPokemonText

RaJah239/Johto-Frontier

engine/events/std_scripts.asm
@@ -86,8 +86,8 @@ PokecenterNurseScript:
 ; EVENT_WELCOMED_TO_POKECOM_CENTER is never set
 
 	opentext
-	checkevent EVENT_NURSE_QUICK_HEAL
-	iftrue .QuickNurse
+	isquicknurseset
+	iffalse .QuickNurse
 	checktime MORN
 	iftrue .morn
 	checktime DAY
@@ -171,8 +171,8 @@ PokecenterNurseScript:
 	special CheckPokerus
 	iftrue .pokerus
 .no
-	checkevent EVENT_NURSE_QUICK_HEAL
-	iftrue .done
+	isquicknurseset
+	iffalse .done
 
 	farwritetext NurseReturnPokemonText
 	pause 20