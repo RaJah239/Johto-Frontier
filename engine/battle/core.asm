5f2b9f469180a03721bc5efb64e200de28eac98e
RaJah239 <ravijagroo239@gmail.com>
2025-12-05 08:51:42 -0400

    Moved `ConvertPercentages` to home/extra_functions.asm

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -5253,7 +5253,7 @@ MoveInfoBox:
 	ld a, BANK(Moves)
 	call GetFarByte
 
-	call Adjust_Percent_Battle
+	call ConvertPercentages
 	ld [wBuffer1], a
 	ld de, wBuffer1
 	lb bc, 1, 3
@@ -5288,55 +5288,6 @@ MoveInfoBox:
 	ld de, wNamedObjectIndex
 	lb bc, 1, 2
 	jmp PrintNum
-
-; This converts values out of 256 into a value
-; out of 100. It achieves this by multiplying
-; the value by 100 and dividing it by 256.
-Adjust_Percent_Battle:
-
-	; Overwrite the "hl" register.
-	ld l, a
-	ld h, 0
-	push af
-
-	; Multiplies the value of the "hl" register by 3.
-	add hl, hl
-	add a, l
-	ld l, a
-	adc h
-	sub l
-	ld h, a
-
-	; Multiplies the value of the "hl" register
-	; by 8. The value of the "hl" register
-	; is now 24 times its original value.
-	add hl, hl
-	add hl, hl
-	add hl, hl
-
-	; Add the original value of the "hl" value to itself,
-	; making it 25 times its original value.
-	pop af
-	add a, l
-	ld l, a
-	adc h
-	sbc l
-	ld h, a
-
-	; Multiply the value of the "hl" register by
-	; 4, making it 100 times its original value.
-	add hl, hl
-	add hl, hl
-
-	; Set the "l" register to 0.5, otherwise the rounded
-	; value may be lower than expected. Round the
-	; high byte to nearest and drop the low byte.
-	ld l, 0.5
-	sla l
-	sbc a
-	and 1
-	add a, h
-	ret
 
 CheckPlayerHasUsableMoves:
 	ld a, STRUGGLE
--- engine/pokemon/mon_menu.asm
+++ engine/pokemon/mon_menu.asm
@@ -1340,7 +1340,7 @@ PlaceMoveData:
 	call GetFarByte
 	cp 1
 	jr c, .if_null_chance
-	Call ConvertPercentages
+	call ConvertPercentages
 	ld [wBuffer1], a
 	ld de, wBuffer1
 	lb bc, 1, 3
@@ -1427,54 +1427,6 @@ PlaceMoveData:
 	predef PrintMoveDescription
 	ld a, $1
 	ldh [hBGMapMode], a
-	ret
-
-; This converts values out of 256 into a value
-; out of 100. It achieves this by multiplying
-; the value by 100 and dividing it by 256.
-ConvertPercentages:
-	; Overwrite the "hl" register.
-	ld l, a
-	ld h, 0
-	push af
-
-	; Multiplies the value of the "hl" register by 3.
-	add hl, hl
-	add a, l
-	ld l, a
-	adc h
-	sub l
-	ld h, a
-
-	; Multiplies the value of the "hl" register
-	; by 8. The value of the "hl" register
-	; is now 24 times its original value.
-	add hl, hl
-	add hl, hl
-	add hl, hl
-
-	; Add the original value of the "hl" value to itself,
-	; making it 25 times its original value.
-	pop af
-	add a, l
-	ld l, a
-	adc h
-	sbc l
-	ld h, a
-
-	; Multiply the value of the "hl" register by
-	; 4, making it 100 times its original value.
-	add hl, hl
-	add hl, hl
-
-	; Set the "l" register to 0.5, otherwise the rounded
-	; value may be lower than expected. Round the
-	; high byte to nearest and drop the low byte.
-	ld l, 0.5
-	sla l
-	sbc a
-	and 1
-	add a, h
 	ret
 
 ; UI elements
