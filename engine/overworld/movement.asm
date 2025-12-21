852353e1246cb875d26a7e52bd55ddc62b7095e0
RaJah239 <ravijagroo239@gmail.com>
2025-12-21 18:22:01 -0400

    fallthrough

--- engine/overworld/movement.asm
+++ engine/overworld/movement.asm
@@ -427,7 +427,7 @@ Movement_turn_head_left:
 
 Movement_turn_head_right:
 	ld a, OW_RIGHT
-	jr TurnHead
+	; fallthrough
 
 TurnHead:
 	ld hl, OBJECT_DIRECTION
@@ -670,7 +670,7 @@ Movement_turn_step_left:
 
 Movement_turn_step_right:
 	ld a, OW_RIGHT
-	jr TurnStep
+	; fallthrough
 
 TurnStep:
 	ld hl, OBJECT_1D ; new facing
