95f375ba34808b4ecf341ae8a5af50337f5ad344
RaJah239 <ravijagroo239@gmail.com>
2025-12-21 18:20:58 -0400

    fallthrough

--- engine/overworld/decorations.asm
+++ engine/overworld/decorations.asm
@@ -1055,7 +1055,7 @@ DecorationDesc_RightOrnament:
 
 DecorationDesc_Console:
 	ld a, [wDecoConsole]
-	jr DecorationDesc_OrnamentOrConsole
+	; fallthrough
 
 DecorationDesc_OrnamentOrConsole:
 	ld c, a
@@ -1152,8 +1152,7 @@ ToggleDecorationsVisibility:
 	ld de, EVENT_PLAYERS_HOUSE_2F_BIG_DOLL
 	ld hl, wVariableSprites + SPRITE_BIG_DOLL - SPRITE_VARS
 	ld a, [wDecoBigDoll]
-	call ToggleDecorationVisibility
-	ret
+	; fallthrough
 
 ToggleDecorationVisibility:
 	and a
