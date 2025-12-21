b55df30e2815dd5b7f45ed1103857a3527fd3b9d
RaJah239 <ravijagroo239@gmail.com>
2025-12-21 18:23:52 -0400

    fallthrough

--- engine/phone/phone.asm
+++ engine/phone/phone.asm
@@ -23,7 +23,7 @@ DelCellNum::
 	ret
 
 CheckCellNum::
-	jr _CheckCellNum ; useless
+	; fallthrough
 
 _CheckCellNum:
 	ld hl, wPhoneList
@@ -570,7 +570,7 @@ Phone_StartRinging:
 	ret
 
 HangUp_Wait20Frames:
-	jr Phone_Wait20Frames
+	; fallthrough
 
 Phone_Wait20Frames:
 	ld c, 20
