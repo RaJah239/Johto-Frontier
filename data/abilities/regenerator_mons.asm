deed7cf1f2984942a1e7746c60c181acae06d027
RaJah239 <ravijagroo239@gmail.com>
2025-12-25 20:53:28 -0400

    Comment

--- data/abilities/regenerator_mons.asm
+++ data/abilities/regenerator_mons.asm
@@ -1,4 +1,4 @@
-; List of all mons that auto regenerates 1/16 HP at the end of each turn
+; List of all Pokemon that auto regenerates 1/16 HP at the end of each turn
 
 RegeneratorPokemon:
     db MEW
