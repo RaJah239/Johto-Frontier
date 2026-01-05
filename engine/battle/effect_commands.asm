b2bb01190acc0edc17f164a412a2a22bc35efd1b
RaJah239 <ravijagroo239@gmail.com>
2026-01-05 15:21:42 -0400

    Ability: Serenity - Prevents added on Confusion like from Swagger, Hurricane etc.

--- engine/battle/effect_commands.asm
+++ engine/battle/effect_commands.asm
@@ -5860,6 +5860,15 @@ BattleCommand_Confuse:
 	and a
 	jr nz, BattleCommand_Confuse_CheckSnore_Swagger_ConfuseHit
 BattleCommand_FinishConfusingTarget:
+
+; =======================================================
+; === Ability: Serenity - Prevents added on Confusion ===
+; =======================================================
+	call GetOpposingMon
+	ld hl, SerenityPokemon
+	call IsInByteArray
+	jmp c, Serenity
+
 	ld bc, wEnemyConfuseCount
 	ldh a, [hBattleTurn]
 	and a
