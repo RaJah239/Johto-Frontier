4816f36891c120e051fae7ca5d46c449a24803c9
RaJah239 <ravijagroo239@gmail.com>
2026-01-12 16:21:27 -0400

    refactor `AI_Types`

--- engine/battle/ai/scoring.asm
+++ engine/battle/ai/scoring.asm
@@ -2,6 +2,7 @@ AIScoring: ; used only for BANK(AIScoring)
 
 INCLUDE "engine/battle/ai/layers/ai_basic.asm"
 INCLUDE "engine/battle/ai/ability_lists.asm"
+INCLUDE "engine/battle/ai/layers/ai_types.asm"
 
 INCLUDE "data/battle/ai/status_only_effects.asm"
 
@@ -99,99 +100,6 @@ AI_Setup:
 	inc [hl]
 	inc [hl]
 	jr .checkmove
-
-
-AI_Types:
-; Dismiss any move that the player is immune to.
-; Encourage super-effective moves.
-; Discourage not very effective moves unless
-; all damaging moves are of the same type.
-
-	ld hl, wEnemyAIMoveScores - 1
-	ld de, wEnemyMonMoves
-	ld b, NUM_MOVES + 1
-.checkmove
-	dec b
-	ret z
-
-	inc hl
-	ld a, [de]
-	and a
-	ret z
-
-	inc de
-	call AIGetEnemyMove
-
-	push hl
-	push bc
-	push de
-	ld a, 1
-	ldh [hBattleTurn], a
-	callfar BattleCheckTypeMatchup
-	pop de
-	pop bc
-	pop hl
-
-	ld a, [wTypeMatchup]
-	and a
-	jr z, .immune
-	cp EFFECTIVE
-	jr z, .checkmove
-	jr c, .noteffective
-
-; effective
-	ld a, [wEnemyMoveStruct + MOVE_POWER]
-	and a
-	jr z, .checkmove
-	dec [hl]
-	jr .checkmove
-
-.noteffective
-; Discourage this move if there are any moves
-; that do damage of a different type.
-	push hl
-	push de
-	push bc
-	ld a, [wEnemyMoveStruct + MOVE_TYPE]
-	and TYPE_MASK
-	ld d, a
-	ld hl, wEnemyMonMoves
-	ld b, NUM_MOVES + 1
-	ld c, 0
-.checkmove2
-	dec b
-	jr z, .movesdone
-
-	ld a, [hli]
-	and a
-	jr z, .movesdone
-
-	call AIGetEnemyMove
-	ld a, [wEnemyMoveStruct + MOVE_TYPE]
-	and TYPE_MASK
-	cp d
-	jr z, .checkmove2
-	ld a, [wEnemyMoveStruct + MOVE_POWER]
-	and a
-	jr nz, .damaging
-	jr .checkmove2
-
-.damaging
-	ld c, a
-.movesdone
-	ld a, c
-	pop bc
-	pop de
-	pop hl
-	and a
-	jr z, .checkmove
-	inc [hl]
-	jr .checkmove
-
-.immune
-	call AIDiscourageMove
-	jr .checkmove
-
 
 AI_Offensive:
 ; Greatly discourage non-damaging moves.
