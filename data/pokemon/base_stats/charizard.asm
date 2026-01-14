1b61c4b02cf0d0520edd32a5839d6ac8d183474b
RaJah239 <ravijagroo239@gmail.com>
2026-01-14 11:06:24 -0400

    Updated data/pokemon/base_stats/charizard.asm

--- data/pokemon/base_stats/charizard.asm
+++ data/pokemon/base_stats/charizard.asm
@@ -1,12 +1,12 @@
 	db CHARIZARD ; 006
 
-	db  78,  84,  78, 100, 109,  85
+	db  78,  84,  78, 100, 110,  85
 	evs  0,   0,   0,   0,   3,   0
 	;   hp  atk  def  spd  sat  sdf
 
-	db FIRE, FLYING ; type
+	db FIRE, DRAGON ; type
 	db 45 ; catch rate
-	db 209 ; base exp
+	db 245 ; base exp
 	db NO_ITEM, NO_ITEM ; items
 	db GENDER_F12_5 ; gender ratio
 	db 20 ; step cycles to hatch
