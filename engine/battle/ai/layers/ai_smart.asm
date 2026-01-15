ad2ca0c58258ed78c92a1baf2ae17e9b2fe25e72
RaJah239 <ravijagroo239@gmail.com>
2026-01-15 18:38:11 -0400

    Drowzee and Hypno with Zen Headbutt get Priority Move treatment by the AI

--- engine/battle/ai/layers/ai_smart.asm
+++ engine/battle/ai/layers/ai_smart.asm
@@ -2333,6 +2333,19 @@ AI_Smart_Flinch:
 	jr AI_Smart_PriorityHit
 
 .not_blastoise_or_iron_head
+	ld a, [wEnemyMonSpecies]
+	cp DROWZEE
+	jr z, .not_drowzee_or_hypno_or_zen_headbutt
+	cp HYPNO
+	jr z, .not_drowzee_or_hypno_or_zen_headbutt
+
+	; check if drowzee or hypno has zen headbutt
+	ld a, [wEnemyMoveStruct + MOVE_ANIM]
+	cp ZEN_HEADBUTT
+	jr nc, .not_drowzee_or_hypno_or_zen_headbutt
+	jr AI_Smart_PriorityHit
+
+.not_drowzee_or_hypno_or_zen_headbutt
 ; do nothing if slower than player
 	call DoesAIOutSpeedPlayer
 	ret nc
