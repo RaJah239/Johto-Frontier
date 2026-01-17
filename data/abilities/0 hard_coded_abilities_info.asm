0e956931237d34b70be16dfd7e0be76d69bc051d
RaJah239 <ravijagroo239@gmail.com>
2026-01-17 17:43:02 -0400

    added file

---
+++ data/abilities/0 hard_coded_abilities_info.asm
@@ -0,0 +1,10 @@
+; location(s) of abilties that had to be hard coded elsewhere
+
+; ===================================
+; === Ability: Priority Abilities ===
+; ===================================
+; in engine/battle/core.asm" `GetMovePriority:`
+
+; Snolax gets priority Curse
+; Blastoise gets priority Iron Head
+; Drowzee and Hypno both get priority Zen Headbutt
