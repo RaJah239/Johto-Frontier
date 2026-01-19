51aa3834835bfa706e95b3c4e0332eab504a67f5
RaJah239 <ravijagroo239@gmail.com>
2026-01-19 14:22:19 -0400

    Removed `BATTLE_ANIM_OBJ_B0`

--- constants/battle_anim_constants.asm
+++ constants/battle_anim_constants.asm
@@ -183,7 +183,6 @@ DEF BATTLEANIM_BASE_TILE EQU 7 * 7  ; Maximum size of a pokemon picture
 	const BATTLE_ANIM_OBJ_BETA_PURSUIT        ; ad
 	const BATTLE_ANIM_OBJ_SHOOTING_SPARKLE    ; ae
 	const BATTLE_ANIM_OBJ_RAIN                ; af
-	const BATTLE_ANIM_OBJ_B0                  ; b0
 	const BATTLE_ANIM_OBJ_PSYCH_UP            ; b1
 	const BATTLE_ANIM_OBJ_ANCIENTPOWER        ; b2
 	const BATTLE_ANIM_OBJ_AEROBLAST           ; b3
@@ -304,7 +303,6 @@ DEF NUM_BATTLE_ANIM_OBJS EQU const_value
 	const BATTLE_ANIM_FUNC_RAPID_SPIN                ; 48
 	const BATTLE_ANIM_FUNC_BETA_PURSUIT              ; 49
 	const BATTLE_ANIM_FUNC_RAIN_SANDSTORM            ; 4a
-	const BATTLE_ANIM_FUNC_BATTLE_ANIM_OBJ_B0        ; 4b
 	const BATTLE_ANIM_FUNC_PSYCH_UP                  ; 4c
 	const BATTLE_ANIM_FUNC_ANCIENT_POWER             ; 4d
 	const BATTLE_ANIM_FUNC_ROCK_SMASH                ; 4e
