4a8c66bc998b18380a11f58ad7278760bbbc2444
RaJah239 <ravijagroo239@gmail.com>
2026-01-19 20:28:23 -0400

    Updated Dragon Dance's custom palette

--- data/moves/animations.asm
+++ data/moves/animations.asm
@@ -3731,7 +3731,7 @@ BattleAnim_SpiderWeb:
 	anim_ret
 
 BattleAnim_DragonDance:
-	anim_setobjpal PAL_BATTLE_OB_RED, PAL_BTLCUSTOM_FIRE
+	anim_setobjpal PAL_BATTLE_OB_RED, PAL_BTLCUSTOM_WATER
 	anim_2gfx BATTLE_ANIM_GFX_FIRE, BATTLE_ANIM_GFX_SPEED
 	anim_call BattleAnim_TargetObj_2Row
 	anim_bgeffect BATTLE_BG_EFFECT_WOBBLE_MON, $0, BG_EFFECT_USER, $0
@@ -3746,7 +3746,7 @@ BattleAnim_DragonDance:
 	anim_wait 96
 	anim_incbgeffect BATTLE_BG_EFFECT_WOBBLE_MON
 	anim_call BattleAnim_ShowMon_0
-	anim_setobjpal PAL_BATTLE_OB_GRAY, PAL_BTLCUSTOM_GRAY
+	anim_setobjpal PAL_BATTLE_OB_RED, PAL_BTLCUSTOM_RED
 	anim_ret
 
 BattleAnim_QuiverDance:
