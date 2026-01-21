41e305d245ca26707ad30318fe33cd8293ceb962
RaJah239 <ravijagroo239@gmail.com>
2026-01-20 20:13:34 -0400

    Removed unused `BattleAnimCmd_IncVar`

--- engine/battle_anims/anim_commands.asm
+++ engine/battle_anims/anim_commands.asm
@@ -352,13 +352,12 @@ BattleAnimCommands::
 	dw BattleAnimCmd_KeepSpritesAndOAM
 	dw BattleAnimCmd_IfParamEqual
 	dw BattleAnimCmd_SetVar
-	dw BattleAnimCmd_IncVar
 	dw BattleAnimCmd_IfVarEqual
 	dw BattleAnimCmd_Jump
 	dw BattleAnimCmd_Loop
 	dw BattleAnimCmd_Call
 	dw BattleAnimCmd_Ret
-	assert_table_length 243 - FIRST_BATTLE_ANIM_CMD
+	assert_table_length 242 - FIRST_BATTLE_ANIM_CMD
 
 BattleAnimCmd_Ret:
 	ld hl, wBattleAnimFlags
@@ -451,11 +450,6 @@ BattleAnimCmd_Loop:
 BattleAnimCmd_SetVar:
 	call GetBattleAnimByte
 	ld [wBattleAnimVar], a
-	ret
-
-BattleAnimCmd_IncVar:
-	ld hl, wBattleAnimVar
-	inc [hl]
 	ret
 
 BattleAnimCmd_IfVarEqual:
