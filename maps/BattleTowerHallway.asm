83d6200106b28f6736ef8aef7c5b999c8e091eb1
RaJah239 <ravijagroo239@gmail.com>
2026-02-05 21:38:35 -0400

    battle tower - always go to a random room

--- maps/BattleTowerHallway.asm
+++ maps/BattleTowerHallway.asm
@@ -17,34 +17,20 @@ BattleTowerHallwayNoopScene:
 
 BattleTowerHallwayChooseBattleRoomScript:
 	follow BATTLETOWERHALLWAY_RECEPTIONIST, PLAYER
-	callasm .asm_load_battle_room
-	sjump .WalkToChosenBattleRoom
-
-.asm_load_battle_room
-	ldh a, [rSVBK]
-	push af
-
-	ld a, BANK(wBTChoiceOfLvlGroup)
-	ldh [rSVBK], a
-	ld a, [wBTChoiceOfLvlGroup]
-	ld [wScriptVar], a
-
-	pop af
-	ldh [rSVBK], a
-	ret
-
-; enter different rooms for different levels to battle against
-; at least it should look like that
-; because all warps lead to the same room
-.WalkToChosenBattleRoom:
+.resample
+	random 9
+	ifequal 1, .L10L20
+	ifequal 2, .L30L40
 	ifequal 3, .L30L40
-	ifequal 4, .L30L40
+	ifequal 4, .L50L60
 	ifequal 5, .L50L60
-	ifequal 6, .L50L60
+	ifequal 6, .L70L80
 	ifequal 7, .L70L80
-	ifequal 8, .L70L80
+	ifequal 8, .L90L100
 	ifequal 9, .L90L100
-	ifequal 10, .L90L100
+	sjump .resample
+
+.L10L20:
 	applymovement BATTLETOWERHALLWAY_RECEPTIONIST, MovementData_BattleTowerHallwayWalkTo1020Room
 	sjump .EnterBattleRoom
 
