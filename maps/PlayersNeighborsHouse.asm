bd165e89adfb0836b2415a5a72dbd1565315f639
RaJah239 <ravijagroo239@gmail.com>
2026-02-18 19:59:09 -0400

    updated maps/PlayersNeighborsHouse.asm

--- maps/PlayersNeighborsHouse.asm
+++ maps/PlayersNeighborsHouse.asm
@@ -1,3 +1,19 @@
+PlayersNeighborsHouse_MapEvents:
+	def_warp_events
+	warp_event  2,  7, NEW_BARK_TOWN, 3
+	warp_event  3,  7, NEW_BARK_TOWN, 3
+
+	def_coord_events
+
+	def_bg_events
+	bg_event  0,  1, BGEVENT_JUMPSTD, MAGAZINE_BOOKSHELF_SCRIPT
+	bg_event  1,  1, BGEVENT_JUMPSTD, MAGAZINE_BOOKSHELF_SCRIPT
+	bg_event  7,  1, BGEVENT_JUMPSTD, RADIO1SCRIPT
+
+	def_object_events
+	object_event  2,  3, SPRITE_COOLTRAINER_F, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_COMMAND, jumptextfaceplayer, PlayersNeighborsDaughterText, -1
+	object_event  5,  3, SPRITE_POKEFAN_F, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_COMMAND, jumptextfaceplayer, PlayersNeighborText, -1
+
 	object_const_def
 	const PLAYERSNEIGHBORSHOUSE_COOLTRAINER_F
 	const PLAYERSNEIGHBORSHOUSE_POKEFAN_F
@@ -6,15 +22,6 @@ PlayersNeighborsHouse_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
-
-PlayersNeighborsDaughterScript:
-	jumptextfaceplayer PlayersNeighborsDaughterText
-
-PlayersNeighborScript:
-	jumptextfaceplayer PlayersNeighborText
-
-PlayersNeighborsHouseRadioScript:
-	jumpstd Radio1Script
 
 PlayersNeighborsDaughterText:
 	text "Prof.Elm's research"
@@ -40,19 +47,3 @@ PlayersNeighborText:
 
 	para "But then, so do I!"
 	done
-
-PlayersNeighborsHouse_MapEvents:
-	def_warp_events
-	warp_event  2,  7, NEW_BARK_TOWN, 3
-	warp_event  3,  7, NEW_BARK_TOWN, 3
-
-	def_coord_events
-
-	def_bg_events
-	bg_event  0,  1, BGEVENT_JUMPSTD, MAGAZINE_BOOKSHELF_SCRIPT
-	bg_event  1,  1, BGEVENT_JUMPSTD, MAGAZINE_BOOKSHELF_SCRIPT
-	bg_event  7,  1, BGEVENT_READ, PlayersNeighborsHouseRadioScript
-
-	def_object_events
-	object_event  2,  3, SPRITE_COOLTRAINER_F, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, PlayersNeighborsDaughterScript, -1
-	object_event  5,  3, SPRITE_POKEFAN_F, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, PlayersNeighborScript, -1
