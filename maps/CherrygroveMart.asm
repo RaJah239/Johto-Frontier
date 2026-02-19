fdfbef7050eeedb945d2723b994883d0f236f005
RaJah239 <ravijagroo239@gmail.com>
2026-02-19 17:48:34 -0400

    updated maps/CherrygroveMart.asm

--- maps/CherrygroveMart.asm
+++ maps/CherrygroveMart.asm
@@ -1,3 +1,17 @@
+CherrygroveMart_MapEvents:
+	def_warp_events
+	warp_event  2,  7, CHERRYGROVE_CITY, 2
+	warp_event  3,  7, CHERRYGROVE_CITY, 2
+
+	def_coord_events
+
+	def_bg_events
+
+	def_object_events
+	object_event  1,  3, SPRITE_CLERK, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_PINK, OBJECTTYPE_SCRIPT, 0, CherrygroveMartClerkScript, -1
+	object_event  7,  6, SPRITE_COOLTRAINER_M, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 2, 0, -1, -1, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, CherrygroveMartCooltrainerMText, -1
+	object_event  2,  5, SPRITE_YOUNGSTER, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_COMMAND, jumptextfaceplayer, CherrygroveMartYoungsterText, -1
+
 	object_const_def
 	const CHERRYGROVEMART_CLERK
 	const CHERRYGROVEMART_COOLTRAINER_M
@@ -17,74 +31,42 @@ CherrygroveMartClerkScript:
 	ifgreater 2, .ThreeBadgesMart
 	ifgreater 0, .OneBadgeMart
 	pokemart MARTTYPE_STANDARD, MART_NO_BADGES
-	closetext
-	end
+	endtext
 
 .OneBadgeMart:
 	pokemart MARTTYPE_STANDARD, MART_ONE_BADGE
-	closetext
-	end
-
+	endtext
 .ThreeBadgesMart:
 	pokemart MARTTYPE_STANDARD, MART_THREE_BADGES
-	closetext
-	end
-
+	endtext
 .FiveBadgesMart:
 	pokemart MARTTYPE_STANDARD, MART_FIVE_BADGES
-	closetext
-	end
-
+	endtext
 .SevenBadgesMart:
 	pokemart MARTTYPE_STANDARD, MART_SEVEN_BADGES
-	closetext
-	end
-
+	endtext
 .EightBadgesMart:
 	pokemart MARTTYPE_STANDARD, MART_EIGHT_BADGES
-	closetext
-	end
+	endtext
 
-CherrygroveMartCooltrainerMScript:
-	jumptextfaceplayer CherrygroveMartCooltrainerMText_PokeBallsInStock
-
-CherrygroveMartCooltrainerMText_PokeBallsInStock:
+CherrygroveMartCooltrainerMText:
 	text "I'm here to buy"
 	line "more #balls to"
 	cont "catch #mon!"
 	done
 
-CherrygroveMartYoungsterScript:
-	jumptextfaceplayer CherrygroveMartYoungsterText
-
 CherrygroveMartYoungsterText:
 	text "When I was walking"
 	line "in the grass, a"
-
-	para "bug #MON poi-"
-	line "soned my own!"
+	cont "bug #mon poi-"
+	cont "soned my own!"
 
 	para "I just kept going,"
-	line "but my #MON"
-	
-	para "steadily lost its"
-	line "health until it"
+	line "but my #mon"
+	cont "steadily lost its"
+	cont "health until it"
 	cont "almost fainted!"
 
 	para "You should keep an"
-	line "ANTIDOTE with you."
+	line "Antidote with you."
 	done
-
-CherrygroveMart_MapEvents:
-	def_warp_events
-	warp_event  2,  7, CHERRYGROVE_CITY, 2
-	warp_event  3,  7, CHERRYGROVE_CITY, 2
-
-	def_coord_events
-
-	def_bg_events
-
-	def_object_events
-	object_event  1,  3, SPRITE_CLERK, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, CherrygroveMartClerkScript, -1
-	object_event  7,  6, SPRITE_COOLTRAINER_M, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 2, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, CherrygroveMartCooltrainerMScript, -1
-	object_event  2,  5, SPRITE_YOUNGSTER, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, CherrygroveMartYoungsterScript, -1
