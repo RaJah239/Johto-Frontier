8686f847bc2d43cf78782f7b1405498eadc6da41
RaJah239 <ravijagroo239@gmail.com>
2026-02-19 17:57:07 -0400

    updated maps/CherrygroveGymSpeechHouse.asm

--- maps/CherrygroveGymSpeechHouse.asm
+++ maps/CherrygroveGymSpeechHouse.asm
@@ -1,3 +1,18 @@
+CherrygroveGymSpeechHouse_MapEvents:
+	def_warp_events
+	warp_event  2,  7, CHERRYGROVE_CITY, 3
+	warp_event  3,  7, CHERRYGROVE_CITY, 3
+
+	def_coord_events
+
+	def_bg_events
+	bg_event  0,  1, BGEVENT_JUMPSTD, MAGAZINE_BOOKSHELF_SCRIPT
+	bg_event  1,  1, BGEVENT_JUMPSTD, MAGAZINE_BOOKSHELF_SCRIPT
+
+	def_object_events
+	object_event  2,  3, SPRITE_POKEFAN_M, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, CherrygroveGymSpeechHousePokefanMScript, -1
+	object_event  5,  5, SPRITE_BUG_CATCHER, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 1, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_COMMAND, jumptextfaceplayer, CherrygroveGymSpeechHouseBugCatcherText, -1
+
 	object_const_def
 	const CHERRYGROVEGYMSPEECHHOUSE_POKEFAN_M
 	const CHERRYGROVEGYMSPEECHHOUSE_BUG_CATCHER
@@ -9,12 +24,6 @@ CherrygroveGymSpeechHouse_MapScripts:
 
 CherrygroveGymSpeechHousePokefanMScript:
 	jumptextfaceplayer CherrygroveGymSpeechHousePokefanMText
-
-CherrygroveGymSpeechHouseBugCatcherScript:
-	jumptextfaceplayer CherrygroveGymSpeechHouseBugCatcherText
-
-CherrygroveGymSpeechHouseBookshelf:
-	jumpstd PictureBookshelfScript
 
 CherrygroveGymSpeechHousePokefanMText:
 	text "You're trying to"
@@ -28,31 +37,20 @@ CherrygroveGymSpeechHousePokefanMText:
 
 	para "all over JOHTO and"
 	line "collect BADGES."
+	
+	para "If you gather them"
+	line "all, I'll give you"
+	cont "something for your"
+	cont "trouble."
 	done
 
 CherrygroveGymSpeechHouseBugCatcherText:
 	text "When I get older,"
 	line "I'm going to be a"
-	cont "GYM LEADER!"
+	cont "Gym Leader!"
 
-	para "I make my #MON"
+	para "I make my #mon"
 	line "battle with my"
-
-	para "friend's to make"
-	line "them tougher!"
+	cont "friend's to make"
+	cont "them tougher!"
 	done
-
-CherrygroveGymSpeechHouse_MapEvents:
-	def_warp_events
-	warp_event  2,  7, CHERRYGROVE_CITY, 3
-	warp_event  3,  7, CHERRYGROVE_CITY, 3
-
-	def_coord_events
-
-	def_bg_events
-	bg_event  0,  1, BGEVENT_READ, CherrygroveGymSpeechHouseBookshelf
-	bg_event  1,  1, BGEVENT_READ, CherrygroveGymSpeechHouseBookshelf
-
-	def_object_events
-	object_event  2,  3, SPRITE_POKEFAN_M, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, CherrygroveGymSpeechHousePokefanMScript, -1
-	object_event  5,  5, SPRITE_BUG_CATCHER, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 1, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, CherrygroveGymSpeechHouseBugCatcherScript, -1
