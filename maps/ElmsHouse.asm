7e23b8bcd6bf70a48c2cb1fa77b38f7141060cf8
RaJah239 <ravijagroo239@gmail.com>
2026-02-18 20:08:25 -0400

    updated maps/ElmsHouse.asm

--- maps/ElmsHouse.asm
+++ maps/ElmsHouse.asm
@@ -1,72 +1,3 @@
-	object_const_def
-	const ELMSHOUSE_ELMS_WIFE
-	const ELMSHOUSE_ELMS_SON
-
-ElmsHouse_MapScripts:
-	def_scene_scripts
-
-	def_callbacks
-
-ElmsWife:
-	jumptextfaceplayer ElmsWifeText
-
-ElmsSon:
-	jumptextfaceplayer ElmsSonText
-
-ElmsHousePC:
-	jumptext ElmsHousePCText
-
-ElmsHouseBookshelf:
-	jumpstd DifficultBookshelfScript
-
-ElmsWifeText:
-	text "Hi, <PLAY_G>! My"
-	line "husband's always"
-
-	para "so busy--I hope"
-	line "he's OK."
-
-	para "When he's caught"
-	line "up in his #MON"
-
-	para "research, he even"
-	line "forgets to eat."
-	done
-
-ElmsSonText:
-	text "When I grow up,"
-	line "I'm going to help"
-	cont "my dad!"
-
-	para "I'm going to be a"
-	line "great #MON"
-	cont "professor!"
-	done
-
-ElmsHousePCText:
-	text "#MON. Where do"
-	line "they come from? "
-
-	para "Where are they"
-	line "going?"
-
-	para "Why has no one"
-	line "ever witnessed a"
-	cont "#MON's birth?"
-
-	para "I want to know! I"
-	line "will dedicate my"
-
-	para "life to the study"
-	line "of #MON!"
-
-	para "…"
-
-	para "It's a part of"
-	line "PROF.ELM's re-"
-	cont "search papers."
-	done
-
 ElmsHouse_MapEvents:
 	def_warp_events
 	warp_event  2,  7, NEW_BARK_TOWN, 4
@@ -76,9 +7,61 @@ ElmsHouse_MapEvents:
 
 	def_bg_events
 	bg_event  0,  1, BGEVENT_READ, ElmsHousePC
-	bg_event  6,  1, BGEVENT_READ, ElmsHouseBookshelf
-	bg_event  7,  1, BGEVENT_READ, ElmsHouseBookshelf
+	bg_event  6,  1, BGEVENT_JUMPSTD, DIFFICULT_BOOKSHELF_SCRIPT
+	bg_event  7,  1, BGEVENT_JUMPSTD, DIFFICULT_BOOKSHELF_SCRIPT
 
 	def_object_events
-	object_event  1,  5, SPRITE_TEACHER, SPRITEMOVEDATA_WALK_UP_DOWN, 0, 1, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, ElmsWife, -1
-	object_event  5,  4, SPRITE_BUG_CATCHER, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ElmsSon, -1
+	object_event  1,  5, SPRITE_TEACHER, SPRITEMOVEDATA_WALK_UP_DOWN, 0, 1, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_COMMAND, jumptextfaceplayer, ElmsWifeText, -1
+	object_event  5,  4, SPRITE_BUG_CATCHER, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, ElmsSonText, -1
+
+	object_const_def
+	const ELMSHOUSE_ELMS_WIFE
+	const ELMSHOUSE_ELMS_SON
+
+ElmsHouse_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+ElmsWifeText:
+	text "Hi, <PLAY_G>! My"
+	line "husband's always"
+	cont "so busy--I hope"
+	cont "he's OK."
+
+	para "When he's caught"
+	line "up in his #mon"
+	cont "research, he even"
+	cont "forgets to eat."
+	done
+
+ElmsSonText:
+	text "When I grow up,"
+	line "I'm going to help"
+	cont "my dad!"
+
+	para "I'm going to be a"
+	line "great #mon"
+	cont "professor!"
+	done
+
+ElmsHousePC:
+	jumpthistext
+		text "#mon…"
+
+		para "Endless mysteries!"
+		line "Endless wonder!"
+
+		para "I want to know it"
+		line "all!"
+
+		para "I will dedicate my"
+		line "life to the study"
+		cont "of #mon!"
+
+		para "…"
+
+		para "It's a part of"
+		line "Prof.Elm's research"
+		cont "papers."
+		done
