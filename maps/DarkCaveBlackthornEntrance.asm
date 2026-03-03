6b4ea34390ee5a0b4c7741c0e5c6d022af1f6f2c
RaJah239 <ravijagroo239@gmail.com>
2026-03-03 13:46:31 -0400

    Update maps/DarkCaveBlackthornEntrance.asm

--- maps/DarkCaveBlackthornEntrance.asm
+++ maps/DarkCaveBlackthornEntrance.asm
@@ -1,64 +1,3 @@
-	object_const_def
-	const DARKCAVEBLACKTHORNENTRANCE_PHARMACIST
-	const DARKCAVEBLACKTHORNENTRANCE_POKE_BALL1
-	const DARKCAVEBLACKTHORNENTRANCE_POKE_BALL2
-
-DarkCaveBlackthornEntrance_MapScripts:
-	def_scene_scripts
-
-	def_callbacks
-
-DarkCaveBlackthornEntrancePharmacistScript:
-	faceplayer
-	opentext
-	checkevent EVENT_GOT_BLACKGLASSES_IN_DARK_CAVE
-	iftrue .GotBlackglasses
-	writetext DarkCaveBlackthornEntrancePharmacistText1
-	promptbutton
-	verbosegiveitem BLACKGLASSES
-	iffalse .PackFull
-	setevent EVENT_GOT_BLACKGLASSES_IN_DARK_CAVE
-.GotBlackglasses:
-	writetext DarkCaveBlackthornEntrancePharmacistText2
-	waitbutton
-.PackFull:
-	closetext
-	end
-
-DarkCaveBlackthornEntranceRevive:
-	itemball REVIVE
-
-DarkCaveBlackthornEntranceTMSnore:
-	itemball TM_SNORE
-
-DarkCaveBlackthornEntrancePharmacistText1:
-	text "Whoa! You startled"
-	line "me there!"
-
-	para "I had my BLACK-"
-	line "GLASSES on, so I"
-
-	para "didn't notice you"
-	line "at all."
-
-	para "What am I doing"
-	line "here?"
-
-	para "Hey, don't you"
-	line "worry about it."
-
-	para "I'll give you a"
-	line "pair of BLACK-"
-	cont "GLASSES, so forget"
-	cont "you saw me, OK?"
-	done
-
-DarkCaveBlackthornEntrancePharmacistText2:
-	text "BLACKGLASSES ups"
-	line "the power of dark-"
-	cont "type moves."
-	done
-
 DarkCaveBlackthornEntrance_MapEvents:
 	def_warp_events
 	warp_event 23,  3, ROUTE_17, 1
@@ -72,3 +11,53 @@ DarkCaveBlackthornEntrance_MapEvents:
 	object_event  7,  3, SPRITE_PHARMACIST, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, DarkCaveBlackthornEntrancePharmacistScript, -1
 	object_event 21, 24, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, DarkCaveBlackthornEntranceRevive, EVENT_DARK_CAVE_BLACKTHORN_ENTRANCE_REVIVE
 	object_event  7, 22, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, DarkCaveBlackthornEntranceTMSnore, EVENT_DARK_CAVE_BLACKTHORN_ENTRANCE_TM_SNORE
+
+	object_const_def
+	const DARKCAVEBLACKTHORNENTRANCE_PHARMACIST
+	const DARKCAVEBLACKTHORNENTRANCE_POKE_BALL1
+	const DARKCAVEBLACKTHORNENTRANCE_POKE_BALL2
+
+DarkCaveBlackthornEntrance_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+DarkCaveBlackthornEntrancePharmacistScript:
+	faceplayeropentext
+	checkevent EVENT_GOT_BLACKGLASSES_IN_DARK_CAVE
+	iftrue .GotBlackglasses
+	writethistext
+		text "Whoa! You startled"
+		line "me there!"
+
+		para "I had my Black-"
+		line "Glasses on, so I"
+		cont "didn't notice you"
+		cont "at all."
+
+		para "What am I doing"
+		line "here?"
+
+		para "Hey, don't you"
+		line "worry about it."
+
+		para "I'll give you a"
+		line "pair of Black-"
+		cont "Glasses, so forget"
+		cont "you saw me, OK?"
+		done
+	promptbutton
+	verbosegiveitem BLACKGLASSES
+	iffalse_endtext
+	setevent EVENT_GOT_BLACKGLASSES_IN_DARK_CAVE
+.GotBlackglasses:
+	jumpthisopenedtext
+		text "BlackGlasses ups"
+		line "the power of dark-"
+		cont "type moves."
+		done
+
+DarkCaveBlackthornEntranceRevive:
+	itemball REVIVE
+DarkCaveBlackthornEntranceTMSnore:
+	itemball TM_SNORE
