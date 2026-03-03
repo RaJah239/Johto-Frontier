d8d218b590128134540a44b937b8069c5581ddb3
RaJah239 <ravijagroo239@gmail.com>
2026-03-03 13:41:24 -0400

    Update maps/Route1.asm

--- maps/Route1.asm
+++ maps/Route1.asm
@@ -232,12 +232,10 @@ Route1PinkApricornTree:
 	setval PNK_APRICORN
 	setlasttalked ROUTE1_APRICORN1
 	jumpstd BerryOrFruitScript
-
 Route1BerryTree1:
 	setval BERRY
 	setlasttalked ROUTE1_BERRY1
 	jumpstd BerryOrFruitScript
-
 Route1BerryTree2:
 	setval BERRY
 	setlasttalked ROUTE1_BERRY2
