84ad027126ccef3924005581d4ce9e8c66e6de21
RaJah239 <ravijagroo239@gmail.com>
2026-03-03 15:25:39 -0400

    Update maps/VioletKylesHouse.asm

--- maps/VioletKylesHouse.asm
+++ maps/VioletKylesHouse.asm
@@ -1,35 +1,3 @@
-	object_const_def
-	const VIOLETKYLESHOUSE_POKEFAN_M
-	const VIOLETKYLESHOUSE_KYLE
-
-VioletKylesHouse_MapScripts:
-	def_scene_scripts
-
-	def_callbacks
-
-VioletKylesHousePokefanMScript:
-	jumptextfaceplayer VioletKylesHousePokefanMText
-
-Kyle:
-	faceplayer
-	opentext
-	trade NPC_TRADE_KYLE
-	waitbutton
-	closetext
-	end
-
-VioletKylesHousePokefanMText:
-	text "A #MON you get"
-	line "in a trade grows"
-	cont "quickly."
-
-	para "But if you don't"
-	line "have the right GYM"
-
-	para "BADGE, they may"
-	line "disobey you."
-	done
-
 VioletKylesHouse_MapEvents:
 	def_warp_events
 	warp_event  3,  7, VIOLET_CITY, 6
@@ -40,5 +8,25 @@ VioletKylesHouse_MapEvents:
 	def_bg_events
 
 	def_object_events
-	object_event  2,  3, SPRITE_POKEFAN_M, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, VioletKylesHousePokefanMScript, -1
+	object_event  2,  3, SPRITE_POKEFAN_M, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, VioletKylesHousePokefanMText, -1
 	object_event  6,  5, SPRITE_YOUNGSTER, SPRITEMOVEDATA_WALK_UP_DOWN, 0, 2, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, Kyle, -1
+
+	object_const_def
+	const VIOLETKYLESHOUSE_POKEFAN_M
+	const VIOLETKYLESHOUSE_KYLE
+
+VioletKylesHouse_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+Kyle:
+	faceplayeropentext
+	trade NPC_TRADE_KYLE
+	waitendtext
+
+VioletKylesHousePokefanMText:
+	text "A #mon you get"
+	line "in a trade grows"
+	cont "quickly."
+	done
