41dbe44f1250bae1ff5ca7cfb65a404b67c3c339
RaJah239 <ravijagroo239@gmail.com>
2026-03-03 15:35:20 -0400

    Update maps/VioletPokecenter1F.asm

--- maps/VioletPokecenter1F.asm
+++ maps/VioletPokecenter1F.asm
@@ -1,3 +1,20 @@
+VioletPokecenter1F_MapEvents:
+	def_warp_events
+	warp_event  3,  7, VIOLET_CITY, 1
+	warp_event  4,  7, VIOLET_CITY, 1
+	warp_event  0,  7, POKECENTER_2F, 1
+
+	def_coord_events
+
+	def_bg_events
+
+	def_object_events
+	heal_event  3,  1, PAL_NPC_PURPLE
+	object_event  7,  6, SPRITE_GAMEBOY_KID, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_COMMAND, jumptextfaceplayer, VioletPokecenter1FGameboyKidText, -1
+	object_event  1,  4, SPRITE_GENTLEMAN, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, VioletPokecenter1FGentlemanText, -1
+	object_event  8,  1, SPRITE_YOUNGSTER, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_COMMAND, jumptextfaceplayer, VioletPokecenter1FYoungsterText, -1
+	chansey_event  4,  1
+
 	object_const_def
 	const VIOLETPOKECENTER1F_NURSE
 	const VIOLETPOKECENTER1F_GAMEBOY_KID
@@ -10,67 +27,16 @@ VioletPokecenter1F_MapScripts:
 
 	def_callbacks
 
-VioletPokecenterNurse:
-	jumpstd PokecenterNurseScript
-
-VioletPokecenter1FGameboyKidScript:
-	jumptextfaceplayer VioletPokecenter1FGameboyKidText
-
-VioletPokecenter1FGentlemanScript:
-	jumptextfaceplayer VioletPokecenter1FGentlemanText
-
-VioletPokecenter1FYoungsterScript:
-	jumptextfaceplayer VioletPokecenter1FYoungsterText
-
-VioletPokecenter1FGameboyKidText:
+VioletPokecenter1FYoungsterText:
 	text "A guy named BILL"
-	line "made the #MON"
+	line "made the #mon"
 	cont "PC storage system."
 	done
 
 VioletPokecenter1FGentlemanText:
-	text "It was around"
-	line "three years ago."
-
-	para "TEAM ROCKET was up"
-	line "to no good with"
-	cont "#MON."
-
-	para "But justice pre-"
-	line "vailed--a young"
-	cont "kid broke 'em up."
+	text "TODOTEXT"
 	done
 
-VioletPokecenter1FYoungsterText:
-	text "#MON are smart."
-	line "They won't obey a"
-
-	para "trainer they don't"
-	line "respect."
-
-	para "Without the right"
-	line "GYM BADGES, they"
-
-	para "will just do as"
-	line "they please."
+VioletPokecenter1FGameboyKidText:
+	text "TODOTEXT"
 	done
-
-VioletPokecenterChanseyScript:
-	jumpstd ChanseyPokeCenterScript
-
-VioletPokecenter1F_MapEvents:
-	def_warp_events
-	warp_event  3,  7, VIOLET_CITY, 1
-	warp_event  4,  7, VIOLET_CITY, 1
-	warp_event  0,  7, POKECENTER_2F, 1
-
-	def_coord_events
-
-	def_bg_events
-
-	def_object_events
-	object_event  3,  1, SPRITE_NURSE, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, VioletPokecenterNurse, -1
-	object_event  7,  6, SPRITE_GAMEBOY_KID, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, VioletPokecenter1FGameboyKidScript, -1
-	object_event  1,  4, SPRITE_GENTLEMAN, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, VioletPokecenter1FGentlemanScript, -1
-	object_event  8,  1, SPRITE_YOUNGSTER, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, VioletPokecenter1FYoungsterScript, -1
-	object_event  4,  1, SPRITE_CHANSEY, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, VioletPokecenterChanseyScript, -1
