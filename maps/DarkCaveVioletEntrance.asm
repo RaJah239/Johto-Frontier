e334d972181e54f21b0c334680517eafbd7099b3
RaJah239 <ravijagroo239@gmail.com>
2026-03-03 13:38:37 -0400

    Update maps/DarkCaveVioletEntrance.asm

--- maps/DarkCaveVioletEntrance.asm
+++ maps/DarkCaveVioletEntrance.asm
@@ -10,26 +10,26 @@ DarkCaveVioletEntrance_MapEvents:
 	bg_event 26,  3, BGEVENT_ITEM + ELIXER, EVENT_DARK_CAVE_VIOLET_ENTRANCE_HIDDEN_ELIXER
 
 	def_object_events
-	object_event  6,  8, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, DarkCaveVioletEntrancePotion, EVENT_DARK_CAVE_VIOLET_ENTRANCE_POTION
+	chanseyheal_event 29,  2
 	rocksmash_event 16, 14
 	rocksmash_event 27,  6
 	rocksmash_event  7, 14
 	rocksmash_event 36, 31
+	object_event  6,  8, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, DarkCaveVioletEntrancePotion, EVENT_DARK_CAVE_VIOLET_ENTRANCE_POTION
 	object_event 36, 22, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, DarkCaveVioletEntranceFullHeal, EVENT_DARK_CAVE_VIOLET_ENTRANCE_FULL_HEAL
 	object_event 35,  9, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, DarkCaveVioletEntranceHyperPotion, EVENT_DARK_CAVE_VIOLET_ENTRANCE_HYPER_POTION
-	object_event 30, 28, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, DarkCaveVioletEntranceDireHit, EVENT_DARK_CAVE_VIOLET_ENTRANCE_DIRE_HIT
-	chanseyheal_event 29,  2
+	object_event 30, 28, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, DarkCaveVioletEntranceRainShard, EVENT_DARK_CAVE_VIOLET_ENTRANCE_RAIN_SHARD
 
 	object_const_def
-	const DARKCAVEVIOLETENTRANCE_POKE_BALL1
+	const DARKCAVEVIOLETENTRANCE_CHANSEY
 	const DARKCAVEVIOLETENTRANCE_ROCK1
 	const DARKCAVEVIOLETENTRANCE_ROCK2
 	const DARKCAVEVIOLETENTRANCE_ROCK3
 	const DARKCAVEVIOLETENTRANCE_ROCK4
+	const DARKCAVEVIOLETENTRANCE_POKE_BALL1
 	const DARKCAVEVIOLETENTRANCE_POKE_BALL2
 	const DARKCAVEVIOLETENTRANCE_POKE_BALL3
 	const DARKCAVEVIOLETENTRANCE_POKE_BALL4
-	const DARKCAVEVIOLETENTRANCE_CHANSEY
 
 DarkCaveVioletEntrance_MapScripts:
 	def_scene_scripts
@@ -38,12 +38,9 @@ DarkCaveVioletEntrance_MapScripts:
 
 DarkCaveVioletEntrancePotion:
 	itemball POTION
-
 DarkCaveVioletEntranceFullHeal:
 	itemball FULL_HEAL
-
 DarkCaveVioletEntranceHyperPotion:
 	itemball HYPER_POTION
-
-DarkCaveVioletEntranceDireHit:
+DarkCaveVioletEntranceRainShard:
 	itemball SUN_SHARD
