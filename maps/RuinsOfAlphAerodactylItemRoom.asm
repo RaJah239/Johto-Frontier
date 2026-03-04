a3e0ee004cccfcc99fab6dd4b0a54c02d99ab618
RaJah239 <ravijagroo239@gmail.com>
2026-03-04 16:39:38 -0400

    Update maps/RuinsOfAlphAerodactylItemRoom.asm

--- maps/RuinsOfAlphAerodactylItemRoom.asm
+++ maps/RuinsOfAlphAerodactylItemRoom.asm
@@ -12,10 +12,10 @@ RuinsOfAlphAerodactylItemRoom_MapEvents:
 	bg_event  5,  1, BGEVENT_JUMPTEXT, RuinsOfAlphStatueText
 
 	def_object_events
-	object_event  2,  6, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, RuinsOfAlphAerodactylItemRoomGoldBerry, EVENT_PICKED_UP_GOLD_BERRY_FROM_AERODACTYL_ITEM_ROOM
-	object_event  5,  6, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, RuinsOfAlphAerodactylItemRoomMoonStone, EVENT_PICKED_UP_MOON_STONE_FROM_AERODACTYL_ITEM_ROOM
-	object_event  2,  4, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, RuinsOfAlphAerodactylItemRoomHealPowder, EVENT_PICKED_UP_HEAL_POWDER_FROM_AERODACTYL_ITEM_ROOM
-	object_event  5,  4, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, RuinsOfAlphAerodactylItemRoomEnergyRoot, EVENT_PICKED_UP_ENERGY_ROOT_FROM_AERODACTYL_ITEM_ROOM
+	object_event  2,  6, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, RuinsOfAlphAerodactylItemRoomFlameOrb, EVENT_AERODACTYL_ITEM_ROOM_FLAME_ORB
+	object_event  5,  6, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, RuinsOfAlphAerodactylItemRoomMoonStone, EVENT_AERODACTYL_ITEM_ROOM_MOON_STONE
+	object_event  2,  4, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, RuinsOfAlphAerodactylItemRoomMeteorMitts, EVENT_AERODACTYL_ITEM_ROOM_METEOR_MITTS
+	object_event  5,  4, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, RuinsOfAlphAerodactylItemRoomSunStone, EVENT_AERODACTYL_ITEM_ROOM_SUNSTONE
 
 	object_const_def
 	const RUINSOFALPHAERODACTYLITEMROOM_POKE_BALL1
@@ -28,11 +28,11 @@ RuinsOfAlphAerodactylItemRoom_MapScripts:
 
 	def_callbacks
 
-RuinsOfAlphAerodactylItemRoomGoldBerry:
-	itemball GOLD_BERRY
+RuinsOfAlphAerodactylItemRoomFlameOrb:
+	itemball FLAME_ORB
 RuinsOfAlphAerodactylItemRoomMoonStone:
 	itemball MOON_STONE
-RuinsOfAlphAerodactylItemRoomHealPowder:
-	itemball HEAL_POWDER
-RuinsOfAlphAerodactylItemRoomEnergyRoot:
-	itemball ENERGY_ROOT
+RuinsOfAlphAerodactylItemRoomMeteorMitts:
+	itemball METEOR_MITTS
+RuinsOfAlphAerodactylItemRoomSunStone:
+	itemball SUN_STONE
