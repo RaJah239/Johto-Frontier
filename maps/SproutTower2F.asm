a2f267d2a18f40d4c69170e1df8d69fa1894fc61
RaJah239 <ravijagroo239@gmail.com>
2026-03-04 08:51:10 -0400

    Update maps/SproutTower2F.asm

--- maps/SproutTower2F.asm
+++ maps/SproutTower2F.asm
@@ -1,3 +1,20 @@
+SproutTower2F_MapEvents:
+	def_warp_events
+	warp_event  6,  4, SPROUT_TOWER_1F, 3
+	warp_event  2,  6, SPROUT_TOWER_1F, 4
+	warp_event 17,  3, SPROUT_TOWER_1F, 5
+	warp_event 10, 14, SPROUT_TOWER_3F, 1
+
+	def_coord_events
+
+	def_bg_events
+	bg_event 12, 15, BGEVENT_JUMPTEXT, SproutTowerStatueText
+
+	def_object_events
+	object_event 12,  3, SPRITE_SAGE, SPRITEMOVEDATA_SPINRANDOM_FAST, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_GENERICTRAINER, 4, TrainerSageNico, -1
+	object_event  9, 14, SPRITE_SAGE, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_GENERICTRAINER, 5, TrainerSageEdmond, -1
+	object_event  3,  1, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, SproutTower2FXAccuracy, EVENT_SPROUT_TOWER_2F_X_ACCURACY
+
 	object_const_def
 	const SPROUTTOWER2F_SAGE1
 	const SPROUTTOWER2F_SAGE2
@@ -9,92 +26,47 @@ SproutTower2F_MapScripts:
 	def_callbacks
 
 TrainerSageNico:
-	trainer SAGE, NICO, EVENT_BEAT_SAGE_NICO, SageNicoSeenText, SageNicoBeatenText, 0, .Script
+	generictrainer SAGE, NICO, EVENT_BEAT_SAGE_NICO, .SeenText, .BeatenText
 
-.Script:
-	endifjustbattled
-	opentext
-	writetext SageNicoAfterBattleText
-	waitbutton
-	closetext
-	end
+.AfterText
+	text "The flexible pil-"
+	line "lar protects the"
+	cont "tower, even from"
+	cont "earthquakes."
+	done
 
-TrainerSageEdmond:
-	trainer SAGE, EDMOND, EVENT_BEAT_SAGE_EDMOND, SageEdmondSeenText, SageEdmondBeatenText, 0, .Script
-
-.Script:
-	endifjustbattled
-	opentext
-	writetext SageEdmondAfterBattleText
-	waitbutton
-	closetext
-	end
-
-SproutTower2FStatue:
-	jumptext SproutTower2FStatueText
-
-SproutTower2FXAccuracy:
-	itemball X_ACCURACY
-
-SageNicoSeenText:
+.SeenText
 	text "However hard we"
-	line "battle, the TOWER"
+	line "battle, the tower"
 	cont "will stand strong."
 	done
 
-SageNicoBeatenText:
+.BeatenText
 	text "I fought hard but"
 	line "I'm too weak."
 	done
 
-SageNicoAfterBattleText:
-	text "The flexible pil-"
-	line "lar protects the"
+TrainerSageEdmond:
+	generictrainer SAGE, EDMOND, EVENT_BEAT_SAGE_EDMOND, .SeenText, .BeatenText
 
-	para "TOWER, even from"
-	line "earthquakes."
-	done
-
-SageEdmondSeenText:
-	text "…Sway like leaves"
-	line "in the wind…"
-	done
-
-SageEdmondBeatenText:
-	text "Oh, I'm weak!"
-	done
-
-SageEdmondAfterBattleText:
+.AfterText
 	text "I tried to copy"
-	line "BELLSPROUT's"
-
-	para "gentle movements"
-	line "for battle…"
+	line "Bellsprout's"
+	cont "gentle movements"
+	cont "for battle…"
 
 	para "But I didn't train"
 	line "well enough."
 	done
 
-SproutTower2FStatueText:
-	text "A #MON statue…"
-
-	para "It looks very"
-	line "distinguished."
+.SeenText
+	text "…Sway like leaves"
+	line "in the wind…"
 	done
 
-SproutTower2F_MapEvents:
-	def_warp_events
-	warp_event  6,  4, SPROUT_TOWER_1F, 3
-	warp_event  2,  6, SPROUT_TOWER_1F, 4
-	warp_event 17,  3, SPROUT_TOWER_1F, 5
-	warp_event 10, 14, SPROUT_TOWER_3F, 1
+.BeatenText
+	text "Oh, I'm weak!"
+	done
 
-	def_coord_events
-
-	def_bg_events
-	bg_event 12, 15, BGEVENT_READ, SproutTower2FStatue
-
-	def_object_events
-	object_event 12,  3, SPRITE_SAGE, SPRITEMOVEDATA_SPINRANDOM_FAST, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 4, TrainerSageNico, -1
-	object_event  9, 14, SPRITE_SAGE, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 5, TrainerSageEdmond, -1
-	object_event  3,  1, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, SproutTower2FXAccuracy, EVENT_SPROUT_TOWER_2F_X_ACCURACY
+SproutTower2FXAccuracy:
+	itemball X_ACCURACY
