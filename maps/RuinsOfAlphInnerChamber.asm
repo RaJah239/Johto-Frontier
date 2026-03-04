3e26f538eff7f59c9e94c9791af0c25cf47fec81
RaJah239 <ravijagroo239@gmail.com>
2026-03-04 15:27:25 -0400

    Update maps/RuinsOfAlphInnerChamber.asm

--- maps/RuinsOfAlphInnerChamber.asm
+++ maps/RuinsOfAlphInnerChamber.asm
@@ -1,8 +1,56 @@
+RuinsOfAlphInnerChamber_MapEvents:
+	def_warp_events
+	warp_event 10, 13, RUINS_OF_ALPH_OUTSIDE, 6
+	warp_event  3, 15, RUINS_OF_ALPH_HO_OH_CHAMBER, 3
+	warp_event  4, 15, RUINS_OF_ALPH_HO_OH_CHAMBER, 4
+	warp_event 15,  3, RUINS_OF_ALPH_LILEEP_CHAMBER, 3
+	warp_event 16,  3, RUINS_OF_ALPH_LILEEP_CHAMBER, 4
+	warp_event  3, 21, RUINS_OF_ALPH_OMANYTE_CHAMBER, 3
+	warp_event  4, 21, RUINS_OF_ALPH_OMANYTE_CHAMBER, 4
+	warp_event 15, 24, RUINS_OF_ALPH_AERODACTYL_CHAMBER, 3
+	warp_event 16, 24, RUINS_OF_ALPH_AERODACTYL_CHAMBER, 4
+
+	def_coord_events
+
+	def_bg_events
+	bg_event  2,  3, BGEVENT_JUMPTEXT, RuinsOfAlphInnerChamberStatueText
+	bg_event  5,  3, BGEVENT_JUMPTEXT, RuinsOfAlphInnerChamberStatueText
+	bg_event  8,  3, BGEVENT_JUMPTEXT, RuinsOfAlphInnerChamberStatueText
+	bg_event 11,  3, BGEVENT_JUMPTEXT, RuinsOfAlphInnerChamberStatueText
+	bg_event 14,  3, BGEVENT_JUMPTEXT, RuinsOfAlphInnerChamberStatueText
+	bg_event 17,  3, BGEVENT_JUMPTEXT, RuinsOfAlphInnerChamberStatueText
+	bg_event  2,  8, BGEVENT_JUMPTEXT, RuinsOfAlphInnerChamberStatueText
+	bg_event  5,  8, BGEVENT_JUMPTEXT, RuinsOfAlphInnerChamberStatueText
+	bg_event  8,  8, BGEVENT_JUMPTEXT, RuinsOfAlphInnerChamberStatueText
+	bg_event 11,  8, BGEVENT_JUMPTEXT, RuinsOfAlphInnerChamberStatueText
+	bg_event 14,  8, BGEVENT_JUMPTEXT, RuinsOfAlphInnerChamberStatueText
+	bg_event 17,  8, BGEVENT_JUMPTEXT, RuinsOfAlphInnerChamberStatueText
+	bg_event  2, 13, BGEVENT_JUMPTEXT, RuinsOfAlphInnerChamberStatueText
+	bg_event 17, 13, BGEVENT_JUMPTEXT, RuinsOfAlphInnerChamberStatueText
+	bg_event  2, 18, BGEVENT_JUMPTEXT, RuinsOfAlphInnerChamberStatueText
+	bg_event  5, 18, BGEVENT_JUMPTEXT, RuinsOfAlphInnerChamberStatueText
+	bg_event  8, 18, BGEVENT_JUMPTEXT, RuinsOfAlphInnerChamberStatueText
+	bg_event 11, 18, BGEVENT_JUMPTEXT, RuinsOfAlphInnerChamberStatueText
+	bg_event 14, 18, BGEVENT_JUMPTEXT, RuinsOfAlphInnerChamberStatueText
+	bg_event 17, 18, BGEVENT_JUMPTEXT, RuinsOfAlphInnerChamberStatueText
+	bg_event  2, 24, BGEVENT_JUMPTEXT, RuinsOfAlphInnerChamberStatueText
+	bg_event  5, 24, BGEVENT_JUMPTEXT, RuinsOfAlphInnerChamberStatueText
+	bg_event  8, 24, BGEVENT_JUMPTEXT, RuinsOfAlphInnerChamberStatueText
+	bg_event 11, 24, BGEVENT_JUMPTEXT, RuinsOfAlphInnerChamberStatueText
+	bg_event 14, 24, BGEVENT_JUMPTEXT, RuinsOfAlphInnerChamberStatueText
+	bg_event 17, 24, BGEVENT_JUMPTEXT, RuinsOfAlphInnerChamberStatueText
+
+	def_object_events
+	chanseyheal_event  8, 11
+	object_event  3,  7, SPRITE_FISHER, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_COMMAND, jumptextfaceplayer, RuinsOfAlphInnerChamberFisherText, EVENT_RUINS_OF_ALPH_INNER_CHAMBER_TOURISTS
+	object_event 14, 13, SPRITE_TEACHER, SPRITEMOVEDATA_WANDER, 1, 1, -1, -1, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, RuinsOfAlphInnerChamberTeacherText, EVENT_RUINS_OF_ALPH_INNER_CHAMBER_TOURISTS
+	object_event 11, 19, SPRITE_GRAMPS, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_COMMAND, jumptextfaceplayer, RuinsOfAlphInnerChamberGrampsText, EVENT_RUINS_OF_ALPH_INNER_CHAMBER_TOURISTS
+
 	object_const_def
+	const RUINSOFALPHINNERCHAMBER_CHANSEY
 	const RUINSOFALPHINNERCHAMBER_FISHER
 	const RUINSOFALPHINNERCHAMBER_TEACHER
 	const RUINSOFALPHINNERCHAMBER_GRAMPS
-	const RUINSOFALPHINNERCHAMBER_CHANSEY
 
 RuinsOfAlphInnerChamber_MapScripts:
 	def_scene_scripts
@@ -11,39 +59,20 @@ RuinsOfAlphInnerChamber_MapScripts:
 
 	def_callbacks
 
+RuinsOfAlphInnerChamberStrangePresenceScene:
+	sdefer RuinsOfAlphInnerChamberStrangePresenceScript
 RuinsOfAlphInnerChamberNoopScene:
 	end
 
-RuinsOfAlphInnerChamberStrangePresenceScene:
-	sdefer RuinsOfAlphInnerChamberStrangePresenceScript
-	end
-
 RuinsOfAlphInnerChamberStrangePresenceScript:
-	opentext
-	writetext RuinsOfAlphStrangePresenceText
-	waitbutton
-	closetext
+	showthistext
+		text "There is a strange"
+		line "presence here…"
+		done
 	setscene SCENE_RUINSOFALPHINNERCHAMBER_NOOP
 	setevent EVENT_MADE_UNOWN_APPEAR_IN_RUINS
 	clearevent EVENT_RUINS_OF_ALPH_OUTSIDE_TOURIST_FISHER
 	end
-
-RuinsOfAlphInnerChamberFisherScript:
-	jumptextfaceplayer RuinsOfAlphInnerChamberFisherText
-
-RuinsOfAlphInnerChamberTeacherScript:
-	jumptextfaceplayer RuinsOfAlphInnerChamberTeacherText
-
-RuinsOfAlphInnerChamberGrampsScript:
-	jumptextfaceplayer RuinsOfAlphInnerChamberGrampsText
-
-RuinsOfAlphInnerChamberStatue:
-	jumptext RuinsOfAlphInnerChamberStatueText
-
-RuinsOfAlphStrangePresenceText:
-	text "There is a strange"
-	line "presence here…"
-	done
 
 RuinsOfAlphInnerChamberFisherText:
 	text "This is a big"
@@ -72,56 +101,5 @@ RuinsOfAlphInnerChamberGrampsText:
 RuinsOfAlphInnerChamberStatueText:
 	text "It's a replica of"
 	line "an ancient #-"
-	cont "MON."
+	cont "mon."
 	done
-
-RuinsOfAlphInnerChamberChanseyScript:
-	jumpstd ChanseyHealsOWScript
-
-RuinsOfAlphInnerChamber_MapEvents:
-	def_warp_events
-	warp_event 10, 13, RUINS_OF_ALPH_OUTSIDE, 6
-	warp_event  3, 15, RUINS_OF_ALPH_HO_OH_CHAMBER, 3
-	warp_event  4, 15, RUINS_OF_ALPH_HO_OH_CHAMBER, 4
-	warp_event 15,  3, RUINS_OF_ALPH_LILEEP_CHAMBER, 3
-	warp_event 16,  3, RUINS_OF_ALPH_LILEEP_CHAMBER, 4
-	warp_event  3, 21, RUINS_OF_ALPH_OMANYTE_CHAMBER, 3
-	warp_event  4, 21, RUINS_OF_ALPH_OMANYTE_CHAMBER, 4
-	warp_event 15, 24, RUINS_OF_ALPH_AERODACTYL_CHAMBER, 3
-	warp_event 16, 24, RUINS_OF_ALPH_AERODACTYL_CHAMBER, 4
-
-	def_coord_events
-
-	def_bg_events
-	bg_event  2,  3, BGEVENT_READ, RuinsOfAlphInnerChamberStatue
-	bg_event  5,  3, BGEVENT_READ, RuinsOfAlphInnerChamberStatue
-	bg_event  8,  3, BGEVENT_READ, RuinsOfAlphInnerChamberStatue
-	bg_event 11,  3, BGEVENT_READ, RuinsOfAlphInnerChamberStatue
-	bg_event 14,  3, BGEVENT_READ, RuinsOfAlphInnerChamberStatue
-	bg_event 17,  3, BGEVENT_READ, RuinsOfAlphInnerChamberStatue
-	bg_event  2,  8, BGEVENT_READ, RuinsOfAlphInnerChamberStatue
-	bg_event  5,  8, BGEVENT_READ, RuinsOfAlphInnerChamberStatue
-	bg_event  8,  8, BGEVENT_READ, RuinsOfAlphInnerChamberStatue
-	bg_event 11,  8, BGEVENT_READ, RuinsOfAlphInnerChamberStatue
-	bg_event 14,  8, BGEVENT_READ, RuinsOfAlphInnerChamberStatue
-	bg_event 17,  8, BGEVENT_READ, RuinsOfAlphInnerChamberStatue
-	bg_event  2, 13, BGEVENT_READ, RuinsOfAlphInnerChamberStatue
-	bg_event 17, 13, BGEVENT_READ, RuinsOfAlphInnerChamberStatue
-	bg_event  2, 18, BGEVENT_READ, RuinsOfAlphInnerChamberStatue
-	bg_event  5, 18, BGEVENT_READ, RuinsOfAlphInnerChamberStatue
-	bg_event  8, 18, BGEVENT_READ, RuinsOfAlphInnerChamberStatue
-	bg_event 11, 18, BGEVENT_READ, RuinsOfAlphInnerChamberStatue
-	bg_event 14, 18, BGEVENT_READ, RuinsOfAlphInnerChamberStatue
-	bg_event 17, 18, BGEVENT_READ, RuinsOfAlphInnerChamberStatue
-	bg_event  2, 24, BGEVENT_READ, RuinsOfAlphInnerChamberStatue
-	bg_event  5, 24, BGEVENT_READ, RuinsOfAlphInnerChamberStatue
-	bg_event  8, 24, BGEVENT_READ, RuinsOfAlphInnerChamberStatue
-	bg_event 11, 24, BGEVENT_READ, RuinsOfAlphInnerChamberStatue
-	bg_event 14, 24, BGEVENT_READ, RuinsOfAlphInnerChamberStatue
-	bg_event 17, 24, BGEVENT_READ, RuinsOfAlphInnerChamberStatue
-
-	def_object_events
-	object_event  3,  7, SPRITE_FISHER, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, RuinsOfAlphInnerChamberFisherScript, EVENT_RUINS_OF_ALPH_INNER_CHAMBER_TOURISTS
-	object_event 14, 13, SPRITE_TEACHER, SPRITEMOVEDATA_WANDER, 1, 1, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, RuinsOfAlphInnerChamberTeacherScript, EVENT_RUINS_OF_ALPH_INNER_CHAMBER_TOURISTS
-	object_event 11, 19, SPRITE_GRAMPS, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_SCRIPT, 0, RuinsOfAlphInnerChamberGrampsScript, EVENT_RUINS_OF_ALPH_INNER_CHAMBER_TOURISTS
-	object_event  8, 11, SPRITE_CHANSEY_OW, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, RuinsOfAlphInnerChamberChanseyScript, -1
