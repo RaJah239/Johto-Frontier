bf1e8e64d715d7ebbc3002d8049b6d4dd23efb8b
RaJah239 <ravijagroo239@gmail.com>
2026-03-05 14:08:33 -0400

    Update maps/AzaleaMart.asm

--- maps/AzaleaMart.asm
+++ maps/AzaleaMart.asm
@@ -1,79 +1,3 @@
-	object_const_def
-	const AZALEAMART_CLERK
-	const AZALEAMART_COOLTRAINER_M
-	const AZALEAMART_BUG_CATCHER
-
-AzaleaMart_MapScripts:
-	def_scene_scripts
-
-	def_callbacks
-
-AzaleaMartClerkScript:
-	opentext
-	readvar VAR_BADGES
-	ifgreater 7, .EightBadgesMart
-	ifgreater 6, .SevenBadgesMart
-	ifgreater 4, .FiveBadgesMart
-	ifgreater 2, .ThreeBadgesMart
-	ifgreater 0, .OneBadgeMart
-	pokemart MARTTYPE_STANDARD, MART_NO_BADGES
-	closetext
-	end
-
-.OneBadgeMart:
-	pokemart MARTTYPE_STANDARD, MART_ONE_BADGE
-	closetext
-	end
-
-.ThreeBadgesMart:
-	pokemart MARTTYPE_STANDARD, MART_THREE_BADGES
-	closetext
-	end
-
-.FiveBadgesMart:
-	pokemart MARTTYPE_STANDARD, MART_FIVE_BADGES
-	closetext
-	end
-
-.SevenBadgesMart:
-	pokemart MARTTYPE_STANDARD, MART_SEVEN_BADGES
-	closetext
-	end
-
-.EightBadgesMart:
-	pokemart MARTTYPE_STANDARD, MART_EIGHT_BADGES
-	closetext
-	end
-
-AzaleaMartCooltrainerMScript:
-	jumptextfaceplayer AzaleaMartCooltrainerMText
-
-AzaleaMartBugCatcherScript:
-	jumptextfaceplayer AzaleaMartBugCatcherText
-
-AzaleaMartCooltrainerMText:
-	text "There's no GREAT"
-	line "BALL here. #"
-
-	para "BALLS will have"
-	line "to do."
-
-	para "I wish KURT would"
-	line "make me some of"
-	cont "his custom BALLS."
-	done
-
-AzaleaMartBugCatcherText:
-	text "A GREAT BALL is"
-	line "better for catch-"
-	cont "ing #MON than a"
-	cont "# BALL."
-
-	para "But KURT's might"
-	line "be better some-"
-	cont "times."
-	done
-
 AzaleaMart_MapEvents:
 	def_warp_events
 	warp_event  2,  7, AZALEA_TOWN, 3
@@ -84,6 +8,42 @@ AzaleaMart_MapEvents:
 	def_bg_events
 
 	def_object_events
-	object_event  1,  3, SPRITE_CLERK, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, AzaleaMartClerkScript, -1
-	object_event  2,  5, SPRITE_COOLTRAINER_M, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, AzaleaMartCooltrainerMScript, -1
-	object_event  7,  2, SPRITE_BUG_CATCHER, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 2, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, AzaleaMartBugCatcherScript, -1
+	variable_mart_event 1, 3, PAL_NPC_RED
+	object_event  2,  5, SPRITE_COOLTRAINER_M, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, AzaleaMartCooltrainerMText, -1
+	object_event  7,  2, SPRITE_BUG_CATCHER, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 2, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_COMMAND, jumptextfaceplayer, AzaleaMartBugCatcherText, -1
+
+	object_const_def
+	const AZALEAMART_CLERK
+	const AZALEAMART_COOLTRAINER_M
+	const AZALEAMART_BUG_CATCHER
+
+AzaleaMart_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+AzaleaMartCooltrainerMText:
+	text "There's no Great"
+	line "Ball here for me."
+
+	para "#balls will"
+	line "have to do."
+
+	para "I wish Kurt would"
+	line "make me some of"
+	cont "his custom Balls."
+	
+	para "He doesn't think me"
+	line "worthy…"
+	done
+
+AzaleaMartBugCatcherText:
+	text "A Great Ball is"
+	line "better for catch-"
+	cont "ing #mon than a"
+	cont "# Ball."
+
+	para "But Kurt's might"
+	line "be better some-"
+	cont "times."
+	done
