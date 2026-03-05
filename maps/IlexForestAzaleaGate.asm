cf4193c3f02c78f531899e3674437241e92d60c5
RaJah239 <ravijagroo239@gmail.com>
2026-03-05 15:20:02 -0400

    Update maps/IlexForestAzaleaGate.asm

--- maps/IlexForestAzaleaGate.asm
+++ maps/IlexForestAzaleaGate.asm
@@ -1,33 +1,3 @@
-	object_const_def
-	const ILEXFORESTAZALEAGATE_OFFICER
-	const ILEXFORESTAZALEAGATE_GRANNY
-
-IlexForestAzaleaGate_MapScripts:
-	def_scene_scripts
-
-	def_callbacks
-
-IlexForestAzaleaGateOfficerScript:
-	jumptextfaceplayer IlexForestAzaleaGateOfficerText
-
-IlexForestAzaleaGateGrannyScript:
-	jumptextfaceplayer IlexForestAzaleaGateGrannyText
-
-IlexForestAzaleaGateOfficerText:
-	text "ILEX FOREST is"
-	line "big. Be careful!"
-	cont "Don't get lost."
-	done
-
-IlexForestAzaleaGateGrannyText:
-	text "The FOREST is"
-	line "watched over by"
-	cont "its protector."
-
-	para "Stay out of"
-	line "mischief!"
-	done
-
 IlexForestAzaleaGate_MapEvents:
 	def_warp_events
 	warp_event  0,  4, ILEX_FOREST, 2
@@ -40,5 +10,29 @@ IlexForestAzaleaGate_MapEvents:
 	def_bg_events
 
 	def_object_events
-	object_event  5,  2, SPRITE_OFFICER, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, IlexForestAzaleaGateOfficerScript, -1
-	object_event  1,  3, SPRITE_GRANNY, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_SCRIPT, 0, IlexForestAzaleaGateGrannyScript, -1
+	object_event  5,  2, SPRITE_OFFICER, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_COMMAND, jumptextfaceplayer, IlexForestAzaleaGateOfficerText, -1
+	object_event  1,  3, SPRITE_GRANNY, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_COMMAND, jumptextfaceplayer, IlexForestAzaleaGateGrannyText, -1
+
+	object_const_def
+	const ILEXFORESTAZALEAGATE_OFFICER
+	const ILEXFORESTAZALEAGATE_GRANNY
+
+IlexForestAzaleaGate_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+IlexForestAzaleaGateOfficerText:
+	text "Ilex Forest is"
+	line "big. Be careful!"
+	cont "Don't get lost."
+	done
+
+IlexForestAzaleaGateGrannyText:
+	text "The forest is"
+	line "watched over by"
+	cont "its protector."
+
+	para "Stay out of"
+	line "mischief!"
+	done
