9cd05e9d7b76b8688e1e9366c21f6ff12ed19ec4
RaJah239 <ravijagroo239@gmail.com>
2026-03-05 08:07:08 -0400

    Update maps/Route4.asm

--- maps/Route4.asm
+++ maps/Route4.asm
@@ -1,3 +1,36 @@
+Route4_MapEvents:
+	def_warp_events
+	warp_event 11, 73, ROUTE_4_POKECENTER_1F, 1
+	warp_event  4,  2, ROUTE_4_RUINS_OF_ALPH_GATE, 3
+	warp_event  4,  3, ROUTE_4_RUINS_OF_ALPH_GATE, 4
+	warp_event  6, 79, UNION_CAVE_1F, 4
+
+	def_coord_events
+
+	def_bg_events
+	bg_event 13,  5, BGEVENT_JUMPTEXT, Route4SignText
+	bg_event  9,  1, BGEVENT_JUMPTEXT, Route4RuinsSignText
+	bg_event 10, 84, BGEVENT_JUMPTEXT, Route4UnionCaveSignText
+	bg_event 12, 73, BGEVENT_JUMPSTD, POKECENTER_SIGN_SCRIPT
+	bg_event 12, 67, BGEVENT_ITEM + GREAT_BALL, EVENT_ROUTE_4_HIDDEN_GREAT_BALL
+	bg_event 11, 40, BGEVENT_ITEM + SUPER_POTION, EVENT_ROUTE_4_HIDDEN_SUPER_POTION
+
+	def_object_events
+	object_event  8, 49, SPRITE_FISHER, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_GENERICTRAINER, 1, TrainerFisherJustin, -1
+	object_event 12, 56, SPRITE_FISHER, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_TRAINER, 3, TrainerFisherRalph1, -1
+	object_event  6, 48, SPRITE_FISHER, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_TRAINER, 1, TrainerFisherHenry, -1
+	object_event 12, 22, SPRITE_YOUNGSTER, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 3, TrainerYoungsterAlbert, -1
+	object_event  4, 63, SPRITE_YOUNGSTER, SPRITEMOVEDATA_SPINRANDOM_FAST, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 3, TrainerYoungsterGordon, -1
+	object_event  3, 45, SPRITE_YOUNGSTER, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_GENERICTRAINER, 3, TrainerCamperRoland, -1
+	object_event 10, 30, SPRITE_LASS, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_TRAINER, 1, TrainerPicnickerLiz1, -1
+	object_event 19,  8, SPRITE_COOLTRAINER_M, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, Route4CooltrainerMScript, -1
+	object_event 11, 82, SPRITE_YOUNGSTER, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 3, TrainerBirdKeeperPeter, -1
+	object_event  6, 53, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, Route4GreatBall, EVENT_ROUTE_4_GREAT_BALL
+	object_event 15, 13, SPRITE_FISHER, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, Route4RoarTMGuyScript, -1
+	object_event 12, 67, SPRITE_LASS, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, FriedaScript, EVENT_ROUTE_4_FRIEDA_OF_FRIDAY
+	object_event  3, 30, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, Route4Repel, EVENT_ROUTE_4_REPEL
+	object_event  8, 10, SPRITE_OTIS, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, Route4OtisScript, EVENT_ROUTE_4_OTIS
+
 	object_const_def
 	const ROUTE4_FISHER1
 	const ROUTE4_FISHER2
@@ -8,7 +41,6 @@
 	const ROUTE4_LASS1
 	const ROUTE4_COOLTRAINER_M
 	const ROUTE4_YOUNGSTER4
-	const ROUTE4_FISHER4
 	const ROUTE4_POKE_BALL1
 	const ROUTE4_FISHER5
 	const ROUTE4_FRIEDA
@@ -50,85 +82,122 @@ Route4FriedaAndOtisCallback:
 	endcallback
 
 Route4CooltrainerMScript:
-	faceplayer
-	opentext
+	faceplayeropentext
 	checkevent EVENT_GOT_MIRACLE_SEED_IN_ROUTE_4
 	iftrue .GotMiracleSeed
 	checkflag ENGINE_ZEPHYRBADGE
 	iffalse .DontHaveZephyrBadge
-	writetext Route4CooltrainerMText_HaveThisSeed
+	writethistext
+		text "You have some good"
+		line "#mon there."
+
+		para "It must be from"
+		line "the training you"
+		cont "gave them around"
+		cont "Violet City."
+
+		para "The training at"
+		line "the Gym must have"
+		cont "been especially"
+		cont "helpful."
+
+		para "As a souvenir of"
+		line "Violet City, take"
+		cont "this."
+
+		para "It increases the"
+		line "power of grass-"
+		cont "type moves."
+		done
 	promptbutton
 	verbosegiveitem MIRACLE_SEED
-	iffalse .BagFull
+	iffalse_endtext
 	setevent EVENT_GOT_MIRACLE_SEED_IN_ROUTE_4
 .GotMiracleSeed:
-	writetext Route4CooltrainerMText_ExperiencesShouldBeUseful
-	waitbutton
-.BagFull:
-	closetext
-	end
+	jumpthisopenedtext
+		text "Your experiences"
+		line "in Violet City"
+		cont "should be useful"
+		cont "for your journey."
+		done
 
 .DontHaveZephyrBadge:
-	writetext Route4CooltrainerMText_VioletGym
-	waitbutton
-	closetext
-	end
+	jumpthisopenedtext
+		text "You should try get"
+		line "Violet Gym's badge."
+		done
 
 Route4RoarTMGuyScript:
-	faceplayer
-	opentext
+	faceplayeropentext
 	checkevent EVENT_GOT_TM05_ROAR
 	iftrue .AlreadyHaveRoar
-	writetext Text_RoarIntro
+	writethistext
+	text "WROOOOAR!"
+	line "PEOPLE RUN WHEN I"
+	cont "ROAR! BUT YOU"
+	cont "CAME LOOKING!"
+
+	para "THAT PLEASES ME!"
+	line "NOW TAKE THIS!"
+	done
+
+	para "TODOTEXT this"
+	line "entire script."
 	promptbutton
 	verbosegiveitem TM_ROAR
-	iffalse .Finish
+	iffalse_endtext
 	setevent EVENT_GOT_TM05_ROAR
 .AlreadyHaveRoar:
-	writetext Text_RoarOutro
+	writethistext
+		text "WROOOAR!"
+		line "IT'S ROAR!"
+
+		para "EVEN #MON RUN"
+		line "FROM A GOOD ROAR!"
+		done
 	waitbutton
 .Finish:
-	closetext
-	end
-
-SlowpokeTailSalesmanScript:
-	faceplayer
-	opentext
-	writetext Text_MillionDollarSlowpokeTail
-	yesorno
-	iffalse .refused
-	writetext Text_ThoughtKidsWereLoaded
-	waitbutton
-	closetext
-	end
-
-.refused
-	writetext Text_RefusedToBuySlowpokeTail
-	waitbutton
-	closetext
-	end
+	endtext
 
 TrainerCamperRoland:
-	trainer CAMPER, ROLAND, EVENT_BEAT_CAMPER_ROLAND, CamperRolandSeenText, CamperRolandBeatenText, 0, .Script
+	generictrainer CAMPER, ROLAND, EVENT_BEAT_CAMPER_ROLAND, .SeenText, .BeatenText
 
-.Script:
-	endifjustbattled
-	opentext
-	writetext CamperRolandAfterText
-	waitbutton
-	closetext
-	end
+.AfterText
+	text "If you don't want"
+	line "to battle, just"
+	cont "avoid eye contact."
+	done
+
+.SeenText
+	text "That glance…"
+	line "It's intriguing."
+	done
+
+.BeatenText
+	text "Hmmm. This is"
+	line "disappointing."
+	done
 
 TrainerFisherJustin:
-	trainer FISHER, JUSTIN, EVENT_BEAT_FISHER_JUSTIN, FisherJustinSeenText, FisherJustinBeatenText, 0, .Script
+	generictrainer FISHER, JUSTIN, EVENT_BEAT_FISHER_JUSTIN, .SeenText, .BeatenText
 
-.Script:
-	endifjustbattled
-	opentext
-	writetext FisherJustinAfterText
-	waitbutton
-	closetext
-	end
+.AfterText
+	text "Calm, collected…"
+	line "The essence of"
+	cont "fishing and #-"
+	cont "mon is the same."
+	done
+
+.SeenText
+	text "Whoa!"
+
+	para "You made me lose"
+	line "that fish!"
+	done
+
+.BeatenText
+	text "Sploosh!"
+	done
 
 TrainerFisherRalph1:
 	trainer FISHER, RALPH1, EVENT_BEAT_FISHER_RALPH, FisherRalph1SeenText, FisherRalph1BeatenText, 0, .Script
@@ -438,110 +507,7 @@ FriedaScript:
 	closetext
 	end
 
-Route4GreatBall:
-	itemball GREAT_BALL
 
-Route4Repel:
-	itemball REPEL
-
-Route4Sign:
-	jumptext Route4SignText
-
-Route4RuinsSign:
-	jumptext Route4RuinsSignText
-
-Route4UnionCaveSign:
-	jumptext Route4UnionCaveSignText
-
-Route4PokecenterSign:
-	jumpstd PokecenterSignScript
-
-Route4HiddenGreatBall:
-	hiddenitem GREAT_BALL, EVENT_ROUTE_4_HIDDEN_GREAT_BALL
-
-Route4HiddenSuperPotion:
-	hiddenitem SUPER_POTION, EVENT_ROUTE_4_HIDDEN_SUPER_POTION
-
-Route4CooltrainerMText_HaveThisSeed:
-	text "You have some good"
-	line "#MON there."
-
-	para "It must be from"
-	line "the training you"
-
-	para "gave them around"
-	line "VIOLET CITY."
-
-	para "The training at"
-	line "the GYM must have"
-
-	para "been especially"
-	line "helpful."
-
-	para "As a souvenir of"
-	line "VIOLET CITY, take"
-	cont "this."
-
-	para "It increases the"
-	line "power of grass-"
-	cont "type moves."
-	done
-
-Route4CooltrainerMText_VioletGym:
-	text "You should try get"
-	line "Violet Gym's badge."
-	done
-
-Route4CooltrainerMText_ExperiencesShouldBeUseful:
-	text "Your experiences"
-	line "in VIOLET CITY"
-
-	para "should be useful"
-	line "for your journey."
-	done
-
-Text_MillionDollarSlowpokeTail:
-	text "How would you like"
-	line "to have this"
-
-	para "tasty, nutritious"
-	line "SLOWPOKETAIL?"
-
-	para "For you right now,"
-	line "just ¥1,000,000!"
-
-	para "You'll want this!"
-	done
-
-Text_ThoughtKidsWereLoaded:
-	text "Tch! I thought"
-	line "kids these days"
-	cont "were loaded…"
-	done
-
-Text_RefusedToBuySlowpokeTail:
-	text "You don't want it?"
-	line "Then scram. Shoo!"
-	done
-
-FisherJustinSeenText:
-	text "Whoa!"
-
-	para "You made me lose"
-	line "that fish!"
-	done
-
-FisherJustinBeatenText:
-	text "Sploosh!"
-	done
-
-FisherJustinAfterText:
-	text "Calm, collected…"
-	line "The essence of"
-
-	para "fishing and #-"
-	line "MON is the same."
-	done
 
 FisherRalph1SeenText:
 	text "I'm really good at"
@@ -623,21 +589,7 @@ YoungsterGordonAfterText:
 	line "of clingy things."
 	done
 
-CamperRolandSeenText:
-	text "That glance…"
-	line "It's intriguing."
-	done
 
-CamperRolandBeatenText:
-	text "Hmmm. This is"
-	line "disappointing."
-	done
-
-CamperRolandAfterText:
-	text "If you don't want"
-	line "to battle, just"
-	cont "avoid eye contact."
-	done
 
 PicnickerLiz1SeenText:
 	text "Uh-huh. Yeah, and"
@@ -678,24 +630,9 @@ BirdKeeperPeterAfterText:
 	cont "in VIOLET CITY."
 	done
 
-Text_RoarIntro:
-	text "WROOOOAR!"
-	line "PEOPLE RUN WHEN I"
 
-	para "ROAR! BUT YOU"
-	line "CAME LOOKING!"
 
-	para "THAT PLEASES ME!"
-	line "NOW TAKE THIS!"
-	done
 
-Text_RoarOutro:
-	text "WROOOAR!"
-	line "IT'S ROAR!"
-
-	para "EVEN #MON RUN"
-	line "FROM A GOOD ROAR!"
-	done
 
 MeetFriedaText:
 	text "FRIEDA: Yahoo!"
@@ -745,22 +682,11 @@ FriedaNotFridayText:
 	line "when it's not!"
 	done
 
-Route4SignText:
-	text "ROUTE 4"
 
-	para "VIOLET CITY -"
-	line "AZALEA TOWN"
-	done
 
-Route4RuinsSignText:
-	text "RUINS OF ALPH"
-	line "EAST ENTRANCE"
-	done
 
-Route4UnionCaveSignText:
-	text "UNION CAVE"
-	line "AHEAD"
-	done
+
+
 
 FisherRalphText_GiveNuggetAfterBattleText:
 	text "You won after that"
@@ -797,36 +723,24 @@ Route4OtisTeleportAwayMovement:
 	teleport_from
 	step_end
 
-Route4_MapEvents:
-	def_warp_events
-	warp_event 11, 73, ROUTE_4_POKECENTER_1F, 1
-	warp_event  4,  2, ROUTE_4_RUINS_OF_ALPH_GATE, 3
-	warp_event  4,  3, ROUTE_4_RUINS_OF_ALPH_GATE, 4
-	warp_event  6, 79, UNION_CAVE_1F, 4
+Route4SignText:
+	text "Route 4"
 
-	def_coord_events
+	para "Violet City -"
+	line "Azalea Town"
+	done
 
-	def_bg_events
-	bg_event 13,  5, BGEVENT_READ, Route4Sign
-	bg_event  9,  1, BGEVENT_READ, Route4RuinsSign
-	bg_event 10, 84, BGEVENT_READ, Route4UnionCaveSign
-	bg_event 12, 73, BGEVENT_READ, Route4PokecenterSign
-	bg_event 12, 67, BGEVENT_ITEM, Route4HiddenGreatBall
-	bg_event 11, 40, BGEVENT_ITEM, Route4HiddenSuperPotion
+Route4RuinsSignText:
+	text "Ruins Of Alph"
+	line "East Entrance"
+	done
 
-	def_object_events
-	object_event  8, 49, SPRITE_FISHER, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_TRAINER, 1, TrainerFisherJustin, -1
-	object_event 12, 56, SPRITE_FISHER, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_TRAINER, 3, TrainerFisherRalph1, -1
-	object_event  6, 48, SPRITE_FISHER, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_TRAINER, 1, TrainerFisherHenry, -1
-	object_event 12, 22, SPRITE_YOUNGSTER, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 3, TrainerYoungsterAlbert, -1
-	object_event  4, 63, SPRITE_YOUNGSTER, SPRITEMOVEDATA_SPINRANDOM_FAST, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 3, TrainerYoungsterGordon, -1
-	object_event  3, 45, SPRITE_YOUNGSTER, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_TRAINER, 3, TrainerCamperRoland, -1
-	object_event 10, 30, SPRITE_LASS, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_TRAINER, 1, TrainerPicnickerLiz1, -1
-	object_event 19,  8, SPRITE_COOLTRAINER_M, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, Route4CooltrainerMScript, -1
-	object_event 11, 82, SPRITE_YOUNGSTER, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 3, TrainerBirdKeeperPeter, -1
-	object_event  7, 70, SPRITE_FISHER, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, SlowpokeTailSalesmanScript, EVENT_SLOWPOKE_WELL_ROCKETS
-	object_event  6, 53, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, Route4GreatBall, EVENT_ROUTE_4_GREAT_BALL
-	object_event 15, 13, SPRITE_FISHER, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, Route4RoarTMGuyScript, -1
-	object_event 12, 67, SPRITE_LASS, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, FriedaScript, EVENT_ROUTE_4_FRIEDA_OF_FRIDAY
-	object_event  3, 30, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, Route4Repel, EVENT_ROUTE_4_REPEL
-	object_event  8, 10, SPRITE_OTIS, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, Route4OtisScript, EVENT_ROUTE_4_OTIS
+Route4UnionCaveSignText:
+	text "Union Cave"
+	line "Ahead"
+	done
+
+Route4GreatBall:
+	itemball GREAT_BALL
+Route4Repel:
+	itemball REPEL
