f779e271e4e93aa195e043ea429de5955261c368
RaJah239 <ravijagroo239@gmail.com>
2026-03-05 14:02:30 -0400

    Update maps/AzaleaPokecenter1F.asm

--- maps/AzaleaPokecenter1F.asm
+++ maps/AzaleaPokecenter1F.asm
@@ -1,71 +1,3 @@
-	object_const_def
-	const AZALEAPOKECENTER1F_NURSE
-	const AZALEAPOKECENTER1F_GENTLEMAN
-	const AZALEAPOKECENTER1F_FISHING_GURU
-	const AZALEAPOKECENTER1F_POKEFAN_F
-	const AZALEAPOKECENTER1F_CHANSEY
-
-AzaleaPokecenter1F_MapScripts:
-	def_scene_scripts
-	scene_script AzaleaPokecenter1FNoopScene ; unusable
-
-	def_callbacks
-
-AzaleaPokecenter1FNoopScene:
-	end
-
-AzaleaPokecenter1FNurseScript:
-	jumpstd PokecenterNurseScript
-
-AzaleaPokecenter1FGentlemanScript:
-	jumptextfaceplayer AzaleaPokecenter1FGentlemanText
-
-AzaleaPokecenter1FFishingGuruScript:
-	jumptextfaceplayer AzaleaPokecenter1FFishingGuruText
-
-AzaleaPokecenter1FPokefanFScript:
-	jumptextfaceplayer AzaleaPokecenter1FPokefanFText
-
-AzaleaPokecenter1FGentlemanText:
-	text "Do your #MON"
-	line "know HM moves?"
-
-	para "Those moves can"
-	line "be used even if"
-
-	para "your #MON has"
-	line "fainted."
-	done
-
-AzaleaPokecenter1FFishingGuruText:
-	text "BILL's PC can"
-	line "store up to 20"
-	cont "#MON per BOX."
-	done
-
-AzaleaPokecenter1FPokefanFText:
-	text "Do you know about"
-	line "APRICORNS?"
-
-	para "Crack one open,"
-	line "hollow it out and"
-
-	para "fit it with a"
-	line "special device."
-
-	para "Then you can catch"
-	line "#MON with it."
-
-	para "Before # BALLS"
-	line "were invented,"
-
-	para "everyone used"
-	line "APRICORNS."
-	done
-
-AzaleaPokecenterChanseyScript:
-	jumpstd ChanseyPokeCenterScript
-
 AzaleaPokecenter1F_MapEvents:
 	def_warp_events
 	warp_event  3,  7, AZALEA_TOWN, 1
@@ -77,8 +9,54 @@ AzaleaPokecenter1F_MapEvents:
 	def_bg_events
 
 	def_object_events
-	object_event  3,  1, SPRITE_NURSE, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, AzaleaPokecenter1FNurseScript, -1
-	object_event  9,  6, SPRITE_GENTLEMAN, SPRITEMOVEDATA_WALK_UP_DOWN, 0, 1, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, AzaleaPokecenter1FGentlemanScript, -1
-	object_event  6,  1, SPRITE_FISHING_GURU, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, AzaleaPokecenter1FFishingGuruScript, -1
-	object_event  1,  4, SPRITE_POKEFAN_F, SPRITEMOVEDATA_WANDER, 1, 2, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, AzaleaPokecenter1FPokefanFScript, -1
-	object_event  4,  1, SPRITE_CHANSEY, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, AzaleaPokecenterChanseyScript, -1
+	heal_event  3,  1, PAL_NPC_RED
+	chansey_event  4,  1
+	object_event  9,  6, SPRITE_GENTLEMAN, SPRITEMOVEDATA_WALK_UP_DOWN, 0, 1, -1, -1, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, AzaleaPokecenter1FGentlemanText, -1
+	object_event  6,  1, SPRITE_FISHING_GURU, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, AzaleaPokecenter1FFishingGuruText, -1
+	object_event  1,  4, SPRITE_POKEFAN_F, SPRITEMOVEDATA_WANDER, 1, 2, -1, -1, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, AzaleaPokecenter1FPokefanFText, -1
+
+	object_const_def
+	const AZALEAPOKECENTER1F_NURSE
+	const AZALEAPOKECENTER1F_CHANSEY
+	const AZALEAPOKECENTER1F_GENTLEMAN
+	const AZALEAPOKECENTER1F_FISHING_GURU
+	const AZALEAPOKECENTER1F_POKEFAN_F
+
+
+AzaleaPokecenter1F_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+AzaleaPokecenter1FGentlemanText:
+	text "Do your #mon"
+	line "know field moves?"
+
+	para "Most can be used"
+	line "even if your #-"
+	cont "mon has fainted."
+	done
+
+AzaleaPokecenter1FFishingGuruText:
+	text "Bill's PC can"
+	line "store up to 20"
+	cont "#mon per Box."
+	done
+
+AzaleaPokecenter1FPokefanFText:
+	text "Do you know about"
+	line "Apricorns?"
+
+	para "Crack one open,"
+	line "hollow it out and"
+	cont "fit it with a"
+	cont "special device."
+
+	para "Then you can catch"
+	line "#mon with it."
+
+	para "Before # Balls"
+	line "were invented,"
+	cont "everyone used"
+	cont "Apricorns."
+	done
