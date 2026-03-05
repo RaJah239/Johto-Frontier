62501d8e537ad60503f72d6535f2cf0d1a6e0d96
RaJah239 <ravijagroo239@gmail.com>
2026-03-05 14:19:30 -0400

    Update maps/CharcoalKiln.asm

--- maps/CharcoalKiln.asm
+++ maps/CharcoalKiln.asm
@@ -1,3 +1,20 @@
+CharcoalKiln_MapEvents:
+	def_warp_events
+	warp_event  2,  7, AZALEA_TOWN, 2
+	warp_event  3,  7, AZALEA_TOWN, 2
+
+	def_coord_events
+
+	def_bg_events
+	bg_event  0,  1, BGEVENT_JUMPSTD, MAGAZINE_BOOKSHELF_SCRIPT
+	bg_event  1,  1, BGEVENT_JUMPSTD, MAGAZINE_BOOKSHELF_SCRIPT
+	bg_event  7,  1, BGEVENT_JUMPSTD, RADIO2SCRIPT
+
+	def_object_events
+	object_event  2,  3, SPRITE_BLACK_BELT, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, CharcoalKilnBoss, EVENT_CHARCOAL_KILN_BOSS
+	object_event  5,  3, SPRITE_YOUNGSTER, SPRITEMOVEDATA_WANDER, 1, 1, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, CharcoalKilnApprentice, EVENT_CHARCOAL_KILN_APPRENTICE
+	object_event  5,  6, SPRITE_MOLTRES, SPRITEMOVEDATA_POKEMON, 2, 2, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_SCRIPT, 0, CharcoalKilnScyther, EVENT_CHARCOAL_KILN_FARFETCH_D
+
 	object_const_def
 	const CHARCOALKILN_BLACK_BELT
 	const CHARCOALKILN_YOUNGSTER
@@ -9,155 +26,66 @@ CharcoalKiln_MapScripts:
 	def_callbacks
 
 CharcoalKilnBoss:
-	faceplayer
-	opentext
+	faceplayeropentext
 	checkevent EVENT_GOT_SCYTHER_CALL
-	iftrue .GotCut
-	checkevent EVENT_CLEARED_SLOWPOKE_WELL
-	iftrue .SavedSlowpoke
-	writetext CharcoalKilnBossText1
-	waitbutton
-	closetext
-	end
+	iftrue .GotScytherCall
+	jumpthisopenedtext
+		text "My apprentice has"
+		line "not returned from"
+		cont "the Ilex Forest."
 
-.SavedSlowpoke:
-	writetext CharcoalKilnBossText2
-	waitbutton
-	closetext
-	end
+		para "Where in the world"
+		line "is that lazy guy?"
+		done
 
-.GotCut:
-	writetext CharcoalKilnBossText3
-	waitbutton
-	closetext
-	end
+.GotScytherCall:
+	jumpthisopenedtext
+		text "You went to Ilex"
+		line "Forest and aided"
+		cont "my apprentice?"
+
+		para "You're brave! Come"
+		cont "train with us."
+		done
 
 CharcoalKilnApprentice:
-	faceplayer
-	opentext
+	faceplayeropentext
 	checkevent EVENT_GOT_CHARCOAL_IN_CHARCOAL_KILN
 	iftrue .YoureTheCoolest
-	checkevent EVENT_GOT_SCYTHER_CALL
-	iftrue .Thanks
-	writetext CharcoalKilnApprenticeText1
-	waitbutton
-	closetext
-	end
+	writethistext
+		text "I'm sorry--I for-"
+		line "got to thank you."
 
-.Thanks:
-	writetext CharcoalKilnApprenticeText2
+		para "This is Charcoal"
+		line "that I made."
+
+		para "Fire-type #mon"
+		line "would be happy to"
+		cont "hold that."
+		done
 	promptbutton
 	verbosegiveitem CHARCOAL
-	iffalse .Done
+	iffalse_endtext
 	setevent EVENT_GOT_CHARCOAL_IN_CHARCOAL_KILN
-	closetext
-	end
+	endtext
 
 .YoureTheCoolest:
-	writetext CharcoalKilnApprenticeText3
+	jumpthisopenedtext
+		text "You helped me in"
+		line "that bleak forest!"
+
+		para "You're the cool-"
+		line "est!"
+		done
+
+CharcoalKilnScyther:
+	setval SCYTHER
+	special SetMonAsSeen
+	reanchormap
+	pokepic SCYTHER
+	cry SCYTHER
 	waitbutton
-.Done:
-	closetext
-	end
-
-CharcoalKilnFarfetchd:
-	faceplayer
-	opentext
-	writetext FarfetchdText
-	waitbutton
-	closetext
-	end
-
-CharcoalKilnBookshelf:
-	jumpstd MagazineBookshelfScript
-
-CharcoalKilnRadio:
-	jumpstd Radio2Script
-
-CharcoalKilnBossText1:
-	text "All the SLOWPOKE"
-	line "have disappeared"
-	cont "from the town."
-
-	para "The forest's pro-"
-	line "tector may be"
-	cont "angry with us…"
-
-	para "It may be a bad"
-	line "omen. We should"
-	cont "stay in."
-	done
-
-CharcoalKilnBossText2:
-	text "The SLOWPOKE have"
-	line "returned…"
-
-	para "But my APPRENTICE"
-	line "hasn't come back"
-	cont "from ILEX FOREST."
-
-	para "Where in the world"
-	line "is that lazy guy?"
-	done
-
-CharcoalKilnBossText3:
-	text "You chased off"
-	line "TEAM ROCKET and"
-
-	para "went to ILEX"
-	line "FOREST alone?"
-
-	para "That takes guts!"
-	line "I like that. Come"
-	cont "train with us."
-	done
-
-CharcoalKilnApprenticeText1:
-	text "Where have all the"
-	line "SLOWPOKE gone?"
-
-	para "Are they out play-"
-	line "ing somewhere?"
-	done
-
-CharcoalKilnApprenticeText2:
-	text "I'm sorry--I for-"
-	line "got to thank you."
-
-	para "This is CHARCOAL"
-	line "that I made."
-
-	para "Fire-type #MON"
-	line "would be happy to"
-	cont "hold that."
-	done
-
-CharcoalKilnApprenticeText3:
-	text "The SLOWPOKE came"
-	line "back, and you even"
-	cont "found FARFETCH'D."
-
-	para "You're the cool-"
-	line "est, man!"
-	done
-
-FarfetchdText:
-	text "FARFETCH'D: Kwaa!"
-	done
-
-CharcoalKiln_MapEvents:
-	def_warp_events
-	warp_event  2,  7, AZALEA_TOWN, 2
-	warp_event  3,  7, AZALEA_TOWN, 2
-
-	def_coord_events
-
-	def_bg_events
-	bg_event  0,  1, BGEVENT_READ, CharcoalKilnBookshelf
-	bg_event  1,  1, BGEVENT_READ, CharcoalKilnBookshelf
-	bg_event  7,  1, BGEVENT_READ, CharcoalKilnRadio
-
-	def_object_events
-	object_event  2,  3, SPRITE_BLACK_BELT, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, CharcoalKilnBoss, EVENT_CHARCOAL_KILN_BOSS
-	object_event  5,  3, SPRITE_YOUNGSTER, SPRITEMOVEDATA_WANDER, 1, 1, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, CharcoalKilnApprentice, EVENT_CHARCOAL_KILN_APPRENTICE
-	object_event  5,  6, SPRITE_MOLTRES, SPRITEMOVEDATA_POKEMON, 2, 2, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_SCRIPT, 0, CharcoalKilnFarfetchd, EVENT_CHARCOAL_KILN_FARFETCH_D
+	closepokepic
+	jumpthistext
+		text "Scyther: Scyth!"
+		done
