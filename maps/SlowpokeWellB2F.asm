26650e3fbd268ddc3409f41d05b727a13b6960a5
RaJah239 <ravijagroo239@gmail.com>
2026-03-06 05:52:03 -0400

    Update maps/SlowpokeWellB2F.asm

--- maps/SlowpokeWellB2F.asm
+++ maps/SlowpokeWellB2F.asm
@@ -1,64 +1,3 @@
-	object_const_def
-	const SLOWPOKEWELLB2F_GYM_GUIDE
-	const SLOWPOKEWELLB2F_POKE_BALL
-
-SlowpokeWellB2F_MapScripts:
-	def_scene_scripts
-
-	def_callbacks
-
-SlowpokeWellB2FGymGuideScript:
-	faceplayer
-	opentext
-	checkevent EVENT_GOT_KINGS_ROCK_IN_SLOWPOKE_WELL
-	iftrue .GotKingsRock
-	writetext SlowpokeWellB2FGymGuideText
-	promptbutton
-	verbosegiveitem KINGS_ROCK
-	iffalse .NoRoom
-	setevent EVENT_GOT_KINGS_ROCK_IN_SLOWPOKE_WELL
-.NoRoom:
-	closetext
-	end
-
-.GotKingsRock:
-	writetext SlowpokeWellB2FGymGuideText_GotKingsRock
-	waitbutton
-	closetext
-	end
-
-SlowpokeWellB2FTMRainDance:
-	itemball TM_RAIN_DANCE
-
-SlowpokeWellB2FGymGuideText:
-	text "I'm waiting to see"
-	line "SLOWPOKE's moment"
-	cont "of evolution."
-
-	para "Through observa-"
-	line "tion, I made a new"
-	cont "discovery."
-
-	para "A SLOWPOKE with a"
-	line "KING'S ROCK often"
-
-	para "gets bitten by a"
-	line "SHELLDER."
-
-	para "Here, I'll share a"
-	line "KING'S ROCK with"
-	cont "you."
-	done
-
-SlowpokeWellB2FGymGuideText_GotKingsRock:
-	text "I'm going to be"
-	line "like SLOWPOKE."
-
-	para "I'll wait patient-"
-	line "ly, so I can see"
-	cont "one evolve."
-	done
-
 SlowpokeWellB2F_MapEvents:
 	def_warp_events
 	warp_event  9, 11, SLOWPOKE_WELL_B1F, 2
@@ -70,3 +9,46 @@ SlowpokeWellB2F_MapEvents:
 	def_object_events
 	object_event  5,  4, SPRITE_GYM_GUIDE, SPRITEMOVEDATA_WANDER, 1, 2, -1, -1, 0, OBJECTTYPE_SCRIPT, 1, SlowpokeWellB2FGymGuideScript, -1
 	object_event 15,  5, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, SlowpokeWellB2FTMRainDance, EVENT_SLOWPOKE_WELL_B2F_TM_RAIN_DANCE
+
+	object_const_def
+	const SLOWPOKEWELLB2F_GYM_GUIDE
+	const SLOWPOKEWELLB2F_POKE_BALL
+
+SlowpokeWellB2F_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+SlowpokeWellB2FGymGuideScript:
+	faceplayeropentext
+	checkevent EVENT_GOT_KINGS_ROCK_IN_SLOWPOKE_WELL
+	iftrue .GotKingsRock
+	writethistext
+		text "I'm waiting to see"
+		line "Slowpoke's moment"
+		cont "of evolution."
+
+		para "This could take a"
+		line "while…"
+
+		para "Please accept this"
+		line "for stopping by!"
+		done
+	promptbutton
+	verbosegiveitem KINGS_ROCK
+	iffalse_endtext
+	setevent EVENT_GOT_KINGS_ROCK_IN_SLOWPOKE_WELL
+	endtext
+
+.GotKingsRock:
+		jumpthisopenedtext
+		text "I'm going to be"
+		line "like Slowpoke."
+
+		para "I'll wait patient-"
+		line "ly, so I can see"
+		cont "one evolve."
+		done
+
+SlowpokeWellB2FTMRainDance:
+	itemball TM_RAIN_DANCE
