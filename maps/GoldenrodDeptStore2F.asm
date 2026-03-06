7deeb0cc369926018435853298133b8a18d2b0da
RaJah239 <ravijagroo239@gmail.com>
2026-03-06 11:38:18 -0400

    Update maps/GoldenrodDeptStore2F.asm

--- maps/GoldenrodDeptStore2F.asm
+++ maps/GoldenrodDeptStore2F.asm
@@ -1,3 +1,22 @@
+GoldenrodDeptStore2F_MapEvents:
+	def_warp_events
+	warp_event 12,  0, GOLDENROD_DEPT_STORE_3F, 1
+	warp_event 15,  0, GOLDENROD_DEPT_STORE_1F, 3
+	warp_event  2,  0, GOLDENROD_DEPT_STORE_ELEVATOR, 1
+
+	def_coord_events
+
+	def_bg_events
+	bg_event 14,  0, BGEVENT_JUMPTEXT, GoldenrodDeptStore2FDirectoryText
+	bg_event  3,  0, BGEVENT_JUMPSTD, ELEVATOR_BUTTON_SCRIPT
+
+	def_object_events
+	object_event 13,  5, SPRITE_CLERK, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_SCRIPT, 0, GoldenrodDeptStore2FClerk1Script, -1
+	object_event 13,  6, SPRITE_CLERK, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_SCRIPT, 0, GoldenrodDeptStore2FClerk2Script, -1
+	object_event  9,  6, SPRITE_YOUNGSTER, SPRITEMOVEDATA_WALK_UP_DOWN, 0, 1, -1, -1, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, GoldenrodDeptStore2FYoungsterText, -1
+	object_event  6,  2, SPRITE_COOLTRAINER_F, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 2, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_COMMAND, jumptextfaceplayer, GoldenrodDeptStore2FCooltrainerFText, -1
+	object_event  2,  6, SPRITE_GENTLEMAN, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, GoldenrodDeptStore2FGentlemanText, -1
+
 	object_const_def
 	const GOLDENRODDEPTSTORE2F_CLERK1
 	const GOLDENRODDEPTSTORE2F_CLERK2
@@ -12,66 +31,15 @@ GoldenrodDeptStore2F_MapScripts:
 
 GoldenrodDeptStore2FClerk1Script:
 	faceplayer
-	opentext
-	readvar VAR_BADGES
-	ifgreater 7, .EightBadgesMart
-	ifgreater 6, .SevenBadgesMart
-	ifgreater 4, .FiveBadgesMart
-	ifgreater 2, .ThreeBadgesMart
-	ifgreater 0, .OneBadgeMart
-	pokemart MARTTYPE_STANDARD, MART_NO_BADGES
-	closetext
-	end
-
-.OneBadgeMart:
-	pokemart MARTTYPE_STANDARD, MART_ONE_BADGE
-	closetext
-	end
-
-.ThreeBadgesMart:
-	pokemart MARTTYPE_STANDARD, MART_THREE_BADGES
-	closetext
-	end
-
-.FiveBadgesMart:
-	pokemart MARTTYPE_STANDARD, MART_FIVE_BADGES
-	closetext
-	end
-
-.SevenBadgesMart:
-	pokemart MARTTYPE_STANDARD, MART_SEVEN_BADGES
-	closetext
-	end
-
-.EightBadgesMart:
-	pokemart MARTTYPE_STANDARD, MART_EIGHT_BADGES
-	closetext
-	end
+	jumpstd VariableMartScript
 
 GoldenrodDeptStore2FClerk2Script:
-	faceplayer
-	opentext
+	faceplayeropentext
 	pokemart MARTTYPE_STANDARD, MART_GOLDENROD_2F_2
-	closetext
-	end
-
-GoldenrodDeptStore2FYoungsterScript:
-	jumptextfaceplayer GoldenrodDeptStore2FYoungsterText
-
-GoldenrodDeptStore2FCooltrainerFScript:
-	jumptextfaceplayer GoldenrodDeptStore2FCooltrainerFText
-
-GoldenrodDeptStore2FGentlemanScript:
-	jumptextfaceplayer GoldenrodDeptStore2FGentlemanText
-
-GoldenrodDeptStore2FDirectory:
-	jumptext GoldenrodDeptStore2FDirectoryText
-
-GoldenrodDeptStore2FElevatorButton:
-	jumpstd ElevatorButtonScript
+	endtext
 
 GoldenrodDeptStore2FYoungsterText:
-	text "Your #GEAR can"
+	text "Your #Gear can"
 	line "store many phone"
 	cont "numbers!"
 	
@@ -80,19 +48,20 @@ GoldenrodDeptStore2FYoungsterText:
 	done
 
 GoldenrodDeptStore2FCooltrainerFText:
-	text "I got my ABRA at"
-	line "the GAME CORNER."
+	text "I won enough coins"
+	line "to finally get a"
+	cont "Porygon from the"
+	cont "Game Corner."
 
-	para "Now it's my best"
+	para "It's now my best"
 	line "partner."
 	done
 
 GoldenrodDeptStore2FGentlemanText:
-	text "This DEPT.STORE"
+	text "This Dept.Store"
 	line "makes me realize"
-
-	para "that GOLDENROD is"
-	line "a big city."
+	cont "that Goldenrod is"
+	cont "a big city."
 
 	para "The selection here"
 	line "is unmatched any-"
@@ -103,25 +72,6 @@ GoldenrodDeptStore2FDirectoryText:
 	text "Your Travel"
 	line "Companion"
 
-	para "2F TRAINER'S"
-	line "   MARKET"
+	para "2F Trainer's"
+	line "   Market"
 	done
-
-GoldenrodDeptStore2F_MapEvents:
-	def_warp_events
-	warp_event 12,  0, GOLDENROD_DEPT_STORE_3F, 1
-	warp_event 15,  0, GOLDENROD_DEPT_STORE_1F, 3
-	warp_event  2,  0, GOLDENROD_DEPT_STORE_ELEVATOR, 1
-
-	def_coord_events
-
-	def_bg_events
-	bg_event 14,  0, BGEVENT_READ, GoldenrodDeptStore2FDirectory
-	bg_event  3,  0, BGEVENT_READ, GoldenrodDeptStore2FElevatorButton
-
-	def_object_events
-	object_event 13,  5, SPRITE_CLERK, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, GoldenrodDeptStore2FClerk1Script, -1
-	object_event 13,  6, SPRITE_CLERK, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, GoldenrodDeptStore2FClerk2Script, -1
-	object_event  9,  6, SPRITE_YOUNGSTER, SPRITEMOVEDATA_WALK_UP_DOWN, 0, 1, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, GoldenrodDeptStore2FYoungsterScript, -1
-	object_event  6,  2, SPRITE_COOLTRAINER_F, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 2, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, GoldenrodDeptStore2FCooltrainerFScript, -1
-	object_event  2,  6, SPRITE_GENTLEMAN, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, GoldenrodDeptStore2FGentlemanScript, -1
