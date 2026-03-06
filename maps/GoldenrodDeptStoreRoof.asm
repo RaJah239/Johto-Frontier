20fb329569b397c4a46f7b9d7b85d588352966f4
RaJah239 <ravijagroo239@gmail.com>
2026-03-06 13:59:49 -0400

    Update maps/GoldenrodDeptStoreRoof.asm

--- maps/GoldenrodDeptStoreRoof.asm
+++ maps/GoldenrodDeptStoreRoof.asm
@@ -1,3 +1,25 @@
+GoldenrodDeptStoreRoof_MapEvents:
+	def_warp_events
+	warp_event 13,  1, GOLDENROD_DEPT_STORE_6F, 3
+
+	def_coord_events
+
+	def_bg_events
+	bg_event 15,  3, BGEVENT_JUMPTEXT, Binoculars1Text
+	bg_event 15,  5, BGEVENT_JUMPTEXT, Binoculars2Text
+	bg_event 15,  6, BGEVENT_JUMPTEXT, Binoculars3Text
+	bg_event  3,  0, BGEVENT_UP, PokeDollVendingMachine
+
+	def_object_events
+	object_event  1,  4, SPRITE_CLERK, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, GoldenrodDeptStoreRoofClerkScript, EVENT_GOLDENROD_SALE_OFF
+	object_event 10,  3, SPRITE_POKEFAN_F, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 1, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_COMMAND, jumptextfaceplayer, GoldenrodDeptStoreRoofPokefanFText, -1
+	object_event  2,  1, SPRITE_FISHER, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, GoldenrodDeptStoreRoofFisherScript, -1
+	object_event  3,  4, SPRITE_TWIN, SPRITEMOVEDATA_WANDER, 1, 1, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_COMMAND, jumptextfaceplayer, GoldenrodDeptStoreRoofTwinText, EVENT_GOLDENROD_SALE_ON
+	object_event 14,  6, SPRITE_SUPER_NERD, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, GoldenrodDeptStoreRoofSuperNerdScript, EVENT_GOLDENROD_SALE_ON
+	object_event  7,  0, SPRITE_POKEFAN_M, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 1, 0, -1, -1, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, GoldenrodDeptStoreRoofPokefanMText, EVENT_GOLDENROD_SALE_OFF
+	object_event  5,  3, SPRITE_TEACHER, SPRITEMOVEDATA_WANDER, 1, 1, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_COMMAND, jumptextfaceplayer, GoldenrodDeptStoreRoofTeacherText, EVENT_GOLDENROD_SALE_OFF
+	object_event  1,  6, SPRITE_BUG_CATCHER, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_COMMAND, jumptextfaceplayer, GoldenrodDeptStoreRoofBugCatcherText, EVENT_GOLDENROD_SALE_OFF
+
 	object_const_def
 	const GOLDENRODDEPTSTOREROOF_CLERK
 	const GOLDENRODDEPTSTOREROOF_POKEFAN_F
@@ -40,104 +62,63 @@ GoldenrodDeptStoreRoofCheckSaleChangeClerkCallback:
 GoldenrodDeptStoreRoofClerkScript:
 	opentext
 	pokemart MARTTYPE_ROOFTOP, 0
-	closetext
-	end
-
-GoldenrodDeptStoreRoofPokefanFScript:
-	jumptextfaceplayer GoldenrodDeptStoreRoofPokefanFText
+	endtext
 
 GoldenrodDeptStoreRoofFisherScript:
-	faceplayer
-	opentext
-	writetext GoldenrodDeptStoreRoofFisherText
-	waitbutton
-	closetext
+	showthistextfaceplayer
+		text "Pardon? Who says"
+		line "an adult can't get"
+		cont "into this?"
+
+		para "I'm going to be"
+		line "back every day to"
+
+		para "collect all the"
+		line "dolls!"
+		done
 	turnobject GOLDENRODDEPTSTOREROOF_FISHER, UP
 	end
 
-GoldenrodDeptStoreRoofTwinScript:
-	jumptextfaceplayer GoldenrodDeptStoreRoofTwinText
-
 GoldenrodDeptStoreRoofSuperNerdScript:
-	opentext
-	writetext GoldenrodDeptStoreRoofSuperNerdOhWowText
-	waitbutton
-	closetext
+	showthistext
+		text "Oh, wow!"
+		done
 	turnobject GOLDENRODDEPTSTOREROOF_SUPER_NERD, UP
-	opentext
-	writetext GoldenrodDeptStoreRoofSuperNerdQuitBotheringMeText
-	waitbutton
-	closetext
+	showthistext
+		text "Will you quit"
+		line "bothering me?"
+		done
 	turnobject GOLDENRODDEPTSTOREROOF_SUPER_NERD, RIGHT
 	end
-
-GoldenrodDeptStoreRoofPokefanMScript:
-	jumptextfaceplayer GoldenrodDeptStoreRoofPokefanMText
-
-GoldenrodDeptStoreRoofTeacherScript:
-	jumptextfaceplayer GoldenrodDeptStoreRoofTeacherText
-
-GoldenrodDeptStoreRoofBugCatcherScript:
-	jumptextfaceplayer GoldenrodDeptStoreRoofBugCatcherText
-
-Binoculars1:
-	jumptext Binoculars1Text
-
-Binoculars2:
-	jumptext Binoculars2Text
-
-Binoculars3:
-	jumptext Binoculars3Text
-
-PokeDollVendingMachine:
-	jumptext PokeDollVendingMachineText
 
 GoldenrodDeptStoreRoofPokefanFText:
 	text "Whew, I'm tired."
 
 	para "I sometimes come"
 	line "up to the rooftop"
-
-	para "to take a break"
-	line "from shopping."
-	done
-
-GoldenrodDeptStoreRoofFisherText:
-	text "Pardon? Who says"
-	line "an adult can't get"
-	cont "into this?"
-
-	para "I'm going to be"
-	line "back every day to"
-
-	para "collect all the"
-	line "dolls!"
+	cont "to take a break"
+	cont "from shopping."
 	done
 
 GoldenrodDeptStoreRoofTwinText:
 	text "They have bargain"
 	line "sales here every"
 	cont "so often."
-	done
 
-GoldenrodDeptStoreRoofSuperNerdOhWowText:
-	text "Oh, wow!"
-	done
-
-GoldenrodDeptStoreRoofSuperNerdQuitBotheringMeText:
-	text "Will you quit"
-	line "bothering me?"
+	text "A kid named Todd"
+	line "on Route 6 always"
+	cont "knows when there's"
+	cont "a bargain."
 	done
 
 GoldenrodDeptStoreRoofPokefanMText:
 	text "There's something"
 	line "I really want, but"
-
-	para "I don't have the"
-	line "necessary cash…"
+	cont "I don't have the"
+	cont "necessary cash…"
 
 	para "Maybe I'll sell"
-	line "off the BERRIES"
+	line "off the Berries"
 	cont "I've collected…"
 	done
 
@@ -146,18 +127,17 @@ GoldenrodDeptStoreRoofTeacherText:
 	line "so cheap!"
 
 	para "I bought so much,"
-	line "my PACK's crammed!"
+	line "my Bag's crammed!"
 	done
 
 GoldenrodDeptStoreRoofBugCatcherText:
-	text "My #MON always"
+	text "My #mon always"
 	line "get paralyzed or"
-
-	para "poisoned when the"
-	line "chips are down…"
+	cont "poisoned when the"
+	cont "chips are down…"
 
 	para "So I came to buy"
-	line "some FULL HEAL."
+	line "some Full Heal."
 
 	para "I wonder if"
 	line "there's any left?"
@@ -166,9 +146,8 @@ GoldenrodDeptStoreRoofBugCatcherText:
 Binoculars1Text:
 	text "These binoculars"
 	line "let me see far"
-
-	para "away. Maybe I can"
-	line "see my own house."
+	cont "away. Maybe I can"
+	cont "see my own house."
 
 	para "Is it the one with"
 	line "the green roof?"
@@ -179,7 +158,7 @@ Binoculars2Text:
 	line "are battling on"
 	cont "the road!"
 
-	para "A #MON fired a"
+	para "A #mon fired a"
 	line "flurry of leaves!"
 
 	para "That makes me feel"
@@ -188,15 +167,18 @@ Binoculars2Text:
 	done
 
 Binoculars3Text:
-	text "A FISHER caught a"
-	line "lot of MAGIKARP…"
+	text "A Fisher caught a"
+	line "lot of Magikarp…"
 
-	para "They're SPLASHing"
+	para "They're Splashing"
 	line "at the same time!"
 
 	para "Look at the water"
 	line "going everywhere!"
 	done
+
+PokeDollVendingMachine:
+	jumptext PokeDollVendingMachineText
 
 PokeDollVendingMachineText:
 	text "A vending machine"
@@ -208,25 +190,3 @@ PokeDollVendingMachineText:
 	para "But it's almost"
 	line "empty…"
 	done
-
-GoldenrodDeptStoreRoof_MapEvents:
-	def_warp_events
-	warp_event 13,  1, GOLDENROD_DEPT_STORE_6F, 3
-
-	def_coord_events
-
-	def_bg_events
-	bg_event 15,  3, BGEVENT_RIGHT, Binoculars1
-	bg_event 15,  5, BGEVENT_RIGHT, Binoculars2
-	bg_event 15,  6, BGEVENT_RIGHT, Binoculars3
-	bg_event  3,  0, BGEVENT_UP, PokeDollVendingMachine
-
-	def_object_events
-	object_event  1,  4, SPRITE_CLERK, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, GoldenrodDeptStoreRoofClerkScript, EVENT_GOLDENROD_SALE_OFF
-	object_event 10,  3, SPRITE_POKEFAN_F, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 1, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, GoldenrodDeptStoreRoofPokefanFScript, -1
-	object_event  2,  1, SPRITE_FISHER, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, GoldenrodDeptStoreRoofFisherScript, -1
-	object_event  3,  4, SPRITE_TWIN, SPRITEMOVEDATA_WANDER, 1, 1, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, GoldenrodDeptStoreRoofTwinScript, EVENT_GOLDENROD_SALE_ON
-	object_event 14,  6, SPRITE_SUPER_NERD, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, GoldenrodDeptStoreRoofSuperNerdScript, EVENT_GOLDENROD_SALE_ON
-	object_event  7,  0, SPRITE_POKEFAN_M, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 1, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, GoldenrodDeptStoreRoofPokefanMScript, EVENT_GOLDENROD_SALE_OFF
-	object_event  5,  3, SPRITE_TEACHER, SPRITEMOVEDATA_WANDER, 1, 1, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, GoldenrodDeptStoreRoofTeacherScript, EVENT_GOLDENROD_SALE_OFF
-	object_event  1,  6, SPRITE_BUG_CATCHER, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, GoldenrodDeptStoreRoofBugCatcherScript, EVENT_GOLDENROD_SALE_OFF
