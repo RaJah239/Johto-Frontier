5a6fc99fd50f0385c0ce5d71d9add332ac97936a
RaJah239 <ravijagroo239@gmail.com>
2026-03-06 12:52:41 -0400

    Update maps/GoldenrodDeptStore5F.asm

--- maps/GoldenrodDeptStore5F.asm
+++ maps/GoldenrodDeptStore5F.asm
@@ -12,9 +12,9 @@ GoldenrodDeptStore5F_MapEvents:
 
 	def_object_events
 	object_event  8,  5, SPRITE_CLERK, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, GoldenrodDeptStore5FPunchTutorScript, -1
-	object_event  3,  6, SPRITE_LASS, SPRITEMOVEDATA_WANDER, 1, 1, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, GoldenrodDeptStore5FLassScript, -1
+	object_event  3,  6, SPRITE_LASS, SPRITEMOVEDATA_WANDER, 1, 1, -1, -1, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, GoldenrodDeptStore5FLassText, -1
 	object_event  6,  3, SPRITE_COOLTRAINER_M, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, Mike, -1
-	object_event 13,  5, SPRITE_POKEFAN_M, SPRITEMOVEDATA_WANDER, 2, 2, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, GoldenrodDeptStore5FPokefanMScript, -1
+	object_event 13,  5, SPRITE_POKEFAN_M, SPRITEMOVEDATA_WANDER, 2, 2, -1, -1, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, GoldenrodDeptStore5FPokefanMText, -1
 	object_event  7,  5, SPRITE_RECEPTIONIST, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, GoldenrodDeptStore5FReceptionistScript, EVENT_GOLDENROD_DEPT_STORE_5F_HAPPINESS_EVENT_LADY
 	object_event  9,  1, SPRITE_TWIN, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, GoldenrodDeptStore5FMysteryGiftCarrieScript, -1
 
@@ -118,56 +118,25 @@ GoldenrodDeptStore5FReceptionistScript:
 		line "that can be gotten"
 		cont "here but only once"
 		cont "each."
-
-		para "I check if your"
-		line "first #mon is"
-		cont "very happy or not"
-		cont "very happy."
 		done
-
-GoldenrodDeptStore5FLassScript:
-	jumptextfaceplayer GoldenrodDeptStore5FLassText
-
-Mike:
-	faceplayer
-	opentext
-	trade NPC_TRADE_MIKE
-	waitbutton
-	closetext
-	end
-
-GoldenrodDeptStore5FPokefanMScript:
-	jumptextfaceplayer GoldenrodDeptStore5FPokefanMText
-
-
-
-
-
-
-
-
-
-
 
 GoldenrodDeptStore5FLassText:
 	text "On Sundays, a lady"
 	line "comes to check out"
-	cont "#MON."
+	cont "#mon."
 
-	para "She even gives"
-	line "away TMs!"
+	para "She gives away TMs"
+	line "based on how happy"
+	cont "or not the first"
+	cont "#mon in your"
+	cont "party is."
 	done
 
 GoldenrodDeptStore5FPokefanMText:
-	text "You can't rename a"
-	line "#MON you get in"
-	cont "a trade."
-
-	para "The name is a re-"
-	line "flection of the"
-
-	para "original trainer's"
-	line "feelings for it."
+	text "You can rename any"
+	line "#mon you want,"
+	cont "even those obtain-"
+	cont "ed in trades."
 	done
 
 GoldenrodDeptStore5FDirectoryText:
@@ -177,12 +146,23 @@ GoldenrodDeptStore5FDirectoryText:
 	para "5F Tutor Corner"
 	done
 
+Mike:
+	faceplayeropentext
+	trade NPC_TRADE_MIKE
+	waitendtext
+
 GoldenrodDeptStore5FMysteryGiftCarrieScript:
-	faceplayer
-	opentext
+	faceplayeropentext
 	checkflag ENGINE_DAILY_MYSTERY_GIFT
 	iftrue .NoGift
-	writetext MysterGiftText
+	writethistext
+		text "Mystery Gift!"
+		line "Mystery Gift!"
+
+		para "Do you want to"
+		line "share a Mystery"
+		cont "Gift?"
+		done
 	yesorno
 	iffalse .Decline
 	readvar VAR_ITEM_POCKET
@@ -196,132 +176,97 @@ GoldenrodDeptStore5FMysteryGiftCarrieScript:
 
 .AskSave
 	setflag ENGINE_DAILY_MYSTERY_GIFT
-	writetext MysteryGift_SaveGame
+	writethistext
+		text "You need to save"
+		line "your game before"
+		cont "we share, okay?"
+		done
 	yesorno
 	iffalse .Decline
 	special TryQuickSave
 	iffalse .Decline
-	writetext MysteryGiftLinkUp
+	writethistext
+		text "Okay! Let's link"
+		line "up for a sec!"
+		done
 	playsound SFX_MOVE_DELETED
 	waitsfx
-	scall FindMysteryGiftItem
+	callstd MysteryGiftCarrieScript
 	iffalse .NoRoom
-	scall FindMysteryGiftItem
+	callstd MysteryGiftCarrieScript
 	iffalse .NoRoom
-	scall FindMysteryGiftItem
+	callstd MysteryGiftCarrieScript
 	iffalse .NoRoom
-	writetext MysteryGiftReceivedText
-	waitbutton
-	closetext
+	writethistext
+		text "Wow, I got really"
+		line "cool items today!"
+
+		para "I hope you got"
+		line "something good!"
+
+		para "Let's do this"
+		line "again tomorrow!"
+		done
+	waitclosetext
 	turnobject LAST_TALKED, DOWN
 	end
 
 .NoRoom
-	writetext MysterGiftNoRoom
-	waitbutton
-	closetext
+	writethistext
+		text "I guess too many"
+		line "people shared with"
+		cont "you! Hehe!"
+		done
 	turnobject LAST_TALKED, DOWN
 	end
 
 .NoGift
-	writetext NoMysteryGiftText
-	waitbutton
-	closetext
+	writethistext
+		text "We've already"
+		line "shared today."
+
+		para "But I'd be happy"
+		line "to share again"
+		cont "tomorrow."
+		done
+	waitclosetext
 	turnobject LAST_TALKED, DOWN
 	end
 
 .Decline
 	clearflag ENGINE_DAILY_MYSTERY_GIFT
-	writetext DeclineMysteryGiftText
-	waitbutton
-	closetext
+	writethistext
+		text "Oh…"
+
+		para "Some other time,"
+		line "okay?"
+	 	done
+	waitclosetext
 	turnobject LAST_TALKED, DOWN
 	end
 
 .ItemsNearlyFull
-	writetext MysteryGiftItemPocketWarningText
+	writethistext
+		text "…You don't have"
+		line "much space in your"
+		cont "Item Pocket."
+		done
 	waitbutton
-	jump .AskContinueAnyway
+	sjump .AskContinueAnyway
 
 .BallsNearlyFull
-	writetext MysteryGiftBallPocketWarningText
+	writethistext
+		text "…You don't have"
+		line "much space in your"
+		cont "Ball Pocket."
+		done
 	waitbutton
 .AskContinueAnyway
-	writetext MysteryGiftAnywayText
+	writethistext
+		text "Do you want to"
+		line "use Mystery Gift"
+		cont "anyway?"
+		done
 	yesorno
 	iffalse .Decline
-	jump .AskSave
-
-FindMysteryGiftItem:
-	jumpstd MysteryGiftCarrieScript
-
-MysterGiftText:
-	text "Mystery Gift!"
-	line "Mystery Gift!"
-
-	para "Do you want to"
-	line "share a Mystery"
-	cont "Gift?"
-	done
-
-MysteryGift_SaveGame:
-	text "You need to save"
-	line "your game before"
-	cont "we share, okay?"
-	done
-
-MysteryGiftLinkUp:
-	text "Okay! Let's link"
-	line "up for a sec!"
-	done
-
-MysterGiftNoRoom:
-	text "I guess too many"
-	line "people shared with"
-	cont "you! Hehe!"
-	done
-
-MysteryGiftItemPocketWarningText:
-	text "…You don't have"
-	line "much space in your"
-	cont "Item Pocket."
-	done
-
-MysteryGiftBallPocketWarningText:
-	text "…You don't have"
-	line "much space in your"
-	cont "Ball Pocket."
-	done
-
-MysteryGiftAnywayText:
-	text "Do you want to"
-	line "use Mystery Gift"
-	cont "anyway?"
-	done
-
-MysteryGiftReceivedText:
-	text "Wow, I got really"
-	line "cool items today!"
-	cont "I hope you got"
-	cont "something good!"
-
-	para "Let's do this"
-	line "again tomorrow!"
-	done
-
-NoMysteryGiftText:
-	text "We've already"
-	line "shared today."
-
-	para "But I'd be happy"
-	line "to share again"
-	cont "tomorrow."
-	done
-
-DeclineMysteryGiftText:
-	text "Oh…"
-
-	para "Some other time,"
-	line "okay?"
- 	done
-
+	sjump .AskSave
