bf6a815ad0efb8f6df6c704974860c7ae5260205
RaJah239 <ravijagroo239@gmail.com>
2026-03-06 07:56:14 -0400

    redid Route6IlexForestGate.asm with tangela blocking too

--- maps/Route6IlexForestGate.asm
+++ maps/Route6IlexForestGate.asm
@@ -6,7 +6,6 @@ Route6IlexForestGate_MapEvents:
 	warp_event  5,  7, ILEX_FOREST, 1
 
 	def_coord_events
-	coord_event  4,  7, SCENE_ROUTE6ILEXFORESTGATE_TEACHER_BLOCKS_IF_FOREST_IS_RESTLESS, Route6IlexForestGateCelebiEvent
 
 	def_bg_events
 
@@ -15,18 +14,19 @@ Route6IlexForestGate_MapEvents:
 	object_event  9,  3, SPRITE_TEACHER, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, Route6IlexForestGateTeacherScript, EVENT_ROUTE_6_ILEX_FOREST_GATE_TEACHER_BEHIND_COUNTER
 	object_event  5,  7, SPRITE_TEACHER, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, Route6IlexForestGateTeacherScript, EVENT_ROUTE_6_ILEX_FOREST_GATE_TEACHER_IN_WALKWAY
 	object_event  3,  4, SPRITE_LASS, SPRITEMOVEDATA_WALK_UP_DOWN, 0, 1, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_COMMAND, jumptextfaceplayer, Route6IlexForestGateLassText, EVENT_ROUTE_6_ILEX_FOREST_GATE_LASS
-	object_event  9,  4, SPRITE_TANGELA, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, PAL_NPC_TEAL, OBJECTTYPE_SCRIPT, 0, Route6IlexForestGateTangelaScript, -1
+	object_event  9,  4, SPRITE_TANGELA, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, PAL_NPC_TEAL, OBJECTTYPE_SCRIPT, 0, Route6IlexForestGateTangelaScript, EVENT_ROUTE_6_ILEX_FOREST_GATE_TANGELA_BEHIND_COUNTER
+	object_event  4,  7, SPRITE_TANGELA, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, PAL_NPC_TEAL, OBJECTTYPE_SCRIPT, 0, Route6IlexForestGateTangelaScript, EVENT_ROUTE_6_ILEX_FOREST_GATE_TANGELA_IN_WALKWAY
 
 	object_const_def
 	const ROUTE6ILEXFORESTGATE_PORYGON_PC
 	const ROUTE6ILEXFORESTGATE_TEACHER1
 	const ROUTE6ILEXFORESTGATE_TEACHER2
 	const ROUTE6ILEXFORESTGATE_LASS
-	const ROUTE6ILEXFORESTGATE_TANGELA
+	const ROUTE6ILEXFORESTGATE_TANGELA1
+	const ROUTE6ILEXFORESTGATE_TANGELA2
 
 Route6IlexForestGate_MapScripts:
 	def_scene_scripts
-	scene_const SCENE_ROUTE6ILEXFORESTGATE_TEACHER_BLOCKS_IF_FOREST_IS_RESTLESS
 
 	def_callbacks
 	callback MAPCALLBACK_OBJECTS, Route6IsForestRestlessCallback
@@ -35,40 +35,17 @@ Route6IsForestRestlessCallback:
 	checkevent EVENT_FOREST_IS_RESTLESS
 	iffalse .Normal
 	disappear ROUTE6ILEXFORESTGATE_TEACHER1
+	disappear ROUTE6ILEXFORESTGATE_TANGELA1
 	appear ROUTE6ILEXFORESTGATE_TEACHER2
+	appear ROUTE6ILEXFORESTGATE_TANGELA2
 	endcallback
 
 .Normal:
 	disappear ROUTE6ILEXFORESTGATE_TEACHER2
+	disappear ROUTE6ILEXFORESTGATE_TANGELA2
 	appear ROUTE6ILEXFORESTGATE_TEACHER1
+	appear ROUTE6ILEXFORESTGATE_TANGELA1
 	endcallback
-
-Route6IlexForestGateCelebiEvent:
-	checkevent EVENT_FOREST_IS_RESTLESS
-	iffalse .skip
-	showemote EMOTE_SHOCK, ROUTE6ILEXFORESTGATE_TEACHER2, 20
-	turnobject ROUTE6ILEXFORESTGATE_TEACHER2, LEFT
-	turnobject PLAYER, RIGHT
-	follow PLAYER, ROUTE6ILEXFORESTGATE_TEACHER2
-	applymovement PLAYER, Route6IlexForestGateTeacherBlocksPlayerMovement
-	stopfollow
-	turnobject PLAYER, DOWN
-	opentext
-	writetext Route6IlexForestGateTeacher_ForestIsRestless
-	waitclosetext
-	applymovement ROUTE6ILEXFORESTGATE_TEACHER2, Route6IlexForestGateTeacherReturnsMovement
-.skip:
-	end
-
-Route6IlexForestGateTeacherBlocksPlayerMovement:
-	step UP
-	step UP
-	step_end
-
-Route6IlexForestGateTeacherReturnsMovement:
-	step DOWN
-	step RIGHT
-	step_end
 
 Route6IlexForestGateTeacherScript:
 	faceplayeropentext
@@ -101,16 +78,13 @@ Route6IlexForestGateTeacherScript:
 		done
 
 .ForestIsRestless:
-	writetext Route6IlexForestGateTeacher_ForestIsRestless
-	waitendtext
+	jumpthisopenedtext
+		text "Something's wrong"
+		line "in Ilex Forest…"
 
-Route6IlexForestGateTeacher_ForestIsRestless:
-	text "Something's wrong"
-	line "in Ilex Forest…"
-
-	para "You should stay"
-	line "away right now."
-	done
+		para "You should stay"
+		line "away right now."
+		done
 
 Route6IlexForestGateLassText:
 	text "Did you see the"
