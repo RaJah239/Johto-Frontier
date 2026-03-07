6adb6d4415e61f941a20a53f630712957970b250
RaJah239 <ravijagroo239@gmail.com>
2026-03-07 11:22:13 -0400

    Update maps/GoldenrodMansionRoof.asm

--- maps/GoldenrodMansionRoof.asm
+++ maps/GoldenrodMansionRoof.asm
@@ -1,36 +1,3 @@
-	object_const_def
-	const GOLDENRODMANSIONROOF_FISHER
-
-GoldenrodMansionRoof_MapScripts:
-	def_scene_scripts
-
-	def_callbacks
-
-GoldenrodMansionRoofFisherScript:
-	jumptextfaceplayer GoldenrodMansionRoofFisherText
-
-GoldenrodMansionRoofGraffiti:
-	jumptext GoldenrodMansionRoofGraffitiText
-
-GoldenrodMansionRoofFisherText:
-	text "High places--I do"
-	line "love them so!"
-
-	para "I'd say the only"
-	line "thing that loves"
-
-	para "heights as much as"
-	line "me is smoke!"
-	done
-
-GoldenrodMansionRoofGraffitiText:
-	text "There's graffiti"
-	line "on the wall…"
-
-	para "<PLAYER> added a"
-	line "moustache!"
-	done
-
 GoldenrodMansionRoof_MapEvents:
 	def_warp_events
 	warp_event  1,  1, GOLDENROD_MANSION_3F, 1
@@ -43,4 +10,31 @@ GoldenrodMansionRoof_MapEvents:
 	bg_event  6,  1, BGEVENT_LEFT, GoldenrodMansionRoofGraffiti
 
 	def_object_events
-	object_event  7,  5, SPRITE_FISHER, SPRITEMOVEDATA_WALK_UP_DOWN, 0, 1, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, GoldenrodMansionRoofFisherScript, -1
+	object_event  7,  5, SPRITE_FISHER, SPRITEMOVEDATA_WALK_UP_DOWN, 0, 1, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_COMMAND, jumptextfaceplayer, GoldenrodMansionRoofFisherText, -1
+
+	object_const_def
+	const GOLDENRODMANSIONROOF_FISHER
+
+GoldenrodMansionRoof_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+GoldenrodMansionRoofFisherText:
+	text "High places--I do"
+	line "love them so!"
+
+	para "I'd say the only"
+	line "thing that loves"
+	cont "heights as much as"
+	cont "me is smoke!"
+	done
+
+GoldenrodMansionRoofGraffiti:
+	jumpthistext
+		text "There's graffiti"
+		line "on the wall…"
+
+		para "<PLAYER> added a"
+		line "moustache!"
+		done
