c9bd5baa9ef81d07197b32018d0dbd02d066aca7
RaJah239 <ravijagroo239@gmail.com>
2026-03-07 10:47:05 -0400

    Update maps/GoldenrodMansion2F.asm

--- maps/GoldenrodMansion2F.asm
+++ maps/GoldenrodMansion2F.asm
@@ -1,52 +1,3 @@
-GoldenrodMansion2F_MapScripts:
-	def_scene_scripts
-
-	def_callbacks
-
-GoldenrodMansion2FComputer:
-	jumptext GoldenrodMansion2FComputerText
-
-GoldenrodMansion2FMeetingRoomSign:
-	jumptext GoldenrodMansion2FMeetingRoomSignText
-
-GoldenrodMansion2FBookshelf:
-	jumpstd DifficultBookshelfScript
-
-GoldenrodMansion2FComputerText:
-	text "<PLAYER> turned on"
-	line "the PC."
-
-	para "…"
-
-	para "Someone was in the"
-	line "middle of compos-"
-	cont "ing an e-mail."
-
-	para "…I hope you'll"
-	line "come visit KANTO."
-
-	para "I think you'll be"
-	line "surprised at how"
-
-	para "much things have"
-	line "changed here."
-
-	para "You'll also see"
-	line "many #MON that"
-
-	para "aren't native to"
-	line "JOHTO."
-
-	para "To the PRODUCER"
-
-	para "…"
-	done
-
-GoldenrodMansion2FMeetingRoomSignText:
-	text "GAME FREAK"
-	line "MEETING ROOM"
-	done
-
 GoldenrodMansion2F_MapEvents:
 	def_warp_events
 	warp_event  0,  0, GOLDENROD_MANSION_1F, 1
@@ -57,8 +8,31 @@ GoldenrodMansion2F_MapEvents:
 	def_coord_events
 
 	def_bg_events
-	bg_event  0,  3, BGEVENT_READ, GoldenrodMansion2FComputer
+	bg_event  0,  3, BGEVENT_JUMPTEXT, GoldenrodMansion2FComputerText
 	bg_event  5,  8, BGEVENT_UP, GoldenrodMansion2FMeetingRoomSign
-	bg_event  2,  3, BGEVENT_READ, GoldenrodMansion2FBookshelf
+	bg_event  2,  3, BGEVENT_JUMPSTD, DIFFICULT_BOOKSHELF_SCRIPT
 
 	def_object_events
+
+GoldenrodMansion2F_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+GoldenrodMansion2FComputerText:
+	text "<PLAYER> turned on"
+	line "the PC."
+
+	para "…"
+
+	para "Better not touch"
+	line "someone else's PC."
+
+	para "…"
+	done
+
+GoldenrodMansion2FMeetingRoomSign:
+	jumpthistext
+		text "Game Freak"
+		line "Meeting Room"
+		done
