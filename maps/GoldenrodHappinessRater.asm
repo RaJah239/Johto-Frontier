8c8eadd2521148f74c8bdabcd2b6cfd8cd4d00fa
RaJah239 <ravijagroo239@gmail.com>
2026-03-07 10:11:15 -0400

    Update maps/GoldenrodHappinessRater.asm

--- maps/GoldenrodHappinessRater.asm
+++ maps/GoldenrodHappinessRater.asm
@@ -1,3 +1,20 @@
+GoldenrodHappinessRater_MapEvents:
+	def_warp_events
+	warp_event  2,  7, GOLDENROD_CITY, 4
+	warp_event  3,  7, GOLDENROD_CITY, 4
+
+	def_coord_events
+
+	def_bg_events
+	bg_event  0,  1, BGEVENT_JUMPSTD, PICTURE_BOOKSHELF_SCRIPT
+	bg_event  1,  1, BGEVENT_JUMPSTD, PICTURE_BOOKSHELF_SCRIPT
+	bg_event  7,  1, BGEVENT_JUMPSTD, RADIO2SCRIPT
+
+	def_object_events
+	object_event  2,  4, SPRITE_TEACHER, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, GoldenrodHappinessRaterTeacherScript, -1
+	object_event  5,  3, SPRITE_POKEFAN_M, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, GoldenrodHappinessRaterPokefanMText, -1
+	object_event  5,  6, SPRITE_TWIN, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 1, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_COMMAND, jumptextfaceplayer, GoldenrodHappinessRaterTwinText, -1
+
 	object_const_def
 	const GOLDENRODHAPPINESSRATER_TEACHER
 	const GOLDENRODHAPPINESSRATER_POKEFAN_M
@@ -9,10 +26,19 @@ GoldenrodHappinessRater_MapScripts:
 	def_callbacks
 
 GoldenrodHappinessRaterTeacherScript:
-	faceplayer
-	opentext
+	faceplayeropentext
 	special GetFirstPokemonHappiness
-	writetext GoldenrodHappinessRaterTeacherText
+	writethistext
+		text "If you treat your"
+		line "#mon nicely,"
+		cont "they will love you"
+		cont "in return."
+
+		para "Oh? Let me see"
+		line "your @"
+		text_ram wStringBuffer3
+		text "…"
+		done
 	promptbutton
 	ifgreater 250 - 1, .LovesYouALot
 	ifgreater 200 - 1, .ReallyTrustsYou
@@ -22,131 +48,60 @@ GoldenrodHappinessRaterTeacherScript:
 	sjump .LooksMean
 
 .LovesYouALot:
-	writetext GoldenrodHappinessRatingText_LovesYouALot
-	waitbutton
-	closetext
-	end
+	jumpthisopenedtext
+		text "It looks really"
+		line "happy! It must"
+		cont "love you a lot."
+		done
 
 .ReallyTrustsYou:
-	writetext GoldenrodHappinessRatingText_ReallyTrustsYou
-	waitbutton
-	closetext
-	end
+	jumpthisopenedtext
+		text "I get the feeling"
+		line "that it really"
+		cont "trusts you."
+		done
 
 .SortOfHappy:
-	writetext GoldenrodHappinessRatingText_SortOfHappy
-	waitbutton
-	closetext
-	end
+	jumpthisopenedtext
+		text "It's friendly to-"
+		line "ward you. It looks"
+		cont "sort of happy."
+		done
+
 
 .QuiteCute:
-	writetext GoldenrodHappinessRatingText_QuiteCute
-	waitbutton
-	closetext
-	end
+	jumpthisopenedtext
+		text "It's quite cute."
+		done
 
 .NotUsedToYou:
-	writetext GoldenrodHappinessRatingText_NotUsedToYou
-	waitbutton
-	closetext
-	end
+	jumpthisopenedtext
+		text "You should treat"
+		line "it better. It's"
+		cont "not used to you."
+		done
+
 
 .LooksMean:
-	writetext GoldenrodHappinessRatingText_LooksMean
-	waitbutton
-	closetext
-	end
-
-GoldenrodHappinessRaterPokefanMScript:
-	jumptextfaceplayer GoldenrodHappinessRaterPokefanMText
-
-GoldenrodHappinessRaterTwinScript:
-	jumptextfaceplayer GoldenrodHappinessRaterTwinText
-
-HappinessRatersHouseBookshelf:
-	jumpstd DifficultBookshelfScript
-
-HappinessRatersHouseRadio:
-	jumpstd Radio2Script
-
-GoldenrodHappinessRaterTeacherText:
-	text "If you treat your"
-	line "#MON nicely,"
-
-	para "they will love you"
-	line "in return."
-
-	para "Oh? Let me see"
-	line "your @"
-	text_ram wStringBuffer3
-	text "…"
-	done
-
-GoldenrodHappinessRatingText_LovesYouALot:
-	text "It looks really"
-	line "happy! It must"
-	cont "love you a lot."
-	done
-
-GoldenrodHappinessRatingText_ReallyTrustsYou:
-	text "I get the feeling"
-	line "that it really"
-	cont "trusts you."
-	done
-
-GoldenrodHappinessRatingText_SortOfHappy:
-	text "It's friendly to-"
-	line "ward you. It looks"
-	cont "sort of happy."
-	done
-
-GoldenrodHappinessRatingText_QuiteCute:
-	text "It's quite cute."
-	done
-
-GoldenrodHappinessRatingText_NotUsedToYou:
-	text "You should treat"
-	line "it better. It's"
-	cont "not used to you."
-	done
-
-GoldenrodHappinessRatingText_LooksMean:
-	text "It doesn't seem to"
-	line "like you at all."
-	cont "It looks mean."
-	done
+	jumpthisopenedtext
+		text "It doesn't seem to"
+		line "like you at all."
+		cont "It looks mean."
+		done
 
 GoldenrodHappinessRaterPokefanMText:
 	text "I keep losing in"
 	line "battles, and my"
-
-	para "#MON end up"
-	line "fainting…"
+	cont "#mon end up"
+	cont "fainting…"
 
 	para "Maybe that's why"
-	line "my #MON don't"
+	line "my #mon don't"
 	cont "like me much…"
 	done
 
 GoldenrodHappinessRaterTwinText:
 	text "When I use an item"
-	line "on my #MON, it"
+	line "on my #mon, it"
 	cont "acts really glad!"
 	done
-
-GoldenrodHappinessRater_MapEvents:
-	def_warp_events
-	warp_event  2,  7, GOLDENROD_CITY, 4
-	warp_event  3,  7, GOLDENROD_CITY, 4
-
-	def_coord_events
-
-	def_bg_events
-	bg_event  0,  1, BGEVENT_READ, HappinessRatersHouseBookshelf
-	bg_event  1,  1, BGEVENT_READ, HappinessRatersHouseBookshelf
-	bg_event  7,  1, BGEVENT_READ, HappinessRatersHouseRadio
-
-	def_object_events
-	object_event  2,  4, SPRITE_TEACHER, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, GoldenrodHappinessRaterTeacherScript, -1
-	object_event  5,  3, SPRITE_POKEFAN_M, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, GoldenrodHappinessRaterPokefanMScript, -1
-	object_event  5,  6, SPRITE_TWIN, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 1, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, GoldenrodHappinessRaterTwinScript, -1
