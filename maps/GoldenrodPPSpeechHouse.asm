5cfe806f695dbbb689b068ecadfd6afb7fc992fc
RaJah239 <ravijagroo239@gmail.com>
2026-03-08 07:39:03 -0400

    Update maps/GoldenrodPPSpeechHouse.asm

--- maps/GoldenrodPPSpeechHouse.asm
+++ maps/GoldenrodPPSpeechHouse.asm
@@ -1,3 +1,19 @@
+GoldenrodPPSpeechHouse_MapEvents:
+	def_warp_events
+	warp_event  2,  7, GOLDENROD_CITY, 8
+	warp_event  3,  7, GOLDENROD_CITY, 8
+
+	def_coord_events
+
+	def_bg_events
+	bg_event  0,  1, BGEVENT_JUMPSTD, MAGAZINE_BOOKSHELF_SCRIPT
+	bg_event  1,  1, BGEVENT_JUMPSTD, DIFFICULT_BOOKSHELF_SCRIPT
+	bg_event  7,  1, BGEVENT_JUMPSTD, RADIO2SCRIPT
+
+	def_object_events
+	object_event  2,  4, SPRITE_FISHER, SPRITEMOVEDATA_WALK_UP_DOWN, 0, 1, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_COMMAND, jumptextfaceplayer, GoldenrodPPSpeechHouseFisherText, -1
+	object_event  5,  3, SPRITE_LASS, SPRITEMOVEDATA_STANDING_LEFT, 0, 1, -1, -1, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, GoldenrodPPSpeechHouseLassText, -1
+
 	object_const_def
 	const GOLDENRODPPSPEECHHOUSE_FISHER
 	const GOLDENRODPPSPEECHHOUSE_LASS
@@ -7,60 +23,26 @@ GoldenrodPPSpeechHouse_MapScripts:
 
 	def_callbacks
 
-GoldenrodPPSpeechHouseFisherScript:
-	jumptextfaceplayer GoldenrodPPSpeechHouseFisherText
-
-GoldenrodPPSpeechHouseLassScript:
-	jumptextfaceplayer GoldenrodPPSpeechHouseLassText
-
-GoldenrodPPSpeechHouseBookshelf2:
-	jumpstd DifficultBookshelfScript
-
-GoldenrodPPSpeechHouseBookshelf1:
-	jumpstd MagazineBookshelfScript
-
-GoldenrodPPSpeechHouseRadio:
-	jumpstd Radio2Script
-
 GoldenrodPPSpeechHouseFisherText:
 	text "Once while I was"
 	line "battling, my"
+	cont "#mon couldn't"
+	cont "make any moves."
 
-	para "#MON couldn't"
-	line "make any moves."
-
-	para "The POWER POINTS,"
+	para "The Power Points,"
 	line "or PP, of its"
-
-	para "moves were all"
-	line "gone."
+	cont "moves were all"
+	cont "gone."
 	done
 
 GoldenrodPPSpeechHouseLassText:
 	text "Sometimes, a"
-	line "healthy #MON"
-
-	para "may be unable to"
-	line "use its moves."
+	line "healthy #mon"
+	cont "may be unable to"
+	cont "use its moves."
 
 	para "If that happens,"
 	line "heal it at a #-"
-	cont "MON CENTER or use"
+	cont "Mon Center or use"
 	cont "an item."
 	done
-
-GoldenrodPPSpeechHouse_MapEvents:
-	def_warp_events
-	warp_event  2,  7, GOLDENROD_CITY, 8
-	warp_event  3,  7, GOLDENROD_CITY, 8
-
-	def_coord_events
-
-	def_bg_events
-	bg_event  0,  1, BGEVENT_READ, GoldenrodPPSpeechHouseBookshelf1
-	bg_event  1,  1, BGEVENT_READ, GoldenrodPPSpeechHouseBookshelf2
-	bg_event  7,  1, BGEVENT_READ, GoldenrodPPSpeechHouseRadio
-
-	def_object_events
-	object_event  2,  4, SPRITE_FISHER, SPRITEMOVEDATA_WALK_UP_DOWN, 0, 1, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, GoldenrodPPSpeechHouseFisherScript, -1
-	object_event  5,  3, SPRITE_LASS, SPRITEMOVEDATA_STANDING_LEFT, 0, 1, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, GoldenrodPPSpeechHouseLassScript, -1
