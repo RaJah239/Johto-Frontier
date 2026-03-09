d7db4ae671fc14eb11b0bf66a755d54f294397d3
RaJah239 <ravijagroo239@gmail.com>
2026-03-09 17:39:18 -0400

    Update maps/EcruteakItemfinderHouse.asm

--- maps/EcruteakItemfinderHouse.asm
+++ maps/EcruteakItemfinderHouse.asm
@@ -1,3 +1,17 @@
+EcruteakItemfinderHouse_MapEvents:
+	def_warp_events
+	warp_event  3,  7, ECRUTEAK_CITY, 11
+	warp_event  4,  7, ECRUTEAK_CITY, 11
+
+	def_coord_events
+
+	def_bg_events
+	bg_event  2,  1, BGEVENT_JUMPSTD, RADIO2SCRIPT
+
+	def_object_events
+	object_event  2,  3, SPRITE_COOLTRAINER_M, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, EcruteakItemfinderGuy, -1
+	object_event  3,  3, SPRITE_POKEDEX, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, EcruteakHistoryBook, -1
+
 	object_const_def
 	const ECRUTEAKITEMFINDERHOUSE_COOLTRAINER_M
 	const ECRUTEAKITEMFINDERHOUSE_POKEDEX
@@ -8,168 +22,116 @@ EcruteakItemfinderHouse_MapScripts:
 	def_callbacks
 
 EcruteakItemfinderGuy:
-	faceplayer
-	opentext
+	faceplayeropentext
 	checkevent EVENT_GOT_ITEMFINDER
 	iftrue .itemfinder
-	writetext EcruteakItemfinderAdventureText
+	writethistext
+		text "Ah. You're on an"
+		line "adventure with"
+		cont "your #mon?"
+
+		para "Well, what's an"
+		line "adventure without"
+		cont "treasure hunting?"
+
+		para "Am I right, or am"
+		line "I right?"
+		done
 	yesorno
 	iffalse .no
-	writetext EcruteakItemfinderTrueSpiritText
+	writethistext
+		text "Good! You under-"
+		line "stand the true"
+		cont "spirit of adven-"
+		cont "ture."
+
+		para "I like that! Take"
+		line "this with you."
+		done
 	promptbutton
 	verbosegiveitem ITEMFINDER
 	setevent EVENT_GOT_ITEMFINDER
 .itemfinder:
-	writetext ItemfinderExplanationText
-	waitbutton
-	closetext
-	end
+	jumpthisopenedtext
+		text "There are many"
+		line "items lying about"
+		cont "that aren't ob-"
+		cont "vious."
+
+		para "Use Itemfinder to"
+		line "check if there is"
+		cont "an item on the"
+		cont "ground near you."
+
+		para "It doesn't show"
+		line "the exact spot but"
+		cont "beeps faster the"
+		cont "nearer you are to"
+		cont "the item."
+
+		para "You'll still have"
+		line "to search yourself"
+		cont "by pressing A."
+
+		para "Oh yeah--I heard"
+		line "there are items"
+		cont "in Ecruteak's"
+		cont "Burned Tower."
+		done
 
 .no:
-	writetext EcruteakItemfinderToEachHisOwnText
-	waitbutton
-	closetext
-	end
+	jumpthisopenedtext
+		text "Oh… To each his"
+		line "own, I suppose…"
+		done
 
 EcruteakHistoryBook:
 	opentext
-	writetext EcruteakHistoryBookText
+	writethistext
+		text "History Of"
+		line "Ecruteak"
+
+		para "Want to read it?"
+		done
 	yesorno
-	iftrue .ReadBook
-	closetext
-	end
+	iffalse_endtext
+	writethistext
+		text "In Ecruteak, there"
+		line "were two towers."
 
-.ReadBook:
-	writetext EcruteakTwoTowersText
+		para "Each tower was the"
+		line "roost of powerful"
+		cont "flying #mon."
+
+		para "But one of the"
+		line "towers burned to"
+		cont "the ground."
+
+		para "The two #mon"
+		line "haven't been seen"
+		cont "since…"
+
+		para "Keep reading?"
+		done
 	yesorno
-	iftrue .KeepReading
-	closetext
-	end
+	iffalse_endtext
+	jumpthisopenedtext
+		text "Ecruteak was also"
+		line "home to three"
+		cont "#mon that raced"
+		cont "around the town."
 
-.KeepReading:
-	writetext EcruteakThreeMonText
-	waitbutton
-	closetext
-	end
+		para "They were said to"
+		line "have been born of"
+		cont "water, lightning"
+		cont "and fire."
 
-ItemFinderHouseRadio:
-	jumpstd Radio2Script
+		para "But they could not"
+		line "contain their"
+		cont "excessive power."
 
-EcruteakItemfinderAdventureText:
-	text "Ah. You're on an"
-	line "adventure with"
-	cont "your #MON?"
-
-	para "Well, what's an"
-	line "adventure without"
-	cont "treasure hunting?"
-
-	para "Am I right, or am"
-	line "I right?"
-	done
-
-EcruteakItemfinderTrueSpiritText:
-	text "Good! You under-"
-	line "stand the true"
-
-	para "spirit of adven-"
-	line "ture."
-
-	para "I like that! Take"
-	line "this with you."
-	done
-
-ItemfinderExplanationText:
-	text "There are many"
-	line "items lying about"
-
-	para "that aren't ob-"
-	line "vious."
-
-	para "Use ITEMFINDER to"
-	line "check if there is"
-
-	para "an item on the"
-	line "ground near you."
-
-	para "It doesn't show"
-	line "the exact spot,"
-
-	para "so you'll have to"
-	line "look yourself."
-
-	para "Oh yeah--I heard"
-	line "there are items"
-
-	para "in ECRUTEAK's"
-	line "BURNED TOWER."
-	done
-
-EcruteakItemfinderToEachHisOwnText:
-	text "Oh… To each his"
-	line "own, I suppose…"
-	done
-
-EcruteakHistoryBookText:
-	text "HISTORY OF"
-	line "ECRUTEAK"
-
-	para "Want to read it?"
-	done
-
-EcruteakTwoTowersText:
-	text "In ECRUTEAK, there"
-	line "were two towers."
-
-	para "Each tower was the"
-	line "roost of powerful"
-	cont "flying #MON."
-
-	para "But one of the"
-	line "towers burned to"
-	cont "the ground."
-
-	para "The two #MON"
-	line "haven't been seen"
-	cont "since…"
-
-	para "Keep reading?"
-	done
-
-EcruteakThreeMonText:
-	text "ECRUTEAK was also"
-	line "home to three"
-
-	para "#MON that raced"
-	line "around the town."
-
-	para "They were said to"
-	line "have been born of"
-
-	para "water, lightning"
-	line "and fire."
-
-	para "But they could not"
-	line "contain their"
-	cont "excessive power."
-
-	para "So they say the"
-	line "three ran like the"
-
-	para "wind off into the"
-	line "grassland."
-	done
-
-EcruteakItemfinderHouse_MapEvents:
-	def_warp_events
-	warp_event  3,  7, ECRUTEAK_CITY, 11
-	warp_event  4,  7, ECRUTEAK_CITY, 11
-
-	def_coord_events
-
-	def_bg_events
-	bg_event  2,  1, BGEVENT_READ, ItemFinderHouseRadio
-
-	def_object_events
-	object_event  2,  3, SPRITE_COOLTRAINER_M, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, EcruteakItemfinderGuy, -1
-	object_event  3,  3, SPRITE_POKEDEX, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, EcruteakHistoryBook, -1
+		para "So they say the"
+		line "three ran like the"
+		cont "wind off into the"
+		cont "grassland."
+		done
