1fe04429c0e61db626f11441ced2cd595b292028
RaJah239 <ravijagroo239@gmail.com>
2026-03-09 18:09:24 -0400

    Update maps/TinTower3F.asm

--- maps/TinTower3F.asm
+++ maps/TinTower3F.asm
@@ -1,14 +1,3 @@
-	object_const_def
-	const TINTOWER3F_POKE_BALL
-
-TinTower3F_MapScripts:
-	def_scene_scripts
-
-	def_callbacks
-
-TinTower3FFullHeal:
-	itemball FULL_HEAL
-
 TinTower3F_MapEvents:
 	def_warp_events
 	warp_event 10, 14, TIN_TOWER_2F, 1
@@ -20,3 +9,14 @@ TinTower3F_MapEvents:
 
 	def_object_events
 	object_event  3, 14, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, TinTower3FFullHeal, EVENT_TIN_TOWER_3F_FULL_HEAL
+
+	object_const_def
+	const TINTOWER3F_POKE_BALL
+
+TinTower3F_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+TinTower3FFullHeal:
+	itemball FULL_HEAL
