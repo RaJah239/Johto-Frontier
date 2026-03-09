5c70388bcc60333e4e2e2c7bcdbab21ba0a2b29e
RaJah239 <ravijagroo239@gmail.com>
2026-03-09 17:58:07 -0400

    Update maps/EcruteakPokecenter1F.asm

--- maps/EcruteakPokecenter1F.asm
+++ maps/EcruteakPokecenter1F.asm
@@ -1,79 +1,3 @@
-	object_const_def
-	const ECRUTEAKPOKECENTER1F_NURSE
-	const ECRUTEAKPOKECENTER1F_POKEFAN_M
-	const ECRUTEAKPOKECENTER1F_COOLTRAINER_F
-	const ECRUTEAKPOKECENTER1F_GYM_GUIDE
-	const ECRUTEAKPOKECENTER1F_CHANSEY
-
-EcruteakPokecenter1F_MapScripts:
-	def_scene_scripts
-
-	def_callbacks
-
-EcruteakPokecenter1FNurseScript:
-	jumpstd PokecenterNurseScript
-
-EcruteakPokecenterChanseyScript:
-	jumpstd ChanseyPokeCenterScript
-
-EcruteakPokecenter1FPokefanMScript:
-	special CheckMobileAdapterStatusSpecial
-	iftrue .mobile
-	jumptextfaceplayer EcruteakPokecenter1FPokefanMText
-
-.mobile
-	jumptextfaceplayer EcruteakPokecenter1FPokefanMTextMobile
-
-EcruteakPokecenter1FCooltrainerFScript:
-	jumptextfaceplayer EcruteakPokecenter1FCooltrainerFText
-
-EcruteakPokecenter1FGymGuideScript:
-	jumptextfaceplayer EcruteakPokecenter1FGymGuideText
-
-EcruteakPokecenter1FPokefanMText:
-	text "The way the KIMONO"
-	line "GIRLS dance is"
-
-	para "marvelous. Just"
-	line "like the way they"
-	cont "use their #MON."
-	done
-
-EcruteakPokecenter1FPokefanMTextMobile:
-	text "You must be hoping"
-	line "to battle more"
-
-	para "people, right?"
-	line "There's apparently"
-
-	para "some place where"
-	line "trainers gather."
-
-	para "Where, you ask?"
-
-	para "It's a little past"
-	line "OLIVINE CITY."
-	done
-
-EcruteakPokecenter1FCooltrainerFText:
-	text "MORTY, the GYM"
-	line "LEADER, is soooo"
-	cont "cool."
-
-	para "His #MON are"
-	line "really tough too."
-	done
-
-EcruteakPokecenter1FGymGuideText:
-	text "LAKE OF RAGE…"
-
-	para "The appearance of"
-	line "a GYARADOS swarm…"
-
-	para "I smell a conspir-"
-	line "acy. I know it!"
-	done
-
 EcruteakPokecenter1F_MapEvents:
 	def_warp_events
 	warp_event  3,  7, ECRUTEAK_CITY, 1
@@ -85,8 +9,47 @@ EcruteakPokecenter1F_MapEvents:
 	def_bg_events
 
 	def_object_events
-	object_event  3,  1, SPRITE_NURSE, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, EcruteakPokecenter1FNurseScript, -1
-	object_event  7,  6, SPRITE_POKEFAN_M, SPRITEMOVEDATA_SPINRANDOM_FAST, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, EcruteakPokecenter1FPokefanMScript, -1
-	object_event  1,  4, SPRITE_COOLTRAINER_F, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, EcruteakPokecenter1FCooltrainerFScript, -1
-	object_event  7,  1, SPRITE_GYM_GUIDE, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, EcruteakPokecenter1FGymGuideScript, -1
-	object_event  4,  1, SPRITE_CHANSEY, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, EcruteakPokecenterChanseyScript, -1
+	heal_event  3,  1, PAL_NPC_BROWN
+	chansey_event  4,  1
+	object_event  7,  6, SPRITE_POKEFAN_M, SPRITEMOVEDATA_SPINRANDOM_FAST, 0, 0, -1, -1, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, EcruteakPokecenter1FPokefanMText, -1
+	object_event  1,  4, SPRITE_COOLTRAINER_F, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, EcruteakPokecenter1FCooltrainerFText, -1
+	object_event  7,  1, SPRITE_GYM_GUIDE, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_COMMAND, jumptextfaceplayer, EcruteakPokecenter1FGymGuideText, -1
+
+	object_const_def
+	const ECRUTEAKPOKECENTER1F_NURSE
+	const ECRUTEAKPOKECENTER1F_CHANSEY
+	const ECRUTEAKPOKECENTER1F_POKEFAN_M
+	const ECRUTEAKPOKECENTER1F_COOLTRAINER_F
+	const ECRUTEAKPOKECENTER1F_GYM_GUIDE
+
+EcruteakPokecenter1F_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+EcruteakPokecenter1FPokefanMText:
+	text "The way the Kimono"
+	line "Girls dance is"
+	cont "marvelous. Just"
+	cont "like the way they"
+	cont "use their #mon."
+	done
+
+EcruteakPokecenter1FCooltrainerFText:
+	text "Morty, the Gym"
+	line "Leader, is soooo"
+	cont "cool."
+
+	para "His #mon are"
+	line "really tough too."
+	done
+
+EcruteakPokecenter1FGymGuideText:
+	text "Lake Of Rage…"
+
+	para "The appearance of"
+	line "a Gyarados swarm…"
+
+	para "It must be a sight"
+	line "to behold!"
+	done
