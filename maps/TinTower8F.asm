5170261c79960d9d4fb672180dd1ad9431895332
RaJah239 <ravijagroo239@gmail.com>
2026-03-09 18:25:16 -0400

    Update maps/TinTower8F.asm

--- maps/TinTower8F.asm
+++ maps/TinTower8F.asm
@@ -1,22 +1,3 @@
-	object_const_def
-	const TINTOWER8F_POKE_BALL1
-	const TINTOWER8F_POKE_BALL2
-	const TINTOWER8F_POKE_BALL3
-
-TinTower8F_MapScripts:
-	def_scene_scripts
-
-	def_callbacks
-
-TinTower8FNugget:
-	itemball NUGGET
-
-TinTower8FMaxElixer:
-	itemball MAX_ELIXER
-
-TinTower8FFullRestore:
-	itemball FULL_RESTORE
-
 TinTower8F_MapEvents:
 	def_warp_events
 	warp_event  2,  5, TIN_TOWER_7F, 2
@@ -34,3 +15,20 @@ TinTower8F_MapEvents:
 	object_event  7, 13, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, TinTower8FNugget, EVENT_TIN_TOWER_8F_NUGGET
 	object_event 11,  6, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, TinTower8FMaxElixer, EVENT_TIN_TOWER_8F_MAX_ELIXER
 	object_event  3,  1, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, TinTower8FFullRestore, EVENT_TIN_TOWER_8F_FULL_RESTORE
+
+	object_const_def
+	const TINTOWER8F_POKE_BALL1
+	const TINTOWER8F_POKE_BALL2
+	const TINTOWER8F_POKE_BALL3
+
+TinTower8F_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+TinTower8FNugget:
+	itemball NUGGET
+TinTower8FMaxElixer:
+	itemball MAX_ELIXER
+TinTower8FFullRestore:
+	itemball FULL_RESTORE
