b539027581d1d07ec235133be05b456a9e4df45e
RaJah239 <ravijagroo239@gmail.com>
2026-03-09 18:06:09 -0400

    Update maps/EcruteakTinTowerEntrance.asm

--- maps/EcruteakTinTowerEntrance.asm
+++ maps/EcruteakTinTowerEntrance.asm
@@ -1,12 +1,32 @@
+; TODOTEXT here's how to set the blocking sage here:
+; 	setmapscene ECRUTEAK_TIN_TOWER_ENTRANCE, SCENE_ECRUTEAKTINTOWERENTRANCE_SAGE_BLOCKS
+; come back to edit then when I know what I want this for
+
+EcruteakTinTowerEntrance_MapEvents:
+	def_warp_events
+	warp_event  4, 17, ECRUTEAK_CITY, 4
+	warp_event  5, 17, ECRUTEAK_CITY, 4
+	warp_event  5,  3, ECRUTEAK_TIN_TOWER_ENTRANCE, 4
+	warp_event 17, 15, ECRUTEAK_TIN_TOWER_ENTRANCE, 3
+	warp_event 17,  3, WISE_TRIOS_ROOM, 3
+
+	def_coord_events
+	coord_event  4,  7, SCENE_ECRUTEAKTINTOWERENTRANCE_SAGE_BLOCKS, EcruteakTinTowerEntranceSageBlocksLeft
+	coord_event  5,  7, SCENE_ECRUTEAKTINTOWERENTRANCE_SAGE_BLOCKS, EcruteakTinTowerEntranceSageBlocksRight
+
+	def_bg_events
+
+	def_object_events
+	object_event  4,  6, SPRITE_SAGE, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, EcruteakTinTowerEntranceSageScript, EVENT_RANG_CLEAR_BELL_1
+	object_event  5,  6, SPRITE_SAGE, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, EcruteakTinTowerEntranceSageScript, EVENT_RANG_CLEAR_BELL_2
+	object_event  6,  9, SPRITE_SAGE, SPRITEMOVEDATA_WANDER, 1, 1, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, EcruteakTinTowerEntranceWanderingSageScript, EVENT_ECRUTEAK_TIN_TOWER_ENTRANCE_WANDERING_SAGE
+	object_event  3, 11, SPRITE_GRAMPS, SPRITEMOVEDATA_WANDER, 1, 1, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, EcruteakTinTowerEntranceGrampsScript, EVENT_ECRUTEAK_TIN_TOWER_ENTRANCE_WANDERING_SAGE
+
 	object_const_def
 	const ECRUTEAKTINTOWERENTRANCE_SAGE1
 	const ECRUTEAKTINTOWERENTRANCE_SAGE2
 	const ECRUTEAKTINTOWERENTRANCE_SAGE3
 	const ECRUTEAKTINTOWERENTRANCE_GRAMPS
-
-; TODOTEXT here's how to set the blocking sage here:
-; 	setmapscene ECRUTEAK_TIN_TOWER_ENTRANCE, SCENE_ECRUTEAKTINTOWERENTRANCE_SAGE_BLOCKS
-
 
 EcruteakTinTowerEntrance_MapScripts:
 	def_scene_scripts
@@ -17,8 +37,6 @@ EcruteakTinTowerEntrance_MapScripts:
 	callback MAPCALLBACK_OBJECTS, EcruteakTinTowerEntranceInitializeSagesCallback
 
 EcruteakTinTowerEntranceNoop1Scene:
-	end
-
 EcruteakTinTowerEntranceNoop2Scene:
 	end
 
@@ -280,23 +298,3 @@ EcruteakTinTowerEntranceGrampsText:
 	para "#MON flew away,"
 	line "never to return."
 	done
-
-EcruteakTinTowerEntrance_MapEvents:
-	def_warp_events
-	warp_event  4, 17, ECRUTEAK_CITY, 4
-	warp_event  5, 17, ECRUTEAK_CITY, 4
-	warp_event  5,  3, ECRUTEAK_TIN_TOWER_ENTRANCE, 4
-	warp_event 17, 15, ECRUTEAK_TIN_TOWER_ENTRANCE, 3
-	warp_event 17,  3, WISE_TRIOS_ROOM, 3
-
-	def_coord_events
-	coord_event  4,  7, SCENE_ECRUTEAKTINTOWERENTRANCE_SAGE_BLOCKS, EcruteakTinTowerEntranceSageBlocksLeft
-	coord_event  5,  7, SCENE_ECRUTEAKTINTOWERENTRANCE_SAGE_BLOCKS, EcruteakTinTowerEntranceSageBlocksRight
-
-	def_bg_events
-
-	def_object_events
-	object_event  4,  6, SPRITE_SAGE, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, EcruteakTinTowerEntranceSageScript, EVENT_RANG_CLEAR_BELL_1
-	object_event  5,  6, SPRITE_SAGE, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, EcruteakTinTowerEntranceSageScript, EVENT_RANG_CLEAR_BELL_2
-	object_event  6,  9, SPRITE_SAGE, SPRITEMOVEDATA_WANDER, 1, 1, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, EcruteakTinTowerEntranceWanderingSageScript, EVENT_ECRUTEAK_TIN_TOWER_ENTRANCE_WANDERING_SAGE
-	object_event  3, 11, SPRITE_GRAMPS, SPRITEMOVEDATA_WANDER, 1, 1, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, EcruteakTinTowerEntranceGrampsScript, EVENT_ECRUTEAK_TIN_TOWER_ENTRANCE_WANDERING_SAGE
