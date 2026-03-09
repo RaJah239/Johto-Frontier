ac1d24e02fa26f848c53e139e2aa15c77be7c998
RaJah239 <ravijagroo239@gmail.com>
2026-03-09 18:11:50 -0400

    Update maps/TinTower5F.asm

--- maps/TinTower5F.asm
+++ maps/TinTower5F.asm
@@ -1,20 +1,3 @@
-	object_const_def
-	const TINTOWER5F_POKE_BALL
-
-TinTower5F_MapScripts:
-	def_scene_scripts
-
-	def_callbacks
-
-TinTower5FRareCandy:
-	itemball RARE_CANDY
-
-TinTower5FHiddenFullRestore:
-	hiddenitem FULL_RESTORE, EVENT_TIN_TOWER_5F_HIDDEN_FULL_RESTORE
-
-TinTower5FHiddenCarbos:
-	hiddenitem CARBOS, EVENT_TIN_TOWER_5F_HIDDEN_CARBOS
-
 TinTower5F_MapEvents:
 	def_warp_events
 	warp_event 11, 15, TIN_TOWER_6F, 2
@@ -25,8 +8,19 @@ TinTower5F_MapEvents:
 	def_coord_events
 
 	def_bg_events
-	bg_event 16, 14, BGEVENT_ITEM, TinTower5FHiddenFullRestore
-	bg_event  3, 15, BGEVENT_ITEM, TinTower5FHiddenCarbos
+	bg_event 16, 14, BGEVENT_ITEM + FULL_RESTORE, EVENT_TIN_TOWER_5F_HIDDEN_FULL_RESTORE
+	bg_event  3, 15, BGEVENT_ITEM + CARBOS, EVENT_TIN_TOWER_5F_HIDDEN_CARBOS
 
 	def_object_events
 	object_event  9,  9, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, TinTower5FRareCandy, EVENT_TIN_TOWER_5F_RARE_CANDY
+
+	object_const_def
+	const TINTOWER5F_POKE_BALL
+
+TinTower5F_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+TinTower5FRareCandy:
+	itemball RARE_CANDY
