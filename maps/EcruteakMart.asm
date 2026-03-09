66a0649faae8cefe8e28cff06b90464ae9e49f86
RaJah239 <ravijagroo239@gmail.com>
2026-03-09 17:50:50 -0400

    Update maps/EcruteakMart.asm

--- maps/EcruteakMart.asm
+++ maps/EcruteakMart.asm
@@ -1,79 +1,3 @@
-	object_const_def
-	const ECRUTEAKMART_CLERK
-	const ECRUTEAKMART_SUPER_NERD
-	const ECRUTEAKMART_GRANNY
-
-EcruteakMart_MapScripts:
-	def_scene_scripts
-
-	def_callbacks
-
-EcruteakMartClerkScript:
-	opentext
-	readvar VAR_BADGES
-	ifgreater 7, .EightBadgesMart
-	ifgreater 6, .SevenBadgesMart
-	ifgreater 4, .FiveBadgesMart
-	ifgreater 2, .ThreeBadgesMart
-	ifgreater 0, .OneBadgeMart
-	pokemart MARTTYPE_STANDARD, MART_NO_BADGES
-	closetext
-	end
-
-.OneBadgeMart:
-	pokemart MARTTYPE_STANDARD, MART_ONE_BADGE
-	closetext
-	end
-
-.ThreeBadgesMart:
-	pokemart MARTTYPE_STANDARD, MART_THREE_BADGES
-	closetext
-	end
-
-.FiveBadgesMart:
-	pokemart MARTTYPE_STANDARD, MART_FIVE_BADGES
-	closetext
-	end
-
-.SevenBadgesMart:
-	pokemart MARTTYPE_STANDARD, MART_SEVEN_BADGES
-	closetext
-	end
-
-.EightBadgesMart:
-	pokemart MARTTYPE_STANDARD, MART_EIGHT_BADGES
-	closetext
-	end
-
-EcruteakMartSuperNerdScript:
-	jumptextfaceplayer EcruteakMartSuperNerdText
-
-EcruteakMartGrannyScript:
-	jumptextfaceplayer EcruteakMartGrannyText
-
-EcruteakMartSuperNerdText:
-	text "My EEVEE evolved"
-	line "into an ESPEON."
-
-	para "But my friend's"
-	line "EEVEE turned into"
-	cont "an UMBREON."
-
-	para "I wonder why? We"
-	line "both were raising"
-
-	para "our EEVEE in the"
-	line "same way…"
-	done
-
-EcruteakMartGrannyText:
-	text "If you use REVIVE,"
-	line "a #MON that's"
-
-	para "fainted will wake"
-	line "right up."
-	done
-
 EcruteakMart_MapEvents:
 	def_warp_events
 	warp_event  2,  7, ECRUTEAK_CITY, 9
@@ -84,6 +8,37 @@ EcruteakMart_MapEvents:
 	def_bg_events
 
 	def_object_events
-	object_event  1,  3, SPRITE_CLERK, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, EcruteakMartClerkScript, -1
-	object_event  5,  2, SPRITE_SUPER_NERD, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 1, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, EcruteakMartSuperNerdScript, -1
-	object_event  6,  6, SPRITE_GRANNY, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, EcruteakMartGrannyScript, -1
+	variable_mart_event 1, 3, PAL_NPC_BROWN
+	object_event  5,  2, SPRITE_SUPER_NERD, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 1, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_COMMAND, jumptextfaceplayer, EcruteakMartSuperNerdText, -1
+	object_event  6,  6, SPRITE_GRANNY, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, EcruteakMartGrannyText, -1
+
+	object_const_def
+	const ECRUTEAKMART_CLERK
+	const ECRUTEAKMART_SUPER_NERD
+	const ECRUTEAKMART_GRANNY
+
+EcruteakMart_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+EcruteakMartSuperNerdText:
+	text "My Eevee evolved"
+	line "into an Umbreon."
+
+	para "But my friend's"
+	line "Eevee turned into"
+	cont "a Sylveon."
+
+	para "I wonder why? We"
+	line "both were raising"
+	cont "our Eevee in the"
+	cont "same way…"
+	done
+
+EcruteakMartGrannyText:
+	text "If you use Revive,"
+	line "a #mon that's"
+	cont "fainted will wake"
+	cont "right up."
+	done
