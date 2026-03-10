b5075f8f058663bca4ba498c79fd5514200accb8
RaJah239 <ravijagroo239@gmail.com>
2026-03-10 12:39:27 -0400

    Update maps/OlivinePunishmentSpeechHouse.asm

--- maps/OlivinePunishmentSpeechHouse.asm
+++ maps/OlivinePunishmentSpeechHouse.asm
@@ -1,3 +1,18 @@
+OlivinePunishmentSpeechHouse_MapEvents:
+	def_warp_events
+	warp_event  2,  7, OLIVINE_CITY, 4
+	warp_event  3,  7, OLIVINE_CITY, 4
+
+	def_coord_events
+
+	def_bg_events
+	bg_event  0,  1, BGEVENT_JUMPSTD, MAGAZINE_BOOKSHELF_SCRIPT
+	bg_event  1,  1, BGEVENT_JUMPSTD, PICTURE_BOOKSHELF_SCRIPT
+
+	def_object_events
+	object_event  2,  3, SPRITE_POKEFAN_M, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, OlivinePunishmentSpeechHouseDadText, -1
+	object_event  5,  5, SPRITE_LASS, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 2, 0, -1, -1, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, OlivinePunishmentSpeechHouseDaughterText, -1
+
 	object_const_def
 	const OLIVINEPUNISHMENTSPEECHHOUSE_POKEFAN_M
 	const OLIVINEPUNISHMENTSPEECHHOUSE_LASS
@@ -7,24 +22,11 @@ OlivinePunishmentSpeechHouse_MapScripts:
 
 	def_callbacks
 
-OlivinePunishmentSpeechHouseDad:
-	jumptextfaceplayer OlivinePunishmentSpeechHouseDadText
-
-OlivinePunishmentSpeechHouseDaughter:
-	jumptextfaceplayer OlivinePunishmentSpeechHouseDaughterText
-
-OlivinePunishmentSpeechHouseBookshelf2:
-	jumpstd PictureBookshelfScript
-
-OlivinePunishmentSpeechHouseBookshelf1:
-	jumpstd MagazineBookshelfScript
-
 OlivinePunishmentSpeechHouseDadText:
 	text "Along the way to"
-	line "CIANWOOD, there"
-
-	para "are four deserted"
-	line "islands."
+	line "Cianwood, there"
+	cont "are four deserted"
+	cont "islands."
 
 	para "Bad kids are taken"
 	line "to the islands as"
@@ -36,18 +38,3 @@ OlivinePunishmentSpeechHouseDaughterText:
 	line "trouble, Daddy"
 	cont "always scares me."
 	done
-
-OlivinePunishmentSpeechHouse_MapEvents:
-	def_warp_events
-	warp_event  2,  7, OLIVINE_CITY, 4
-	warp_event  3,  7, OLIVINE_CITY, 4
-
-	def_coord_events
-
-	def_bg_events
-	bg_event  0,  1, BGEVENT_READ, OlivinePunishmentSpeechHouseBookshelf1
-	bg_event  1,  1, BGEVENT_READ, OlivinePunishmentSpeechHouseBookshelf2
-
-	def_object_events
-	object_event  1,  2, SPRITE_POKEFAN_M, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, OlivinePunishmentSpeechHouseDad, -1
-	object_event  5,  5, SPRITE_LASS, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 2, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, OlivinePunishmentSpeechHouseDaughter, -1
