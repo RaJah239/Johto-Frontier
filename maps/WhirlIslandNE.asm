7229c99e737dd13179ebfe2f84b40e0c7e8a2cbd
RaJah239 <ravijagroo239@gmail.com>
2026-03-10 17:02:56 -0400

    Update maps/WhirlIslandNE.asm

--- maps/WhirlIslandNE.asm
+++ maps/WhirlIslandNE.asm
@@ -1,14 +1,3 @@
-	object_const_def
-	const WHIRLISLANDNE_POKE_BALL
-
-WhirlIslandNE_MapScripts:
-	def_scene_scripts
-
-	def_callbacks
-
-WhirlIslandNEUltraBall:
-	itemball ULTRA_BALL
-
 WhirlIslandNE_MapEvents:
 	def_warp_events
 	warp_event  3, 13, ROUTE_13, 2
@@ -21,3 +10,14 @@ WhirlIslandNE_MapEvents:
 
 	def_object_events
 	object_event 11, 11, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, WhirlIslandNEUltraBall, EVENT_WHIRL_ISLAND_NE_ULTRA_BALL
+
+	object_const_def
+	const WHIRLISLANDNE_POKE_BALL
+
+WhirlIslandNE_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+WhirlIslandNEUltraBall:
+	itemball ULTRA_BALL
