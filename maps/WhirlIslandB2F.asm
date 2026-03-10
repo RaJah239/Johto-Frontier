f6dd978a4de3ab8c554711fb57c28cb3b3a6e213
RaJah239 <ravijagroo239@gmail.com>
2026-03-10 17:01:50 -0400

    Update maps/WhirlIslandB2F.asm

--- maps/WhirlIslandB2F.asm
+++ maps/WhirlIslandB2F.asm
@@ -1,22 +1,3 @@
-	object_const_def
-	const WHIRLISLANDB2F_POKE_BALL1
-	const WHIRLISLANDB2F_POKE_BALL2
-	const WHIRLISLANDB2F_POKE_BALL3
-
-WhirlIslandB2F_MapScripts:
-	def_scene_scripts
-
-	def_callbacks
-
-WhirlIslandB2FFullRestore:
-	itemball FULL_RESTORE
-
-WhirlIslandB2FMaxRevive:
-	itemball MAX_REVIVE
-
-WhirlIslandB2FMaxElixer:
-	itemball MAX_ELIXER
-
 WhirlIslandB2F_MapEvents:
 	def_warp_events
 	warp_event 11,  5, WHIRL_ISLAND_B1F, 7
@@ -32,3 +13,20 @@ WhirlIslandB2F_MapEvents:
 	object_event 10, 11, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, WhirlIslandB2FFullRestore, EVENT_WHIRL_ISLAND_B2F_FULL_RESTORE
 	object_event  6,  4, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, WhirlIslandB2FMaxRevive, EVENT_WHIRL_ISLAND_B2F_MAX_REVIVE
 	object_event  5, 12, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, WhirlIslandB2FMaxElixer, EVENT_WHIRL_ISLAND_B2F_MAX_ELIXER
+
+	object_const_def
+	const WHIRLISLANDB2F_POKE_BALL1
+	const WHIRLISLANDB2F_POKE_BALL2
+	const WHIRLISLANDB2F_POKE_BALL3
+
+WhirlIslandB2F_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+WhirlIslandB2FFullRestore:
+	itemball FULL_RESTORE
+WhirlIslandB2FMaxRevive:
+	itemball MAX_REVIVE
+WhirlIslandB2FMaxElixer:
+	itemball MAX_ELIXER
