d7a4b8f13eb82079268d0b2d30e68617e3a8959a
RaJah239 <ravijagroo239@gmail.com>
2026-03-10 13:08:17 -0400

    Update maps/OlivineLighthouse2F.asm

--- maps/OlivineLighthouse2F.asm
+++ maps/OlivineLighthouse2F.asm
@@ -1,3 +1,20 @@
+OlivineLighthouse2F_MapEvents:
+	def_warp_events
+	warp_event  3, 11, OLIVINE_LIGHTHOUSE_1F, 3
+	warp_event  5,  3, OLIVINE_LIGHTHOUSE_3F, 2
+	warp_event 16, 13, OLIVINE_LIGHTHOUSE_1F, 4
+	warp_event 17, 13, OLIVINE_LIGHTHOUSE_1F, 5
+	warp_event 16, 11, OLIVINE_LIGHTHOUSE_3F, 4
+	warp_event 17, 11, OLIVINE_LIGHTHOUSE_3F, 5
+
+	def_coord_events
+
+	def_bg_events
+
+	def_object_events
+	object_event  9,  3, SPRITE_SAILOR, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 3, TrainerSailorHuey, -1
+	object_event 17,  8, SPRITE_GENTLEMAN, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 3, TrainerGentlemanAlfred, -1
+
 	object_const_def
 	const OLIVINELIGHTHOUSE2F_SAILOR
 	const OLIVINELIGHTHOUSE2F_GENTLEMAN
@@ -8,15 +25,23 @@ OlivineLighthouse2F_MapScripts:
 	def_callbacks
 
 TrainerGentlemanAlfred:
-	trainer GENTLEMAN, ALFRED, EVENT_BEAT_GENTLEMAN_ALFRED, GentlemanAlfredSeenText, GentlemanAlfredBeatenText, 0, .Script
+	trainer GENTLEMAN, ALFRED, EVENT_BEAT_GENTLEMAN_ALFRED,.SeenText, .BeatenText
 
-.Script:
-	endifjustbattled
-	opentext
-	writetext GentlemanAlfredAfterBattleText
-	waitbutton
-	closetext
-	end
+.AfterText
+	text "Up top is a #-"
+	line "mon that keeps the"
+	cont "Lighthouse lit."
+	done
+
+.SeenText
+	text "Hm? This is no"
+	line "place for playing."
+	done
+
+.BeatenText
+	text "Ah! I can see that"
+	line "you're serious."
+	done
 
 TrainerSailorHuey:
 	trainer SAILOR, HUEY1, EVENT_BEAT_SAILOR_HUEY, SailorHueySeenText, SailorHueyBeatenText, 0, .Script
@@ -132,27 +157,7 @@ SailorHueyBeatenText:
 	line "I lose!"
 	done
 
-GentlemanAlfredSeenText:
-	text "Hm? This is no"
-	line "place for playing."
-	done
 
-GentlemanAlfredBeatenText:
-	text "Ah! I can see that"
-	line "you're serious."
-	done
-
-GentlemanAlfredAfterBattleText:
-	text "Up top is a #-"
-	line "MON that keeps the"
-	cont "LIGHTHOUSE lit."
-
-	para "But I hear that"
-	line "it's sick now and"
-
-	para "can't be cured by"
-	line "ordinary medicine."
-	done
 
 SailorHueyGiveProteinText:
 	text "Man! You're as"
@@ -168,19 +173,3 @@ SailorHueyGiveProteinAgainText:
 	para "Made space right?"
 	done
 
-OlivineLighthouse2F_MapEvents:
-	def_warp_events
-	warp_event  3, 11, OLIVINE_LIGHTHOUSE_1F, 3
-	warp_event  5,  3, OLIVINE_LIGHTHOUSE_3F, 2
-	warp_event 16, 13, OLIVINE_LIGHTHOUSE_1F, 4
-	warp_event 17, 13, OLIVINE_LIGHTHOUSE_1F, 5
-	warp_event 16, 11, OLIVINE_LIGHTHOUSE_3F, 4
-	warp_event 17, 11, OLIVINE_LIGHTHOUSE_3F, 5
-
-	def_coord_events
-
-	def_bg_events
-
-	def_object_events
-	object_event  9,  3, SPRITE_SAILOR, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 3, TrainerSailorHuey, -1
-	object_event 17,  8, SPRITE_GENTLEMAN, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 3, TrainerGentlemanAlfred, -1
