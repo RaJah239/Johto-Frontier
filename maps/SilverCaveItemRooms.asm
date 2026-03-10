74ea45a9a65be7a8e58f2380ecf208af630b20c5
RaJah239 <ravijagroo239@gmail.com>
2026-03-10 10:59:58 -0400

    Update maps/SilverCaveItemRooms.asm

--- maps/SilverCaveItemRooms.asm
+++ maps/SilverCaveItemRooms.asm
@@ -1,18 +1,3 @@
-	object_const_def
-	const SILVERCAVEITEMROOMS_POKE_BALL1
-	const SILVERCAVEITEMROOMS_POKE_BALL2
-
-SilverCaveItemRooms_MapScripts:
-	def_scene_scripts
-
-	def_callbacks
-
-SilverCaveItemRoomsMaxRevive:
-	itemball MAX_REVIVE
-
-SilverCaveItemRoomsFullRestore:
-	itemball FULL_RESTORE
-
 SilverCaveItemRooms_MapEvents:
 	def_warp_events
 	warp_event 13,  3, SILVER_CAVE_ROOM_2, 3
@@ -25,3 +10,17 @@ SilverCaveItemRooms_MapEvents:
 	def_object_events
 	object_event  6,  3, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, SilverCaveItemRoomsMaxRevive, EVENT_SILVER_CAVE_ITEM_ROOMS_MAX_REVIVE
 	object_event 15, 11, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, SilverCaveItemRoomsFullRestore, EVENT_SILVER_CAVE_ITEM_ROOMS_FULL_RESTORE
+
+	object_const_def
+	const SILVERCAVEITEMROOMS_POKE_BALL1
+	const SILVERCAVEITEMROOMS_POKE_BALL2
+
+SilverCaveItemRooms_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+SilverCaveItemRoomsMaxRevive:
+	itemball MAX_REVIVE
+SilverCaveItemRoomsFullRestore:
+	itemball FULL_RESTORE
