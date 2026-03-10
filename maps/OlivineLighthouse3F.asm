27642d5ca45902abf60b56196294a132ebcf70e3
RaJah239 <ravijagroo239@gmail.com>
2026-03-10 13:29:31 -0400

    Update maps/OlivineLighthouse3F.asm

--- maps/OlivineLighthouse3F.asm
+++ maps/OlivineLighthouse3F.asm
@@ -1,119 +1,3 @@
-	object_const_def
-	const OLIVINELIGHTHOUSE3F_SAILOR
-	const OLIVINELIGHTHOUSE3F_GENTLEMAN
-	const OLIVINELIGHTHOUSE3F_YOUNGSTER
-	const OLIVINELIGHTHOUSE3F_POKE_BALL
-	const OLIVINELIGHTHOUSE3F_CHANSEY
-
-OlivineLighthouse3F_MapScripts:
-	def_scene_scripts
-
-	def_callbacks
-
-TrainerBirdKeeperTheo:
-	trainer BIRD_KEEPER, THEO, EVENT_BEAT_BIRD_KEEPER_THEO, BirdKeeperTheoSeenText, BirdKeeperTheoBeatenText, 0, .Script
-
-.Script:
-	endifjustbattled
-	opentext
-	writetext BirdKeeperTheoAfterBattleText
-	waitbutton
-	closetext
-	end
-
-TrainerGentlemanPreston:
-	trainer GENTLEMAN, PRESTON, EVENT_BEAT_GENTLEMAN_PRESTON, GentlemanPrestonSeenText, GentlemanPrestonBeatenText, 0, .Script
-
-.Script:
-	endifjustbattled
-	opentext
-	writetext GentlemanPrestonAfterBattleText
-	waitbutton
-	closetext
-	end
-
-TrainerSailorTerrell:
-	trainer SAILOR, TERRELL, EVENT_BEAT_SAILOR_TERRELL, SailorTerrellSeenText, SailorTerrellBeatenText, 0, .Script
-
-.Script:
-	endifjustbattled
-	opentext
-	writetext SailorTerrellAfterBattleText
-	waitbutton
-	closetext
-	end
-
-OlivineLighthouse3FEther:
-	itemball ETHER
-
-BirdKeeperTheoSeenText:
-	text "Why are you here?"
-	line "Are you just going"
-
-	para "to gawk? I suggest"
-	line "that you leave!"
-	done
-
-BirdKeeperTheoBeatenText:
-	text "You really are"
-	line "concerned…"
-	done
-
-BirdKeeperTheoAfterBattleText:
-	text "How the heck do"
-	line "you go up?"
-
-	para "I want to visit"
-	line "the sick #MON,"
-
-	para "but I can't get up"
-	line "there…"
-	done
-
-SailorTerrellSeenText:
-	text "Sailors are both"
-	line "kind and strong."
-	cont "How about you?"
-	done
-
-SailorTerrellBeatenText:
-	text "You are both kind"
-	line "and strong…"
-	done
-
-SailorTerrellAfterBattleText:
-	text "Every time I come"
-	line "back to OLIVINE, I"
-	cont "visit the GYM."
-
-	para "The GYM LEADER's"
-	line "#MON type has"
-
-	para "changed without me"
-	line "noticing."
-	done
-
-GentlemanPrestonSeenText:
-	text "I travel the world"
-	line "to train my #-"
-	cont "MON. I wish to"
-	cont "battle with you."
-	done
-
-GentlemanPrestonBeatenText:
-	text "…sigh… I must"
-	line "train some more…"
-	done
-
-GentlemanPrestonAfterBattleText:
-	text "JASMINE used to"
-	line "use rock #MON"
-	cont "like ONIX."
-	done
-
-OlivineLightHouseChanseyScript:
-	jumpstd ChanseyHealsOWScript
-
 OlivineLighthouse3F_MapEvents:
 	def_warp_events
 	warp_event 13,  3, OLIVINE_LIGHTHOUSE_4F, 1
@@ -131,8 +15,89 @@ OlivineLighthouse3F_MapEvents:
 	def_bg_events
 
 	def_object_events
-	object_event  9,  2, SPRITE_SAILOR, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 1, TrainerSailorTerrell, -1
-	object_event 13,  5, SPRITE_GENTLEMAN, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 4, TrainerGentlemanPreston, -1
-	object_event  3,  9, SPRITE_YOUNGSTER, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 3, TrainerBirdKeeperTheo, -1
+	chanseyheal_event 13, 13
+	object_event  9,  2, SPRITE_SAILOR, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_GENERICTRAINER, 1, TrainerSailorTerrell, -1
+	object_event 13,  5, SPRITE_GENTLEMAN, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_GENERICTRAINER, 3, TrainerGentlemanPreston, -1
+	object_event  3,  9, SPRITE_YOUNGSTER, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_GENERICTRAINER, 3, TrainerBirdKeeperTheo, -1
 	object_event  8,  2, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, OlivineLighthouse3FEther, EVENT_OLIVINE_LIGHTHOUSE_3F_ETHER
-	object_event 13, 13, SPRITE_CHANSEY_OW, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, OlivineLightHouseChanseyScript, -1
+
+	object_const_def
+	const OLIVINELIGHTHOUSE3F_CHANSEY
+	const OLIVINELIGHTHOUSE3F_SAILOR
+	const OLIVINELIGHTHOUSE3F_GENTLEMAN
+	const OLIVINELIGHTHOUSE3F_YOUNGSTER
+	const OLIVINELIGHTHOUSE3F_POKE_BALL
+
+OlivineLighthouse3F_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+TrainerBirdKeeperTheo:
+	generictrainer BIRD_KEEPER, THEO, EVENT_BEAT_BIRD_KEEPER_THEO, .SeenText, .BeatenText
+
+.AfterText
+	text "That helped me as"
+	line "well."
+	done
+
+.SeenText
+	text "You're here for"
+	line "training too?"
+
+	para "Let's help each-"
+	line "other out!"
+	done
+
+.BeatenText
+	text "Thank you!"
+	done
+
+TrainerGentlemanPreston:
+	generictrainer GENTLEMAN, PRESTON, EVENT_BEAT_GENTLEMAN_PRESTON, .SeenText, .BeatenText
+
+.AfterText
+	text "Jasmine used to"
+	line "use rock #mon"
+	cont "like Onix."
+	done
+
+.SeenText
+	text "I travel the world"
+	line "to train my #-"
+	cont "mon. I wish to"
+	cont "battle with you."
+	done
+
+.BeatenText
+	text "…sigh… I must"
+	line "train some more…"
+	done
+
+TrainerSailorTerrell:
+	generictrainer SAILOR, TERRELL, EVENT_BEAT_SAILOR_TERRELL, .SeenText, .BeatenText
+
+.AfterText
+	text "Every time I come"
+	line "back to Olivine, I"
+	cont "visit the Gym."
+
+	para "The Gym Leader's"
+	line "#mon type has"
+	cont "changed without me"
+	cont "noticing."
+	done
+
+.SeenText
+	text "Sailors are both"
+	line "kind and strong."
+	cont "How about you?"
+	done
+
+.BeatenText
+	text "You are both kind"
+	line "and strong…"
+	done
+
+OlivineLighthouse3FEther:
+	itemball ETHER
