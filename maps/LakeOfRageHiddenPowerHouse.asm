1ad645d35b13f677badc00fda1fee5e90ed01276
RaJah239 <ravijagroo239@gmail.com>
2026-03-11 19:49:27 -0400

    Update maps/LakeOfRageHiddenPowerHouse.asm

--- maps/LakeOfRageHiddenPowerHouse.asm
+++ maps/LakeOfRageHiddenPowerHouse.asm
@@ -1,3 +1,19 @@
+LakeOfRageHiddenPowerHouse_MapEvents:
+	def_warp_events
+	warp_event  2,  7, LAKE_OF_RAGE, 2
+	warp_event  3,  7, LAKE_OF_RAGE, 2
+
+	def_coord_events
+
+	def_bg_events
+	bg_event  0,  1, BGEVENT_JUMPSTD, DIFFICULT_BOOKSHELF_SCRIPT
+	bg_event  1,  1, BGEVENT_JUMPSTD, DIFFICULT_BOOKSHELF_SCRIPT
+
+	def_object_events
+	object_event  2,  4, SPRITE_FISHER, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, HiddenPowerGuy, -1
+	object_event  3,  4, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, LakeOfRageHiddenPowerHouseTMHiddenPower, EVENT_TM_HIDDEN_POWER
+	object_event  4,  4, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, LakeOfRageHiddenPowerHouseTMHiddenForce, EVENT_TM_HIDDEN_FORCE
+
 	object_const_def
 	const LAKEOFRAGEHIDDENPOWERHOUSE_FISHER
 	const LAKEOFRAGEHIDDENPOWERHOUSE_TM_HIDDEN_POWER
@@ -9,81 +25,51 @@ LakeOfRageHiddenPowerHouse_MapScripts:
 	def_callbacks
 
 HiddenPowerGuy:
-	faceplayer
-	opentext
 	checkevent EVENT_MET_HIDDEN_POWER_GUY
 	iftrue .AlreadyMet
 	setevent EVENT_MET_HIDDEN_POWER_GUY
-	writetext HiddenPowerGuyText1
-	waitendtext
+	jumpthistextfaceplayer
+		text "…You have strayed"
+		line "far…"
+
+		para "Here I have medi-"
+		line "tated. Inside me,"
+		cont "a new power has"
+		cont "been awakened."
+
+		para "Let me share my"
+		line "power with your"
+		cont "#mon."
+
+		para "Take both from the"
+		line "table."
+
+		para "Do you see them?"
+
+		para "Hidden Power and"
+		line "Hidden Force!"
+
+		para "They draw out the"
+		line "core trait of"
+		cont "#mon to attack."
+
+		para "Remember this: its"
+		line "type depends on"
+		cont "the #mon using"
+		cont "it and can't be"
+		cont "Fairy or Normal."
+		done
 
 .AlreadyMet:
-	writetext HiddenPowerGuyText2
-	waitendtext
-
-HiddenPowerGuyText1:
-	text "…You have strayed"
-	line "far…"
-
-	para "Here I have medi-"
-	line "tated. Inside me,"
-
-	para "a new power has"
-	line "been awakened."
-
-	para "Let me share my"
-	line "power with your"
-
-	para "#mon. Take"
-	line "them both from the"
-	cont "table."
-
-	para "Do you see them?"
-	line "Hidden Power and"
-	cont "Hidden Force!"
-
-	para "They draw out the"
-	line "core trait of"
-	cont "#mon to attack."
-
-	para "Remember this: its"
-	line "type and power de-"
-	cont "pend on the #-"
-	cont "mon using it"
-	cont "and can't be either"
-	cont "Fairy or Normal."
-	done
-
-HiddenPowerGuyText2:
-	text "Remember this: its"
-	line "type and power de-"
-	cont "pend on the #-"
-	cont "mon using it"
-	cont "and can't be either"
-	cont "Fairy or Normal."
-	done
+	jumpthistextfaceplayer
+		text "Remember this: its"
+		line "type depends on"
+		cont "the #mon using"
+		cont "it and can't be"
+		cont "Fairy or Normal."
+		done
 
 LakeOfRageHiddenPowerHouseTMHiddenPower:
 	itemball TM_HIDDEN_POWER
-
 LakeOfRageHiddenPowerHouseTMHiddenForce:
 	itemball TM_HIDDEN_FORCE
-
-HiddenPowerHouseBookshelf:
-	jumpstd DifficultBookshelfScript
-
-LakeOfRageHiddenPowerHouse_MapEvents:
-	def_warp_events
-	warp_event  2,  7, LAKE_OF_RAGE, 2
-	warp_event  3,  7, LAKE_OF_RAGE, 2
-
-	def_coord_events
-
-	def_bg_events
-	bg_event  0,  1, BGEVENT_READ, HiddenPowerHouseBookshelf
-	bg_event  1,  1, BGEVENT_READ, HiddenPowerHouseBookshelf
-
-	def_object_events
-	object_event  2,  4, SPRITE_FISHER, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, HiddenPowerGuy, -1
-	object_event  3,  4, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, LakeOfRageHiddenPowerHouseTMHiddenPower, EVENT_TM_HIDDEN_POWER
-	object_event  4,  4, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, LakeOfRageHiddenPowerHouseTMHiddenForce, EVENT_TM_HIDDEN_FORCE
