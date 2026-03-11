18c690c4904ba2e27ec2a41100b25ed488f7eebf
RaJah239 <ravijagroo239@gmail.com>
2026-03-11 09:55:50 -0400

    Update maps/MountMortarGiftShop.asm

--- maps/MountMortarGiftShop.asm
+++ maps/MountMortarGiftShop.asm
@@ -1,30 +1,3 @@
-	object_const_def
-	const MOUNTMORTARGIFTSHOP_GRAMPS1 ; morning only
-	const MOUNTMORTARGIFTSHOP_GRAMPS2 ; day only
-	const MOUNTMORTARGIFTSHOP_LASS1 ; morning only
-	const MOUNTMORTARGIFTSHOP_LASS2 ; day only
-
-MountMortarGiftShop_MapScripts:
-	def_scene_scripts
-
-	def_callbacks
-
-MountMortarGiftShopClerkScript:
-	faceplayer
-	opentext
-	pokemart MARTTYPE_STANDARD, MART_MT_MOON
-	closetext
-	end
-
-MountMortarGiftShopLassScript:
-	jumptextfaceplayer MountMortarGiftShopLassText
-
-MountMortarGiftShopLassText:
-	text "When the sun goes"
-	line "down, CLEFAIRY"
-	cont "come out to play."
-	done
-
 MountMortarGiftShop_MapEvents:
 	def_warp_events
 	warp_event  3,  7, MT_MORTAR_SQUARE, 1
@@ -37,5 +10,27 @@ MountMortarGiftShop_MapEvents:
 	def_object_events
 	object_event  4,  3, SPRITE_GRAMPS, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, MORN, 0, OBJECTTYPE_SCRIPT, 0, MountMortarGiftShopClerkScript, -1
 	object_event  1,  2, SPRITE_GRAMPS, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, DAY, 0, OBJECTTYPE_SCRIPT, 0, MountMortarGiftShopClerkScript, -1
-	object_event  1,  6, SPRITE_LASS, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 1, 0, -1, MORN, 0, OBJECTTYPE_SCRIPT, 0, MountMortarGiftShopLassScript, -1
-	object_event  5,  4, SPRITE_LASS, SPRITEMOVEDATA_WALK_UP_DOWN, 0, 1, -1, DAY, 0, OBJECTTYPE_SCRIPT, 0, MountMortarGiftShopLassScript, -1
+	object_event  1,  6, SPRITE_LASS, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 1, 0, -1, MORN, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, MountMortarGiftShopLassText, -1
+	object_event  5,  4, SPRITE_LASS, SPRITEMOVEDATA_WALK_UP_DOWN, 0, 1, -1, DAY, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, MountMortarGiftShopLassText, -1
+
+	object_const_def
+	const MOUNTMORTARGIFTSHOP_GRAMPS1 ; morning only
+	const MOUNTMORTARGIFTSHOP_GRAMPS2 ; day only
+	const MOUNTMORTARGIFTSHOP_LASS1 ; morning only
+	const MOUNTMORTARGIFTSHOP_LASS2 ; day only
+
+MountMortarGiftShop_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+MountMortarGiftShopClerkScript:
+	faceplayeropentext
+	pokemart MARTTYPE_STANDARD, MART_MT_MOON
+	endtext
+
+MountMortarGiftShopLassText:
+	text "When the sun goes"
+	line "down, Clefairy"
+	cont "come out to play."
+	done
