1132a4c0422c456c0ccc0b0a3bf6b590b54aa3ae
RaJah239 <ravijagroo239@gmail.com>
2026-03-11 15:17:11 -0400

    Update hideout 2f and 3f

--- data/tilesets/facility_collision.asm
+++ data/tilesets/facility_collision.asm
@@ -62,3 +62,5 @@
 	tilecoll WALL, WALL, FLOOR, WALL ; 3d
 	tilecoll WALL, FLOOR, WALL, WALL ; 3e
 	tilecoll FLOOR, COUNTER, COUNTER, COUNTER ; 3f
+	tilecoll FLOOR, FLOOR, WALL, WALL ; 40
+	tilecoll FLOOR, FLOOR, WALL, WALL ; 41
