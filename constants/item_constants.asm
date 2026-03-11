81f4bb19839ad5eef0eb5f7f7654d34c88a053a4
RaJah239 <ravijagroo239@gmail.com>
2026-03-10 20:29:18 -0400

    Update maps/CianwoodPharmacy.asm

--- constants/item_constants.asm
+++ constants/item_constants.asm
@@ -72,7 +72,7 @@
 	const MAX_ETHER    ; 40
 	const ELIXER       ; 41
 	const RED_SCALE    ; 42 ; unused, replace this
-	const SECRETPOTION ; 43
+	const SECRETPOTION ; 43 ; unused, replace this
 	const MAREEP_CALL  ; 44
 	const MYSTERY_EGG  ; 45 ; unused, replace this
 	const CLEAR_BELL   ; 46
--- maps/CianwoodPharmacy.asm
+++ maps/CianwoodPharmacy.asm
@@ -1,63 +1,3 @@
-	object_const_def
-	const CIANWOODPHARMACY_PHARMACIST
-
-CianwoodPharmacy_MapScripts:
-	def_scene_scripts
-	scene_script CianwoodPharmacyNoopScene ; unusable
-
-	def_callbacks
-
-CianwoodPharmacyNoopScene:
-	end
-
-CianwoodPharmacist:
-	faceplayer
-	opentext
-	checkevent EVENT_GOT_SECRETPOTION_FROM_PHARMACY
-	iftrue .Mart
-	checkevent EVENT_JASMINE_EXPLAINED_AMPHYS_SICKNESS
-	iffalse .Mart
-	writetext PharmacistGiveSecretpotionText
-	promptbutton
-	verbosegiveitem SECRETPOTION
-	setevent EVENT_GOT_SECRETPOTION_FROM_PHARMACY
-	writetext PharmacistDescribeSecretpotionText
-	waitbutton
-	closetext
-	end
-
-.Mart:
-	pokemart MARTTYPE_PHARMACY, MART_CIANWOOD
-	closetext
-	end
-
-PharmacistGiveSecretpotionText:
-	text "Your #MON ap-"
-	line "pear to be fine."
-
-	para "Is something wor- "
-	line "rying you?"
-
-	para "…"
-
-	para "The LIGHTHOUSE"
-	line "#MON is in"
-	cont "trouble?"
-
-	para "I got it!"
-
-	para "This ought to do"
-	line "the trick."
-	done
-
-PharmacistDescribeSecretpotionText:
-	text "My SECRETPOTION is"
-	line "a tad too strong."
-
-	para "I only offer it in"
-	line "an emergency."
-	done
-
 CianwoodPharmacy_MapEvents:
 	def_warp_events
 	warp_event  2,  7, CIANWOOD_CITY, 4
@@ -71,3 +11,16 @@ CianwoodPharmacy_MapEvents:
 
 	def_object_events
 	object_event  2,  3, SPRITE_PHARMACIST, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, CianwoodPharmacist, -1
+
+	object_const_def
+	const CIANWOODPHARMACY_PHARMACIST
+
+CianwoodPharmacy_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+CianwoodPharmacist:
+	faceplayeropentext
+	pokemart MARTTYPE_PHARMACY, MART_CIANWOOD
+	endtext
