e252151c335ec80a4329646e73edd7c0ca4fd923
RaJah239 <ravijagroo239@gmail.com>
2026-03-11 10:47:22 -0400

    Update maps/MahoganyRedGyaradosSpeechHouse.asm

--- maps/MahoganyRedGyaradosSpeechHouse.asm
+++ maps/MahoganyRedGyaradosSpeechHouse.asm
@@ -1,57 +1,3 @@
-	object_const_def
-	const MAHOGANYREDGYARADOSSPEECHHOUSE_BLACK_BELT
-	const MAHOGANYREDGYARADOSSPEECHHOUSE_TEACHER
-
-MahoganyRedGyaradosSpeechHouse_MapScripts:
-	def_scene_scripts
-
-	def_callbacks
-
-MahoganyRedGyaradosSpeechHouseBlackBeltScript:
-	jumptextfaceplayer MahoganyRedGyaradosSpeechHouseBlackBeltText
-
-MahoganyRedGyaradosSpeechHouseTeacherScript:
-	faceplayer
-	opentext
-	checkflag ENGINE_ROCKETS_IN_RADIO_TOWER
-	iftrue .RocketsInRadioTower
-	writetext MahoganyRedGyaradosSpeechHouseTeacherText
-	waitbutton
-	closetext
-	end
-
-.RocketsInRadioTower:
-	writetext MahoganyRedGyaradosSpeechHouseTeacherText_RocketsInRadioTower
-	waitbutton
-	closetext
-	end
-
-MahoganyRedGyaradosSpeechHouseBlackBeltText:
-	text "I heard that a red"
-	line "GYARADOS appeared"
-	cont "at the LAKE."
-
-	para "That's odd, since"
-	line "even ordinary"
-
-	para "GYARADOS are rare"
-	line "in that lake…"
-	done
-
-MahoganyRedGyaradosSpeechHouseTeacherText:
-	text "My favorite radio"
-	line "program? I'd say"
-	cont "#MON MUSIC."
-	done
-
-MahoganyRedGyaradosSpeechHouseTeacherText_RocketsInRadioTower:
-	text "I've been hearing"
-	line "laughter on the"
-
-	para "radio…"
-	line "It's creepy."
-	done
-
 MahoganyRedGyaradosSpeechHouse_MapEvents:
 	def_warp_events
 	warp_event  2,  7, MAHOGANY_TOWN, 3
@@ -62,5 +8,24 @@ MahoganyRedGyaradosSpeechHouse_MapEvents:
 	def_bg_events
 
 	def_object_events
-	object_event  2,  3, SPRITE_BLACK_BELT, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, MahoganyRedGyaradosSpeechHouseBlackBeltScript, -1
-	object_event  6,  5, SPRITE_TEACHER, SPRITEMOVEDATA_WALK_UP_DOWN, 0, 1, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, MahoganyRedGyaradosSpeechHouseTeacherScript, -1
+	object_event  2,  3, SPRITE_BLACK_BELT, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, MahoganyRedGyaradosSpeechHouseBlackBeltText, -1
+	object_event  6,  5, SPRITE_TEACHER, SPRITEMOVEDATA_WALK_UP_DOWN, 0, 1, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_COMMAND, jumptextfaceplayer, MahoganyRedGyaradosSpeechHouseTeacherText, -1
+
+	object_const_def
+	const MAHOGANYREDGYARADOSSPEECHHOUSE_BLACK_BELT
+	const MAHOGANYREDGYARADOSSPEECHHOUSE_TEACHER
+
+MahoganyRedGyaradosSpeechHouse_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+MahoganyRedGyaradosSpeechHouseBlackBeltText:
+	text "TODOTEXT"
+	done
+
+MahoganyRedGyaradosSpeechHouseTeacherText:
+	text "My favorite radio"
+	line "program? I'd say"
+	cont "#mon Music."
+	done
