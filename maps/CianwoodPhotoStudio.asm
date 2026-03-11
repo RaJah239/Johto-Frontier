3eb5d26610c1c521ef904de7da72c3d844429a58
RaJah239 <ravijagroo239@gmail.com>
2026-03-10 20:08:04 -0400

    Update maps/CianwoodPhotoStudio.asm

--- maps/CianwoodPhotoStudio.asm
+++ maps/CianwoodPhotoStudio.asm
@@ -1,3 +1,15 @@
+CianwoodPhotoStudio_MapEvents:
+	def_warp_events
+	warp_event  2,  7, CIANWOOD_CITY, 5
+	warp_event  3,  7, CIANWOOD_CITY, 5
+
+	def_coord_events
+
+	def_bg_events
+
+	def_object_events
+	object_event  2,  3, SPRITE_FISHING_GURU, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, CianwoodPhotoStudioFishingGuruScript, -1
+
 	object_const_def
 	const CIANWOODPHOTOSTUDIO_FISHING_GURU
 
@@ -7,22 +19,36 @@ CianwoodPhotoStudio_MapScripts:
 	def_callbacks
 
 CianwoodPhotoStudioFishingGuruScript:
-	faceplayer
-	opentext
+	faceplayeropentext
 	checkflag ENGINE_CIANWOOD_PHOTOGRAPH
 	iftrue .AlreadyDone
-	writetext CianwoodPhotoStudioFishingGuruText_Question
+	writethistext
+		text "You have magnifi-"
+		line "cent #mon with"
+		cont "you."
+
+		para "How about a photo"
+		line "for a souvenir?"
+		done
 	yesorno
 	iffalse .Refused
-	writetext CianwoodPhotoStudioFishingGuruText_Yes
+	writethistext
+		text "OK! Big smile now!"
+
+		para "Which #mon"
+		line "should I photo-"
+		cont "graph?"
+		done
 	promptbutton
 	special CianwoodPhotograph
 	ifequal $0, .NoPicture
 	ifequal $1, .EggPicture
 	setflag ENGINE_CIANWOOD_PHOTOGRAPH
-	writetext PhotoStudioHoldStillText
-	waitbutton
-	closetext
+	writethistext
+		text "All righty. Hold"
+		line "still for a bit."
+		done
+	waitclosetext
 	special FadeOutPalettes
 	special LoadMapPalettes
 	pause 5
@@ -38,65 +64,39 @@ CianwoodPhotoStudioFishingGuruScript:
 	opentext
 	writetext PhotoStudioPrestoText
 	special PlayCurMonCry
-	writetext PhotoStudioComeAgainText
-	waitbutton
-	closetext
-	end
+	jumpthisopenedtext
+		text "Come again, OK?"
+		done
 
 .Refused:
-	writetext CianwoodPhotoStudioFishingGuruText_No
-	waitbutton
-	closetext
-	end
+	jumpthisopenedtext
+		text "Oh, that's too"
+		line "bad. I thought it"
+		cont "would be a great"
+		cont "memento…"
+		done
 
 .AlreadyDone
-	writetext PhotoStudioAlreadyDoneText
-	waitbutton
-	closetext
-	end
+	jumpthisopenedtext
+		text "I've already taken"
+		line "a photo for you"
+		cont "today."
+
+		para "Come back again"
+		line "tomorrow."
+		done
 
 .NoPicture:
-	writetext PhotoStudioNoPictureText
-	waitbutton
-	closetext
-	end
+	jumpthisopenedtext
+		text "Oh, no picture?"
+		line "Come again, OK?"
+		done
 
 .EggPicture:
-	writetext PhotoStudioEggPictureText
-    waitbutton
-	closetext
-	end
-
-CianwoodPhotoStudioFishingGuruText_Question:
-	text "You have magnifi-"
-	line "cent #MON with"
-	cont "you."
-
-	para "How about a photo"
-	line "for a souvenir?"
-	done
-
-CianwoodPhotoStudioFishingGuruText_Yes:
-	text "OK! Big smile now!"
-	done
-
-	para "Which #MON"
-	line "should I photo-"
-	cont "graph?"
-	done
-
-CianwoodPhotoStudioFishingGuruText_No:
-	text "Oh, that's too"
-	line "bad. I thought it"
-
-	para "would be a great"
-	line "memento…"
-	done
-
-PhotoStudioHoldStillText:
-	text "All righty. Hold"
-	line "still for a bit."
-	done
+	jumpthisopenedtext
+		text "An Egg? My talent"
+		line "is worth more…"
+		done
 
 PhotoStudioPrestoText:
 	text "Presto! All done."
@@ -106,38 +106,3 @@ PhotoStudioPrestoText:
 	text ""
 	line "looks happier!"
 	done
-
-PhotoStudioAlreadyDoneText:
-	text "I've already taken"
-	line "a photo for you"
-	cont "today."
-
-	para "Come back again"
-	line "tomorrow."
-	done
-
-PhotoStudioNoPictureText:
-	text "Oh, no picture?"
-	line "Come again, OK?"
-	done
-
-PhotoStudioEggPictureText:
-	text "An Egg? My talent"
-	line "is worth more…"
-	done
-
-PhotoStudioComeAgainText:
-	text "Come again, OK?"
-	done
-
-CianwoodPhotoStudio_MapEvents:
-	def_warp_events
-	warp_event  2,  7, CIANWOOD_CITY, 5
-	warp_event  3,  7, CIANWOOD_CITY, 5
-
-	def_coord_events
-
-	def_bg_events
-
-	def_object_events
-	object_event  2,  3, SPRITE_FISHING_GURU, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, CianwoodPhotoStudioFishingGuruScript, -1
