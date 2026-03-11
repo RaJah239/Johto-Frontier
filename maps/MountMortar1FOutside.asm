d28d2bdd310b110ce61ccee3fcc575c3dd70cee7
RaJah239 <ravijagroo239@gmail.com>
2026-03-11 09:27:11 -0400

    Update maps/MountMortar1FOutside.asm

--- maps/MountMortar1FOutside.asm
+++ maps/MountMortar1FOutside.asm
@@ -1,21 +1,3 @@
-	object_const_def
-	const MOUNTMORTAR1FOUTSIDE_POKE_BALL1
-	const MOUNTMORTAR1FOUTSIDE_POKE_BALL2
-
-MountMortar1FOutside_MapScripts:
-	def_scene_scripts
-
-	def_callbacks
-
-MountMortar1FOutsideEther:
-	itemball ETHER
-
-MountMortar1FOutsideRevive:
-	itemball REVIVE
-
-MountMortar1FOutsideHiddenHyperPotion:
-	hiddenitem HYPER_POTION, EVENT_MOUNT_MORTAR_1F_OUTSIDE_HIDDEN_HYPER_POTION
-
 MountMortar1FOutside_MapEvents:
 	def_warp_events
 	warp_event  3, 33, ROUTE_14, 3
@@ -31,8 +13,22 @@ MountMortar1FOutside_MapEvents:
 	def_coord_events
 
 	def_bg_events
-	bg_event 25, 22, BGEVENT_ITEM, MountMortar1FOutsideHiddenHyperPotion
+	bg_event 25, 22, BGEVENT_ITEM + HYPER_POTION, EVENT_MOUNT_MORTAR_1F_OUTSIDE_HIDDEN_HYPER_POTION
 
 	def_object_events
 	object_event 13, 15, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, MountMortar1FOutsideEther, EVENT_MOUNT_MORTAR_1F_OUTSIDE_ETHER
 	object_event 31, 18, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, MountMortar1FOutsideRevive, EVENT_MOUNT_MORTAR_1F_OUTSIDE_REVIVE
+
+	object_const_def
+	const MOUNTMORTAR1FOUTSIDE_POKE_BALL1
+	const MOUNTMORTAR1FOUTSIDE_POKE_BALL2
+
+MountMortar1FOutside_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+MountMortar1FOutsideEther:
+	itemball ETHER
+MountMortar1FOutsideRevive:
+	itemball REVIVE
