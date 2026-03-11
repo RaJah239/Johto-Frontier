6918d2a4c205f91e19a6461621e006f44dd6c402
RaJah239 <ravijagroo239@gmail.com>
2026-03-11 10:53:31 -0400

    Update maps/MahoganyTradeBackKidHouse.asm

--- maps/MahoganyTradeBackKidHouse.asm
+++ maps/MahoganyTradeBackKidHouse.asm
@@ -1,108 +1,3 @@
-	object_const_def
-	const MAHOGANYTRADEBACKKIDHOUSE_TRADEKID
-
-MahoganyTradeBackKidHouse_MapScripts:
-	def_scene_scripts
-
-	def_callbacks
-
-TradebackKidScript:
-	faceplayer
-	opentext
-	clearevent EVENT_COMPLETED_TRADE_WITH_TRADE_BACK_KID
-	checkevent EVENT_MET_TRADE_KID
-	iftrue .WantToTrade
-	writetext TradebackIntroKidText
-	setevent EVENT_MET_TRADE_KID
-	waitbutton
-.WantToTrade
-	writetext TradebackKidWantToTradeText
-	yesorno
-	iftrue .DoYouHaveASilverLeaf
-	writetext TradebackKidNoTradeText
-	waitbutton
-	closetext
-	turnobject MAHOGANYTRADEBACKKIDHOUSE_TRADEKID, DOWN
-	end
-
-.DoYouHaveASilverLeaf
-	checkitem SILVER_LEAF
-	iftrue .LetsLinkUp
-	writetext TradeBackKidNoSilverLeafText
-	waitbutton
-	closetext
-	turnobject MAHOGANYTRADEBACKKIDHOUSE_TRADEKID, DOWN
-	end
-
-.LetsLinkUp
-	special TradebackNPC
-	checkevent EVENT_COMPLETED_TRADE_WITH_TRADE_BACK_KID
-	iftrue .TakeSilverLeaf
-	waitbutton
-	closetext
-	turnobject MAHOGANYTRADEBACKKIDHOUSE_TRADEKID, DOWN
-	end
-
-.TakeSilverLeaf
-	writetext ThanksForTheSilverLeafText
-	takeitem SILVER_LEAF
-	waitsfx
-	playsound SFX_TRANSACTION
-	waitbutton
-	closetext
-	turnobject MAHOGANYTRADEBACKKIDHOUSE_TRADEKID, DOWN
-	end
-
-TradebackIntroKidText:
-	text "Hello there! I'm"
-	line "the TRADEBACK KID."
-
-	para "Some #MON can"
-	line "only evolve by"
-	cont "being traded."
-
-	para "Your #DEX has"
-	line "this information."
-
-	para "I can trade any"
-	line "#MON you want"
-
-	para "back to you for a"
-	line "SILVER LEAF."
-	done
-
-TradebackKidWantToTradeText:
-	text "Want me to trade"
-	line "back one of your"
-
-	para "own #MON for"
-	line "a SILVER LEAF?"
-	done
-
-TradebackKidNoTradeText:
-	text "Oh, ok then."
-
-	para "Come back if you"
-	line "change your mind."
-	done
-
-TradeBackKidNoSilverLeafText:
-	text "Aww… You don't have"
-	line "a SILVER LEAF…"
-
-	para "Come back when you"
-	line "get one alright?"
-	done
-
-ThanksForTheSilverLeafText:
-	text "And… Done!"
-
-	para "I'll take that"
-	line "SILVER LEAF now."
-
-	para "Swing by anytime."
-	done
-
 MahoganyTradeBackKidHouse_MapEvents:
 	def_warp_events
 	warp_event  2,  7, MAHOGANY_TOWN, 6
@@ -114,3 +9,91 @@ MahoganyTradeBackKidHouse_MapEvents:
 
 	def_object_events
 	object_event  2,  4, SPRITE_GAMEBOY_KID, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, TradebackKidScript, -1
+
+	object_const_def
+	const MAHOGANYTRADEBACKKIDHOUSE_TRADEKID
+
+MahoganyTradeBackKidHouse_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+TradebackKidScript:
+	faceplayeropentext
+	clearevent EVENT_COMPLETED_TRADE_WITH_TRADE_BACK_KID
+	checkevent EVENT_MET_TRADE_KID
+	iftrue .WantToTrade
+	writethistext
+		text "Hello there! I'm"
+		line "the Tradeback Kid."
+
+		para "Some #mon can"
+		line "only evolve by"
+		cont "being traded."
+
+		para "Your #dex has"
+		line "this information."
+
+		para "I can trade any"
+		line "#mon you want"
+		cont "back to you for a"
+		cont "Silver Leaf."
+		done
+	setevent EVENT_MET_TRADE_KID
+	waitbutton
+.WantToTrade
+	writethistext
+		text "Want me to trade"
+		line "back one of your"
+		cont "own #mon for"
+		cont "a Silver Leaf?"
+		done
+	yesorno
+	iftrue .DoYouHaveASilverLeaf
+	writethistext
+		text "Oh, ok then."
+
+		para "Come back if you"
+		line "change your mind."
+		done
+	waitclosetext
+	turnobject MAHOGANYTRADEBACKKIDHOUSE_TRADEKID, DOWN
+	end
+
+.DoYouHaveASilverLeaf
+	checkitem SILVER_LEAF
+	iftrue .LetsLinkUp
+	writethistext
+		text "Aww… You don't have"
+		line "a Silver Leaf…"
+
+		para "Come back when you"
+		line "get one alright?"
+		done
+	waitclosetext
+	turnobject MAHOGANYTRADEBACKKIDHOUSE_TRADEKID, DOWN
+	end
+
+.LetsLinkUp
+	special TradebackNPC
+	checkevent EVENT_COMPLETED_TRADE_WITH_TRADE_BACK_KID
+	iftrue .TakeSilverLeaf
+	waitclosetext
+	turnobject MAHOGANYTRADEBACKKIDHOUSE_TRADEKID, DOWN
+	end
+
+.TakeSilverLeaf
+	writethistext
+		text "And… Done!"
+
+		para "I'll take that"
+		line "Silver Leaf now."
+
+		para "Swing by anytime."
+		done
+	takeitem SILVER_LEAF
+	waitsfx
+	playsound SFX_TRANSACTION
+	waitclosetext
+	turnobject MAHOGANYTRADEBACKKIDHOUSE_TRADEKID, DOWN
+	end
