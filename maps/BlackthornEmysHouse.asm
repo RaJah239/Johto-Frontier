aaf634326edb51abc0b3a43bb4805dbe3f77bae0
RaJah239 <ravijagroo239@gmail.com>
2026-03-12 10:39:05 -0400

    Update maps/BlackthornEmysHouse.asm

--- maps/BlackthornEmysHouse.asm
+++ maps/BlackthornEmysHouse.asm
@@ -1,22 +1,3 @@
-	object_const_def
-	const BLACKTHORNEMYSHOUSE_EMY
-
-BlackthornEmysHouse_MapScripts:
-	def_scene_scripts
-
-	def_callbacks
-
-Emy:
-	faceplayer
-	opentext
-	trade NPC_TRADE_EMY
-	waitbutton
-	closetext
-	end
-
-EmysHouseBookshelf:
-	jumpstd MagazineBookshelfScript
-
 BlackthornEmysHouse_MapEvents:
 	def_warp_events
 	warp_event  2,  7, BLACKTHORN_CITY, 4
@@ -25,8 +6,21 @@ BlackthornEmysHouse_MapEvents:
 	def_coord_events
 
 	def_bg_events
-	bg_event  0,  1, BGEVENT_READ, EmysHouseBookshelf
-	bg_event  1,  1, BGEVENT_READ, EmysHouseBookshelf
+	bg_event  0,  1, BGEVENT_JUMPSTD, MAGAZINE_BOOKSHELF_SCRIPT
+	bg_event  1,  1, BGEVENT_JUMPSTD, MAGAZINE_BOOKSHELF_SCRIPT
 
 	def_object_events
-	object_event  2,  3, SPRITE_LASS, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, Emy, -1
+	object_event  2,  3, SPRITE_LASS, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, EmyTradeScript, -1
+
+	object_const_def
+	const BLACKTHORNEMYSHOUSE_EMY
+
+BlackthornEmysHouse_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+EmyTradeScript:
+	faceplayeropentext
+	trade NPC_TRADE_EMY
+	waitendtext
