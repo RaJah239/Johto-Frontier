06ba6cf0033af040416dc8ff350d90421ac45437
RaJah239 <ravijagroo239@gmail.com>
2026-03-12 15:26:57 -0400

    Update maps/VictoryRoadGate.asm

--- maps/VictoryRoadGate.asm
+++ maps/VictoryRoadGate.asm
@@ -1,72 +1,3 @@
-	object_const_def
-	const VICTORYROADGATE_OFFICER
-
-VictoryRoadGate_MapScripts:
-	def_scene_scripts
-	scene_script VictoryRoadGateNoop1Scene, SCENE_VICTORYROADGATE_BADGE_CHECK
-	scene_script VictoryRoadGateNoop2Scene, SCENE_VICTORYROADGATE_NOOP
-
-	def_callbacks
-
-VictoryRoadGateNoop1Scene:
-	end
-
-VictoryRoadGateNoop2Scene:
-	end
-
-VictoryRoadGateBadgeCheckScript:
-	turnobject PLAYER, LEFT
-	sjump _VictoryRoadGateBadgeCheckScript
-
-VictoryRoadGateOfficerScript:
-	faceplayer
-_VictoryRoadGateBadgeCheckScript:
-	opentext
-	writetext VictoryRoadGateOfficerText
-	promptbutton
-	readvar VAR_BADGES
-	ifgreater NUM_JOHTO_BADGES - 1, .AllEightBadges
-	writetext VictoryRoadGateNotEnoughBadgesText
-	waitbutton
-	closetext
-	applymovement PLAYER, VictoryRoadGateStepDownMovement
-	end
-
-.AllEightBadges:
-	writetext VictoryRoadGateEightBadgesText
-	waitbutton
-	closetext
-	setscene SCENE_VICTORYROADGATE_NOOP
-	end
-
-VictoryRoadGateStepDownMovement:
-	step DOWN
-	step_end
-
-VictoryRoadGateOfficerText:
-	text "Only trainers who"
-	line "have proven them-"
-	cont "selves may pass."
-	done
-
-VictoryRoadGateNotEnoughBadgesText:
-	text "You don't have all"
-	line "the GYM BADGES of"
-	cont "JOHTO."
-
-	para "I'm sorry, but I"
-	line "can't let you go"
-	cont "through."
-	done
-
-VictoryRoadGateEightBadgesText:
-	text "Oh! The eight"
-	line "BADGES of JOHTO!"
-
-	para "Please, go right"
-	line "on through!"
-	done
-
 VictoryRoadGate_MapEvents:
 	def_warp_events
 	warp_event  5, 17, ROUTE_20, 2
@@ -81,3 +12,60 @@ VictoryRoadGate_MapEvents:
 
 	def_object_events
 	object_event  4, 11, SPRITE_OFFICER, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, VictoryRoadGateOfficerScript, -1
+
+	object_const_def
+	const VICTORYROADGATE_OFFICER
+
+VictoryRoadGate_MapScripts:
+	def_scene_scripts
+	scene_script VictoryRoadGateNoop1Scene, SCENE_VICTORYROADGATE_BADGE_CHECK
+	scene_script VictoryRoadGateNoop2Scene, SCENE_VICTORYROADGATE_NOOP
+
+	def_callbacks
+
+VictoryRoadGateNoop1Scene:
+VictoryRoadGateNoop2Scene:
+	end
+
+VictoryRoadGateBadgeCheckScript:
+	turnobject PLAYER, LEFT
+	sjump _VictoryRoadGateBadgeCheckScript
+
+VictoryRoadGateOfficerScript:
+	faceplayer
+_VictoryRoadGateBadgeCheckScript:
+	opentext
+	writethistext
+		text "Only trainers who"
+		line "have proven them-"
+		cont "selves may pass."
+		done
+	promptbutton
+	readvar VAR_BADGES
+	ifgreater NUM_JOHTO_BADGES - 1, .AllEightBadges
+	writethistext
+		text "You don't have all"
+		line "the Gym Badges of"
+		cont "Johto."
+
+		para "I'm sorry, but I"
+		line "can't let you go"
+		cont "through."
+		done
+	waitclosetext
+	applymovement PLAYER, VictoryRoadGateStepDownMovement
+	end
+
+.AllEightBadges:
+	setscene SCENE_VICTORYROADGATE_NOOP
+	jumpthisopenedtext
+		text "Oh! The eight"
+		line "Badges of Johto!"
+
+		para "Please, go right"
+		line "on through!"
+		done
+
+VictoryRoadGateStepDownMovement:
+	step DOWN
+	step_end
