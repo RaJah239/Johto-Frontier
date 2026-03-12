004e4868f68b8a1ba0c518aa2fe32288f2e9d65d
RaJah239 <ravijagroo239@gmail.com>
2026-03-12 14:05:55 -0400

    Update maps/TohjoFalls.asm

--- maps/TohjoFalls.asm
+++ maps/TohjoFalls.asm
@@ -1,14 +1,3 @@
-	object_const_def
-	const TOHJOFALLS_POKE_BALL
-
-TohjoFalls_MapScripts:
-	def_scene_scripts
-
-	def_callbacks
-
-TohjoFallsMoonStone:
-	itemball MOON_STONE
-
 TohjoFalls_MapEvents:
 	def_warp_events
 	warp_event 13, 15, ROUTE_19, 2
@@ -20,3 +9,14 @@ TohjoFalls_MapEvents:
 
 	def_object_events
 	object_event  2,  6, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, TohjoFallsMoonStone, EVENT_TOHJO_FALLS_MOON_STONE
+
+	object_const_def
+	const TOHJOFALLS_POKE_BALL
+
+TohjoFalls_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+TohjoFallsMoonStone:
+	itemball MOON_STONE
