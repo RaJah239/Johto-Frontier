2bf6f248c8ab9e54517a8cff8bb91a4d6b45b9ee
RaJah239 <ravijagroo239@gmail.com>
2026-03-12 10:31:02 -0400

    Update maps/BlackthornMart.asm

--- maps/BlackthornMart.asm
+++ maps/BlackthornMart.asm
@@ -1,80 +1,3 @@
-	object_const_def
-	const BLACKTHORNMART_CLERK
-	const BLACKTHORNMART_COOLTRAINER_M
-	const BLACKTHORNMART_BLACK_BELT
-
-BlackthornMart_MapScripts:
-	def_scene_scripts
-
-	def_callbacks
-
-BlackthornMartClerkScript:
-	opentext
-	readvar VAR_BADGES
-	ifgreater 7, .EightBadgesMart
-	ifgreater 6, .SevenBadgesMart
-	ifgreater 4, .FiveBadgesMart
-	ifgreater 2, .ThreeBadgesMart
-	ifgreater 0, .OneBadgeMart
-	pokemart MARTTYPE_STANDARD, MART_NO_BADGES
-	closetext
-	end
-
-.OneBadgeMart:
-	pokemart MARTTYPE_STANDARD, MART_ONE_BADGE
-	closetext
-	end
-
-.ThreeBadgesMart:
-	pokemart MARTTYPE_STANDARD, MART_THREE_BADGES
-	closetext
-	end
-
-.FiveBadgesMart:
-	pokemart MARTTYPE_STANDARD, MART_FIVE_BADGES
-	closetext
-	end
-
-.SevenBadgesMart:
-	pokemart MARTTYPE_STANDARD, MART_SEVEN_BADGES
-	closetext
-	end
-
-.EightBadgesMart:
-	pokemart MARTTYPE_STANDARD, MART_EIGHT_BADGES
-	closetext
-	end
-
-BlackthornMartCooltrainerMScript:
-	jumptextfaceplayer BlackthornMartCooltrainerMText
-
-BlackthornMartBlackBeltScript:
-	jumptextfaceplayer BlackthornMartBlackBeltText
-
-BlackthornMartCooltrainerMText:
-	text "You can't buy MAX"
-	line "REVIVE, but it"
-
-	para "fully restores a"
-	line "fainted #MON."
-
-	para "Beware--it won't"
-	line "restore PP, the"
-
-	para "POWER POINTS"
-	line "needed for moves."
-	done
-
-BlackthornMartBlackBeltText:
-	text "MAX REPEL keeps"
-	line "weak #MON away"
-	cont "from you."
-
-	para "It's the longest"
-	line "lasting of the"
-	cont "REPEL sprays."
-	done
-
 BlackthornMart_MapEvents:
 	def_warp_events
 	warp_event  2,  7, BLACKTHORN_CITY, 5
@@ -85,6 +8,41 @@ BlackthornMart_MapEvents:
 	def_bg_events
 
 	def_object_events
-	object_event  1,  3, SPRITE_CLERK, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, BlackthornMartClerkScript, -1
-	object_event  7,  6, SPRITE_COOLTRAINER_M, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 2, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, BlackthornMartCooltrainerMScript, -1
-	object_event  5,  2, SPRITE_BLACK_BELT, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, BlackthornMartBlackBeltScript, -1
+	variable_mart_event 1, 3, PAL_NPC_BLACK
+	object_event  7,  6, SPRITE_COOLTRAINER_M, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 2, 0, -1, -1, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, BlackthornMartCooltrainerMText, -1
+	object_event  5,  2, SPRITE_BLACK_BELT, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_COMMAND, jumptextfaceplayer, BlackthornMartBlackBeltText, -1
+
+	object_const_def
+	const BLACKTHORNMART_CLERK
+	const BLACKTHORNMART_COOLTRAINER_M
+	const BLACKTHORNMART_BLACK_BELT
+
+BlackthornMart_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+BlackthornMartCooltrainerMText:
+	text "You can't buy Max"
+	line "Revive, but it"
+	cont "fully restores a"
+	cont "fainted #mon."
+
+	para "Beware--it won't"
+	line "restore PP, the"
+	cont "Power Points"
+	cont "needed for moves."
+	done
+
+BlackthornMartBlackBeltText:
+	text "Max Repel keeps"
+	line "weak #mon away"
+	cont "from you."
+
+	para "It's the longest"
+	line "lasting of the"
+	cont "Repel sprays."
+
+	para "There may be some-"
+	line "thing else…"
+	done
