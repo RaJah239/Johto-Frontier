5f3b46e2289c812228327cff761d9a37ac0e0ef1
RaJah239 <ravijagroo239@gmail.com>
2026-03-12 14:50:51 -0400

    Update maps/DayOfWeekSiblingsHouse.asm

--- maps/DayOfWeekSiblingsHouse.asm
+++ maps/DayOfWeekSiblingsHouse.asm
@@ -1,73 +1,3 @@
-	object_const_def
-	const DAYOFWEEKSIBLINGSHOUSE_POKEDEX
-
-DayOfWeekSiblingsHouse_MapScripts:
-	def_scene_scripts
-
-	def_callbacks
-
-DayOfWeekSiblingsHousePokedexScript:
-	opentext
-	writetext DayOfWeekSiblingsHousePokedexText1
-	yesorno
-	iffalse .End
-	writetext DayOfWeekSiblingsHousePokedexText2
-	yesorno
-	iffalse .End
-	writetext DayOfWeekSiblingsHousePokedexText3
-	waitbutton
-.End:
-	closetext
-	end
-
-DayOfWeekSiblingsHousePokedexText1:
-	text "There's something"
-	line "written here."
-
-	para "Read it?"
-	done
-
-DayOfWeekSiblingsHousePokedexText2:
-	text "To my brothers and"
-	line "sisters:"
-
-	para "We take pride in"
-	line "serving #MON"
-	cont "trainers."
-
-	para "When trainers talk"
-	line "to you, give them"
-	cont "something useful."
-
-	para "Love,"
-	line "MONICA"
-
-	para "Keep reading?"
-	done
-
-DayOfWeekSiblingsHousePokedexText3:
-	text "Monday, MONICA"
-	line "ROUTE 12"
-
-	para "Tuesday, TUSCANY"
-	line "ROUTE 1"
-
-	para "Wednesday, WESLEY"
-	line "LAKE OF RAGE"
-
-	para "Thursday, ARTHUR"
-	line "ROUTE 8"
-
-	para "Friday, FRIEDA"
-	line "ROUTE 4"
-
-	para "Saturday, SANTOS"
-	line "BLACKTHORN CITY"
-
-	para "Sunday, SUNNY"
-	line "ROUTE 9"
-	done
-
 DayOfWeekSiblingsHouse_MapEvents:
 	def_warp_events
 	warp_event  2,  7, ROUTE_20, 3
@@ -79,3 +9,63 @@ DayOfWeekSiblingsHouse_MapEvents:
 
 	def_object_events
 	object_event  3,  3, SPRITE_POKEDEX, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, DayOfWeekSiblingsHousePokedexScript, -1
+
+	object_const_def
+	const DAYOFWEEKSIBLINGSHOUSE_POKEDEX
+
+DayOfWeekSiblingsHouse_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+DayOfWeekSiblingsHousePokedexScript:
+	opentext
+	writethistext
+		text "There's something"
+		line "written here."
+
+		para "Read it?"
+		done
+	yesorno
+	iffalse_endtext
+	writethistext
+		text "To my brothers and"
+		line "sisters:"
+
+		para "We take pride in"
+		line "serving #mon"
+		cont "trainers."
+
+		para "When trainers talk"
+		line "to you, give them"
+		cont "something useful."
+
+		para "Love,"
+		line "Monica"
+
+		para "Keep reading?"
+		done
+	yesorno
+	iffalse_endtext
+	jumpthisopenedtext
+		text "Monday, Monica"
+		line "Route 12"
+
+		para "Tuesday, Tuscany"
+		line "Route 1"
+
+		para "Wednesday, Wesley"
+		line "Lake Of Rage"
+
+		para "Thursday, Arthur"
+		line "Route 8"
+
+		para "Friday, Frieda"
+		line "Route 4"
+
+		para "Saturday, Santos"
+		line "Blackthorn City"
+
+		para "Sunday, Sunny"
+		line "Route 9"
+		done
