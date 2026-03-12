12888cc6efd399c43b673c37c90ce0a2b613b94f
RaJah239 <ravijagroo239@gmail.com>
2026-03-12 15:44:19 -0400

    Update maps/Route21.asm

--- maps/Route21.asm
+++ maps/Route21.asm
@@ -1,68 +1,3 @@
-Route21_MapScripts:
-	def_scene_scripts
-	scene_script Route21ForcedToLeaveScene, SCENE_ROUTE_21_FORCED_TO_LEAVE
-	scene_script Route21NoopScene,          SCENE_ROUTE_21_NOOP
-
-	def_callbacks
-	callback MAPCALLBACK_NEWMAP, Route21FlypointCallback
-
-Route21FlypointCallback:
-	setflag ENGINE_FLYPOINT_ROUTE_20
-	setflag ENGINE_FLYPOINT_INDIGO_PLATEAU
-	endcallback
-
-Route21ForcedToLeaveScene:
-	sdefer NoWarpingHereFirstTime
-	; fallthrough
-
-Route21NoopScene:
-	end
-
-NoWarpingHereFirstTime:
-	applymovement PLAYER, Route21_StepDownMovement
-	opentext
-	writetext FirstTimeHereNoticeText
-	waitbutton
-	special FadeOutPalettes
-	playsound SFX_WARP_TO
-	waitsfx
-	warp NEW_BARK_TOWN, 13, 6
-	end
-
-Route21_StepDownMovement:
-	step DOWN
-	step_end
-
-FirstTimeHereNoticeText:
-	text "Notice: First time"
-	line "here must be via"
-	cont "Victory Road."
-	done
-
-IndigoPlateauSign:
-	jumptext IndigoPlateauSignText
-
-IndigoPlateauSignText:
-	text "INDIGO PLATEAU"
-
-	para "The Ultimate Goal"
-	line "for Trainers!"
-
-	para "#MON LEAGUE HQ"
-	done
-
-NurseJoyTrainingSign:
-	jumptext NurseJoyTrainingSignText
-
-NurseJoyTrainingSignText:
-	text "Nurse Joy's"
-	line "training academy."
-
-	para "Get your #mon"
-	line "ready for the"
-	cont "#mon League!"
-	done
-
 Route21_MapEvents:
 	def_warp_events
 	warp_event  9,  5, INDIGO_PLATEAU_POKECENTER_1F, 1
@@ -74,7 +9,59 @@ Route21_MapEvents:
 	def_coord_events
 
 	def_bg_events
-	bg_event 11,  7, BGEVENT_READ, IndigoPlateauSign
-	bg_event 14, 10, BGEVENT_READ, NurseJoyTrainingSign
+	bg_event 11,  7, BGEVENT_JUMPTEXT, IndigoPlateauSignText
+	bg_event 14, 10, BGEVENT_JUMPTEXT, NurseJoyTrainingSignText
 
 	def_object_events
+
+Route21_MapScripts:
+	def_scene_scripts
+	scene_script Route21ForcedToLeaveScene, SCENE_ROUTE_21_FORCED_TO_LEAVE
+	scene_script Route21NoopScene,          SCENE_ROUTE_21_NOOP
+
+	def_callbacks
+	callback MAPCALLBACK_NEWMAP, Route21FlypointCallback
+
+Route21FlypointCallback:
+	setflag ENGINE_FLYPOINT_INDIGO_PLATEAU
+	endcallback
+
+Route21ForcedToLeaveScene:
+	sdefer NoWarpingHereFirstTime
+Route21NoopScene:
+	end
+
+NoWarpingHereFirstTime:
+	applymovement PLAYER, Route21_StepDownMovement
+	showthistext
+		text "Notice: First time"
+		line "here must be via"
+		cont "Victory Road."
+		done
+	special FadeOutPalettes
+	playsound SFX_WARP_TO
+	waitsfx
+	warp NEW_BARK_TOWN, 13, 6
+	end
+
+Route21_StepDownMovement:
+	step DOWN
+	step_end
+
+IndigoPlateauSignText:
+	text "Indigo Plateau"
+
+	para "The Ultimate Goal"
+	line "for Trainers!"
+
+	para "#mon League HQ"
+	done
+
+NurseJoyTrainingSignText:
+	text "Nurse Joy's"
+	line "training academy."
+
+	para "Get your #mon"
+	line "ready for the"
+	cont "#mon League!"
+	done
