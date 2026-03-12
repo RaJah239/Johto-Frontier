2f48afdb233f59cc2b0e01605c5c62b1ef651914
RaJah239 <ravijagroo239@gmail.com>
2026-03-12 18:48:45 -0400

    Update maps/HallOfFame.asm

--- maps/HallOfFame.asm
+++ maps/HallOfFame.asm
@@ -1,3 +1,15 @@
+HallOfFame_MapEvents:
+	def_warp_events
+	warp_event  4, 13, LANCES_ROOM, 3
+	warp_event  5, 13, LANCES_ROOM, 4
+
+	def_coord_events
+
+	def_bg_events
+
+	def_object_events
+	object_event  4, 12, SPRITE_LANCE, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, -1
+
 	object_const_def
 	const HALLOFFAME_LANCE
 
@@ -10,8 +22,6 @@ HallOfFame_MapScripts:
 
 HallOfFameEnterScene:
 	sdefer HallOfFameEnterScript
-	end
-
 HallOfFameNoopScene:
 	end
 
@@ -20,10 +30,44 @@ HallOfFameEnterScript:
 	applymovement HALLOFFAME_LANCE, HallOfFame_WalkUpWithLance
 	stopfollow
 	turnobject PLAYER, RIGHT
-	opentext
-	writetext HallOfFame_LanceText
-	waitbutton
-	closetext
+	showthistext
+		text "Lance: It's been a"
+		line "long time since I"
+		cont "last came here."
+
+		para "This is where we"
+		line "honor the League"
+		cont "Champions for all"
+		cont "eternity."
+
+		para "Their courageous"
+		line "#mon are also"
+		cont "inducted."
+
+		para "Here today, we"
+		line "witnessed the rise"
+		cont "of a new League"
+		cont "Champion--a"
+		cont "trainer who feels"
+		cont "compassion for,"
+		cont "and trust toward,"
+		cont "all #mon."
+
+		para "A trainer who"
+		line "succeeded through"
+		cont "perseverance and"
+		cont "determination."
+
+		para "The new League"
+		line "Champion who has"
+		cont "all the makings"
+		cont "of greatness!"
+
+		para "<PLAY_G>, allow me"
+		line "to register you"
+		cont "and your partners"
+		cont "as Champions!"
+		done
 	turnobject HALLOFFAME_LANCE, UP
 	applymovement PLAYER, HallOfFame_SlowlyApproachMachine
 	setscene SCENE_HALLOFFAME_NOOP
@@ -52,61 +96,3 @@ HallOfFame_WalkUpWithLance:
 HallOfFame_SlowlyApproachMachine:
 	slow_step UP
 	step_end
-
-HallOfFame_LanceText:
-	text "LANCE: It's been a"
-	line "long time since I"
-	cont "last came here."
-
-	para "This is where we"
-	line "honor the LEAGUE"
-
-	para "CHAMPIONS for all"
-	line "eternity."
-
-	para "Their courageous"
-	line "#MON are also"
-	cont "inducted."
-
-	para "Here today, we"
-	line "witnessed the rise"
-
-	para "of a new LEAGUE"
-	line "CHAMPION--a"
-
-	para "trainer who feels"
-	line "compassion for,"
-
-	para "and trust toward,"
-	line "all #MON."
-
-	para "A trainer who"
-	line "succeeded through"
-
-	para "perseverance and"
-	line "determination."
-
-	para "The new LEAGUE"
-	line "CHAMPION who has"
-
-	para "all the makings"
-	line "of greatness!"
-
-	para "<PLAY_G>, allow me"
-	line "to register you"
-
-	para "and your partners"
-	line "as CHAMPIONS!"
-	done
-
-HallOfFame_MapEvents:
-	def_warp_events
-	warp_event  4, 13, LANCES_ROOM, 3
-	warp_event  5, 13, LANCES_ROOM, 4
-
-	def_coord_events
-
-	def_bg_events
-
-	def_object_events
-	object_event  4, 12, SPRITE_LANCE, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, -1
