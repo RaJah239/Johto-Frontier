2a85f2046f1a75d98fcc867cfd96b61b32839c1f
RaJah239 <ravijagroo239@gmail.com>
2026-03-12 18:10:55 -0400

    Update maps/KogasRoom.asm

--- maps/KogasRoom.asm
+++ maps/KogasRoom.asm
@@ -1,3 +1,17 @@
+KogasRoom_MapEvents:
+	def_warp_events
+	warp_event  4, 17, WILLS_ROOM, 2
+	warp_event  5, 17, WILLS_ROOM, 3
+	warp_event  4,  2, BRUNOS_ROOM, 1
+	warp_event  5,  2, BRUNOS_ROOM, 2
+
+	def_coord_events
+
+	def_bg_events
+
+	def_object_events
+	object_event  5,  7, SPRITE_KOGA, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, KogaScript_Battle, -1
+
 	object_const_def
 	const KOGASROOM_KOGA
 
@@ -11,8 +25,6 @@ KogasRoom_MapScripts:
 
 KogasRoomLockDoorScene:
 	sdefer KogasRoomDoorLocksBehindYouScript
-	end
-
 KogasRoomNoopScene:
 	end
 
@@ -40,37 +52,6 @@ KogasRoomDoorLocksBehindYouScript:
 	waitsfx
 	end
 
-KogaScript_Battle:
-	faceplayer
-	opentext
-	checkevent EVENT_BEAT_ELITE_4_KOGA
-	iftrue KogaScript_AfterBattle
-	writetext KogaScript_KogaBeforeText
-	waitbutton
-	closetext
-	winlosstext KogaScript_KogaBeatenText, 0
-	loadtrainer KOGA, KOGA1
-	startbattle
-	reloadmapafterbattle
-	setevent EVENT_BEAT_ELITE_4_KOGA
-	opentext
-	writetext KogaScript_KogaDefeatText
-	waitbutton
-	closetext
-	playsound SFX_ENTER_DOOR
-	changeblock 4, 2, $16 ; open door
-	refreshmap
-	closetext
-	setevent EVENT_KOGAS_ROOM_EXIT_OPEN
-	waitsfx
-	end
-
-KogaScript_AfterBattle:
-	writetext KogaScript_KogaDefeatText
-	waitbutton
-	closetext
-	end
-
 KogasRoom_EnterMovement:
 	step UP
 	step UP
@@ -78,34 +59,58 @@ KogasRoom_EnterMovement:
 	step UP
 	step_end
 
-KogaScript_KogaBeforeText:
-	text "Fwahahahaha!"
+KogaScript_Battle:
+	faceplayeropentext
+	checkevent EVENT_BEAT_ELITE_4_KOGA
+	iftrue KogaScript_AfterBattle
+	writethistext
+		text "Fwahahahaha!"
 
-	para "I am KOGA of the"
-	line "ELITE FOUR."
+		para "I am Koga of the"
+		line "Elite Four."
 
-	para "I live in shadows,"
-	line "a ninja!"
+		para "I live in shadows,"
+		line "a ninja!"
 
-	para "My intricate style"
-	line "will confound and"
-	cont "destroy you!"
+		para "My intricate style"
+		line "will confound and"
+		cont "destroy you!"
 
-	para "Confusion, sleep,"
-	line "poison…"
+		para "Confusion, sleep,"
+		line "poison…"
 
-	para "Prepare to be the"
-	line "victim of my sin-"
-	cont "ister technique!"
+		para "Prepare to be the"
+		line "victim of my sin-"
+		cont "ister technique!"
 
-	para "Fwahahahaha!"
+		para "Fwahahahaha!"
 
-	para "#MON is not"
-	line "merely about brute"
+		para "#mon is not"
+		line "merely about brute"
+		cont "force--you shall"
+		cont "see soon enough!"
+		done
+	waitclosetext
+	winlosstext KogaScript_KogaBeatenText, 0
+	loadtrainer KOGA, KOGA1
+	startbattle
+	reloadmapafterbattle
+	setevent EVENT_BEAT_ELITE_4_KOGA
+	opentext
+	writetext KogaScript_KogaDefeatText
+	waitclosetext
+	playsound SFX_ENTER_DOOR
+	changeblock 4, 2, $16 ; open door
+	refreshmap
+	setevent EVENT_KOGAS_ROOM_EXIT_OPEN
+	waitsfx
+	endtext
 
-	para "force--you shall"
-	line "see soon enough!"
-	done
+KogaScript_AfterBattle:
+	writetext KogaScript_KogaDefeatText
+	waitbutton
+	closetext
+	end
 
 KogaScript_KogaBeatenText:
 	text "Ah!"
@@ -126,17 +131,3 @@ KogaScript_KogaDefeatText:
 	line "room, and put your"
 	cont "abilities to test!"
 	done
-
-KogasRoom_MapEvents:
-	def_warp_events
-	warp_event  4, 17, WILLS_ROOM, 2
-	warp_event  5, 17, WILLS_ROOM, 3
-	warp_event  4,  2, BRUNOS_ROOM, 1
-	warp_event  5,  2, BRUNOS_ROOM, 2
-
-	def_coord_events
-
-	def_bg_events
-
-	def_object_events
-	object_event  5,  7, SPRITE_KOGA, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, KogaScript_Battle, -1
