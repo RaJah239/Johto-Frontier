dfbf73253a13d04c9da4ddddaa62f72182422085
RaJah239 <ravijagroo239@gmail.com>
2026-03-12 14:09:07 -0400

    Update maps/Route20HealHouse.asm

--- maps/Route20HealHouse.asm
+++ maps/Route20HealHouse.asm
@@ -1,49 +1,3 @@
-	object_const_def
-	const ROUTE20HEALHOUSE_TEACHER
-	const ROUTE20HEALHOUSE_PORYGON_PC
-
-Route20HealHouse_MapScripts:
-	def_scene_scripts
-
-	def_callbacks
-
-Route20HealHouseTeacherScript:
-	faceplayer
-	opentext
-	writetext Route20HealHouseRestAWhileText
-	waitbutton
-	closetext
-	special FadeOutPalettes
-	special LoadMapPalettes
-	playsound SFX_FULL_HEAL
-	special HealParty
-	special FadeInPalettes_EnableDynNoApply
-	opentext
-	writetext Route20HealHouseKeepAtItText
-	pause 20
-	closetext
-	turnobject PLAYER, DOWN
-	end
-
-Route20HealHouseBookshelf:
-	jumpstd PictureBookshelfScript
-
-Route20HealHouseRestAWhileText:
-	text "Your #MON look"
-	line "a little tired."
-
-	para "You should rest"
-	line "them a while."
-	done
-
-Route20HealHouseKeepAtItText:
-	text "There!"
-	line "Keep at it!"
-	done
-
-Route20HealHousePorygonPCScript:
-	jumpstd PorygonPCScript
-
 Route20HealHouse_MapEvents:
 	def_warp_events
 	warp_event  2,  7, ROUTE_20, 1
@@ -52,9 +6,41 @@ Route20HealHouse_MapEvents:
 	def_coord_events
 
 	def_bg_events
-	bg_event  0,  1, BGEVENT_READ, Route20HealHouseBookshelf
-	bg_event  1,  1, BGEVENT_READ, Route20HealHouseBookshelf
+	bg_event  0,  1, BGEVENT_JUMPSTD, PICTURE_BOOKSHELF_SCRIPT
+	bg_event  1,  1, BGEVENT_JUMPSTD, PICTURE_BOOKSHELF_SCRIPT
 
 	def_object_events
+	porygonpc_event 6, 5, PAL_NPC_RED
 	object_event  2,  4, SPRITE_TEACHER, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, Route20HealHouseTeacherScript, -1
-	object_event  6,  5, SPRITE_PORYGON_OW, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, Route20HealHousePorygonPCScript, -1
+
+	object_const_def
+	const ROUTE20HEALHOUSE_PORYGON_PC
+	const ROUTE20HEALHOUSE_TEACHER
+
+Route20HealHouse_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+Route20HealHouseTeacherScript:
+	showthistextfaceplayer
+		text "Your #mon look"
+		line "a little tired."
+
+		para "You should rest"
+		line "them a while."
+		done
+	special FadeOutPalettes
+	special LoadMapPalettes
+	playsound SFX_FULL_HEAL
+	special HealParty
+	special FadeInPalettes_EnableDynNoApply
+	opentext
+	writethistext
+		text "There!"
+		line "Keep at it!"
+		done
+	pause 20
+	closetext
+	turnobject PLAYER, DOWN
+	end
