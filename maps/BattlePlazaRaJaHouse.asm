4b31e823eeca5ce2e9c78009d1f906cf7449571a
RaJah239 <ravijagroo239@gmail.com>
2026-03-12 19:16:35 -0400

    Update maps/BattlePlazaRaJaHouse.asm

--- maps/BattlePlazaRaJaHouse.asm
+++ maps/BattlePlazaRaJaHouse.asm
@@ -1,83 +1,3 @@
-	object_const_def
-	const BATTLEPLAZARAJAHOUSE_RAJA
-
-BattlePlazaRaJaHouse_MapScripts:
-	def_scene_scripts
-
-	def_callbacks
-
-BattlePlazaRaJaHouseRaJaScript:
-	faceplayer
-	opentext
-	checkevent EVENT_GOT_START_MENU_WARP
-	iftrue .HopeYouHaveFunWithThisGame
-	writetext TheSpeechText
-	playsound SFX_DEX_FANFARE_50_79
-	waitsfx
-	setflag ENGINE_START_MENU_WARP
-	setevent EVENT_GOT_START_MENU_WARP
-	writetext RaJaWarpMenuText
-	waitbutton
-	writetext RaJaTeleportingText
-	waitendtext
-
-.HopeYouHaveFunWithThisGame:
-	writetext HopeYouHaveFunWithThisGameText
-	waitendtext
-
-TheSpeechText:
-	text "RaJa239: Yes, yes,"
-	line "I made this ROM"
-	cont "hack of my very"
-	cont "favorite game,"
-	cont "#mon Crystal."
-
-	para "I could give you a"
-	line "story of childhood"
-	cont "and escapism but"
-	cont "that would bore"
-	cont "you and worse,"
-	cont "waste ROM bytes."
-
-	para "What's this? You"
-	line "think I can gift"
-	cont "you something as"
-	cont "I made this?"
-
-	para "Well of course!"
-
-	para "But, you'll have"
-	line "to beat me in a"
-	cont "battle to earn it!"
-
-	para "Or so you'd think"
-	line "right? I'm no"
-	cont "trainer."
-
-	para "In fact, go on"
-	line "please accept this"
-	cont "just for playing"
-	cont "my game!"
-	done
-
-RaJaWarpMenuText:
-	text "<PLAYER> received"
-	line "Warp Option from"
-	cont "RaJa239!"
-	done
-
-RaJaTeleportingText:
-	text "Have fun teleport-"
-	line "ing! Swing by any"
-	cont "time, why not."
-	done
-
-HopeYouHaveFunWithThisGameText:
-	text "RaJa239: I hope"
-	line "you're enjoying"
-	cont "this game."
-	done 
-
 BattlePlazaRaJaHouse_MapEvents:
 	def_warp_events
 	warp_event  2,  7, BATTLE_PLAZA, 5
@@ -89,3 +9,72 @@ BattlePlazaRaJaHouse_MapEvents:
 
 	def_object_events
 	object_event  2,  4, SPRITE_SUPER_NERD, SPRITEMOVEDATA_STANDING_DOWN, 0, 1, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, BattlePlazaRaJaHouseRaJaScript, -1
+
+	object_const_def
+	const BATTLEPLAZARAJAHOUSE_RAJA
+
+BattlePlazaRaJaHouse_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+BattlePlazaRaJaHouseRaJaScript:
+	faceplayeropentext
+	checkevent EVENT_GOT_START_MENU_WARP
+	iftrue .HopeYouHaveFunWithThisGame
+	writethistext
+		text "RaJa239: Yes, yes,"
+		line "I made this ROM"
+		cont "hack of my very"
+		cont "favorite game,"
+		cont "#mon Crystal."
+
+		para "I could give you a"
+		line "story of childhood"
+		cont "and escapism but"
+		cont "that would bore"
+		cont "you and worse,"
+		cont "waste ROM bytes."
+
+		para "What's this? You"
+		line "think I can gift"
+		cont "you something as"
+		cont "I made this?"
+
+		para "Well of course!"
+
+		para "But, you'll have"
+		line "to beat me in a"
+		cont "battle to earn it!"
+
+		para "Or so you'd think"
+		line "right? I'm no"
+		cont "trainer."
+
+		para "In fact, go on"
+		line "please accept this"
+		cont "just for playing"
+		cont "my game!"
+		done
+	playsound SFX_DEX_FANFARE_50_79
+	waitsfx
+	setflag ENGINE_START_MENU_WARP
+	setevent EVENT_GOT_START_MENU_WARP
+	writethistext
+		text "<PLAYER> received"
+		line "Warp Option from"
+		cont "RaJa239!"
+		done
+	waitbutton
+	jumpthisopenedtext
+	text "Have fun teleport-"
+	line "ing! Swing by any"
+	cont "time, why not."
+	done
+
+.HopeYouHaveFunWithThisGame:
+	jumpthisopenedtext
+		text "RaJa239: I hope"
+		line "you're enjoying"
+		cont "this game."
+		done 
