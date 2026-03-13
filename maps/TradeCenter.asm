8366b26c35d75b336d310c286f3758bc2aff97ac
RaJah239 <ravijagroo239@gmail.com>
2026-03-13 11:04:05 -0400

    Update maps/TradeCenter.asm

--- maps/TradeCenter.asm
+++ maps/TradeCenter.asm
@@ -1,3 +1,18 @@
+TradeCenter_MapEvents:
+	def_warp_events
+	warp_event  4,  7, POKECENTER_2F, 2
+	warp_event  5,  7, POKECENTER_2F, 2
+
+	def_coord_events
+
+	def_bg_events
+	bg_event  4,  4, BGEVENT_RIGHT, TradeCenterConsoleScript
+	bg_event  5,  4, BGEVENT_LEFT, TradeCenterConsoleScript
+
+	def_object_events
+	object_event  3,  4, SPRITE_LINK_TRAINER, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, CableClubFriendScript, EVENT_TEMPORARY_UNTIL_MAP_RELOAD_1
+	object_event  6,  4, SPRITE_LINK_TRAINER, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, CableClubFriendScript, EVENT_TEMPORARY_UNTIL_MAP_RELOAD_2
+
 	object_const_def
 	const TRADECENTER_LINK_TRAINER1
 	const TRADECENTER_LINK_TRAINER2
@@ -12,8 +27,6 @@ TradeCenter_MapScripts:
 
 TradeCenterInitializeScene:
 	sdefer TradeCenterInitializeAndPreparePokecenter2FScript
-	end
-
 TradeCenterNoopScene:
 	end
 
@@ -38,18 +51,3 @@ TradeCenterConsoleScript:
 	special TradeCenter
 	newloadmap MAPSETUP_LINKRETURN
 	end
-
-TradeCenter_MapEvents:
-	def_warp_events
-	warp_event  4,  7, POKECENTER_2F, 2
-	warp_event  5,  7, POKECENTER_2F, 2
-
-	def_coord_events
-
-	def_bg_events
-	bg_event  4,  4, BGEVENT_RIGHT, TradeCenterConsoleScript
-	bg_event  5,  4, BGEVENT_LEFT, TradeCenterConsoleScript
-
-	def_object_events
-	object_event  3,  4, SPRITE_LINK_TRAINER, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, CableClubFriendScript, EVENT_TEMPORARY_UNTIL_MAP_RELOAD_1
-	object_event  6,  4, SPRITE_LINK_TRAINER, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, CableClubFriendScript, EVENT_TEMPORARY_UNTIL_MAP_RELOAD_2
