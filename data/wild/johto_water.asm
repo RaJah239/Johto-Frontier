b87b4a0a18854c0e96308ce680a7cc9a6032fbb3
RaJah239 <ravijagroo239@gmail.com>
2026-03-17 17:00:27 -0400

    Update data/wild/johto_water.asm

--- data/wild/johto_water.asm
+++ data/wild/johto_water.asm
@@ -2,298 +2,298 @@
 
 JohtoWaterWildMons:
 
-	def_water_wildmons RUINS_OF_ALPH_OUTSIDE
-	db 4 percent ; encounter rate
-	db 15, WOOPER
-	db 20, QUAGSIRE
-	db 15, QUAGSIRE
-	end_water_wildmons
-
-	def_water_wildmons UNION_CAVE_1F
-	db 4 percent ; encounter rate
-	db 15, WOOPER
-	db 20, QUAGSIRE
-	db 15, QUAGSIRE
-	end_water_wildmons
-
-	def_water_wildmons UNION_CAVE_B1F
-	db 4 percent ; encounter rate
-	db 15, WOOPER
-	db 20, QUAGSIRE
-	db 15, QUAGSIRE
-	end_water_wildmons
-
-	def_water_wildmons UNION_CAVE_B2F
-	db 4 percent ; encounter rate
-	db 15, WOOPER
-	db 20, QUAGSIRE
-	db 15, QUAGSIRE
-	end_water_wildmons
-
-	def_water_wildmons SLOWPOKE_WELL_B1F
-	db 4 percent ; encounter rate
-	db 15, WOOPER
-	db 20, QUAGSIRE
-	db 15, QUAGSIRE
-	end_water_wildmons
-
-	def_water_wildmons SLOWPOKE_WELL_B2F
-	db 4 percent ; encounter rate
-	db 15, WOOPER
-	db 20, QUAGSIRE
-	db 15, QUAGSIRE
-	end_water_wildmons
-
-	def_water_wildmons ILEX_FOREST
-	db 4 percent ; encounter rate
-	db 15, WOOPER
-	db 20, QUAGSIRE
-	db 15, QUAGSIRE
-	end_water_wildmons
-
-	def_water_wildmons MOUNT_MORTAR_1F_OUTSIDE
-	db 4 percent ; encounter rate
-	db 15, WOOPER
-	db 20, QUAGSIRE
-	db 15, QUAGSIRE
-	end_water_wildmons
-
-	def_water_wildmons MOUNT_MORTAR_2F_INSIDE
-	db 4 percent ; encounter rate
-	db 15, WOOPER
-	db 20, QUAGSIRE
-	db 15, QUAGSIRE
-	end_water_wildmons
-
-	def_water_wildmons MOUNT_MORTAR_B1F
-	db 4 percent ; encounter rate
-	db 15, WOOPER
-	db 20, QUAGSIRE
-	db 15, QUAGSIRE
-	end_water_wildmons
-
-	def_water_wildmons WHIRL_ISLAND_SW
-	db 4 percent ; encounter rate
-	db 15, WOOPER
-	db 20, QUAGSIRE
-	db 15, QUAGSIRE
-	end_water_wildmons
-
-	def_water_wildmons WHIRL_ISLAND_B2F
-	db 4 percent ; encounter rate
-	db 15, WOOPER
-	db 20, QUAGSIRE
-	db 15, QUAGSIRE
-	end_water_wildmons
-
-	def_water_wildmons WHIRL_ISLAND_LUGIA_CHAMBER
-	db 4 percent ; encounter rate
-	db 15, WOOPER
-	db 20, QUAGSIRE
-	db 15, QUAGSIRE
-	end_water_wildmons
-
-	def_water_wildmons SILVER_CAVE_ROOM_2
-	db 4 percent ; encounter rate
-	db 15, WOOPER
-	db 20, QUAGSIRE
-	db 15, QUAGSIRE
-	end_water_wildmons
-
-	def_water_wildmons DARK_CAVE_VIOLET_ENTRANCE
-	db 4 percent ; encounter rate
-	db 15, MAGIKARP
-	db 10, MAGIKARP
-	db 5, MAGIKARP
-	end_water_wildmons
-
-	def_water_wildmons DARK_CAVE_BLACKTHORN_ENTRANCE
-	db 4 percent ; encounter rate
-	db 15, MAGIKARP
-	db 10, MAGIKARP
-	db 5, MAGIKARP
-	end_water_wildmons
-
-	def_water_wildmons DRAGONS_DEN_B1F
-	db 4 percent ; encounter rate
-	db 15, MAGIKARP
-	db 10, MAGIKARP
-	db 10, MAGIKARP
-	end_water_wildmons
-
-	def_water_wildmons OLIVINE_PORT
-	db 4 percent ; encounter rate
-	db 20, TENTACOOL
-	db 15, TENTACOOL
-	db 20, TENTACRUEL
-	end_water_wildmons
-
-	def_water_wildmons ROUTE_2
-	db 4 percent ; encounter rate
-	db 15, WOOPER
-	db 20, QUAGSIRE
-	db 15, QUAGSIRE
-	end_water_wildmons
-
-	def_water_wildmons ROUTE_3
-	db 4 percent ; encounter rate
-	db 15, WOOPER
-	db 20, QUAGSIRE
-	db 15, QUAGSIRE
-	end_water_wildmons
-
-	def_water_wildmons ROUTE_4
-	db 4 percent ; encounter rate
-	db 15, WOOPER
-	db 20, QUAGSIRE
-	db 15, QUAGSIRE
-	end_water_wildmons
-
-	def_water_wildmons ROUTE_6
-	db 4 percent ; encounter rate
-	db 15, WOOPER
-	db 20, QUAGSIRE
-	db 15, QUAGSIRE
-	end_water_wildmons
-
-	def_water_wildmons ROUTE_7
-	db 4 percent ; encounter rate
-	db 15, WOOPER
-	db 20, QUAGSIRE
-	db 15, QUAGSIRE
-	end_water_wildmons
-
-	def_water_wildmons ROUTE_12
-	db 4 percent ; encounter rate
-	db 15, WOOPER
-	db 20, QUAGSIRE
-	db 15, QUAGSIRE
-	end_water_wildmons
-
-	def_water_wildmons ROUTE_13
-	db 4 percent ; encounter rate
-	db 15, WOOPER
-	db 20, QUAGSIRE
-	db 15, QUAGSIRE
-	end_water_wildmons
-
-	def_water_wildmons ROUTE_14
-	db 4 percent ; encounter rate
-	db 15, WOOPER
-	db 20, QUAGSIRE
-	db 15, QUAGSIRE
-	end_water_wildmons
-
-	def_water_wildmons ROUTE_15
-	db 4 percent ; encounter rate
-	db 20, MAGIKARP
-	db 15, MAGIKARP
-	db 10, MAGIKARP
-	end_water_wildmons
-
-	def_water_wildmons ROUTE_16
-	db 4 percent ; encounter rate
-	db 15, WOOPER
-	db 20, QUAGSIRE
-	db 15, QUAGSIRE
-	end_water_wildmons
-
-	def_water_wildmons ROUTE_17
-	db 4 percent ; encounter rate
-	db 20, MAGIKARP
-	db 15, MAGIKARP
-	db 5, MAGIKARP
-	end_water_wildmons
-
 	def_water_wildmons NEW_BARK_TOWN
 	db 4 percent ; encounter rate
-	db 20, TENTACOOL
-	db 15, TENTACOOL
-	db 20, TENTACRUEL
+	db 20, MARILL
+	db 20, MARILL
+	db 25, AZUMARILL
 	end_water_wildmons
 
 	def_water_wildmons CHERRYGROVE_CITY
 	db 4 percent ; encounter rate
-	db 20, TENTACOOL
-	db 15, TENTACOOL
-	db 20, TENTACRUEL
+	db 20, CHINCHOU
+	db 20, HORSEA
+	db 25, LANTURN
+	end_water_wildmons
+
+	def_water_wildmons ROUTE_2
+	db 4 percent ; encounter rate
+	db 20, MARILL
+	db 20, MARILL
+	db 25, AZUMARILL
+	end_water_wildmons
+
+	def_water_wildmons ROUTE_3
+	db 4 percent ; encounter rate
+	db 20, WIMPOD
+	db 20, WIMPOD
+	db 25, GOLISOPOD
+	end_water_wildmons
+
+	def_water_wildmons DARK_CAVE_VIOLET_ENTRANCE
+	db 4 percent ; encounter rate
+	db 20, MAGIKARP
+	db 20, MAGIKARP
+	db 20, FEEBAS
+	end_water_wildmons
+
+	def_water_wildmons DARK_CAVE_BLACKTHORN_ENTRANCE
+	db 4 percent ; encounter rate
+	db 20, FEEBAS
+	db 20, FEEBAS
+	db 20, MAGIKARP
 	end_water_wildmons
 
 	def_water_wildmons VIOLET_CITY
 	db 4 percent ; encounter rate
-	db 15, WOOPER
-	db 20, QUAGSIRE
-	db 15, QUAGSIRE
+	db 20, FEEBAS
+	db 20, MAGIKARP
+	db 20, REMORAID
 	end_water_wildmons
 
-	def_water_wildmons CIANWOOD_CITY
+	def_water_wildmons RUINS_OF_ALPH_OUTSIDE
 	db 4 percent ; encounter rate
-	db 20, TENTACOOL
-	db 15, TENTACOOL
-	db 20, TENTACRUEL
+	db 25, QUAGSIRE
+	db 25, QUAGSIRE
+	db 25, QUAGSIRE
 	end_water_wildmons
 
-	def_water_wildmons CIANWOOD_PORT
+	def_water_wildmons ROUTE_4
 	db 4 percent ; encounter rate
-	db 20, TENTACOOL
-	db 15, TENTACOOL
-	db 20, TENTACRUEL
+	db 20, FEEBAS
+	db 20, MAGIKARP
+	db 30, GYARADOS
 	end_water_wildmons
 
-	def_water_wildmons OLIVINE_CITY
+	def_water_wildmons UNION_CAVE_1F
 	db 4 percent ; encounter rate
-	db 20, TENTACOOL
-	db 15, TENTACOOL
-	db 20, TENTACRUEL
+	db 30, LANTURN
+	db 30, GOLISOPOD
+	db 30, TENTACRUEL
+	end_water_wildmons
+
+	def_water_wildmons UNION_CAVE_B1F
+	db 4 percent ; encounter rate
+	db 30, LANTURN
+	db 30, GOLISOPOD
+	db 30, TENTACRUEL
+	end_water_wildmons
+
+	def_water_wildmons UNION_CAVE_B2F
+	db 4 percent ; encounter rate
+	db 30, LANTURN
+	db 30, GOLISOPOD
+	db 30, TENTACRUEL
+	end_water_wildmons
+
+	def_water_wildmons SLOWPOKE_WELL_B1F
+	db 4 percent ; encounter rate
+	db 20, SLOWPOKE
+	db 20, REMORAID
+	db 30, SLOWBRO
+	end_water_wildmons
+
+	def_water_wildmons SLOWPOKE_WELL_B2F
+	db 4 percent ; encounter rate
+	db 20, SLOWPOKE
+	db 20, REMORAID
+	db 30, SLOWBRO
+	end_water_wildmons
+
+	def_water_wildmons ILEX_FOREST
+	db 4 percent ; encounter rate
+	db 20, MAGIKARP
+	db 20, TANGELA
+	db 30, TANGROWTH
+	end_water_wildmons
+
+	def_water_wildmons ROUTE_6
+	db 4 percent ; encounter rate
+	db 15, STARYU
+	db 20, HORSEA
+	db 15, STARMIE
+	end_water_wildmons
+
+	def_water_wildmons ROUTE_7
+	db 4 percent ; encounter rate
+	db 20, MARILL
+	db 20, TANGELA
+	db 25, AZUMARILL
 	end_water_wildmons
 
 	def_water_wildmons ECRUTEAK_CITY
 	db 4 percent ; encounter rate
-	db 15, WOOPER
-	db 20, QUAGSIRE
-	db 15, QUAGSIRE
+	db 20, WOOPER
+	db 20, REMORAID
+	db 20, MAGIKARP
+	end_water_wildmons
+
+	def_water_wildmons MOUNT_MORTAR_1F_OUTSIDE
+	db 4 percent ; encounter rate
+	db 30, AZUMARILL
+	db 20, SEEL
+	db 30, QUAGSIRE
+	end_water_wildmons
+
+	def_water_wildmons MOUNT_MORTAR_2F_INSIDE
+	db 4 percent ; encounter rate
+	db 30, AZUMARILL
+	db 20, SEEL
+	db 30, QUAGSIRE
+	end_water_wildmons
+
+	def_water_wildmons MOUNT_MORTAR_B1F
+	db 4 percent ; encounter rate
+	db 30, AZUMARILL
+	db 20, SEEL
+	db 30, QUAGSIRE
+	end_water_wildmons
+
+	def_water_wildmons WHIRL_ISLAND_SW
+	db 4 percent ; encounter rate
+	db 35, TENTACRUEL
+	db 35, KINGDRA
+	db 35, DEWGONG
+	end_water_wildmons
+
+	def_water_wildmons WHIRL_ISLAND_B2F
+	db 4 percent ; encounter rate
+	db 35, TENTACRUEL
+	db 35, KINGDRA
+	db 35, DEWGONG
+	end_water_wildmons
+
+	def_water_wildmons WHIRL_ISLAND_LUGIA_CHAMBER
+	db 4 percent ; encounter rate
+	db 35, TENTACRUEL
+	db 35, KINGDRA
+	db 35, DEWGONG
+	end_water_wildmons
+
+	def_water_wildmons SILVER_CAVE_ROOM_2
+	db 4 percent ; encounter rate
+	db 37, KINGDRA
+	db 37, LAPRAS
+	db 37, MANTINE
+	end_water_wildmons
+
+	def_water_wildmons DRAGONS_DEN_B1F
+	db 4 percent ; encounter rate
+	db 32, KINGDRA
+	db 32, GYARADOS
+	db 32, KINGDRA
+	end_water_wildmons
+
+	def_water_wildmons OLIVINE_PORT
+	db 4 percent ; encounter rate
+	db 20, REMORAID
+	db 30, OCTILLERY
+	db 30, TENTACRUEL
+	end_water_wildmons
+
+	def_water_wildmons OLIVINE_CITY
+	db 4 percent ; encounter rate
+	db 20, REMORAID
+	db 30, OCTILLERY
+	db 30, TENTACRUEL
+	end_water_wildmons
+
+	def_water_wildmons ROUTE_12
+	db 4 percent ; encounter rate
+	db 30, OCTILLERY
+	db 30, MANTINE
+	db 30, TENTACRUEL
+	end_water_wildmons
+
+	def_water_wildmons ROUTE_13
+	db 4 percent ; encounter rate
+	db 30, OCTILLERY
+	db 30, MANTINE
+	db 30, TENTACRUEL
+	end_water_wildmons
+
+	def_water_wildmons CIANWOOD_CITY
+	db 4 percent ; encounter rate
+	db 30, OCTILLERY
+	db 30, MANTINE
+	db 30, TENTACRUEL
+	end_water_wildmons
+
+	def_water_wildmons CIANWOOD_PORT
+	db 4 percent ; encounter rate
+	db 20, REMORAID
+	db 30, OCTILLERY
+	db 30, TENTACRUEL
+	end_water_wildmons
+
+	def_water_wildmons ROUTE_14
+	db 4 percent ; encounter rate
+	db 30, LANTURN
+	db 30, MANTINE
+	db 30, QUAGSIRE
+	end_water_wildmons
+
+	def_water_wildmons ROUTE_15
+	db 4 percent ; encounter rate
+	db 20, REMORAID
+	db 30, GOLISOPOD
+	db 30, AZUMARILL
 	end_water_wildmons
 
 	def_water_wildmons LAKE_OF_RAGE
 	db 4 percent ; encounter rate
-	db 15, MAGIKARP
-	db 10, MAGIKARP
 	db 15, GYARADOS
+	db 30, GYARADOS
+	db 30, MAGIKARP
+	end_water_wildmons
+
+	def_water_wildmons ROUTE_16
+	db 4 percent ; encounter rate
+	db 20, REMORAID
+	db 30, GOLISOPOD
+	db 30, TANGROWTH
+	end_water_wildmons
+
+	def_water_wildmons ROUTE_17
+	db 4 percent ; encounter rate
+	db 30, GYARADOS
+	db 30, GYARADOS
+	db 30, GYARADOS
 	end_water_wildmons
 
 	def_water_wildmons BLACKTHORN_CITY
 	db 4 percent ; encounter rate
-	db 15, MAGIKARP
-	db 10, MAGIKARP
-	db 5, MAGIKARP
+	db 20, MAGIKARP
+	db 20, FEEBAS
+	db 20, REMORAID
 	end_water_wildmons
 
 	def_water_wildmons SILVER_CAVE_OUTSIDE
 	db 4 percent ; encounter rate
-	db 15, WOOPER
-	db 20, QUAGSIRE
-	db 15, QUAGSIRE
+	db 37, KINGDRA
+	db 37, TANGROWTH
+	db 37, MANTINE
 	end_water_wildmons
 
 	def_water_wildmons TOHJO_FALLS
 	db 4 percent ; encounter rate
-	db 15, WOOPER
-	db 20, QUAGSIRE
-	db 15, QUAGSIRE
-	end_water_wildmons
-
-	def_water_wildmons ROUTE_20
-	db 4 percent ; encounter rate
-	db 15, WOOPER
-	db 20, QUAGSIRE
-	db 15, QUAGSIRE
+	db 30, QUAGSIRE
+	db 30, SLOWBRO
+	db 30, LANTURN
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_19
 	db 4 percent ; encounter rate
-	db 15, WOOPER
-	db 20, QUAGSIRE
-	db 15, QUAGSIRE
+	db 30, SEADRA
+	db 30, SEADRA
+	db 30, QUAGSIRE
+	end_water_wildmons
+
+	def_water_wildmons ROUTE_20
+	db 4 percent ; encounter rate
+	db 20, TENTACOOL
+	db 30, TENTACRUEL
+	db 30, TENTACRUEL
 	end_water_wildmons
 
 	db -1 ; end
