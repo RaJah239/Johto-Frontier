139e4aa3c1fc0cb154272bf5f7229f64ff739d51
RaJah239 <ravijagroo239@gmail.com>
2026-03-17 09:19:00 -0400

    Update data/wild/bug_contest_mons.asm

--- data/wild/bug_contest_mons.asm
+++ data/wild/bug_contest_mons.asm
@@ -1,13 +1,26 @@
 ContestMons::
+; TODOTEXT update min and max levels when evo levels are finalised
+
 	;   %, species,   min, max
-	db 20, TORCHIC,    7, 18
-	db 20, ENTEI,      7, 18
-	db 10, RAIKOU,     9, 18
-	db 10, SUICUNE,    9, 18
-	db  5, HERACROSS, 12, 15
-	db  5, GENGAR,    12, 15
-	db 10, HAUNTER,   10, 16
-	db 10, PIKACHU,   10, 17
-	db  5, SCYTHER,   13, 14
-	db  5, RAICHU,    13, 14
-	db -1, LAPRAS,    30, 40
+	db  7, WURMPLE,    7, 18 
+	db  6, SILCOON,    7, 18
+	db  5, BEAUTIFLY,  7, 18
+	db  6, CASCOON,    7, 18
+	db  5, DUSTOX,     7, 18
+	db  5, YANMA,      7, 18
+	db  1, YANMEGA,    7, 18
+	db  7, WIMPOD,     7, 18
+	db  2, GOLISOPOD,  7, 18
+	db  7, SPINARAK,   7, 18
+	db  2, ARIADOS,    7, 18
+	db  7, VENIPEDE,   7, 18
+	db  5, WHIRLIPEDE, 7, 18
+	db  2, SCOLIPEDE,  7, 18
+	db  7, JOLTIK,     7, 18
+	db  3, GALVANTULA, 7, 18
+	db  7, LARVESTA,   7, 18
+	db  1, VOLCARONA,  7, 18
+	db  5, SCYTHER,    7, 18
+	db  5, SCIZOR,     7, 18
+	db  5, KLEAVOR,    7, 18
+	db -1, KLEAVOR,    1, 10
