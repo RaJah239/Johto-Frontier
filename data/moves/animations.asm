76f6c247c0ea2bcfb7e5c6e1e1515147e8a06593
RaJah239 <ravijagroo239@gmail.com>
2026-03-23 13:51:24 -0400

    adjusted wrap animations to be shorter
    adapted from sourcrystal (8a4ce35)

--- data/moves/animations.asm
+++ data/moves/animations.asm
@@ -2034,18 +2034,20 @@ BattleAnim_IcicleCrash:
 
 BattleAnim_Wrap:
 	anim_1gfx BATTLE_ANIM_GFX_ROPE
-	anim_sound 0, 1, SFX_BIND
+	anim_sound 6, 2, SFX_MENU
 	anim_obj BATTLE_ANIM_OBJ_BIND, 132, 64, $0
 	anim_wait 8
+	anim_sound 6, 2, SFX_MENU
 	anim_obj BATTLE_ANIM_OBJ_BIND, 132, 56, $0
 	anim_wait 8
+	anim_sound 6, 2, SFX_MENU
 	anim_obj BATTLE_ANIM_OBJ_BIND, 132, 48, $0
-	anim_wait 64
+	anim_wait 8
 	anim_sound 0, 1, SFX_BIND
 	anim_incobj 1
 	anim_incobj 2
 	anim_incobj 3
-	anim_wait 96
+	anim_wait 64
 	anim_ret
 
 BattleAnim_Confusion:
