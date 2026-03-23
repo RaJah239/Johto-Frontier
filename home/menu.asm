1636e113ffe74f78293ad15c0b84b7398367c65a
RaJah239 <ravijagroo239@gmail.com>
2026-03-23 19:44:40 -0400

    delete unreferenced code in home/menu.asm

--- home/menu.asm
+++ home/menu.asm
@@ -281,9 +281,7 @@ MenuBoxCoord2Attr::
 	ld c, a
 	ld a, [wMenuBorderTopCoord]
 	ld b, a
-	; fallthrough
 
-Coord2Attr:: ; unreferenced
 ; Return the address of wAttrmap(c, b) in hl.
 	xor a
 	ld h, a
@@ -326,9 +324,6 @@ MenuTextbox::
 	call LoadMenuTextbox
 	pop hl
 	jmp PrintText
-
-Menu_DummyFunction:: ; unreferenced
-	ret
 
 LoadMenuTextbox::
 	ld hl, .MenuHeader
@@ -411,18 +406,15 @@ CopyNameFromMenu::
 	pop hl
 	ret
 
-YesNoBox::
-	lb bc, SCREEN_WIDTH - 6, 7
-
-PlaceYesNoBox::
-	jr _YesNoBox
-
-PlaceGenericTwoOptionBox:: ; unreferenced
-	call LoadMenuHeader
-	jr InterpretTwoOptionMenu
-
 NoYesBox::
 	farjp _NoYesBox
+
+YesNoBox::
+	lb bc, SCREEN_WIDTH - 6, 7
+	; fallthrough
+
+PlaceYesNoBox::
+	; fallthrough
 
 _YesNoBox::
 ; Return nc (yes) or c (no).
