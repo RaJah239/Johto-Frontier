1410ecf7b8661a6b8f65b37de15702a57dfec531
RaJah239 <ravijagroo239@gmail.com>
2026-04-16 15:32:46 -0400

    engine/overworld/map_setup.asm engine/overworld/scripting.asm

--- engine/overworld/scripting.asm
+++ engine/overworld/scripting.asm
@@ -2385,10 +2385,6 @@ Script_warpcheck:
 	farcall EnableEvents
 	ret
 
-Script_enableevents: ; unreferenced
-	farcall EnableEvents
-	ret
-
 Script_newloadmap:
 	call GetScriptByte
 	ldh [hMapEntryMethod], a
@@ -2579,14 +2575,6 @@ Script_checksave:
 	ld a, c
 	ld [wScriptVar], a
 	ret
-
-Script_checkver_duplicate: ; unreferenced
-	ld a, [.gs_version]
-	ld [wScriptVar], a
-	ret
-
-.gs_version:
-	db GS_VERSION
 
 Script_isdialogueminimal:
 	xor a
