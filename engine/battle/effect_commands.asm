43dd466904df8a9901ea3de8c5bec18e19a3db5f
RaJah239 <ravijagroo239@gmail.com>
2026-04-20 08:01:55 -0400

    removed mimic

--- engine/battle/effect_commands.asm
+++ engine/battle/effect_commands.asm
@@ -764,12 +764,6 @@ BattleCommand_DoTurn:
 	ld a, [wCurOTMon]
 
 .player
-	call GetPartyLocation
-	push hl
-	call CheckMimicUsed
-	pop hl
-	ret c
-
 .consume_pp
 	ldh a, [hBattleTurn]
 	and a
@@ -795,15 +789,11 @@ BattleCommand_DoTurn:
 	ld b, 0
 	add hl, bc
 	ld a, [hl]
-	cp MIMIC
-	jr z, .mimic
+
 	ld hl, wWildMonMoves
 	add hl, bc
 	ld a, [hl]
-	cp MIMIC
-	ret z
 
-.mimic
 	ld hl, wWildMonPP
 	call .consume_pp
 	ret
@@ -833,36 +823,6 @@ BattleCommand_DoTurn:
 	db EFFECT_ROLLOUT
 	db EFFECT_RAMPAGE
 	db -1
-
-CheckMimicUsed:
-	ldh a, [hBattleTurn]
-	and a
-	ld a, [wCurMoveNum]
-	jr z, .player
-	ld a, [wCurEnemyMoveNum]
-
-.player
-	ld c, a
-	ld a, MON_MOVES
-	call UserPartyAttr
-
-	ld a, BATTLE_VARS_MOVE
-	call GetBattleVar
-	cp MIMIC
-	jr z, .mimic
-
-	ld b, 0
-	add hl, bc
-	ld a, [hl]
-	cp MIMIC
-	jr nz, .mimic
-
-	scf
-	ret
-
-.mimic
-	and a
-	ret
 
 BattleCommand_Critical:
 ; Determine whether this attack's hit will be critical.
