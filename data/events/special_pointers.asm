da69510e3180d022049db47a16fe4cd736b2d9b5
RaJah239 <ravijagroo239@gmail.com>
2026-04-23 09:00:54 -0400

    unused mobile special `Function11b5e8` removed

--- data/events/special_pointers.asm
+++ data/events/special_pointers.asm
@@ -105,7 +105,6 @@ SpecialsPointers::
 ; Crystal only
 	add_special Function11ac3e
 	add_special TradeCornerHoldMon
-	add_special Function11b5e8
 	add_special Function11b7e5
 	add_special Function11b879
 	add_special Function11b920
