86c4389951bfdf579df85b98f2c6e246c93b6d68
RaJah239 <ravijagroo239@gmail.com>
2026-04-23 20:17:09 -0400

    Update engine/menus/save.asm

--- engine/menus/save.asm
+++ engine/menus/save.asm
@@ -651,29 +651,13 @@ VerifyBackupChecksum:
 	ret
 
 SaveData:
-	; This is called within two scenarios:
-	;   a) ErasePreviousSave (the process of erasing the save from a previous game file)
-	;   b) unused mobile functionality
-	; It is not part of a regular save.
-
+	; the process of erasing the save from a previous game file
 	ld a, BANK(sCrystalData)
 	call OpenSRAM
 	ld hl, wCrystalData
 	ld de, sCrystalData
 	ld bc, wCrystalDataEnd - wCrystalData
 	call CopyBytes
-
-	; This block originally had some mobile functionality, but since we're still in
-	; BANK(sCrystalData), it instead overwrites the sixteen wEventFlags starting at 1:s4_a60e with
-	; garbage from wd479. This isn't an issue, since ErasePreviousSave is followed by a regular
-	; save that unwrites the garbage.
-
-	ld hl, wd479
-	ld a, [hli]
-	ld [s4_a60e + 0], a
-	ld a, [hli]
-	ld [s4_a60e + 1], a
-
 	jmp CloseSRAM
 
 _LoadData:
@@ -683,16 +667,6 @@ _LoadData:
 	ld de, wCrystalData
 	ld bc, wCrystalDataEnd - wCrystalData
 	call CopyBytes
-
-	; This block originally had some mobile functionality to mirror SaveData above, but instead it
-	; (harmlessly) writes the aforementioned wEventFlags to the unused wd479.
-
-	ld hl, wd479
-	ld a, [s4_a60e + 0]
-	ld [hli], a
-	ld a, [s4_a60e + 1]
-	ld [hli], a
-
 	jmp CloseSRAM
 
 Checksum:
