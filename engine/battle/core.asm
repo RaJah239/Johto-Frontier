6a25a2221c7aaafa0e7555cf12554cf04680d0e6
RaJah239 <ravijagroo239@gmail.com>
2026-05-01 13:39:17 -0400

    moved functions to core3.asm

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -223,10 +223,10 @@ HandleBetweenTurnEffects:
 	call HandleWeather
 	call CheckFaint_PlayerThenEnemy
 	ret c
-	call HandleWrap
+	farcall HandleWrap
 	call CheckFaint_PlayerThenEnemy
 	ret c
-	call HandlePerishSong
+	farcall HandlePerishSong
 	call CheckFaint_PlayerThenEnemy
 	ret c
 	jr .NoMoreFaintingConditions
@@ -237,10 +237,10 @@ HandleBetweenTurnEffects:
 	call HandleWeather
 	call CheckFaint_EnemyThenPlayer
 	ret c
-	call HandleWrap
+	farcall HandleWrap
 	call CheckFaint_EnemyThenPlayer
 	ret c
-	call HandlePerishSong
+	farcall HandlePerishSong
 	call CheckFaint_EnemyThenPlayer
 	ret c
 
@@ -1251,138 +1251,6 @@ ResidualDamage:
 	ret
 
 INCLUDE "data/abilities/bad_dreams_mons.asm"
-
-HandlePerishSong:
-	ldh a, [hSerialConnectionStatus]
-	cp USING_EXTERNAL_CLOCK
-	jr z, .EnemyFirst
-	call SetPlayerTurn
-	call .do_it
-	call SetEnemyTurn
-	jr .do_it
-
-.EnemyFirst:
-	call SetEnemyTurn
-	call .do_it
-	call SetPlayerTurn
-
-.do_it
-	ld hl, wPlayerPerishCount
-	ldh a, [hBattleTurn]
-	and a
-	jr z, .got_count
-	ld hl, wEnemyPerishCount
-
-.got_count
-	ld a, BATTLE_VARS_SUBSTATUS1
-	call GetBattleVar
-	bit SUBSTATUS_PERISH, a
-	ret z
-	dec [hl]
-	ld a, [hl]
-	ld [wTextDecimalByte], a
-	push af
-	ld hl, PerishCountText
-	call StdBattleTextbox
-	pop af
-	ret nz
-	ld a, BATTLE_VARS_SUBSTATUS1
-	call GetBattleVarAddr
-	res SUBSTATUS_PERISH, [hl]
-	ldh a, [hBattleTurn]
-	and a
-	jr nz, .kill_enemy
-	ld hl, wBattleMonHP
-	xor a
-	ld [hli], a
-	ld [hl], a
-	ld hl, wPartyMon1HP
-	ld a, [wCurBattleMon]
-	call GetPartyLocation
-	xor a
-	ld [hli], a
-	ld [hl], a
-	ret
-
-.kill_enemy
-	ld hl, wEnemyMonHP
-	xor a
-	ld [hli], a
-	ld [hl], a
-	ld a, [wBattleMode]
-	dec a
-	ret z
-	ld hl, wOTPartyMon1HP
-	ld a, [wCurOTMon]
-	call GetPartyLocation
-	xor a
-	ld [hli], a
-	ld [hl], a
-	ret
-
-HandleWrap:
-	ldh a, [hSerialConnectionStatus]
-	cp USING_EXTERNAL_CLOCK
-	jr z, .EnemyFirst
-	call SetPlayerTurn
-	call .do_it
-	call SetEnemyTurn
-	jr .do_it
-
-.EnemyFirst:
-	call SetEnemyTurn
-	call .do_it
-	call SetPlayerTurn
-
-.do_it
-	ld hl, wPlayerWrapCount
-	ld de, wPlayerTrappingMove
-	ldh a, [hBattleTurn]
-	and a
-	jr z, .got_addrs
-	ld hl, wEnemyWrapCount
-	ld de, wEnemyTrappingMove
-
-.got_addrs
-	ld a, [hl]
-	and a
-	ret z
-
-	ld a, BATTLE_VARS_SUBSTATUS4
-	call GetBattleVar
-	bit SUBSTATUS_SUBSTITUTE, a
-	ret nz
-
-	ld a, [de]
-	ld [wNamedObjectIndex], a
-	ld [wFXAnimID], a
-	call GetMoveName
-	dec [hl]
-	jr z, .release_from_bounds
-
-	ld a, BATTLE_VARS_SUBSTATUS3
-	call GetBattleVar
-	and 1 << SUBSTATUS_FLYING | 1 << SUBSTATUS_UNDERGROUND
-	jr nz, .skip_anim
-
-	call SwitchTurnCore
-	xor a
-	ld [wNumHits], a
-	ld [wFXAnimID + 1], a
-	predef PlayBattleAnim
-	call SwitchTurnCore
-
-.skip_anim
-	call GetEighthMaxHP
-	call SubtractHPFromUser
-	ld hl, BattleText_UsersHurtByStringBuffer1
-	jr .print_text
-
-.release_from_bounds
-	ld hl, BattleText_UserWasReleasedFromStringBuffer1
-
-.print_text
-	jmp StdBattleTextbox
 
 SwitchCoreItemRecoveryAnim:
 	call SwitchTurnCore
--- engine/battle/core3.asm
+++ engine/battle/core3.asm
@@ -127,3 +127,134 @@ DoesMonGetWeatherDefenseBoost:
 	pop hl
 	scf
 	ret
+
+HandlePerishSong:
+	ldh a, [hSerialConnectionStatus]
+	cp USING_EXTERNAL_CLOCK
+	jr z, .EnemyFirst
+	call SetPlayerTurn
+	call .do_it
+	call SetEnemyTurn
+	jr .do_it
+
+.EnemyFirst:
+	call SetEnemyTurn
+	call .do_it
+	call SetPlayerTurn
+
+.do_it
+	ld hl, wPlayerPerishCount
+	ldh a, [hBattleTurn]
+	and a
+	jr z, .got_count
+	ld hl, wEnemyPerishCount
+
+.got_count
+	ld a, BATTLE_VARS_SUBSTATUS1
+	call GetBattleVar
+	bit SUBSTATUS_PERISH, a
+	ret z
+	dec [hl]
+	ld a, [hl]
+	ld [wTextDecimalByte], a
+	push af
+	ld hl, PerishCountText
+	call StdBattleTextbox
+	pop af
+	ret nz
+	ld a, BATTLE_VARS_SUBSTATUS1
+	call GetBattleVarAddr
+	res SUBSTATUS_PERISH, [hl]
+	ldh a, [hBattleTurn]
+	and a
+	jr nz, .kill_enemy
+	ld hl, wBattleMonHP
+	xor a
+	ld [hli], a
+	ld [hl], a
+	ld hl, wPartyMon1HP
+	ld a, [wCurBattleMon]
+	call GetPartyLocation
+	xor a
+	ld [hli], a
+	ld [hl], a
+	ret
+
+.kill_enemy
+	ld hl, wEnemyMonHP
+	xor a
+	ld [hli], a
+	ld [hl], a
+	ld a, [wBattleMode]
+	dec a
+	ret z
+	ld hl, wOTPartyMon1HP
+	ld a, [wCurOTMon]
+	call GetPartyLocation
+	xor a
+	ld [hli], a
+	ld [hl], a
+	ret
+
+HandleWrap:
+	ldh a, [hSerialConnectionStatus]
+	cp USING_EXTERNAL_CLOCK
+	jr z, .EnemyFirst
+	call SetPlayerTurn
+	call .do_it
+	call SetEnemyTurn
+	jr .do_it
+
+.EnemyFirst:
+	call SetEnemyTurn
+	call .do_it
+	call SetPlayerTurn
+
+.do_it
+	ld hl, wPlayerWrapCount
+	ld de, wPlayerTrappingMove
+	ldh a, [hBattleTurn]
+	and a
+	jr z, .got_addrs
+	ld hl, wEnemyWrapCount
+	ld de, wEnemyTrappingMove
+
+.got_addrs
+	ld a, [hl]
+	and a
+	ret z
+
+	ld a, BATTLE_VARS_SUBSTATUS4
+	call GetBattleVar
+	bit SUBSTATUS_SUBSTITUTE, a
+	ret nz
+
+	ld a, [de]
+	ld [wNamedObjectIndex], a
+	ld [wFXAnimID], a
+	call GetMoveName
+	dec [hl]
+	jr z, .release_from_bounds
+
+	ld a, BATTLE_VARS_SUBSTATUS3
+	call GetBattleVar
+	and 1 << SUBSTATUS_FLYING | 1 << SUBSTATUS_UNDERGROUND
+	jr nz, .skip_anim
+
+	farcall SwitchTurnCore
+	xor a
+	ld [wNumHits], a
+	ld [wFXAnimID + 1], a
+	predef PlayBattleAnim
+	farcall SwitchTurnCore
+
+.skip_anim
+	farcall GetEighthMaxHP
+	farcall SubtractHPFromUser
+	ld hl, BattleText_UsersHurtByStringBuffer1
+	jr .print_text
+
+.release_from_bounds
+	ld hl, BattleText_UserWasReleasedFromStringBuffer1
+.print_text
+	jmp StdBattleTextbox
