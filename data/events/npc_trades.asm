638e87497bdc3e778cc7f08a1dedd0528e06baab
RaJah239 <ravijagroo239@gmail.com>
2026-06-11 17:46:57 -0400

    decapped data/events/npc_trades.asm

--- data/events/npc_trades.asm
+++ data/events/npc_trades.asm
@@ -8,8 +8,8 @@ ENDM
 NPCTrades:
 ; entries correspond to NPCTRADE_* constants and locations at data\wild\non_wildmon_locations.asm
 	table_width NPCTRADE_STRUCT_LENGTH, NPCTrades
-	npctrade TRADE_DIALOGSET_COLLECTOR, DITTO,       DITTO,     "MUSCLE@@@@@", $37, $66, GOLD_BERRY,   37460, "MIKE@@@@@@@", TRADE_GENDER_EITHER
-	npctrade TRADE_DIALOGSET_COLLECTOR, BELLSPROUT, ONIX,       "ROCKY@@@@@@", $96, $66, BITTER_BERRY, 48926, "KYLE@@@@@@@", TRADE_GENDER_EITHER
-	npctrade TRADE_DIALOGSET_HAPPY,     FEEBAS,     VOLTORB,    "VOLTY@@@@@@", $98, $88, PRZCUREBERRY, 29189, "TIM@@@@@@@@", TRADE_GENDER_EITHER
-	npctrade TRADE_DIALOGSET_GIRL,      FEEBAS,     DODRIO,     "DORIS@@@@@@", $77, $66, SMOKE_BALL,   00283, "EMY@@@@@@@@", TRADE_GENDER_FEMALE
+	npctrade TRADE_DIALOGSET_COLLECTOR, DITTO,       DITTO,     "Muscle@@@@@", $37, $66, GOLD_BERRY,   37460, "Mike@@@@@@@", TRADE_GENDER_EITHER
+	npctrade TRADE_DIALOGSET_COLLECTOR, BELLSPROUT, ONIX,       "Rocky@@@@@@", $96, $66, BITTER_BERRY, 48926, "Kyle@@@@@@@", TRADE_GENDER_EITHER
+	npctrade TRADE_DIALOGSET_HAPPY,     FEEBAS,     VOLTORB,    "Volty@@@@@@", $98, $88, PRZCUREBERRY, 29189, "Tim@@@@@@@@", TRADE_GENDER_EITHER
+	npctrade TRADE_DIALOGSET_GIRL,      FEEBAS,     DODRIO,     "Doris@@@@@@", $77, $66, SMOKE_BALL,   00283, "Emy@@@@@@@@", TRADE_GENDER_FEMALE
 	assert_table_length NUM_NPC_TRADES
