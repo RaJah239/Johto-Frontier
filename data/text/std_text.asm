17eb1f4ec867d54b57922acb12754ca0b1d6b310
RaJah239 <ravijagroo239@gmail.com>
2026-07-01 14:15:15 -0400

    decapped data/text/std_text.asm

--- data/text/std_text.asm
+++ data/text/std_text.asm
@@ -1,19 +1,19 @@
 NurseMornText:
 	text "Good morning!"
 	line "Welcome to our"
-	cont "#MON CENTER."
+	cont "#mon center."
 	done
 
 NurseDayText:
 	text "Hello!"
 	line "Welcome to our"
-	cont "#MON CENTER."
+	cont "#mon center."
 	done
 
 NurseEveText:
 	text "Good evening!"
 	line "Welcome to our"
-	cont "#MON CENTER."
+	cont "#mon center."
 	done
 
 NurseNiteText:
@@ -21,37 +21,37 @@ NurseNiteText:
 	line "You're out late."
 
 	para "Welcome to our"
-	line "#MON CENTER."
+	line "#mon center."
 	done
 
 PokeComNurseMornText:
 	text "Good morning!"
 
 	para "This is the #-"
-	line "MON COMMUNICATION"
+	line "mon Communication"
 
-	para "CENTER--or the"
-	line "#COM CENTER."
+	para "center--or the"
+	line "#com center."
 	done
 
 PokeComNurseDayText:
 	text "Hello!"
 
 	para "This is the #-"
-	line "MON COMMUNICATION"
+	line "mon Communication"
 
-	para "CENTER--or the"
-	line "#COM CENTER."
+	para "center--or the"
+	line "#com center."
 	done
 
 PokeComNurseEveText:
 	text "Good evening."
 
 	para "This is the #-"
-	line "MON COMMUNICATION"
+	line "mon Communication"
 
-	para "CENTER--or the"
-	line "#COM CENTER."
+	para "center--or the"
+	line "#com center."
 	done
 
 PokeComNurseNiteText:
@@ -59,31 +59,31 @@ PokeComNurseNiteText:
 	line "working so late."
 
 	para "This is the #-"
-	line "MON COMMUNICATION"
+	line "Mon Communication"
 
-	para "CENTER--or the"
-	line "#COM CENTER."
+	para "center--or the"
+	line "#com center."
 	done
 
 NurseAskHealText:
 	text "We can heal your"
-	line "#MON to perfect"
+	line "#mon to perfect"
 	cont "health."
 
 	para "Shall we heal your"
-	line "#MON?"
+	line "#mon?"
 	done
 
 NurseTakePokemonText:
 	text "OK, may I see your"
-	line "#MON?"
+	line "#mon?"
 	done
 
 NurseReturnPokemonText:
 	text "Thank you for"
 	line "waiting."
 
-	para "Your #MON are"
+	para "Your #mon are"
 	line "fully healed."
 	done
 
@@ -98,31 +98,31 @@ NurseGoodbyeText:
 	done
 
 NursePokerusText:
-	text "Your #MON"
+	text "Your #mon"
 	line "appear to be"
 
 	para "infected by tiny"
 	line "life forms."
 
-	para "Your #MON are"
+	para "Your #mon are"
 	line "healthy and seem"
 	cont "to be fine."
 
 	para "But we can't tell"
 	line "you anything more"
 
-	para "at a #MON"
-	line "CENTER."
+	para "at a #mon"
+	line "center."
 	done
 
 PokeComNursePokerusText:
-	text "Your #MON"
+	text "Your #mon"
 	line "appear to be"
 
 	para "infected by tiny"
 	line "life forms."
 
-	para "Your #MON are"
+	para "Your #mon are"
 	line "healthy and seem"
 	cont "to be fine."
 
@@ -137,7 +137,7 @@ DifficultBookshelfText:
 
 PictureBookshelfText:
 	text "A whole collection"
-	line "of #MON picture"
+	line "of #mon picture"
 	cont "books!"
 	done
 
@@ -150,17 +150,17 @@ MagazineBookshelfText:
 	done
 
 TeamRocketOathText:
-	text "TEAM ROCKET OATH"
+	text "Team Rocket Oath"
 
-	para "Steal #MON for"
+	para "Steal #mon for"
 	line "profit!"
 
-	para "Exploit #MON"
+	para "Exploit #mon"
 	line "for profit!"
 
-	para "All #MON exist"
+	para "All #mon exist"
 	line "for the glory of"
-	cont "TEAM ROCKET!"
+	cont "Team Rocket!"
 	done
 
 IncenseBurnerText:
@@ -194,20 +194,20 @@ TrashCanText:
 	done
 
 PokecenterSignText:
-	text "Heal Your #MON!"
-	line "#MON CENTER"
+	text "Heal Your #mon!"
+	line "#mon center"
 	done
 
 MartSignText:
 	text "For All Your"
-	line "#MON Needs"
+	line "#mon Needs"
 
-	para "#MON MART"
+	para "#mon Mart"
 	done
 
 ContestResults_ReadyToJudgeText:
 	text "We will now judge"
-	line "the #MON you've"
+	line "the #mon you've"
 	cont "caught."
 
 	para "<……>"
@@ -244,7 +244,7 @@ ContestResults_JoinUsNextTimeText:
 
 ContestResults_ConsolationPrizeText:
 	text "Everyone else gets"
-	line "a BERRY as a con-"
+	line "a Berry as a con-"
 	cont "solation prize!"
 	done
 
@@ -255,7 +255,7 @@ ContestResults_DidNotWinText:
 
 ContestResults_ReturnPartyText:
 	text "We'll return the"
-	line "#MON we kept"
+	line "#mon we kept"
 
 	para "for you."
 	line "Here you go!"
@@ -263,10 +263,10 @@ ContestResults_ReturnPartyText:
 
 ContestResults_PartyFullText:
 	text "Your party's full,"
-	line "so the #MON was"
+	line "so the #mon was"
 
-	para "sent to your BOX"
-	line "in BILL's PC."
+	para "sent to your Box"
+	line "in Bill's PC."
 	done
 
 GymStatue_CityGymText:
@@ -293,7 +293,7 @@ CoinVendor_NoCoinCaseText:
 	line "coins?"
 
 	para "Oh, you don't have"
-	line "a COIN CASE for"
+	line "a Coin Case for"
 	cont "your coins."
 	done
 
@@ -317,8 +317,8 @@ CoinVendor_NotEnoughMoneyText:
 	done
 
 CoinVendor_CoinCaseFullText:
-	text "Whoops! Your COIN"
-	line "CASE is full."
+	text "Whoops! Your Coin"
+	line "Case is full."
 	done
 
 CoinVendor_CancelText:
@@ -327,7 +327,7 @@ CoinVendor_CancelText:
 	done
 
 BugContestPrizeNoRoomText:
-	text "Oh? Your PACK is"
+	text "Oh? Your Bag is"
 	line "full."
 
 	para "We'll keep this"
@@ -339,12 +339,12 @@ BugContestPrizeNoRoomText:
 
 HappinessText3:
 	text "Wow! You and your"
-	line "#MON are really"
+	line "#mon are really"
 	cont "close!"
 	done
 
 HappinessText2:
-	text "#MON get more"
+	text "#mon get more"
 	line "friendly if you"
 
 	para "spend time with"
@@ -353,7 +353,7 @@ HappinessText2:
 
 HappinessText1:
 	text "You haven't tamed"
-	line "your #MON."
+	line "your #mon."
 
 	para "If you aren't"
 	line "nice, it'll pout."
@@ -374,7 +374,7 @@ RegisteredNumber2Text:
 	done
 
 ChanseyCryText:
-	text "CHANSEY: Seyyyy!"
+	text "Chansey: Seyyyy!"
 	done
 
 OtisIntroText:
